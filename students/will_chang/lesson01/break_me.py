8a976b65568cc93f2d637cf549d4d7cd9b749877
williamchang6 <williamchang6@gmail.com>
2019-01-27 18:34:40 -0800

    update break_me.py

--- students/will_chang/lesson01/break_me.py
+++ students/will_chang/lesson01/break_me.py
@@ -1,12 +1,12 @@
 def name_error():
-	unknown_name = 'this name is not known' + unknown
-	
+    unknown_name = 'this name is not known' + unknown
+    
 def type_error():
-	add_string = 'cannot add these' + 5
-	
+    add_string = 'cannot add these' + 5
+    
 def syntax_error():
-	i =\ 5
+    i =\ 5
 
 def attribute_error():
-	test = 5
-	testing = test.plan
+    test = 5
+    testing = test.plan
