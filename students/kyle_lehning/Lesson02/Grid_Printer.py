8b53fceae439be02575704b4d6c4770a907f7aa4
Kyle Lehning <lehnink@gmail.com>
2019-02-06 17:37:57 -0800

    part1 complete

---
+++ students/kyle_lehning/Lesson02/Grid_Printer.py
@@ -0,0 +1,11 @@
+def print_grid_part1():
+    plus_line = "+ - - - - + - - - - +"
+    pipe_line = "|         |         |"
+    print(plus_line)
+    for i in range(4):
+        print(pipe_line)
+    print(plus_line)
+    for i in range(4):
+        print(pipe_line)
+    print(plus_line)
+print_grid_part1()
