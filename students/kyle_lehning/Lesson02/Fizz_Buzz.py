7160865e79bccb6458e2e8941849fb2dd1762f56
Kyle Lehning <lehnink@gmail.com>
2019-02-06 19:15:06 -0800

    Complete Fizz_Buzz

---
+++ students/kyle_lehning/Lesson02/Fizz_Buzz.py
@@ -0,0 +1,11 @@
+def Fizz_Buzz():
+    for number in range(1, 101):
+        output_string = ""
+        if number%3 ==0: output_string = 'Fizz'
+        if number%5 ==0: output_string += 'Buzz'
+        if output_string == "":
+            print(number)
+        else:
+            print(output_string)
+      
+Fizz_Buzz()
