0b1bb1e2a522933c6f8f5c38d372fbf5496254eb
Kyle Lehning <lehnink@gmail.com>
2019-02-06 18:17:12 -0800

    part2 complete

--- students/kyle_lehning/Lesson02/Grid_Printer.py
+++ students/kyle_lehning/Lesson02/Grid_Printer.py
@@ -1,6 +1,6 @@
 def print_grid_part1():
-    plus_line = "+ - - - - + - - - - +"
-    pipe_line = "|         |         |"
+    plus_line = '+ - - - - + - - - - +'
+    pipe_line = '|         |         |'
     print(plus_line)
     for i in range(4):
         print(pipe_line)
@@ -8,4 +8,16 @@ def print_grid_part1():
     for i in range(4):
         print(pipe_line)
     print(plus_line)
-print_grid_part1()
+
+def print_grid(n):
+    multiplier = int(n/2) #forces an integer, rounds down if odd
+    dash_line = '- ' * multiplier
+    plus_line = '+ ' + dash_line + '+ ' + dash_line + '+'
+    spaces = '  ' * multiplier
+    print(plus_line)
+    for x in range(2):
+        for i in range(multiplier):
+            print('| ' + spaces + '+ ' + spaces + '|')
+        print(plus_line)
+        
+print_grid(15)    
