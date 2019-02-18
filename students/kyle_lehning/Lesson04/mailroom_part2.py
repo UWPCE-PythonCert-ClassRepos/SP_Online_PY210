b42b3e2f6db116c1b4bd72208bb6dab87d6bc5fd
Kyle Lehning <lehnink@gmail.com>
2019-02-17 18:43:50 -0800

    mailroom_part2 created from mailroom and menu selection updated

---
+++ students/kyle_lehning/Lesson04/mailroom_part2.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+donors = [
+    ["Steve Jobs", 1002.40, 2, 501.20],
+    ["Jeff Bezos", 877.33, 1, 877.33],
+    ["Bill Gates", 653784.49, 2, 326892.24],
+    ["Mark Zuckerberg", 16396.10, 3, 5465.37],
+    ["Paul Allen", 708.42, 3, 236.14]
+]
+
+
+def menu_input():
+    return input(
+        "\nSelect an option number:"
+        "\n1. Send a Thank You"
+        "\n2. Create a Report"
+        "\n3. Quit\n"
+    )
+
+
+def thank_you():
+    thank_you_options ={
+        "MENU": False,
+        "LIST": list_donors
+    }
+    while True:
+        user_selection = (
+            input("\nPlease provide a full name to send thank you note to (options 'list' and 'menu'): ")
+        )
+        action = thank_you_options.get(user_selection.upper())
+        if action is False:
+            break
+        elif action is None:
+            write_letter(user_selection)
+        else:
+            action()
+
+
+def list_donors():
+    for name in donors:
+        print(name[0])
+
+
+def write_letter(person):
+    for name in donors:
+        if person.upper() == name[0].upper():
+            current_donor = name
+            break
+    else:
+        current_donor = add_donor(person)
+    donation_amount = input("\nHow much did {} donate?: ".format(person))
+    add_donation(current_donor, donation_amount)
+    print("Thank you {}, for your generous donation of {}!".format(current_donor[0], donation_amount))
+
+
+def add_donor(new_name):
+    donors.append([new_name, 0, 0, 0])
+    return donors[-1]
+
+
+def add_donation(donor, donation_value):
+    donor[2] += 1
+    donor[1] = round(donor[1] + float(donation_value), 2)
+    donor[3] = round(donor[1]/donor[2], 2)
+
+
+def print_report():
+    header = ["Donor Name", "Total Given", "Num Gifts", "Average Gift"]
+    header_string = "{0:<{w1}} | {1:>{w2}} | {2:>{w3}} | {3:>{w4}}"
+    data_string = "{0:<{w1}}  ${1:>{w2}}   {2:>{w3}}  ${3:>{w4}}"
+    report_list = donors[:]
+    width_list = donors[:]
+    report_list.sort(reverse=True, key=lambda x: int(x[1]))
+    width_list.insert(0, header)
+    w = find_widths(width_list)
+    header_to_print = print_line(header_string, header, w)
+    print(header_to_print)
+    print('-' * len(header_to_print))
+    for row in report_list:
+        print(print_line(data_string, row, w))
+
+
+def find_widths(seq):
+    return [max(len(str(row[i])) for row in seq) for i in range(len(seq[0]))]
+
+
+def print_line(line_format, info, width):
+    row_string = (line_format.format(*info, w1=width[0], w2=width[1], w3=width[2], w4=width[3]))
+    return row_string
+
+
+def main():
+    menu_switch = {
+        "1": thank_you,
+        "2": print_report,
+        "3": False
+    }
+    while True:
+        user_selection = menu_input()
+        action = menu_switch.get(user_selection)
+        if action is False:
+            break
+        elif action is None:
+            print("Please provide a valid option\n")
+        else:
+            action()
+
+
+if __name__ == '__main__':
+    main()
