bf52238a78b8b9fa3589b34494384885703b32e1
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-02-21 21:36:58 -0800

    Added function to print variable sized 2x2 grid

--- students/chris_lombardi/Lesson02/grid_printer.py
+++ students/chris_lombardi/Lesson02/grid_printer.py
@@ -5,49 +5,51 @@ PLUS = "+"
 MINUS ="-"
 VERT_BAR = "|"
 
-#Print Header/Footer Row
-print(PLUS, end=" ")
-for i in range(4):
-    print(MINUS, end=" ")
-print(PLUS, end = " ")
-for i in range(4):
-    print(MINUS, end = " ")
-print(PLUS)
+def print_grid(n):
 
-#Print Cell Rows
-for i in range(4):
-    print("|", end = " ")
-    for j in range(4):
-        print(" ", end = " ")
-    print("|", end = " ")
-    for j in range(4):
-        print(" ", end = " ")
-    print("|")
+    #Print Header/Footer Row
+    print(PLUS, end=" ")
+    for i in range(n//2):
+        print(MINUS, end=" ")
+    print(PLUS, end = " ")
+    for i in range(n//2):
+        print(MINUS, end = " ")
+    print(PLUS)
 
-#Print Header/Footer Row
-print(PLUS, end=" ")
-for i in range(4):
-    print(MINUS, end=" ")
-print(PLUS, end = " ")
-for i in range(4):
-    print(MINUS, end = " ")
-print(PLUS)
+    #Print Cell Rows
+    for i in range(n//2):
+        print("|", end = " ")
+        for j in range(n//2):
+            print(" ", end = " ")
+        print("|", end = " ")
+        for j in range(n//2):
+            print(" ", end = " ")
+        print("|")
 
-#Print Cell Rows
-for i in range(4):
-    print("|", end = " ")
-    for j in range(4):
-        print(" ", end = " ")
-    print("|", end = " ")
-    for j in range(4):
-        print(" ", end = " ")
-    print("|")
+    #Print Header/Footer Row
+    print(PLUS, end=" ")
+    for i in range(n//2):
+        print(MINUS, end=" ")
+    print(PLUS, end = " ")
+    for i in range(n//2):
+        print(MINUS, end = " ")
+    print(PLUS)
 
-#Print Header/Footer Row
-print(PLUS, end=" ")
-for i in range(4):
-    print(MINUS, end=" ")
-print(PLUS, end = " ")
-for i in range(4):
-    print(MINUS, end = " ")
-print(PLUS)
+    #Print Cell Rows
+    for i in range(n//2):
+        print("|", end = " ")
+        for j in range(n//2):
+            print(" ", end = " ")
+        print("|", end = " ")
+        for j in range(n//2):
+            print(" ", end = " ")
+        print("|")
+
+    #Print Header/Footer Row
+    print(PLUS, end=" ")
+    for i in range(n//2):
+        print(MINUS, end=" ")
+    print(PLUS, end = " ")
+    for i in range(n//2):
+        print(MINUS, end = " ")
+    print(PLUS)
