e3e17f0271e9251b51834a4a8a680576a7074e7c
Erica Edwards <ericae14@hotmail.com>
2019-02-23 16:12:52 -0800

    Part two converting to function

--- students/erica_edwards/Lesson02/Lesson_2.2_Grid.py
+++ students/erica_edwards/Lesson02/Lesson_2.2_Grid.py
@@ -1,30 +1,34 @@
 import os
 os.system('cls')
 
-size = 9
-if size %2 == 0:
-    size = size + 1
+def print_grid(size):
+    if size %2 == 0:
+        size = size + 1
 
-dash_count = (size - 1)//2 
-pipe_range = size//2
+    dash_count = (size - 1)//2 
+    pipe_range = size//2
 
-plus = "+"
-minus = "-"
-space = " "
-pipe = "|"
-
-#Make the top line
-print(f'{plus}',end='')
-print (f'{(space+minus)*dash_count}{space}{plus}'*2)
-
-for box in range(2):
-    for x in range(pipe_range):
-        #Make all the middle lines
-        print(f'{pipe}',end='')
-        print (f'{space*size}{pipe}{space*size}{pipe}')
-        
-    #Make the bottom line
+    plus = "+"
+    minus = "-"
+    space = " "
+    pipe = "|"
+    
+    #Make the top line
     print(f'{plus}',end='')
     print (f'{(space+minus)*dash_count}{space}{plus}'*2)
 
+    for box in range(2):
+        for x in range(pipe_range):
+            #Make all the middle lines
+            print(f'{pipe}',end='')
+            print (f'{space*size}{pipe}{space*size}{pipe}')
+            
+        #Make the bottom line
+        print(f'{plus}',end='')
+        print (f'{(space+minus)*dash_count}{space}{plus}'*2)
+
+if __name__ == "__main__":
+    print_grid(3)    
+    print_grid(11)    
+    print_grid(15)    
 
