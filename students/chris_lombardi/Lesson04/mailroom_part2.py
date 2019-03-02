89b8cf07c7e95f1a08b323ff8ecde5dde361dddc
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-03-01 17:28:59 -0800

    Updated mailroom_part2.py to include write letter function

--- students/chris_lombardi/Lesson04/mailroom_part2.py
+++ students/chris_lombardi/Lesson04/mailroom_part2.py
@@ -2,7 +2,7 @@
 #Lesson04, Mailroom Part 2
 import sys
 
-DONOR_LOG = {} #Log of donors and their respective donation history.
+donor_log = {} #Log of donors and their respective donation history.
 
 def thank_you():
     """Accepts donation information and prints a thank you email."""
@@ -10,29 +10,30 @@ def thank_you():
 
     #Print the list of donors in the log if requested by the user.
     while response == 'List':
-        for entry in DONOR_LOG:
+        for entry in donor_log:
             print(entry)
         response = input('Please enter a full name: ').title()
 
     amount = float(input("Please enter a donation amount:"))
 
     #Check if name exists in log and either create new entry or update history.
-    if response not in DONOR_LOG:
-        DONOR_LOG[response] = [amount]
+    if response not in donor_log:
+        donor_log[response] = [amount]
     else:
-        DONOR_LOG[response] += [amount]
+        donor_log[response] += [amount]
 
     #Print a thank you email for the latest donation.
-    email_template(response, amount)
+    print(thankyou_note(response))
 
 def create_report():
     """Generate a tabular report of donation history"""
     header ='\n{:<18}|{:^13}|{:^13}|{:>13}'.format("Donor Name", "Total Given",
                                                    "Num Gifts", "Average Gift")
     print(header + '\n' + '-'*len(header))
-    for entry in DONOR_LOG:
+    for entry in donor_log:
         total = total_donation(entry)
-        num = len(DONOR_LOG.get(entry))
+        #total = sum(donor_log.get(name))
+        num = len(donor_log.get(entry))
         average = total/num
         print('{:<18} ${:>12,.2f}{:>13}  ${:>12,.2f}'.format(entry,total,num,average))
     print('')
@@ -40,23 +41,18 @@ def create_report():
 def total_donation(name):
     """Return the sum of donations for a particular donor."""
     total = 0.0
-    for donation in DONOR_LOG.get(name):
+    for donation in donor_log.get(name):
         total += donation
     return total
-
-def email_template(name, amount):
-    print(f'\nDear {name.title()},')
-    print(f'Thank you for your generous donation of ${amount:,.2f}!' +
-          '\nWe appreciate your contribution to our charity.' +
-          '\n\nSincerley,\nThe Mailroom\n')
 
 def display_menu():
     """Displays the interactive menu to the user."""
     print('This program tracks the donation history for a charity.')
     print('Please select from the following options:\n' +
-          '1) Send a Thank You\n' +
+          '1) Record Donation and Send Thank You\n' +
           '2) Create A Report\n' +
-          '3) Quit')
+          '3) Send Thank You Letters to All Donors\n' +
+          '4) Quit')
     user_selection = input('')
     return user_selection
 
@@ -64,16 +60,32 @@ def exit_program():
     print("Exiting program...")
     sys.exit()
 
+def send_letters():
+    for entry in donor_log:
+        filename = entry + '.txt'
+        with open(filename, 'w') as f:
+            f.write(thankyou_note(entry))
+        f.close()
+
+def thankyou_note(entry):
+    note = (f'Dear {entry},\n\n\tThank you for your generous donation of '
+          f'${sum(donor_log.get(entry)):,.2f}!\n\tWe appreciate the '
+          f'{len(donor_log.get(entry)):d} total donation(s) that you have made.'
+          '\n\tYour donation will be put to good use.'
+          '\n\n\tSincerely,\n\t-The Mailroom Team')
+    return note
+
 def initialize_donors():
     """Add preset entries into the donor log."""
-    DONOR_LOG['George Washington'] = [1789.0, 1.0, 1797.0]
-    DONOR_LOG['Abraham Lincoln'] = [1861.0, 16.0]
-    DONOR_LOG['James Madison'] = [1809.0,4.0]
-    DONOR_LOG['Theodore Roosevelt'] = [1901.0, 26.0, 60.0]
-    DONOR_LOG['Dwight Eisenhower'] = [1953.00, 34.0]
+    donor_log['George Washington'] = [1789.0, 1.0, 1797.0]
+    donor_log['Abraham Lincoln'] = [1861.0, 16.0]
+    donor_log['James Madison'] = [1809.0,4.0]
+    donor_log['Theodore Roosevelt'] = [1901.0, 26.0, 60.0]
+    donor_log['Dwight Eisenhower'] = [1953.00, 34.0]
 
 def main():
-    dict_menu_opts = {'1': thank_you, '2': create_report, '3': exit_program}
+    dict_menu_opts = {'1': thank_you, '2': create_report,
+                      '3': send_letters, '4': exit_program}
     menu_options = set(dict_menu_opts.keys())
     while True:
         option = display_menu()
