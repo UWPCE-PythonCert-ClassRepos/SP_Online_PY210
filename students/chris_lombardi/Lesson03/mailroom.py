94298884451b73ac93b8081f2406a0b889acbfc7
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-03-01 17:26:02 -0800

    Updated mailroom.py to include sort function

--- students/chris_lombardi/Lesson03/mailroom.py
+++ students/chris_lombardi/Lesson03/mailroom.py
@@ -2,8 +2,8 @@
 #Lesson03, Mailroom Part 1
 import sys
 
-DONOR_LOG = [] #Log of donors and their respective donation history.
-MENU_OPTIONS = ('1', '2', '3') #Available menu options.
+donor_log = [] #Log of donors and their respective donation history.
+menu_options = ('1', '2', '3') #Available menu options.
 
 def thank_you():
     """Accepts donation information and prints a thank you email."""
@@ -11,7 +11,7 @@ def thank_you():
 
     #Print the list of donors in the log if requested by the user.
     while response == 'List':
-        for entry in DONOR_LOG:
+        for entry in donor_log:
             print(entry[0])
         response = input('Please enter a full name from the list: ').title()
 
@@ -32,23 +32,27 @@ def create_report():
                                                    "Num Gifts", "Average Gift")
     print(header)
     print('-'*len(header))
-    for entry in DONOR_LOG:
-        total = total_donation(entry[0])
+    donor_sort = sorted(donor_log, key = sort_key, reverse = True)
+    for entry in donor_sort:
+        total = sum(entry[1])
         num = len(entry[1])
         average = total/num
         print('{:<18} ${:>12,.2f}{:>13}  ${:>12,.2f}'.format(entry[0],total,num,average))
     print('')
 
+def sort_key(entry):
+    return sum(entry[1])
+
 def check_donor_list(name):
     """Check the global DONOR_LOG for an existing donor name."""
-    for entry in DONOR_LOG:
+    for entry in donor_log:
         if name == entry[0]:
             return True
     return False
 
 def update_donation_history(name, amount):
     """Add a donation to the history of a particular donor."""
-    for entry in DONOR_LOG:
+    for entry in donor_log:
         if name == entry[0]:
             amnt_entry = [float(amount),]
             entry[1] += amnt_entry
@@ -58,16 +62,7 @@ def add_log_entry(name):
     """Create a new donor entry into the global DONOR_LOG."""
     hist = []
     add_entry = [name.title(), hist]
-    DONOR_LOG.append(add_entry)
-
-def total_donation(name):
-    """Return the sum of donations for a particular donor."""
-    for entry in DONOR_LOG:
-        if name == entry[0]:
-            total = 0.0
-            for donation in entry[1]:
-                total += donation
-            return total
+    donor_log.append(add_entry)
 
 def email_template(name, amount):
     print(f'\nDear {name.title()},')
@@ -91,11 +86,11 @@ def exit_program():
 
 def initialize_donors():
     """Add preset entries into the donor log."""
-    DONOR_LOG.append(['George Washington', [1789.0, 1.0, 1797.0]])
-    DONOR_LOG.append(['Abraham Lincoln', [1861.0, 16.0]])
-    DONOR_LOG.append(['James Madison', [1809.0,4.0]])
-    DONOR_LOG.append(['Theodore Roosevelt',[1901.0, 26.0, 60.0]])
-    DONOR_LOG.append(['Dwight Eisenhower', [1953.00, 34.0]])
+    donor_log.append(['George Washington', [1789.0, 1.0, 1797.0]])
+    donor_log.append(['Abraham Lincoln', [1861.0, 16.0]])
+    donor_log.append(['James Madison', [1809.0,4.0]])
+    donor_log.append(['Theodore Roosevelt',[1901.0, 26.0, 60.0]])
+    donor_log.append(['Dwight Eisenhower', [1953.00, 34.0]])
 
 def main():
     while True:
@@ -106,7 +101,7 @@ def main():
             create_report()
         elif option == '3':
             exit_program()
-        elif option not in MENU_OPTIONS:
+        elif option not in menu_options:
             print("\nNot a valid option.\n"
                   "Please enter a valid option from the menu.\n")
 
