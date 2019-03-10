55b6f8f1803f52141d4994cb5bc98acfdc7a01f6
Jim Krecek <jkrecek3@gmail.com>
2019-03-09 16:33:25 -0800

    adds functions for parts 1 and 2 of grid.py that were omitted earlier

--- students/jim_krecek/lesson02/grid.py
+++ students/jim_krecek/lesson02/grid.py
@@ -1,4 +1,48 @@
-def print_grid(num,size):
+def print_grid():
+	#prints 2x2 grid
+	#part 1
+	plus = "+ "
+	minus = "- "
+	endline = "| "
+	space = "  "
+	top = (plus + minus * (4))*2 + plus
+	line = (endline+ ((4)*space))*2 + endline
+	i=0
+	print(top)
+	while i < 4:
+		print(line)
+		i += 1
+	print(top)
+	i=0
+	while i < 4:
+		print(line)
+		i += 1
+	print(top)
+
+
+def print_grid2(n):
+	#part 2
+	plus = "+ "
+	minus = "- "
+	endline = "| "
+	space = "  "
+	top = (plus + minus * (n//2))*2 + plus
+	line = (endline+ ((n//2)*space))*2 + endline
+	i=0
+	print(top)
+	while i < n//2:
+		print(line)
+		i += 1
+	print(top)
+	i=0
+	while i < n//2:
+		print(line)
+		i += 1
+	print(top)
+
+
+def print_grid3(num,size):
+	#part 3
 	plus = "+ "
 	minus = "- "
 	endline = "| "
@@ -6,7 +50,6 @@ def print_grid(num,size):
 	top = (plus + minus * (size))*num + plus
 	line = (endline+ ((size)*space))*num + endline
 	i=0
-	j=0
 	m=0
 	print(top)
 	while m < num:
