8cb7f04aff8af6468bb6227901b8121c40f02cfa
Jim Krecek <jkrecek3@gmail.com>
2019-03-18 19:44:56 -0700

    completed list_lab.py

--- students/jim_krecek/lesson03/list_lab.py
+++ students/jim_krecek/lesson03/list_lab.py
@@ -1,6 +1,8 @@
 #response = input("a prompt for the user > ")
 
 # Series 1
+##################
+print('Series 1')
 fruit = ['Apples','Pears','Oranges','Peaches']
 print(fruit)
 
@@ -23,11 +25,53 @@ print(fruit)
 
 #display all fruits that begin with 'P'
 pfruit = []
-for i in range(0,len(fruit)):
-	if fruit[i][0] == 'P':
-		pfruit.append(fruit[i])
-	i += 1
+for i in range(len(fruit)):
+    if fruit[i][0] == 'P':
+        pfruit.append(fruit[i])
+    i += 1
 print(pfruit)
 
 # Series 2
+##################
 
+print('Series 2')
+print(fruit)
+#delete last fruit
+del fruit[-1]
+print(fruit)
+
+#ask user for a fruit to delete
+delfruit = input('What fruit would you like to remove? ')
+for i in range(len(fruit)):
+    if delfruit.lower() == fruit[i].lower():
+        del fruit[i]
+        break
+    i += 1
+    
+print(fruit)
+
+
+# Series 3
+##################
+
+print('Series 3')
+for f in fruit:
+    q = input('Do you like %s ? ' %f)
+    while not (q.lower() == 'yes' or 'no'):
+        q = input('Please answer with either "yes" or "no" ')
+    if q.lower() == 'no':
+        fruit.remove(f)
+
+print(fruit)
+
+# Series 4
+##################
+
+print('Series 4')
+reverse = []
+for f in fruit:
+    reverse.append(f[::-1])
+print(reverse)
+del reverse[-1]
+print(reverse)
+print(fruit)
