edd9e8e244d1e0c6a270d6dd8498887bbd3dc20f
Erica Edwards <ericae14@hotmail.com>
2019-03-20 21:04:11 -0700

    Final

--- students/erica_edwards/Lesson04/mailroom2.py
+++ students/erica_edwards/Lesson04/mailroom2.py
@@ -13,40 +13,37 @@ def menu():
         print()
         print('''Menu:
                     1. Send a Thank You.
-                
+
                     2. Send all Thank You's
-                   
+
                     3. Create a Report.
-            
+
                     4. Quit.''')
         print()
         response = input("Please enter a number to make your selection. ")
-        
-        switch_func_dict = {1: ask_donor_name, 2: mulit_thanks 3: totals, 4: break}
+
+        switch_func_dict = {0: ask_donor_name, 1: multi_thanks, 2: totals, }
         if response == '1':
-        #     ask_donor_name()
-            switch_func_dict.get(0)()    
+            switch_func_dict.get(0)()
         if response == '2':
-        #     totals()
             switch_func_dict.get(1)()
         if response == '3':
-        #     break
             switch_func_dict.get(2)()
         if response == '4':
-            switch_func_dict.get(3)()
+            break
 
 
 def list_donors():
     for item in donors:
         print(f'{item[0]}')
-        
-    
+
+
 def ask_donor_name():
 
     # While collecting user input.
     while True:
         donor_name = input("Enter the full name of the donor. (or q to quit) ")
-        
+
         if donor_name == 'q':
             return
         # If user types list print donors
@@ -54,37 +51,39 @@ def ask_donor_name():
             list_donors()
             continue
         else:  # user didn't type list
-            donors[donor_name] = donors.get(donor_name, 0)
-            for item in donors.items():
-                if item == donor_name:
-                    break
-                else:
-                    #donors.append((donor_name, list()))
-                    donors[donor_name] = []
-        donation(donor_name)
-        break
+            donation(donor_name)
+            return
 
 
 def donation(donor_name):
-    input_value = (input("Enter the donation amount (or q to quit) "))
-    if input_value == 'q':
+    donation_amount = input("Enter the donation amount (or q to quit) ")
+    if donation_amount == 'q':
         return
     else:
-        donation_amount = int(input_value)
-        donors[donor_name].append(donation_amount)
-        print(donors)
-        #donorIndex = [item[0] for item in donors].index(donorName)
-        # name, donations = donors[donorIndex]
-        # donations.append(donation_amount)
+        donation_amount = int(donation_amount)
+        for name, donations in donors.items():
+            if name == donor_name:
+                donations.append(donation_amount)
+                break
+        else:
+            donors[donor_name] = [donation_amount]
+
         send_thank_you(donor_name, donation_amount)
-        return
 
 
 def send_thank_you(donor_name, donation_amount):
-    print(f'Thank you {donor_name} for your donation of ${donation_amount} dollars.')
-    print('We appeciate your generous suport of our club.')
+    print(f'Thank you {donor_name} for your donation of ${donation_amount}. We appreciate your generous support of our club.')
     print()
-    
+
+
+def multi_thanks():
+    # In order to do the .format(**dict), I converted my dictionary to list of dictionaries. Otherwise refactor everything.
+    named_list = []
+    for item in donors.items():
+        named_list.append({'donor_name': item[0], 'total': str(sum(item[1]))})
+    for named_dict in named_list:
+        with open('{donor_name}.txt'.format(**named_dict), 'w') as f:
+            f.write('Thank you {donor_name}. You have donated a total of ${total}. We appreciate your generous support for our club.'.format(**named_dict))
 
 # Create Report
 
@@ -92,11 +91,11 @@ def send_thank_you(donor_name, donation_amount):
 def totals():
     output_list = []
     for name, donations in donors.items():
-        total_Given = sum(donations)
-        number_Gifts = len(donations)
-        average_Gift = sum(donations)/len(donations)
-        output_list.append((name, total_Given, number_Gifts, average_Gift))
-    
+        total_given = sum(donations)
+        number_gifts = len(donations)
+        average_gift = sum(donations)/len(donations)
+        output_list.append((name, total_given, number_gifts, average_gift))
+
     sorted_output = sorted(output_list, key=itemgetter(1), reverse=True)
     create_report(sorted_output)
 
@@ -105,8 +104,8 @@ def create_report(sorted_output):
     print()
     print('Donor Name                | Total Given | Num Gifts | Average Gift')
     print('-'*66)
-    for name, total_Given, number_Gifts, average_Gift in sorted_output:
-        print(f'{name:<27}${total_Given:>11.2f}{number_Gifts:>12}  ${average_Gift:>11.2f}')
+    for name, total_given, number_gifts, average_gift in sorted_output:
+        print(f'{name:<27}${total_given:>11.2f}{number_gifts:>12}  ${average_gift:>11.2f}')
     return
 
 
