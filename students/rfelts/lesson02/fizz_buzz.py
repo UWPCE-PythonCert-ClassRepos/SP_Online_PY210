fc4585b157b837f55dbaa4b18e836afadec5b5a5
rfelts <rfelts@adobe.com>
2019-03-26 13:47:50 -0700

    Updated lesson02 fizz_buzz file

--- students/rfelts/lesson02/fizz_buzz.py
+++ students/rfelts/lesson02/fizz_buzz.py
@@ -5,16 +5,18 @@
 # For the multiples of five print “Buzz” instead of the number.
 # For numbers which are multiples of both three and five print “FizzBuzz” instead.
 
+
 def fizz_buzz():
-    """Print 1-100, print "fizz buzz" for x3&5 "fizz" for x3 and "buzz" for x5"""
-    for loop in range(1,101):
-        if(loop % 3 == 0 and loop % 5 == 0):
-            print("fizz buzz")
-        elif(loop % 3 == 0):
-            print("fizz")
-        elif(loop % 5 == 0):
-            print("buzz")
+    """Print 1-100, print "FizzBuzz" for x3&5 "Fizz" for x3 and "Buzz" for x5"""
+    for loop in range(1, 101):
+        if loop % 3 == 0 and loop % 5 == 0:
+            print("FizzBuzz")
+        elif loop % 3 == 0:
+            print("Fizz")
+        elif loop % 5 == 0:
+            print("Buzz")
         else:
             print(loop)
+
 
 fizz_buzz()
