15b2930af11656589a4ec03d70c7326194233f7c
Jim Krecek <jkrecek3@gmail.com>
2019-03-26 17:53:40 -0700

    adds mailroom.py modifications

--- students/jim_krecek/lesson03/mailroom.py
+++ students/jim_krecek/lesson03/mailroom.py
@@ -8,27 +8,35 @@ def send_ty():
         print("\nHere is a list of all of the donors:")
         for d in data:
             print(d[0])
+    else:
+        for d in data:
+            if askname.lower() == d[0].lower():
+                usename = askname
+                askmoney = input("\nHow much did this person donate?\n>>> ")
+                d.append(int(askmoney))
+                emailtext = "\nDear {},\n\nThank you for your generous donation of ${}.\n\nSincerely, Jim".format(d[0],askmoney)
+                print(emailtext)
+                break
     
-    for d in data:
-        if askname.lower() == d[0].lower():
-            usename = askname
-            askmoney = input("\nHow much did this person donate?\n>>> ")
-            d.append(int(askmoney))
-            emailtext = "\nDear {},\n\nThank you for your generous donation of ${}.\n\nSincerely, Jim".format(d[0],askmoney)
-            print(emailtext)
-            break
-    
-    if askname not in d[0] and askname.lower() != "list":
-        data.append([askname])
+        if askname not in d[0] and askname.lower() != "list":
+            data.append([askname])
 
 def create_report():
     firstline = "{:<25}| {:<12}| {:<10}| {:>12}".format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
     print(firstline)
     print("-"*len(firstline))
+    data.sort(reverse=True, key=lambda list: sum(list[1:]))
     for d in data:
-        total = sum(d[1:])
-        average = total/(len(d)-1)
-        print("{:<25} ${:>11.2f}  {:>10}  ${:>12.2f}".format(d[0],total,len(d)-1,average))
+        if len(d)==1: #zeros out the numbers if a new entry is created with no donations
+                num=0
+                total=0
+                average=0
+        else:
+            total = sum(d[1:])
+            average = total/(len(d)-1)
+            num = len(d)-1
+        
+        print("{:<25s} ${:>11.2f}  {:>10d}  ${:>12.2f}".format(d[0],total,num,average))
         
     
 def main():
@@ -36,9 +44,9 @@ def main():
         response = input(prompt)
         if response == "1" or response.lower() == "send a thank you":
             send_ty()
-        if response == '2' or response.lower() == "create a report":
+        elif response == '2' or response.lower() == "create a report":
             create_report()
-        if response == '3' or response.lower() == "quit":
+        elif response == '3' or response.lower() == "quit":
             break
 
             
