3c5f36802df5b0d4001e174609aaae4152d1c5ce
annguan07@gmail.com <annguan07@yians-mbp.lan>
2019-03-28 18:17:07 -0700

    finished version of lesson 2 Print_Grid.py assignment

--- students/annguan/lesson02/Print_Grid.py
+++ students/annguan/lesson02/Print_Grid.py
@@ -1,28 +1,35 @@
-def print_horiz(m,b):
-    """print horizontal header and footer"""
-    for i in range (m):
+#Lesson 2
+#Grid Printer Exercise
+#Run program print_grid (c,s) where c is the number of rows/columns
+# and s i the size of each cell.
+
+def print_horiz(c,s):
+    """print grid dividers +---+ 
+    c -- number of rows/columns, where rows == columns
+    s -- size of each cell, s is the number of -s and |s for each grid
+    """
+    for i in range (c):
         print("+", end = " ")
-        for j in range (b):
+        for j in range (s):
             print("-", end = " ")
     print("+")
 
-def print_vertical(m,b):
+def print_vertical(c,s):
     """print vertical bars and spaces"""
-    for i in range (b):
-        for j in range (m):
+    for i in range (s):
+        for j in range (c):
             print("|", end = " ")
-            for k in range(b):
+            for k in range(s):
                 print(" ", end = " ")
         print("|")
 
-def print_grid(m,b):
-    """print grid using variable how many cells and how big is each cell.
-    m -- how many cells/number of rows and columns
-    b -- how big is each cell/size of each cell
+def print_grid(c,s):
+    """print grid using variable how many cells (c) and 
+    how big is each cell (s)
     """
-    print_horiz(m,b)
-    print_vertical(m,b)
-    for i in range(m-1):
-        print_horiz(m,b)
-        print_vertical(m,b)
-    print_horiz(m,b)
+    print_horiz(c,s)
+    print_vertical(c,s)
+    for i in range(c-1):
+        print_horiz(c,s)
+        print_vertical(c,s)
+    print_horiz(c,s)
