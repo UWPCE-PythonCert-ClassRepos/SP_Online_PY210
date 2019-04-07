1574818ac053f194350977da72662cf867a90281
Kyle Lehning <lehnink@gmail.com>
2019-04-07 16:21:34 -0700

    most of oo mailroom complete

--- students/kyle_lehning/Lesson09/cli_main.py
+++ students/kyle_lehning/Lesson09/cli_main.py
@@ -1,4 +1,53 @@
 #!/usr/bin/env python3
+import tempfile
+import time
+import sys
+import donor_models as dm
 """
 A  module for handling command line interface for a donor program
 """
+
+
+def thank_you():
+    new_donor = dm.donor()
+
+def print_report():
+    pass
+
+
+def print_all():
+    pass
+
+
+def menu_input():
+    return input(
+        "\nSelect an option number:"
+        "\n1. Send a Thank You"
+        "\n2. Create a Report"
+        "\n3. Send letters to all donors"
+        "\n4. Quit\n"
+    )
+
+
+def list_donors():
+    pass
+
+
+def main():
+    menu_switch = {
+        "1": thank_you,
+        "2": print_report,
+        "3": print_all,
+        "4": sys.exit
+    }
+    while True:
+        user_selection = menu_input()
+        action = menu_switch.get(user_selection)
+        try:
+            action()
+        except TypeError:
+            print("Please provide a valid option \n")
+
+
+if __name__ == '__main__':
+    main()
--- students/kyle_lehning/Lesson09/donor_models.py
+++ students/kyle_lehning/Lesson09/donor_models.py
@@ -5,8 +5,52 @@ A class-based system for donors
 
 
 class Donor(object):
-    pass
+    def __init__(self, don_name):
+        self.name = don_name
+        self.total_donation = 0
+        self.donation_num = 0
+        self.most_recent_donation = 0
+
+    @property
+    def avg_donation(self):
+        # Read only attribute
+        return round(self.total_donation / self.donation_num, 2)
+
+    def new_donation(self, value):
+        self.total_donation += round(value, 2)
+        self.donation_num += 1
+        self.most_recent_donation = round(value, 2)
+
+    @classmethod
+    def from_existing(cls, don_name, don_total, don_number):
+        self = cls(don_name)
+        self.total_donation = don_total
+        self.donation_num = don_number
+        return self
+
+    def generate_recent_thanks(self):
+        string_to_format = ("Dear {},"
+                            "\n\nThank you for your kind donation of ${:.2f}."
+                            "\nIt will be put to very good use."
+                            "\n\nSincerely,"
+                            "\n-The Team")
+        return string_to_format.format(self.name, self.most_recent_donation)
+
+    def generate_total_thanks(self):
+        string_to_format = ("Dear {},"
+                            "\n\nThank you for your kind donations totalling ${:.2f}."
+                            "\nIt will be put to very good use."
+                            "\n\nSincerely,"
+                            "\n-The Team")
+        return string_to_format.format(self.name, self.total_donation)
 
 
 class DonorCollection(object):
-    pass
+    def __init__(self, ):
+        self.donor_list = []
+
+    def add_new_donor(self, donor_name):
+        self.donor_list.append(Donor(donor_name))
+
+    def list_donor_names(self):
+        return [x.name for x in self.donor_list]
