f4471466a557acec0389f2c1a9d68a66ce2c1fbb
Chelsea Nayan <chelseanayan@gmail.com>
2019-04-09 21:16:32 -0700

    Redoing L03 exercises and adding mailroom.py

--- students/chelsea_nayan/lesson03/slicing_practice.py
+++ students/chelsea_nayan/lesson03/slicing_practice.py
@@ -17,55 +17,35 @@ seq_list3 = ["Number", "time", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "end"]
 # ---------- THE FUNCTIONS ------------------
 
 # This function takes a sequence and returns a copy of that sequence with the first and last items exchanged
-def exchange_first_last(seq):
+def exchange_first_last(seq): # Corrected to remove type checking and indexing
     if len(seq) < 2: # This deals with sequences that have the length of 1. Returns the one item.
         return seq
-    if type(seq) is list or tuple: # This deals with list or tuple type cases
-        new_seq = list(seq)
-        new_seq = [seq[-1]] + new_seq[1:-1] + [seq[0]] # Had to type cast the integers into lists by adding the [] around them. It was definitely hard figuring out how to fix the TypeError when I didn't have the [].
-    if type(seq) is str: # This deals with string type cases
-        new_seq = seq[-1] + seq[1:-1] + seq[0]
-    return type(seq)(new_seq) # Returns the new sequence and its correct type
+    new_seq = seq[-1:] + seq[1:-1] + seq[0:1]
+    return new_seq
 
 
 # This function takes a sequence and returns a copy of that sequence with every other item removed
-def rm_every_other(seq):
-    if type(seq) is list or tuple: # This deals with lists or tuples type cases
-        new_seq = list(seq)
-        new_seq = new_seq[::2] # Slices out every other item in the list or tuple
-    if type(seq) is str: # This deals with string type cases
-        new_seq = seq[::2] # Slices out every other item in the string
-    return type(seq)(new_seq) # Returns the new sequence and its correct type
+def rm_every_other(seq): # Corrected to remove type checking
+    new_seq = seq[::2]
+    return new_seq
 
 
 # This function takes a sequence and returns a copy of that sequence with the first 4 and the last 4 items removed, and then every other item inbetween starting from seq[4]
-def rm_ends_and_every_other(seq):
-    if type(seq) is list or tuple: # This deals with lists or tuples type cases
-        new_seq = list(seq[4:-4:2])
-    if type(seq) is str: # This deals with string type cases
-        new_seq = seq[4:-4:2]
-    return type(seq)(new_seq) # Returns the new sequence and its correct type
-
+def rm_ends_and_every_other(seq): # Corrected to remove type checking
+    new_seq = seq[4:-4:2]
+    return new_seq
 
 # This function takes a sequence and returns a copy of that sequence with the elements reversed (just with slicing)
-def reverse(seq):
-    if type(seq) is list or tuple: # This deals with lists or tuples type cases
-        new_seq = list(seq)
-        new_seq = new_seq[::-1]
-    if type(seq) is str: # This deals with string type cases
-        new_seq = seq[::-1]
-    return type(seq)(new_seq) # Returns the new sequence and its correct type
+def reverse(seq): # Corrected to remove type checking
+    new_seq = seq[::-1]
+    return new_seq
 
 
 # This function takes a sequence and returns a copy of that sequence with the middle third, then last third, then the first third in the new order. If sequence is not divisble by 3, it should round down to the nearest integer.
-def thirds(seq):
+def thirds(seq): # Corrected to remove type checking
     third_len = int(len(seq)/3)
-    if type(seq) is list or tuple: # This deals with lists or tuples type cases
-        new_seq = list(seq)
-        new_seq = new_seq[third_len:] + new_seq[:third_len] # the middle - last thirds should just be the rest of the sequence
-    if type(seq) is str: # This deals with string type cases
-        new_seq = seq[third_len:] + seq[:third_len]
-    return type(seq)(new_seq) # Returns the new sequence and its correct type
+    new_seq = seq[third_len:] + seq[:third_len]
+    return new_seq # Returns the new sequence and its correct type
 
 
 # ----------- PRINTS OUT MY TEST CASES ------------------
