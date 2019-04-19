31254f7098a48e7b1458e73d5a3c57fbf3cf5b6b
Matthew Mitchell <matthew.allen.mitchell@gmail.com>
2019-04-18 20:48:28 -0700

    Initialize List Lab

---
+++ students/mitch334/lesson03/list_lab.py
@@ -0,0 +1,8 @@
+"""Lesson 03 | List Lab"""
+# Goal: Learn the basic ins and outs of Python lists.
+#
+# Hint: to query the user for info at the command line, you use:
+# response = input("a prompt for the user > ")
+# response will be a string of whatever the user types (until a <return>).
+
+#!/usr/bin/env python3
