82572f3fc0483f12ac7811be3ba9855bbd596e0e
Matthew Mitchell <matthew.allen.mitchell@gmail.com>
2019-04-21 20:20:31 -0700

    Commit Stage 2.

--- students/mitch334/lesson03/strformat_lab.py
+++ students/mitch334/lesson03/strformat_lab.py
@@ -17,6 +17,12 @@ unformatted_tuple = (2, 123.4567, 10000, 12345.67)
 formatted_tuple = 'file_{:03} :   {:.2f},{:.2e},{:.2e}'.format(unformatted_tuple[0],unformatted_tuple[1],unformatted_tuple[2],unformatted_tuple[3])
 # output = "file_{:0=3} :   {}, {:.2e}, {:.2e}".format(input[0], round(input[1],2), input[2], input[3])
 
-print(unformatted_tuple)
-print(formatted_tuple)
-print('file_002 :   123.46,1.00e+04,1.23e+04', ': REQUIRED OUTCOME')
+print(unformatted_tuple,"unformatted")
+print('file_002 :   123.46,1.00e+04,1.23e+04', 'required format')
+print(formatted_tuple,'formatted TASK 2')
+
+"""Task 2"""
+# Using your results from Task One, repeat the exercise, but this time using an alternate type of format string (hint: think about alternative ways to use .format() (keywords anyone?), and also consider f-strings if you’ve not used them already).
+
+formatted_tuple = f'file_{unformatted_tuple[0]:03} :   {unformatted_tuple[1]:.2f},{unformatted_tuple[2]:.2e},{unformatted_tuple[3]:.2e}'
+print(formatted_tuple,'formatted TASK 2')
