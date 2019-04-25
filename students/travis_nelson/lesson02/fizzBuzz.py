c44bda6ef475aa9df087d0d7698a774fd51d0e66
Travis Nelson <travisjamesnelson@gmail.com>
2019-04-24 21:38:24 -0700

    Installed proper linter and brought code up to pep 8 standards

--- students/travis_nelson/lesson02/fizzBuzz.py
+++ students/travis_nelson/lesson02/fizzBuzz.py
@@ -1,16 +1,17 @@
 
 def fizzBuzz1(fizzUntil=100):
     """Prints fizzBuzz from 1 through user-input integer"""
-    for i in range(1,fizzUntil+1):
+    for i in range(1, fizzUntil+1):
         if i % 3 == 0:
             if i % 5 == 0:
                 print("FizzBuzz")
-            else: 
+            else:
                 print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
         else:
             print(i)
+
 
 def fizzBuzz2(fizzUntil=100):
     """Also prints fizzbuzzes. I wanted to use more functions"""
@@ -18,10 +19,12 @@ def fizzBuzz2(fizzUntil=100):
         """Returns true if an int parameter is divisible by 3"""
         if i % 3 == 0:
             return True
+
     def isBuzz(i):
         """Returns true if an int parameter is divisible by 5"""
         if i % 5 == 0:
             return True
+
     def isFizzBuzz(i):
         """Returns true if an int parameter is divible by 3 and 5"""
         if i % 3 == 0 and i % 5 == 0:
@@ -35,4 +38,3 @@ def fizzBuzz2(fizzUntil=100):
             print('Buzz')
         else:
             print(i)
-        
