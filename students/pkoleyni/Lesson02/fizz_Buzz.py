3838d794d26afd4fb319f22475715263ef2c93f7
Pooria <p.koleyni@gmail.com>
2019-04-24 20:06:16 -0700

    Adding Space between variables and modifiying fizz_buzz() so the user can pass in any number

--- students/pkoleyni/Lesson02/fizz_Buzz.py
+++ students/pkoleyni/Lesson02/fizz_Buzz.py
@@ -1,20 +1,20 @@
 
-def multiples_of_num(num1,num2):
+def multiples_of_num(num1, num2):
     if num1%num2 ==0:
         return True
     else:
         return False
 
-def fizz_buzz():
-    for num in range(1,101):
-        if multiples_of_num(num,3) and multiples_of_num(num,5):
+def fizz_buzz(n):
+    for num in range(1, n+1):
+        if multiples_of_num(num, 3) and multiples_of_num(num, 5):
             print('FizzBuzz')
-        elif multiples_of_num(num,3):
+        elif multiples_of_num(num, 3):
             print('Fizz')
-        elif multiples_of_num(num,5):
+        elif multiples_of_num(num, 5):
             print('Buzz')
         else:
             print(num)
 
 
-fizz_buzz()
+fizz_buzz(100)
