c1a68dcc65c5bdd5d0fa0fb5884044d4e9a4fb11
Scott Betts <scbetts@adobe.com>
2019-05-05 13:51:13 -0700

    this is a good checkpoint for now

--- students/scottebetts/lesson04/trigrams.py
+++ students/scottebetts/lesson04/trigrams.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
 import random
-import defaultdict
+from collections import defaultdict
+import pprint
+import re
+import random
+
 
 def words(file_in):
-        with open(file_in, 'rb') as infile:
-                infile.read().split
-                infile.close()
+    with open(file_in, 'r') as infile:
+        x = infile.read()
+        x = re.sub(r'[^\w\s]', ' ', x).lower()
+        x = x.split()
+    return x
+
 
 def build_trigrams(words):
-
-    trigrams = {}
-    for i in range(len(words) - 2)
-    element = "{} {}".format(words[i], words[i+1])
-    trigrams[element] = {words[i+3]}
-
-    # build up the dict here!
-
+    trigrams = defaultdict(list)
+    for i in range(len(words) - 2):
+        element = "{} {}".format(words[i], words[i+1])
+        trigrams[element].append(words[i+2])
     return trigrams
 
 
+def build_text(trigrams):
+    seed_key = random.choice(list(trigrams.keys()))
+    sentence_length = random.randint(15, 20)
+    while True:
+        current_key
+
+    return first_key
+
+
 if __name__ == "__main__":
-    file_in = "sherlock.txt"
+    file_in = "./sherlock.txt"
+    # print(words(file_in))
     trigrams = build_trigrams(words(file_in))
-    print(trigrams)
+    pprint.pprint(trigrams)
+    # build_text(trigrams)
