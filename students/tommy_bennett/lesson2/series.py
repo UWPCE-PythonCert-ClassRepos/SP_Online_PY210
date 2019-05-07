c64ebc14c944eddab01705dda083f406fa3447f0
Tommy Bennett <tommy_bennett@icloud.com>
2019-05-07 14:17:53 -0500

    Added the lucas numbers series function

--- students/tommy_bennett/lesson2/series.py
+++ students/tommy_bennett/lesson2/series.py
@@ -7,4 +7,12 @@ def fibonacci(n):
     else:
         return fibonacci(n - 2) + fibonacci(n - 1)
 
+def lucas(n):
+    """Calculate the lucas numbers series"""
+    if n <= 0:
+        return 2
+    elif  n == 1:
+        return 1
+    else:
+        return lucas(n - 2) + lucas(n - 1)
 
