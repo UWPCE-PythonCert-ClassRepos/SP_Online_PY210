ed1d75b5634991c69ff8b5dc8164eed1fddcc767
Tommy Bennett <tommy_bennett@icloud.com>
2019-05-07 14:31:46 -0500

    Added the sum_series general series function

--- students/tommy_bennett/lesson2/series.py
+++ students/tommy_bennett/lesson2/series.py
@@ -16,3 +16,13 @@ def lucas(n):
     else:
         return lucas(n - 2) + lucas(n - 1)
 
+def sum_series(n, first=0, second=1):
+    """Calculate a general numbers series"""
+    if n <= 0:
+        return first 
+    elif  n == 1:
+        return second 
+    else:
+        return sum_series(n - 2, first, second) + \
+               sum_series(n - 1, first, second)
+
