e7012d35b05f6da8345e2616590c19617e52807b
Greg DeVore <greg.h.devore@gmail.com>
2019-05-08 09:14:13 -0700

    Finished mailroom part 2

--- students/gregdevore/lesson04/mailroom.py
+++ students/gregdevore/lesson04/mailroom.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
+from datetime import datetime
 
 # Donor database (use dictionary)
 donors = {'Eleanor Shellstrop':[25.00, 57.00],
@@ -13,18 +14,20 @@ prompt = '\n'.join(['','Welcome to The Good Place charity donor database.',
             'Please select from the following options:',
             '1 - Send a Thank You',
             '2 - Create a Report',
-            '3 - Exit Program',
+            '3 - Send letters to all donors',
+            '4 - Exit Program',
             '','Input > '])
 
-def generate_email(donor_name, donation_amount):
-    email_dict = {'donor_name':donor_name, 'donation_amount':donation_amount}
+def generate_email(donor_name, donation_amount, total_amount):
+    email_dict = {'donor_name':donor_name, 'donation_amount':donation_amount, 'total_amount':total_amount}
     # Create formatted email that can be copied & pasted
-    email = ('\n'.join(['','Dear {donor_name},','',
+    email = ('\n'.join(['Dear {donor_name},','',
     'Thank you for your generous donation of ${donation_amount:.2f}.',
-    'Your contribution will help new arrivals receive the highest quality care possible.',
-    'Please know that your donation makes a world of difference!',
+    'To date, you have donated a total of ${total_amount:.2f} to our charity.',
+    'Your contributions help new arrivals receive the highest quality care possible.',
+    'Please know that your donations make a world of difference!',
     '','Sincerely,','The Good Place Team'])).format(**email_dict)
-    print(email)
+    return(email)
 
 def write_thank_you():
     # Add donation for new or existing donor and compose 'Thank You' message
@@ -40,7 +43,7 @@ def write_thank_you():
         else: # Get donations for donors
             donations = donors.get(name,[])
             # Prompt for donation amount
-            amount = input('Enter donation amount in dollars: ')
+            amount = input('Enter donation amount in dollars (type \'quit\' to exit): ')
             # If the user wants to bail mid-entry, remove the donor that was just
             # added (if they were new) and return to main prompt
             if amount == 'quit':
@@ -50,8 +53,9 @@ def write_thank_you():
             # Add donation to database
             donations.append(amount)
             donors[name] = donations
-            # Generate email and return to main program
-            generate_email(name, amount)
+            # Generate & print email, return to main program
+            email = generate_email(name, amount, sum(donations))
+            print(email)
             return
 
 def donor_key(donor):
@@ -89,12 +93,29 @@ def create_report():
     report = generate_report_data()
     print_formatted_report(report)
 
+def send_letters():
+    # Prompt for directory to write to
+    target = input('Enter directory to put letters > ')
+    # Create directory if it does not exist within current directory
+    if not os.path.exists(target):
+        os.makedirs(target)
+    # Format current date to add as timestamp
+    date = datetime.today().strftime('%Y-%m-%d')
+    for donor, donation in donors.items():
+        print('Writing letter to {}'.format(donor))
+        # Generate email with donor name, last donation amount, and total donation amount
+        email = generate_email(donor,donation[-1],sum(donation))
+        # Create file with donor name and timestamp
+        filename = '{}/{}_{}.txt'.format(target, donor.replace(' ','_'), date)
+        with open(filename,'w') as f:
+            f.write(email)
+
 def exit_program():
     print('Exiting program...')
     sys.exit()
 
 def main():
-    response_dict = {'1':write_thank_you,'2':create_report,'3':exit_program}
+    response_dict = {'1':write_thank_you,'2':create_report,'3':send_letters,'4':exit_program}
     # Main function, repeatedly display prompt and react based on user input
     while True:
         response = input(prompt)
