9c7ed2018abb99d8d0e267afe588c8a970f8c495
Travis Nelson <travis.nelson@zonarsystems.com>
2019-05-08 19:05:43 -0700

    Finished Series 1

--- students/travis_nelson/lesson03/list_lab.py
+++ students/travis_nelson/lesson03/list_lab.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+# Series 1
+
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruit)
+received_fruit = input("Another fruit, please. ---> ")
+fruit.append(received_fruit)
+print(fruit)
+received_number = input("What number fruit would you like to know? ---> ")
+print("Fruit number {} is ".format(received_number) +
+      fruit[int(received_number)-1])
+fruit = ["Durian"] + fruit
+print(fruit)
+fruit.insert(1, "Cantaloupe")
+print(fruit)
+for i in fruit:
+    if i[0].upper() == 'P':
+        print(i)
