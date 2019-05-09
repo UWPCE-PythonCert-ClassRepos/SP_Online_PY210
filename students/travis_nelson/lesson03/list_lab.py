c9ec45d96d51ed561ca3e2bb475f9d67ab5b6ad5
Travis Nelson <travis.nelson@zonarsystems.com>
2019-05-09 10:49:37 -0700

    Finished series_3

--- students/travis_nelson/lesson03/list_lab.py
+++ students/travis_nelson/lesson03/list_lab.py
@@ -2,18 +2,54 @@
 
 # Series 1
 
-fruit = ["Apples", "Pears", "Oranges", "Peaches"]
-print(fruit)
-received_fruit = input("Another fruit, please. ---> ")
-fruit.append(received_fruit)
-print(fruit)
-received_number = input("What number fruit would you like to know? ---> ")
-print("Fruit number {} is ".format(received_number) +
-      fruit[int(received_number)-1])
-fruit = ["Durian"] + fruit
-print(fruit)
-fruit.insert(1, "Cantaloupe")
-print(fruit)
-for i in fruit:
-    if i[0].upper() == 'P':
-        print(i)
+
+def series_1():
+    """Returns a list of fruit"""
+    fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+    print(fruit)
+    received_fruit = input("Another fruit, please. ---> ").title()
+    fruit.append(received_fruit)
+    print(fruit)
+    received_number = input("What number fruit would you like to know? ---> ")
+    print("Fruit number {} is ".format(received_number) +
+          fruit[int(received_number)-1])
+    fruit = ["Durian"] + fruit
+    print(fruit)
+    fruit.insert(1, "Cantaloupe")
+    print(fruit)
+    for i in fruit:
+        if i[0].upper() == 'P':
+            print(i)
+    return fruit
+
+
+def series_2():
+    fruit = series_1() * 2
+    print(fruit)
+    del fruit[-1:]
+    print(fruit)
+    deletion_request = ""
+    while deletion_request.title() not in fruit:
+        deletion_request = input("Which fruit should be deleted? ---> ")
+    while deletion_request.title() in fruit:
+        fruit.remove(deletion_request.title())
+    print(fruit)
+
+
+def series_3():
+    fruit = series_1()
+    for i in fruit[:]:
+        keep_asking = True
+        received_preference = ""
+        while keep_asking:
+            received_preference = input("Do you like {}?---> ".format(
+                                        i.lower()))
+            if received_preference.lower() == "yes":
+                keep_asking = False
+                break
+            if received_preference.lower() == "no":
+                fruit.remove(i)
+                keep_asking = False
+                break
+            print("Please respond with 'yes' or 'no'.")
+    return fruit
