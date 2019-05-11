1fa113a4a11728b6c113f80b24685e17c77a6ebc
Erica Edwards <ericae14@hotmail.com>
2019-05-11 13:05:11 -0700

    Finished copy for class

--- students/erica_edwards/Lesson07/html_render.py
+++ students/erica_edwards/Lesson07/html_render.py
@@ -10,7 +10,8 @@ A class-based system for rendering html.
 
 class Element(object):
     tag = "html"
-
+    indent = "   "
+    
     def __init__(self, content=None, **kwargs):
         self.attributes = kwargs if kwargs is not None else {}
         self.contents = [content] if content is not None else []
@@ -29,18 +30,20 @@ class Element(object):
     def _close_tag(self):
         return '</{}>'.format(self.tag)
         
-    def render(self, out_file):
+    def render(self, out_file, ind=""):
         # loop through the list of contents
-        open_tag = [self._open_tag()]
+        open_tag = [ind]                                 # Indent
+        open_tag.append(self._open_tag())
         open_tag.append("\n")
         out_file.write("".join(open_tag))
         for content in self.contents:
             # out_file.write('<{}>\n'.format(self.tag))
             if hasattr(content, 'render'):
-                content.render(out_file)        # new
+                content.render(out_file, ind=self.indent + ind)        # new      # Indent -> pass
             else:
-                out_file.write(f'{content}\n')  # old
+                out_file.write(f'{self.indent + ind}{content}\n')  # old      # Indent 
             #out_file.write("\n")
+        out_file.write(f'{ind}')                                # Indent
         out_file.write(self._close_tag())
         out_file.write("\n")
         
@@ -54,8 +57,26 @@ class Element(object):
         #     out_file.write(content)
 
 
+# class Doctype(Element):
+#     tag = "!DOCTYPE"
+
+#     def __init__(self, doc_type, content=None, **kwargs):
+#         kwargs['doc_type'] = doc_type
+#         super().__init__(content, **kwargs)
+    
+#     def _open_tag(self):
+#         open_tag = f"<{self.tag} {self.attributes['doc_type']}>"
+#         return open_tag
+
+#     def render(self, out_file, cur_ind=""):
+#         out_file.write(self._open_tag())
+
+
 class Html(Element):
     tag = "html"
+
+    def _open_tag(self):
+        return "<!DOCTYPE html>\n" + super()._open_tag()
 
 
 class Body(Element):
@@ -71,12 +92,14 @@ class Head(Element):
 
 
 class OneLineTag(Element):
-    def render(self, out_file):
-        # out_file.write('<{}>'.format(self.tag))
+    def render(self, out_file, ind=""):
+        out_file.write(ind)
         out_file.write(self._open_tag())
+        # out_file.write(self.indent + ind)
         out_file.write(self.contents[0])
+        # out_file.write(ind)
         out_file.write(self._close_tag())
-        # out_file.write('</{}>\n'.format(self.tag))
+        out_file.write('\n')
 
     def append(self, content):
         raise NotImplementedError
@@ -94,8 +117,10 @@ class SelfClosingTag(Element):
             raise self.self_closing_typeerror
         super().__init__(content=content, **kwargs)
 
-    def render(self, out_file):
+    def render(self, out_file, ind=""):
+        out_file.write(ind)
         tag = self._open_tag()[:-1] + " />\n"
+        # out_file.write(ind)
         out_file.write(tag)
     
     def append(self, *args):
@@ -110,9 +135,31 @@ class Br(SelfClosingTag):
     tag = "br"
 
 
+class Meta(SelfClosingTag):
+    tag = "meta"
+
+
 class A(OneLineTag):
     tag = "a"
     
     def __init__(self, link, content=None, **kwargs):
         kwargs['href'] = link
         super().__init__(content, **kwargs)
+
+
+class Ul(Element):
+    tag = "ul"
+
+
+class Li(Element):
+    tag = "li"
+
+
+class H(OneLineTag):
+    tag = "h"
+
+    def __init__(self, level, content=None, **kwargs):
+        self.tag = f'{self.tag}{level}'
+        super().__init__(content, **kwargs)
+
+
