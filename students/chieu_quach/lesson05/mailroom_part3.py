bab471bebda3c5ff5445982a602ee0a92932318e
Chieu1 <chieuq@uw.edu>
2019-05-14 10:11:30 -0500

    updated latest mailroom 3

--- students/chieu_quach/lesson05/mailroom_part3.py
+++ students/chieu_quach/lesson05/mailroom_part3.py
@@ -41,122 +41,121 @@ msg  = ( " Dear {}, "
 
 def send_thankyou():
   
-   e = 0
+    e = 0
   
-   while True:
+    while True:
       
-       try:
-           fname = input(" Please enter full name \n" 
-           "(select list to list report or exit to return to menu) ")
-           if fname == "exit":
-               break
-           if not  fname:
-               raise ValueError
-           elif fname.isdigit():
-               raise TypeError
-       except TypeError as e:
-           print ("Please re-enter string name ")
-           print (e)
-       except ValueError as i:
-           print ("Please re-enter (empty string)")
-           print (i)
-       except NameError:
-           print ("Name Error")
-           print (z)
-       else:
-           print("fname ", fname)
-           name_found = ""
-           # newlist = sorted(donation_list, key=itemgetter('Amount'), reverse=True)
-           chk_input = [value for  value in donation_list if value['full name'] == fname]        
-           if fname == "list":
-               e = e + 1
-               if e == 1:
+        try:
+            fname = input(" Please enter full name \n" 
+            "(select list to list report or exit to return to menu) ")
+            if fname == "exit":
+                break
+            if not  fname:
+                raise ValueError
+            elif fname.isdigit():
+                raise TypeError
+        except TypeError as e:
+            print ("Please re-enter string name ")
+            print (e)
+        except ValueError as i:
+            print ("Please re-enter (empty string)")
+            print (i)
+        except NameError:
+            print ("Name Error")
+            print (z)
+        else:
+            print("fname ", fname)
+            name_found = ""
+            # newlist = sorted(donation_list, key=itemgetter('Amount'), reverse=True)
+            chk_input = [value for  value in donation_list if value['full name'] == fname]        
+            if fname == "list":
+                e = e + 1
+                if e == 1:
                     print ("List of Names \n")
-               chk_inputz = [value for  value in donation_list]
-               new_chk    = sorted(chk_inputz, key=itemgetter('Amount'), reverse=True)
-               chkout = [print("{:<19} {:14.2f}{:^15d}{:=21.2f} ".format(value['full name'],value['Amount'],
+                chk_inputz = [value for  value in donation_list]
+                new_chk    = sorted(chk_inputz, key=itemgetter('Amount'), reverse=True)
+                chkout = [print("{:<19} {:14.2f}{:^15d}{:=21.2f} ".format(value['full name'],value['Amount'],
                     int(value['num_gift']), value['avg_amt'])) for value in new_chk]
-               print ("\n")
-               break    
-           if not chk_input:
-               name_found = "no"
-               fullname = fname
-           else:
-               name_found = "yes"
-               fullname = chk_input[0]['full name']
-               amount   = chk_input[0]['Amount']
-               giftnum  = chk_input[0]['num_gift']
-               avg_amt  = chk_input[0]['avg_amt']           
-
-          
-           complete = False
-           while not complete: 
-               try:
-                   nu_amount =  float(input(" Please Enter Donation Amount: "))
-               except ValueError:
-                   print ("Value Error ")
-               except IndexError:
-                   print ("Index Error ", response)
-               except ValueError as e:
-                   print ("ValueError ")
-                   print (e)
-               except KeyError as o:
-                   print ("Invalid selection ")
-                   print (o)
-               else:
-                   print ("no Error")
-                   print ("nu amount ", nu_amount)
-                   # break
-                   if name_found == "yes":
-                       name = ""
-                       new_list = ""
-                       new_len_list = len(donation_list) + 1         
-                                    
-                       grand_amount = nu_amount + float(amount)                                
-                       gift_tot     = int(giftnum) + 1
-                       amount_avg   = grand_amount / gift_tot
-                       name     = {'full name': fname, 'Amount': amount, 'num_gift': giftnum, 'avg_amt': avg_amt}
-                       # print("full name ", fullname)
+                print ("\n")
+                break    
+            if not chk_input:
+                name_found = "no"
+                fullname = fname
+            else:
+                name_found = "yes"
+                fullname = chk_input[0]['full name']
+                amount   = chk_input[0]['Amount']
+                giftnum  = chk_input[0]['num_gift']
+                avg_amt  = chk_input[0]['avg_amt']           
+         
+            complete = False
+            while not complete: 
+                try:
+                    nu_amount =  float(input(" Please Enter Donation Amount: "))
+                except ValueError:
+                    print ("Value Error ")
+                except IndexError:
+                    print ("Index Error ", response)
+                except ValueError as e:
+                    print ("ValueError ")
+                    print (e)
+                except KeyError as o:
+                    print ("Invalid selection ")
+                    print (o)
+                else:
+                    print ("no Error")
+                    print ("nu amount ", nu_amount)
+                    # break
+                    if name_found == "yes":
+                        name = ""
+                        new_list = ""
+                        new_len_list = len(donation_list) + 1         
+                                       
+                        grand_amount = nu_amount + float(amount)                                
+                        gift_tot     = int(giftnum) + 1
+                        amount_avg   = grand_amount / gift_tot
+                        name     = {'full name': fname, 'Amount': amount, 'num_gift': giftnum, 'avg_amt': avg_amt}
+                        # print("full name ", fullname)
+                         
+                          # Update record to reflect new changes made
+                        new_list = {"full name": fname,
+                            "Amount": grand_amount, "num_gift": gift_tot, "avg_amt": amount_avg}            
                       
-                       # Update record to reflect new changes made
-                       new_list = {"full name": fname,
-                          "Amount": grand_amount, "num_gift": gift_tot, "avg_amt": amount_avg}            
-                   
-                       full_name = fname
-                       # added "_" to last name
-                       full_name = ("_".join(fullname.split()))
-            
-                       full_name = full_name + ".txt"
-                       output_msg = [print(msg.format(fullname, nu_amount))]                
-                       donation_list.remove(name)
-                       donation_list.append(new_list)             
-                       # sends text message to output file
-                       with open(full_name, "w") as outfile:
-                           outfile.writelines(msg.format(fullname, nu_amount))
-                                 
-                       outfile.close()
-                       complete = True
-                       break
+                        full_name = fname
+                        # added "_" to last name
+                        full_name = ("_".join(fullname.split()))
                
-                   else:
-                      # New user
-                      # Append new item to list
-                      num_gift = 1
+                        full_name = full_name + ".txt"
+                        output_msg = [print(msg.format(fullname, nu_amount))]                
+                        donation_list.remove(name)
+                        donation_list.append(new_list)             
+                          # sends text message to output file
+                        with open(full_name, "w") as outfile:
+                             outfile.writelines(msg.format(fullname, nu_amount))
+                                    
+                        outfile.close()
+                        complete = True
+                        break
+               
+                    else:
+                        # New user
+                        # Append new item to list
+                        num_gift = 1
             
-                      # update new length
-                      new_len_list = len(donation_list) + 1                  
-                      full_name = ("_".join(fname.split()))          
-                      full_name = full_name + ".txt"                           
-                      output_msg = [print(msg.format(fullname, nu_amount))]
-                      new_listz = {"full name": fname, 
-                         "Amount": nu_amount, "num_gift": num_gift, "avg_amt": nu_amount}  
-                      # sends text message to output file
-                      with open(full_name, "w") as outfile:
-                          outfile.writelines(msg.format(fullname, nu_amount))                       
-                      donation_list.append(new_listz)                 
-                      outfile.close()
-                      complete = True
-                      break
+                        # update new length
+                        new_len_list = len(donation_list) + 1                  
+                        full_name = ("_".join(fname.split()))          
+                        full_name = full_name + ".txt"                           
+                        output_msg = [print(msg.format(fullname, nu_amount))]
+                        new_listz = {"full name": fname, 
+                           "Amount": nu_amount, "num_gift": num_gift, "avg_amt": nu_amount}  
+                        # sends text message to output file
+                        with open(full_name, "w") as outfile:
+                           outfile.writelines(msg.format(fullname, nu_amount))                       
+                        donation_list.append(new_listz)                 
+                        outfile.close()
+                        complete = True
+                        break
               
 def create_report():
     # print ("create Report")
@@ -193,8 +192,10 @@ def send_letters():
     outfile.close()
    
 def exit_program():
-    print ("program")
-          
+    
+    print ("Thanks for visiting ")
+    sys.exit()
+    
 def main():
 
     while True:
