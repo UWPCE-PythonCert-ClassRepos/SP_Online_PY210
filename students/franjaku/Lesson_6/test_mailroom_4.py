7fc0e1122c14fa6d2cc193ce73019392e8b36afd
franjaku <kulyckyf@umich.edu>
2019-05-14 18:30:50 -0700

    added baseline test functions

--- students/franjaku/Lesson_6/test_mailroom_4.py
+++ students/franjaku/Lesson_6/test_mailroom_4.py
@@ -6,8 +6,41 @@ Uses pytest to test all the code in the mailroom_4 module.
 
 """
 
+
 def test_initialize_database():
     pass
 
+
 def test_add_new_donation():
+    pass
+
+def test_sort_key():
+    pass
+
+
+def test_prompt_user():
+    pass
+
+
+def test_get_donation_amount():
+    pass
+
+
+def test_thank_you_note_prompt():
+    pass
+
+
+def test_send_thank_you_note():
+    pass
+
+
+def test_send_thank_you_note_all():
+    pass
+
+
+def test_create_report():
+    pass
+
+
+def test_mail_room():
     pass
