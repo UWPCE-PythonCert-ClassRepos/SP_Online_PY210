69e6e7e28bc84cd792a540a2266b7ae9ed7ac200
franjaku <kulyckyf@umich.edu>
2019-05-14 18:22:41 -0700

    initialize test_mailroom_4.py

--- students/franjaku/Lesson_6/test_mailroom_4.py
+++ students/franjaku/Lesson_6/test_mailroom_4.py
@@ -1,1 +1,13 @@
 import mailroom_4 as ml
+
+"""
+Uses pytest to test all the code in the mailroom_4 module.
+
+
+"""
+
+def test_initialize_database():
+    pass
+
+def test_add_new_donation():
+    pass
