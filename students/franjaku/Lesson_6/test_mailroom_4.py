2f93462cd86be2c61fff72a65ae8f195150058e8
franjaku <kulyckyf@umich.edu>
2019-05-14 18:49:13 -0700

    added initalize database test, add new donation tests

--- students/franjaku/Lesson_6/test_mailroom_4.py
+++ students/franjaku/Lesson_6/test_mailroom_4.py
@@ -8,7 +8,11 @@ Uses pytest to test all the code in the mailroom_4 module.
 
 
 def test_initialize_database():
-    pass
+    assert ml.initialize_database() == {'John Smith': [5000, 52048, 20],
+                                        'Jane Adams': [25000, 5498, 3333, 87469],
+                                        'Brett Johnson': [50, 6584, 20, 68, 9857, 5412],
+                                        'Sofia Pippy': [623, 98, 40658],
+                                        'Maddy North': [85426, 10, 25]}
 
 
 def test_add_new_donation():
@@ -16,7 +20,14 @@ def test_add_new_donation():
 
 
 def test_sort_key():
-    pass
+    # Test Existing Donor
+    database = ml.initialize_database()
+    ml.add_new_donation(database, 'John Smith',50)
+    assert sum(database['John Smith']) == 5000+52048+20+50
+
+    # Test New Donor
+    ml.add_new_donation(database, 'Wiggle Jiggle', 500)
+    assert sum(database['Wiggle Jiggle']) == 500
 
 
 def test_prompt_user():
