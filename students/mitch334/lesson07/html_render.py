684ac2be2ff07cc4f01d0e938a4b296fd322d2c0
Matthew Mitchell <matthew.allen.mitchell@gmail.com>
2019-05-29 20:43:56 -0700

    Commit Step 2 part A.

--- students/mitch334/lesson07/html_render.py
+++ students/mitch334/lesson07/html_render.py
@@ -8,11 +8,35 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
+    tag = "html"
+
     def __init__(self, content=None):
-        pass
+        self.contents = []
+
+        if content is not None:
+            self.contents = [content]
+        # print("contents is:", self.contents)
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        print('self.contents: ', self.contents)
+
+        out_file.write('<{}>\n'.format(self.tag))
+
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write('\n')
+
+        out_file.write('</{}>\n'.format(self.tag))
+
+
+class Html(Element):
+    tag = "html"
+
+class Body(Element):
+    tag = "body"
+
+class P(Element):
+    tag = "p"
