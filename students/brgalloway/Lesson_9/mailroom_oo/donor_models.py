59842b2e2f1420b39acfd064d6d5cce38ca6b2fd
Billy Galloway <billy.galloway@allstardirectories.com>
2019-06-12 21:46:07 -0700

    updates to lesson 9

--- students/brgalloway/Lesson_9/mailroom_oo/donor_models.py
+++ students/brgalloway/Lesson_9/mailroom_oo/donor_models.py
@@ -2,22 +2,23 @@
 
 class Donor(object):
 
-   def __init__(self, fullname):
+   def __init__(self, fullname, donation):
       self.fullname = fullname
-      
+      self.donation = donation
+      # on creation set donations total to 0
+      # self.donation_total = 0
       self.times_donated = 0
       self.average_donation = 0
-
+   
    def __repr__(self):
-      return "{{\"{}\":{{{}, {}, {}}}".format(self.fullname,self.donation_total, self.times_donated, self.average_donation)
+      return "\"{}\": {{{}, {}, {}}}".format(self.fullname, self.donation_total, self.times_donated, self.average_donation)
    
    @property
-   def donated(self):
+   def donation_total(self):
       return self.donation_total
    
-   @donated.setter
-   def donated(self, donation):
-      self.donation_total = 0
+   @donation_total.setter
+   def donation_total(self, donation):
       self.donation = donation
       self.donation_total = self.donation_total + self.donation
       self.times_donated+=1
--- students/brgalloway/Lesson_9/mailroom_oo/test_donors.py
+++ students/brgalloway/Lesson_9/mailroom_oo/test_donors.py
@@ -3,20 +3,10 @@ from mailroom_oo.donor_models import Donor as d
 
 
 def test_donors():
-    d1 = d("Billy Galloway")
+    d1 = d("Billy Galloway", 100)
     assert d1.fullname == "Billy Galloway"
-   
+    assert d1.donation_total == 100
     
-    
-    
-    d1.donated = 100
-    d1.donated = 100
-    d1.donated = 80
-    d1.donated = 20
-   
    
     print(repr(d1))
     print(d1.average_donation)
-    # print(d1.donation_total)
-    # d1.donation_total = 100
-    # print(d1.donation_total)
