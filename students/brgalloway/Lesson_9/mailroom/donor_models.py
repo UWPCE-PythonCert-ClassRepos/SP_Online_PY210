4d2adf5c6e793c6a875e97796a5a1964e70156bf
brgalloway <digital.locust@gmail.com>
2019-06-13 22:52:52 -0700

    updates to donor model

--- students/brgalloway/Lesson_9/mailroom/donor_models.py
+++ students/brgalloway/Lesson_9/mailroom/donor_models.py
@@ -1,46 +1,58 @@
 # from mailroom_oo import database as db
+class Donor(object):
+   '''
+   Donor class takes a fullname to initiate a single donor. 
+   Each donor has three attributes: total donated, number of donations, and average donation
+   '''
+   def __init__(self, fullname):
+      self.fullname = fullname
+      self.donation_total = 0
+      self.times_donated = 0
+      self.average_donation = 0
+      
+   def __repr__(self):
+      # Prints out the object in dictionary format
+      # TODO output Donor object as dictionary to match representation
+      return f"{{\"{self.fullname}\": {{\"donation_total\": {self.donation_total:.2f}, \"times_donated\": {self.times_donated}, \"average_donation\": {self.average_donation:.2f}}}"
+       
+   @property
+   def calculate_donations(self):
+      # returns the donation total amount
+      return self.donation_total
+   
+   @calculate_donations.setter
+   def calculate_donations(self, donation):
+      # calculates all donor attribute actions
+      self.donation_total = donation + self.donation_total
+      self.times_donated+=1
+      self.average_donation = self.donation_total / self.times_donated
+      self.donor_attributes = [self.fullname, self.donation_total, self.times_donated, self.average_donation]
+
+   @classmethod
+   def from_donor(cls, fullname, donation_total, times_donated=1):
+      # alternative constructor
+      cls.fullname = fullname
+      cls.donation_total = donation_total
+      cls.times_donated = times_donated
+      cls.average_donation = donation_total / cls.times_donated
+      cls.donor_attributes = [fullname, donation_total, cls.times_donated, cls.average_donation]
+   
+      return  Donor.donor_attributes
+
+   def send_thankyou(self):
+      pass
 
 class DonorCollection(object):
    '''
    A collection of donors and the donations contributed
    '''
-   def __init__(self, fullname):
-      pass
+   def __init__(self, donor):
+      self.donor = donor
 
    def new_donor(self):
       pass
    def existing_donor(self):
       pass
 
-class Donor(object):
-   '''
-   Donor class takes a fullname to initiate a single donor. 
-   Each donor has three attributes: total donated, number of donations, and average donation
-   '''
-   def __init__(self, fullname, **kwargs):
-      self.fullname = fullname
-      self.donation_atttimes_donated = 0
-      self.donation_total = 0
-      self.average_donation = 0
-      self.donordb = {}
-      
-   def __repr__(self):
-      return f"{{\"{self.fullname}\": {{\"donation_total\": {self.donation_total:.2f}, \"times_donated\": {self.times_donated}, \"average_donation\": {self.average_donation:.2f}}}"
-       
-   @property
-   def calculate_donations(self):
-      return self.donation_total
-   
-   @calculate_donations.setter
-   def calculate_donations(self, donation):
-      self.donation_total = donation + self.donation_total
-      self.times_donated+=1
-      self.average_donation = self.donation_total / self.times_donated
 
-   # @classmethod
-   # def from_donor(cls, fullname, donation):
-   #    pass
-  
-   def send_thankyou(self):
-      pass
 
--- students/brgalloway/Lesson_9/mailroom/test_mailroom_oo.py
+++ students/brgalloway/Lesson_9/mailroom/test_mailroom_oo.py
@@ -9,13 +9,18 @@ def test_donors():
     assert d1.fullname == "Billy Galloway"
     assert d1.donation_total == 160
     print(repr(d1))
+    print(d1.donor_attributes)
+    d2 = Donor.from_donor("larry", 110)
+    d2.calculate_donations = 200
+    print(d2)
+    
     # donors_ = (d1.fullname, d1.donation_total, d1.times_donated, d1.average_donation)
     # print(donors_)
     # d2 = d("billy")
     # d3 = d("gina")
-def test_database():
-    d1 = Donor("Billy")
-    d2 = Donor("Gina")
-    db = DonorCollection(d1)
-    print(db)
+# def test_database():
+#     d1 = Donor("Billy")
+#     d2 = Donor("Gina")
+#     db = DonorCollection(d1)
+#     print(db)
     
