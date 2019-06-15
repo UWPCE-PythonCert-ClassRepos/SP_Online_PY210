c46bdbf2fbfd690a45db55744094343e98513bed
brgalloway <digital.locust@gmail.com>
2019-06-15 15:42:50 -0700

    completed donor and started on database

--- students/brgalloway/Lesson_9/mailroom/donor_models.py
+++ students/brgalloway/Lesson_9/mailroom/donor_models.py
@@ -1,4 +1,4 @@
-# from mailroom_oo import database as db
+
 class Donor(object):
    '''
    Donor class takes a fullname to initiate a single donor. 
@@ -16,18 +16,19 @@ class Donor(object):
       return f"{{\"{self.fullname}\": {{\"donation_total\": {self.donation_total:.2f}, \"times_donated\": {self.times_donated}, \"average_donation\": {self.average_donation:.2f}}}"
        
    @property
-   def calculate_donations(self):
+   def donate(self):
       # returns the donation total amount
       return self.donation_total
    
-   @calculate_donations.setter
-   def calculate_donations(self, donation):
+   @donate.setter
+   def donate(self, donation):
       # calculates all donor attribute actions
       self.donation_total = donation + self.donation_total
       self.times_donated+=1
       self.average_donation = self.donation_total / self.times_donated
       self.donor_attributes = [self.fullname, self.donation_total, self.times_donated, self.average_donation]
 
+   # TODO Fix to properly function as an alternative constructor 
    @classmethod
    def from_donor(cls, fullname, donation_total, times_donated=1):
       # alternative constructor
@@ -37,7 +38,10 @@ class Donor(object):
       cls.average_donation = donation_total / cls.times_donated
       cls.donor_attributes = [fullname, donation_total, cls.times_donated, cls.average_donation]
    
-      return  Donor.donor_attributes
+      return  cls
+   
+   def __lt__(self, other):
+      return self.donation_total < other.donation_total
 
    def send_thankyou(self):
       pass
@@ -46,13 +50,51 @@ class DonorCollection(object):
    '''
    A collection of donors and the donations contributed
    '''
-   def __init__(self, donor):
-      self.donor = donor
+   # TODO replace list with dictionary
+   def __init__(self):
+      self.donor_list = []
 
-   def new_donor(self):
-      pass
-   def existing_donor(self):
-      pass
+   def __iter__(self):
+      return self.donor_list.__iter__()
 
+   def append(self, donor):
+      self.donor_list.append(donor)
+   
+   def search(self, fullname):
+      self.fullname = fullname
+      for i in self.donor_list:
+         if i.fullname == fullname:
+            print(i.fullname + " found in database.")   
+         else:
+            return print(fullname, "not in database")
+
+   def sort_donors(a_list):
+    return self.donor_list[0].donation_total
+
+   def generate_report(self):
+      '''
+      Generate report based on menu choice
+      and return user to the menu prompt  
+      '''  
+      print("{:<20}|{:^15}|{:^15}|{:^15}".format("Donor Name", "Total Given", "Num Gifts", "Average Gifts"))
+      print("-" * 70)
+
+      # print(self.donor_list[0].donation_total)
+      self.donor_list.sort(key=lambda x: self.donor_list.donation_total)
+      print([item.donation_total for item in donor_list])
+
+      for donors in self.sorted_list:
+         name = donors.fullname 
+         total = donors.donation_total
+         times = donors.times_donated
+         average = donors.average_donation
+         print(f"{name:<20}${total:>14.2f}{times:^18}${average:>12.2f}".format())
+         
+      
+   def __repr__(self):
+      return self.donor_list
+
+   def bulk_thankyou(self):
+      pass
 
 
--- students/brgalloway/Lesson_9/mailroom/test_mailroom_oo.py
+++ students/brgalloway/Lesson_9/mailroom/test_mailroom_oo.py
@@ -3,24 +3,25 @@ from donor_models import *
 
 def test_donors():
     d1 = Donor("Billy Galloway")
-    d1.calculate_donations = 100
-    d1.calculate_donations = 10
-    d1.calculate_donations = 50
+    d1.donate = 100
+    d1.donate = 10
+    d1.donate = 50
     assert d1.fullname == "Billy Galloway"
     assert d1.donation_total == 160
-    print(repr(d1))
-    print(d1.donor_attributes)
-    d2 = Donor.from_donor("larry", 110)
-    d2.calculate_donations = 200
-    print(d2)
-    
-    # donors_ = (d1.fullname, d1.donation_total, d1.times_donated, d1.average_donation)
-    # print(donors_)
-    # d2 = d("billy")
-    # d3 = d("gina")
-# def test_database():
-#     d1 = Donor("Billy")
-#     d2 = Donor("Gina")
-#     db = DonorCollection(d1)
-#     print(db)
+    print(d1)
+
+def test_database():
+    d1 = Donor("Billy")
+    d2 = Donor("Gina")
+    db = DonorCollection()
+
+    db.append(d1)
+    db.append(d2)
+    d2.donate = 100
+    d2.donate = 100
+    assert db.donor_list[1].fullname == "Gina"
+    db.search("Roger")
+    print(db.generate_report())
+
+
     
