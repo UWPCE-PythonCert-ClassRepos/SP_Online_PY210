731207eb379e801a985f667438845ee032c5cd63
brgalloway <digital.locust@gmail.com>
2019-06-16 21:59:07 -0700

    lesson 9 finished mailroom oop

--- students/brgalloway/Lesson_9/mailroom/cli_main.py
+++ students/brgalloway/Lesson_9/mailroom/cli_main.py
@@ -1,7 +1,7 @@
 import sys
 import donor_models as d
 
-
+# form some starting dating 
 d1 = d.Donor("Jeff Bezos")
 d2 = d.Donor("Paul Allen")
 d3 = d.Donor("William Gates, III")
@@ -11,8 +11,11 @@ d1.donation_total, d1.times_donated, d1.average_donation = 877.33, 1, 877.33
 d2.donation_total, d2.times_donated, d2.average_donation = 708.42, 3, 236.14
 d3.donation_total, d3.times_donated, d3.average_donation = 653784.49, 2, 326892.24
 d4.donation_total, d4.times_donated, d4.average_donation = 2354.05, 3, 784.68
-d5.donation_total, d5.times_donated, d5.average_donation =16396.10, 3, 5465.37
+d5.donation_total, d5.times_donated, d5.average_donation = 16396.10, 3, 5465.37
 
+# TODO add another function to create new donors from user input
+
+# Take the 5 donors and put them into a collection
 donor_db = d.DonorCollection()
 donor_db.append(d1)
 donor_db.append(d2)
@@ -20,39 +23,49 @@ donor_db.append(d3)
 donor_db.append(d4)
 donor_db.append(d5)
 
-print(donor_db.donor_list)
+
+def quit_app():
+    return "quit"
+
 # menu prompt and options to select from
-self.prompt = "Choose one of the following options. \n\n" \
+prompt = "Choose one of the following options. \n\n" \
     "1 - Send a Thank You to a single donor \n" \
     "2 - Create a Report \n" \
     "3 - Send letters to all donors \n" \
     "4 - Quit \n" \
     ">> "
 
+# shortend names for dipatch dictionary
+find_donor = d.DonorCollection.find_donor
+generate_report = d.DonorCollection.generate_report
+bulk_thankyou = d.DonorCollection.bulk_thankyou
+
 # value returned from choice keys
-# TODO configure options 1-3 as classes
-self.main_dispatch = {
-    "1": d.find_donor,
-    "2": m.generate_report,
-    "3": m.bulk_thankyou,
-    "4": self.quit_app
+main_dispatch = {
+    "1": find_donor,
+    "2": generate_report,
+    "3": bulk_thankyou,
+    "4": quit_app
 }
     
-def quit_app(self):
-    return "quit"
-
-
-def menu_selection(self):
+def menu_selection(prompt, main_dispatch):
     
     while True:
-        response = input(self.prompt)
+        response = input(prompt)
         response = response.lower()
         response = response.strip()
         try:
-            if self.main_dispatch[response]() == "quit":
-                sys.exit()
+            if response in main_dispatch:
+                if response == "1":
+                    find_donor(d.DonorCollection, donor_db)
+                elif response == "2":
+                    generate_report(d.DonorCollection, donor_db)
+                elif response == "3":
+                    bulk_thankyou(d.DonorCollection, donor_db)
+                elif response == "4":
+                    sys.exit()
         except KeyError:
             print("\n\ninvalid response\n")
 
 if __name__ == '__main__':
-    menu_selection()
+    menu_selection(prompt, main_dispatch)
--- students/brgalloway/Lesson_9/mailroom/donor_models.py
+++ students/brgalloway/Lesson_9/mailroom/donor_models.py
@@ -43,43 +43,87 @@ class Donor(object):
    def __lt__(self, other):
       return self.donation_total < other.donation_total
 
-   def send_thankyou(self):
-      pass
+   def send_thankyou(self, fullname, donation):
+      '''Send email to a single donor showing their single donation'''
+      email_output = []
+      self.fullname = fullname
+      self.donation = donation
+      email_template = "\n".join((f"Dear {fullname},\n\nThank you for your very kind donations this year totaling at ${donation:.2f}.\n",
+                     "It will be put to very good use.\n",
+                     "Sincerely,",
+                     "-The Team"))
+         
+      filename = fullname.replace(" ","_") + ".txt"
 
-class DonorCollection(object):
+      if "," in filename:
+         filename = filename.replace(",","") + ".txt"
+         with open(filename.replace(" ", "_"), "w") as file:
+               email_output.append(filename)
+               file.write(email_template)
+      else:
+         with open(filename, "w") as file:
+               email_output.append(filename)
+               file.write(email_template)
+
+      return email_output
+
+class DonorCollection(Donor):
    '''
-   A collection of donors and the donations contributed
+   A collection of donors and the donations contributed. 
+   Donor Collection can also be used to send an email to 
+   all donors. 
    '''
-   # TODO replace list with dictionary
    def __init__(self):
+      '''
+      initailizes with a list to hold donors and their information
+      as well as a tracking variable
+      '''
       self.donor_list = []
+      self.new_user = False
 
    def __iter__(self):
+      # allows the list to be iterated over
       return self.donor_list.__iter__()
 
+   def __len__(self):
+      # used to loop over database
+      return self.donor_list.__len__()
+
+   def __getitem__(self, index):
+      # output values at  specific index
+      return self.donor_list[index]
+
    def append(self, donor):
+      # append donors to database
       self.donor_list.append(donor)
    
    def search(self, fullname):
+      '''
+      Search database for users and return true or false
+      depending on the user is already in the database or not
+      '''
       self.fullname = fullname
-      for i in self.donor_list:
-         if i.fullname == fullname:
-            print(i.fullname + " found in database.")   
-         else:
-            return print(fullname, "not in database")
+      for i in range(len(self.donor_list)):
+         if self.donor_list[i].fullname == fullname:
+            # found user in database
+            self.new_user = False 
+            return self.new_user 
+      else:
+         # did not find user in database
+         self.new_user = True 
+         return self.new_user
 
-   def generate_report(self):
+   def generate_report(self, donor_list):
       '''
       Generate report based on menu choice
       and return user to the menu prompt  
       '''  
-      sorted_list = sorted(self.donor_list)
+      self.donor_list = donor_list
+      sorted_list = sorted(self.donor_list, reverse=True)
 
       print("{:<20}|{:^15}|{:^15}|{:^15}".format("Donor Name", "Total Given", "Num Gifts", "Average Gifts"))
       print("-" * 70)
-
-      # print(self.donor_list[0].donation_total)
-   
+  
       for donors in sorted_list:
          name = donors.fullname 
          total = donors.donation_total
@@ -87,11 +131,90 @@ class DonorCollection(object):
          average = donors.average_donation
          print(f"{name:<20}${total:>14.2f}{times:^18}${average:>12.2f}".format())
          
-      
    def __repr__(self):
+      # Prints full database
       return self.donor_list
+   
+   def list_names(self):
+      for i in self.donor_list:
+         print(i.fullname)
+   
+   def update_donation(self, fullname, donation_amount):
+      '''
+      Searches the database looking for the donors name given
+      and adds their new contribution to the database
+      '''
+      self.fullanme = fullname
+      self.donation_amount = donation_amount
+      for i in range(len(self.donor_list)):
+         if self.donor_list[i].fullname == fullname:
+            self.donor_list[i].donation_total+=donation_amount
+            self.donor_list[i].times_donated+=1
+            self.donor_list[i].average_donation = self.donor_list[i].donation_total / self.donor_list[i].times_donated
 
-   def bulk_thankyou(self):
-      pass
+   def find_donor(self, donor_list):
+      '''
+      Searches database with the search function, list names in database, and sends email
+      '''
+      self.donor_list = donor_list
+      while True:
+         fullname = input("type list to display names or quit to exit to main menu\n" \
+                         "Enter full name of donor: ")
+         if fullname == "list":
+            self.list_names(DonorCollection)
+         elif fullname and fullname != "quit":
+            try:
+               # search database for user and set flag
+               self.search(DonorCollection, fullname)
+               if self.new_user == False:
+                  #update donors inside the database
+                  donation_amount = float(input("Donation amount: "))
+                  self.update_donation(DonorCollection, fullname, donation_amount)
+                  self.send_thankyou(Donor, fullname, donation_amount)
+               elif self.new_user == True:
+                  # ask the user if they want to add this value to the database
+                  print("User not found")
+                  add_user = input("Would you like to add a new donor? (yes or no)\n>> ")
+                  add_user = add_user.lower()
+                  add_user = add_user.strip()
+                  if add_user == "yes":
+                     # if user wants to add them to the database the perform login and email
+                     new_donor = Donor(fullname)
+                     self.donor_list.append(new_donor)
+                     donation_amount = float(input("Donation amount: "))
+                     self.update_donation(DonorCollection, fullname, donation_amount)
+                     self.send_thankyou(fullname, donation_amount)
+                  else:
+                     return
+            except ValueError:
+               print("not a valid response exiting to donor selection")
+               return
+         elif fullname == "quit":
+            return
+
+   def bulk_thankyou(self, donor_list):
+      '''Send email to all donors showing their total donations'''
+      email_output = []
+      self.donor_list = donor_list
+      for i in range(len(self.donor_list)):
+         donation_amount = self.donor_list[i].donation_total
+         email_template = "\n".join((f"Dear {self.donor_list[i].fullname},\n\nThank you for your very kind donations this year totaling at ${donation_amount:.2f}.\n",
+                     "It will be put to very good use.\n",
+                     "Sincerely,",
+                     "-The Team"))
+         
+         filename = self.donor_list[i].fullname.replace(" ","_") + ".txt"
+         if "," in filename:
+            filename = filename.replace(",","") + ".txt"
+            with open(filename.replace(" ", "_"), "w") as file:
+                  email_output.append(filename)
+                  file.write(email_template)
+         else:
+            with open(filename, "w") as file:
+                  email_output.append(filename)
+                  file.write(email_template)
+
+      return email_output
+
 
 
