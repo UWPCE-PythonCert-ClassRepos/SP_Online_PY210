28e4eb2c70f8cae6e20675729eeb7695c09b3b00
Zach Thomson <thomsonzachary@gmail.com>
2019-06-18 20:51:42 -0700

    Series 1 first pass complete for list_lab.py. Double check

---
+++ students/zach_thomson/lesson03/list_lab.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#series 1 tasks
+fruit_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
+print(fruit_list)
+new_fruit = input('Please add another fruit: ')
+fruit_list.append(new_fruit)
+print(fruit_list)
+fruit_idx = input('Please choose a number between 1-5: ')
+fruit_idx = int(fruit_idx)
+print(fruit_idx, fruit_list[fruit_idx - 1])
+new_fruit2 = ['Mangos']
+fruit_list2 = new_fruit2 + fruit_list
+print(fruit_list2)
+fruit_list2.insert(0, 'Nectarines')
+print(fruit_list2)
+starts_with_P_list = []
+for fruit in fruit_list2:
+    if fruit.startswith('P'):
+        starts_with_P_list.append(fruit)
+print(starts_with_P_list)
