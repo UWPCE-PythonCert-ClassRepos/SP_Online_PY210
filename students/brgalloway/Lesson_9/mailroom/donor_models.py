23cc570ce985db0622842019930e3c0ff563c2ec
brgalloway <digital.locust@gmail.com>
2019-06-20 23:48:24 -0700

    updates to lesson 9

--- students/brgalloway/Lesson_9/mailroom/donor_models.py
+++ students/brgalloway/Lesson_9/mailroom/donor_models.py
@@ -38,10 +38,10 @@ class Donor(object):
       self.donation_total.append(donation)
 
    
-   def send_thankyou(self, fullname, donation):
+   def send_thankyou(self):
       '''Send email to a single donor showing their single donation'''
       email_output = []
-      email_template = "\n".join((f"Dear {self.fullname},\n\nThank you for your very kind donations this year totaling at ${self.donation:.2f}.\n",
+      email_template = "\n".join((f"Dear {self.fullname},\n\nThank you for your very kind donations this year totaling at ${self.sum_of_donations:.2f}.\n",
                      "It will be put to very good use.\n",
                      "Sincerely,",
                      "-The Team"))
@@ -74,10 +74,6 @@ class DonorCollection(object):
       self.donor_list = {d.fullname: d for d in args}
       self.new_user = False
 
-   def __repr__(self):
-      # Prints full database
-      return self.donor_list
-
    def __iter__(self):
       # allows the list to be iterated over
       return self.donor_list.__iter__()
@@ -86,128 +82,31 @@ class DonorCollection(object):
       # used to loop over database
       return self.donor_list.__len__()
 
-   def __getitem__(self, index):
-      # output values at  specific index
-      return self.donor_list[index]
-
-   def append(self, donor):
-      # append donors to database
-      self.donor_list.append(donor)
-   
-   def search(self, fullname):
-      '''
-      Search database for users and return true or false
-      depending on the user is already in the database or not
-      '''
-      self.fullname = fullname
-      for i in range(len(self.donor_list)):
-         if self.donor_list[i].fullname == fullname:
-            # found user in database
-            self.new_user = False 
-            return self.new_user 
+   def apply_donation(self, name, donation):
+      if self.donor_list.get(name):
+      # if user exists add donation to list
+         self.donor_list[name].apply_donation(donation)
       else:
-         # did not find user in database
-         self.new_user = True 
-         return self.new_user
+      # add donor if it does not exist
+         self.donor_list[name] = Donor(name, donation)
 
-   def generate_report(self, donor_list):
+   def generate_report(self):
       '''
       Generate report based on menu choice
       and return user to the menu prompt  
       '''  
-      self.donor_list = donor_list
-      sorted_list = sorted(self.donor_list, reverse=True)
-
       print("{:<20}|{:^15}|{:^15}|{:^15}".format("Donor Name", "Total Given", "Num Gifts", "Average Gifts"))
       print("-" * 70)
   
-      for donors in sorted_list:
-         name = donors.fullname 
-         total = donors.donation_total
+      for donors in self.donor_list.values():
+         name = donors.fullname
+         total = donors.sum_of_donations
          times = donors.times_donated
          average = donors.average_donation
-         print(f"{name:<20}${total:>14.2f}{times:^18}${average:>12.2f}".format())
-         
-   def list_names(self):
-      for i in self.donor_list:
-         print(i.fullname)
-   
-   def update_donation(self, fullname, donation_amount):
-      '''
-      Searches the database looking for the donors name given
-      and adds their new contribution to the database
-      '''
-      self.fullanme = fullname
-      self.donation_amount = donation_amount
-      for i in range(len(self.donor_list)):
-         if self.donor_list[i].fullname == fullname:
-            self.donor_list[i].donation_total+=donation_amount
-            self.donor_list[i].times_donated+=1
-            self.donor_list[i].average_donation = self.donor_list[i].donation_total / self.donor_list[i].times_donated
-
-   def find_donor(self, donor_list):
-      '''
-      Searches database with the search function, list names in database, and sends email
-      '''
-      self.donor_list = donor_list
-      while True:
-         fullname = input("type list to display names or quit to exit to main menu\n" \
-                         "Enter full name of donor: ")
-         if fullname == "list":
-            self.list_names(DonorCollection)
-         elif fullname and fullname != "quit":
-            try:
-               # search database for user and set flag
-               self.search(DonorCollection, fullname)
-               if self.new_user == False:
-                  #update donors inside the database
-                  donation_amount = float(input("Donation amount: "))
-                  self.update_donation(DonorCollection, fullname, donation_amount)
-                  self.send_thankyou(Donor, fullname, donation_amount)
-               elif self.new_user == True:
-                  # ask the user if they want to add this value to the database
-                  print("User not found")
-                  add_user = input("Would you like to add a new donor? (yes or no)\n>> ")
-                  add_user = add_user.lower()
-                  add_user = add_user.strip()
-                  if add_user == "yes":
-                     # if user wants to add them to the database the perform login and email
-                     new_donor = Donor(fullname)
-                     self.donor_list.append(new_donor)
-                     donation_amount = float(input("Donation amount: "))
-                     self.update_donation(DonorCollection, fullname, donation_amount)
-                     self.send_thankyou(fullname, donation_amount)
-                  else:
-                     return
-            except ValueError:
-               print("not a valid response exiting to donor selection")
-               return
-         elif fullname == "quit":
-            return
-
-   def bulk_thankyou(self, donor_list):
+         print(f"{name:<20}${total:>14.2f}{times:^18}${average:>12.2f}")
+          
+   def bulk_thankyou(self):
       '''Send email to all donors showing their total donations'''
-      email_output = []
-      self.donor_list = donor_list
-      for i in range(len(self.donor_list)):
-         donation_amount = self.donor_list[i].donation_total
-         email_template = "\n".join((f"Dear {self.donor_list[i].fullname},\n\nThank you for your very kind donations this year totaling at ${donation_amount:.2f}.\n",
-                     "It will be put to very good use.\n",
-                     "Sincerely,",
-                     "-The Team"))
-         
-         filename = self.donor_list[i].fullname.replace(" ","_") + ".txt"
-         if "," in filename:
-            filename = filename.replace(",","") + ".txt"
-            with open(filename.replace(" ", "_"), "w") as file:
-                  email_output.append(filename)
-                  file.write(email_template)
-         else:
-            with open(filename, "w") as file:
-                  email_output.append(filename)
-                  file.write(email_template)
-
-      return email_output
-
-
-
+      for donors in self.donor_list.values():
+         donors.send_thankyou()
+    
--- students/brgalloway/Lesson_9/mailroom/test_mailroom.py
+++ students/brgalloway/Lesson_9/mailroom/test_mailroom.py
@@ -9,43 +9,15 @@ def test_donors():
     assert d1.sum_of_donations == 201
     assert d1.times_donated == 3
 
-    print(repr(d1))
-
 def test_database():
-    db = DonorCollection(Donor("Billy", [100, 1, 100]))
-    print(db.donor_list)
-    db.add_donations()
     
-    # db = DonorCollection()
-    # db.append(d1)
-    # db.append(d2)
-    # d2.donate = 100
-    # d2.donate = 100
-    # assert db.donor_list[1].times_donated == 2
-    # assert db.donor_list[1].fullname == "Gina"
-    # assert db.donor_list[1].donation_total == 200
-    # assert db.donor_list[1].average_donation == 100
+    db = DonorCollection(Donor("Billy Galloway", [100, 1, 100]))
+    db.generate_report()
+    db.apply_donation("Billy Galloway", 10)
+    db.bulk_thankyou()
+    assert os.path.isfile("Billy_Galloway.txt") == True
+    assert db.donor_list["Billy Galloway"].times_donated == 4
+    db.generate_report()
 
-# def test_send_thankyou():
-#     email_template = Donor.send_thankyou(Donor, "Paul Allen", 10)
-#     with open("Paul_Allen.txt", "r") as file:
-#         email_output = file.readlines()
-#     assert os.path.isfile("Paul_Allen.txt") == True
-#     email_template = Donor.send_thankyou(Donor, "Larry David", 10)
-#     with open("Larry_David.txt", "r") as file:
-#         email_output = file.readlines()
 
-#     assert os.path.isfile("Larry_David.txt") == True
 
-# def test_bulk_email():
-#     d1 = Donor("Billy")
-#     d2 = Donor("Gina")
-#     db = DonorCollection()
-#     db.append(d1)
-#     db.append(d2)
-#     d2.donate = 100
-#     d2.donate = 100
-    
-#     email_output = DonorCollection.bulk_thankyou(DonorCollection, db.donor_list)
-#     assert os.path.isfile("Billy.txt") == True
-#     assert os.path.isfile("Gina.txt") == True
