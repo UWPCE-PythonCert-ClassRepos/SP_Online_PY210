cb3989319cae4d6caec8c3caef233d10272316c9
mattcasari <mjcasari@gmail.com>
2019-06-20 17:57:41 -0700

    Initial commit of the list lab.

---
+++ students/mattcasari/lesson03/list_lab.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Lesson 3, Excercise 2
+
+@author: Matt Casari
+
+Link: https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/list_lab.html
+
+Tasks:
+    Series 1:
+        - Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+        - Display the list (plain old print() is fine…).
+        - Ask the user for another fruit and add it to the end of the list.
+        - Display the list.
+        - Ask the user for a number and display the number back to the user and the fruit corresponding to that number (on a 1-is-first basis). Remember that Python uses zero-based indexing, so you will need to correct.
+        - Add another fruit to the beginning of the list using “+” and display the list.
+        - Add another fruit to the beginning of the list using insert() and disp
+
+    Series 2:
+        Using the list created in series 1 above:
+        - Display the list.
+        - Remove the last fruit from the list.
+        - Display the list.
+        - Ask the user for a fruit to delete, find it and delete it.
+        - (Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+    
+    Series 3:
+        Again, using the list from series 1:
+        - Ask the user for input displaying a line like “Do you like apples?” for each fruit in the list (making the fruit all lowercase).
+        - For each “no”, delete that fruit from the list.
+        - For any answer that is not “yes” or “no”, prompt the user to answer with one of those two values (a while loop is good here)
+        - Display the list
+
+    Series 4:
+        Once more, using the list from series 1:
+        - Make a new list with the contents of the original, but with all the letters in each item reversed.
+        - Delete the last item of the original list. Display the original list and the copy.
+
+"""
+
+
+
