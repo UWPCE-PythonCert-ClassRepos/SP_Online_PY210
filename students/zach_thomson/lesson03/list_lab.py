e4589a91ab7c0754b01f89fe09b1b749bb43f1a7
Zach Thomson <thomsonzachary@gmail.com>
2019-06-23 17:22:29 -0700

    Added some extra strings to clarify all the lists and the tasks in the console

--- students/zach_thomson/lesson03/list_lab.py
+++ students/zach_thomson/lesson03/list_lab.py
@@ -3,43 +3,58 @@
 #Series 1 tasks
 fruit_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print(fruit_list)
+
 new_fruit = input('Please add another fruit: ')
 fruit_list.append(new_fruit)
-print(fruit_list)
+print('This is the new list of fruits: ' + str(fruit_list) + '\n')
+
 fruit_idx = input('Please choose a number between 1-5: ')
 fruit_idx = int(fruit_idx)
 print(fruit_idx, fruit_list[fruit_idx - 1])
+
 new_fruit2 = ['Mangos']
 fruit_list2 = new_fruit2 + fruit_list
-print(fruit_list2)
+print('Adding Mangos to fruit list using "+": ' + str(fruit_list2) + '\n')
+
 fruit_list2.insert(0, 'Nectarines')
-print(fruit_list2)
+print('Adding Nectarines to fruit list using ".insert": ' + str(fruit_list2) + '\n')
+
 starts_with_P_list = []
 for fruit in fruit_list2:
     if fruit.startswith('P'):
         starts_with_P_list.append(fruit)
-print(starts_with_P_list)
+print('All the fruits in the list that start with "P": ' + str(starts_with_P_list) + '\n')
 
 #Series 2 tasks - Make shallow copy of list so can change it without affecting original
 series2_fruit = fruit_list2[:]
-print(series2_fruit)
+print('List for the start of series 2: ' + str(series2_fruit) + '\n')
 series2_fruit.pop(-1)
-print(series2_fruit)
+print('Removed last item from list: ' + str(series2_fruit) + '\n')
 delete_fruit = input('Please choose a fruit to toss: ')
 series2_fruit.remove(delete_fruit)
 print(series2_fruit)
 
-#Series 3 tasks - oddly skipping values. Figure out how to use while loop 
+#Series 3 tasks
 series3_fruit = fruit_list2[:]
-print(series3_fruit)
-for fruit in series3_fruit:
-    answer = input('Do you like ' + fruit.lower())
-    if  answer.lower() == 'no':
-        series3_fruit.remove(fruit)
-    elif answer.lower() == 'yes':
-        pass
-    else:
-        print('Please provide a yes or no answer.')
-print(series3_fruit)
+print('This is the list for the start of series3: '+ str(series3_fruit) + '\n')
+
+def customer_favorite():
+    i = 0
+    while i < len(series3_fruit):
+        answer = input("Do you like " + series3_fruit[i].lower() + ': ')
+        if answer.lower() == 'no':
+            series3_fruit.pop(i)
+        elif answer.lower() == 'yes':
+            i = i + 1
+        else:
+            print('Please answer yes or no')
+            continue
+
+customer_favorite()
+
+print("These are the fruits you like: " + str(series3_fruit))
 
 #Series 4 tasks
+series4_fruit = fruit_list2[:]
+reversed_fruit = [x[::-1] for x in series4_fruit[::-1]]
+print(str(fruit_list2[:-1]) + "\n" + str(reversed_fruit))
