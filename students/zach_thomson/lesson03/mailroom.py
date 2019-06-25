1362470616fc2446d615599ba8579bb89bc85283
Zach Thomson <thomsonzachary@gmail.com>
2019-06-24 20:16:02 -0700

    slowly making some progress on mailroom

--- students/zach_thomson/lesson03/mailroom.py
+++ students/zach_thomson/lesson03/mailroom.py
@@ -1,11 +1,15 @@
 #import sys
 
+#for adding new donor, just add to list with addition as a tuple but issue with editing previous donors
+
 donor_db = [('donor 1', [10000.00, 20000.00, 4500.00]),
             ('donor 2', [100.00, 500.00]),
             ('donor 3', [25.00]),
             ('donor 4', [100000.00, 50000.00, 125000.00]),
             ('donor 5', [50.00])]
 
+
+#print(donor_db)
 #print(donor_db)
 #print(donor_db[1][1])
 
@@ -18,12 +22,61 @@ prompt = '\n'.join(('Welcome to the mailroom',
 
 #send a thank you tasks
 #initial_thank_you_prompt = input('Please enter a full name: ')
+def donor_in_list(x):
+    i = 0
+    result = False
+    while i < len(donor_db):
+        if x == donor_db[i][0]:
+            result = True
+            break
+        else:
+            i = i + 1
+    return result
 
+def donor_idx(x):
+    i = 0
+    while i < len(donor_db):
+        if x == donor_db[i][0]:
+            return i
+        else:
+            i += 1
+
+def donor_names():
+    i = 0
+    donors = []
+    while i < len(donor_db):
+        donors.append(donor_db[i][0])
+        i += 1
+    return donors
+
+def add_donor():
+    pass
+
+def thank_you_initial():
+    prompt = input('Please enter a full name or type list to see all the current donors: ')
+    if donor_in_list(prompt) == True:
+        donation_amount = float(input('Please enter a donation amount: '))
+        print(donor_idx(prompt))
+        #add donation amount to data structure
+    elif prompt.lower() == 'list':
+        #display names and rerun prompt
+        print(donor_names())
+        #figure out how to get back to original prompt (while loop most likely)
+        prompt
+    else:
+        donation_amount = float(input('Please enter a donation amount: '))
+        new_entry = (prompt, [donation_amount])
+        donor_db.append(new_entry)
+        print(donor_db)
+
+thank_you_initial()
 
 def thank_you():
     pass
 
 #create a report functions
+donor_db_copy = donor_db[:]
+
 def sum_function(x):
     total = 0
     for i in donor_db_copy[x][1]:
@@ -42,7 +95,6 @@ def sum_second(elem):
         total = i + total
     return total
 
-donor_db_copy = donor_db[:]
 donor_db_copy.sort(key = sum_second, reverse = True)
 #print(donor_db_copy)
 
@@ -69,7 +121,7 @@ def create_report():
     line_format = ("{:<20}" + " $" + "{:>12.2f}" + "{:>11}" + "  $" + "{:>12.2f}" + '\n') * len(donor_db)
     print(line_format.format(*table))
 
-create_report()
+#create_report()
 
 def exit_program():
     print('Bye')
