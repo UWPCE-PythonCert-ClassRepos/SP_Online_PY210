2e1987ea72edfde879b00ce821bab4e626eb8aff
franjaku <kulyckyf@umich.edu>
2019-07-03 17:28:10 -0700

    added tests for step 4

--- students/franjaku/Lesson_8/test_circle.py
+++ students/franjaku/Lesson_8/test_circle.py
@@ -3,6 +3,7 @@ Test code for circle.py
 """
 
 import pytest
+import math
 
 from circle import *
 
@@ -33,6 +34,8 @@ def test_init():
 """
 Add a diameter property so the user can get the diameter of the circle
 """
+
+
 def test_diameter():
     """Test that diameter attribute exists and twice the radius"""
     c = Circle(5)
@@ -48,6 +51,7 @@ Allow the user to set the diameter of the circle.
 Ensure that the radius and diameter are always in sync.
 Make the diameter and radius both properties.
 """
+
 
 def test_diameter_radius_property():
     c = Circle(5)
@@ -66,3 +70,34 @@ def test_diameter_radius_property():
 
     assert c.radius == 25
     assert c.diameter == 50
+
+
+########
+# Step 4
+########
+"""
+Add an area property so the user can get the property of the circle.
+
+Make sure they user can't set the area.
+"""
+
+
+def test_area():
+
+    c = Circle(5)
+
+    # test area gets calculated properly
+    assert c.area == 5*5*math.pi()
+
+    # test user can't set area property
+    with pytest.raises(AttributeError):
+        c.area = 12
+
+    # ensure area gets updated with radius and diameter updates
+    c.diameter = 20
+    assert c.radius == 10
+    assert c.area == 100*math.pi()
+
+    c.radius = 20
+    assert c.diameter == 40
+    assert c.area == 20*20*math.pi()
