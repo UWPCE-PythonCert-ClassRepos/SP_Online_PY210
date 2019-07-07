2800b8ed59813a625c2a98b4f0143a9e20987c02
Matthew Mitchell <matthew.allen.mitchell@gmail.com>
2019-07-06 20:05:18 -0700

    commit updated send_thankyou().

--- students/mitch334/lesson09/mailroom.py
+++ students/mitch334/lesson09/mailroom.py
@@ -1,8 +1,9 @@
-"""Lesson 06 | Mailroom Part 4"""
+"""Lesson 09 | Mailroom Part 5"""
 
 """You work in the mail room at a local charity. Part of your job is to write incredibly boring,
-repetitive emails thanking your donors for their generous gifts. You are tired of doing this over and over again,
-so you’ve decided to let Python help you out of a jam and do your work for you."""
+repetitive emails thanking your donors for their generous gifts.
+You are tired of doing this over and over again, so you’ve decided to let Python help
+you out of a jam and do your work for you."""
 
 # The Program: Part 1
 # Write a small command-line script called mailroom.py. This script should be executable. The script should accomplish the following goals:
@@ -25,78 +26,91 @@ so you’ve decided to let Python help you out of a jam and do your work for you
 # Comprehensions
 # Can you use comprehensions to clean up your code a bit?
 
-# The Program: Part 4
 # Add a full suite of unit tests.
+# The Program: Part 4
 # “Full suite” means all the code is tested. In practice, it’s very hard to test the user interaction, but you can test everything else. Therefore you should make sure that there is as little logic and untested code in the user interaction portion of the program as possible.
 # This is a big step; you may find that your code is hard to test. If that’s the case, it’s a good sign that you should refactor your code.
 # I like to say: “If it’s hard to test, it’s not well structured.”
 # Put in the tests before you make the other changes below. That’s much of the point of tests. You can know that you haven’t broken anything when you refactor!
 
+# The Program: Part 5
+# Goal: Refactor the mailroom program using classes to help organize the code.
+
+
 import os
 import tempfile
 import datetime
 
-""" Initial donor list with donation history
-    Data: Donor Name: [Donation Amount(s)]"""
-donors = {"William Gates, III": [100.00, 50.00],
-            "Jeff Bezos": [1000.00, 10.00, 500.00],
-            "Mark Zuckerberg": [200.00, 20.00, 50.00],
-            "Warren Buffet": [600.00, 300.00],
-            "Paul Allen": [1000.00]
-            }
+# Class responsible for donor data encapsulation
+class Donor:
+    def __init__(self, name, donations=None):
+        self.name = name
 
-# Send a Thank You
-# If the user (you) selects “Send a Thank You” option, prompt for a Full Name.
-# If the user types list show them a list of the donor names and re-prompt.
-# If the user types a name not in the list, add that name to the data structure and use it.
-# If the user types a name in the list, use it.
-# Once a name has been selected, prompt for a donation amount.
-# Convert the amount into a number; it is OK at this point for the program to crash if someone types a bogus amount.
-# Add that amount to the donation history of the selected user.
-# Finally, use string formatting to compose an email thanking the donor for their generous donation. Print the email to the terminal and return to the original prompt.
-# It is fine (for now) for the program not to store the names of the new donors that had been added, in other words, to forget new donors once the script quits running.
+        if donations == None:
+            self._donations = []
+        else:
+            self._donations = list(donations)
 
-def donors_list(list_output):
-    if list_output == 'list':
-        list = '\nDonor List:\n'
-        for donor in donors:
-            list += '\t' + f'{donor}' + '\n'
+    @property
+    def name(self):
+        return self._name
 
-    elif list_output == 'db':
-        list = '\nDonor Database:\n'
-        for name, val in donors.items():
-            list += '\t' + f'{name:26}{val}' + '\n'
-    else:
-        list = ''
+    @name.setter
+    def name(self, value):
+        self._name = value
 
-    return list
+    @property
+    def donations(self):
+        return self._donations
 
 
-def add_donation(name, amount = None):
-    donor_names = [donor for donor in donors]
+# Class responsible for donor collection data encapsulation
+class DonorCollection:
+    def __init__(self, donors=None):
+        if donors == None:
+            donors = []
+        else:
+            self._donors = donors
 
-    if name not in donor_names:
-        donors[name] = []
+    @property
+    def donors(self):
+        return self._donors
 
-    if amount == None:
-        amount = input('Enter donation amount: ')
+    def donors_list(self, list_output):
+        list = '\n'
 
-    try:
-        amount = round(float(amount),2)
-        donors[name].append(amount)
-
-        # print()
-        # print(create_email(name, amount))
-
-    except ValueError:
-        print('Error. Enter a valid donation amount.')
-        add_donation(name)
+        for donor in self.donors:
+            if list_output == 'list':
+                list += f'{donor.name}' + '\n'
+            elif list_output == 'db':
+                list += f'{donor.name:26}{donor.donations}' + '\n'
+            else:
+                list = ''
+        return list
 
 
-def create_email(name, amount):
+    def add_donor(self, name):
+        donor_exists = False
 
-    return (f'Dear {name},\n\nThank you for the generous donation of ${amount:.2f}.\n\n'
-      'Sincerely,\nMatthew Mitchell')
+        for donor in self.donors:
+            if donor.name == name:
+                donor_exists = True
+                break
+
+        if not donor_exists:
+            donor = Donor(name)
+            self.donors.append(donor)
+
+        return donor
+
+""" Initial donor list"""
+donors = DonorCollection([
+    Donor("William Gates, III", [100.00, 50.00]),
+    Donor("Jeff Bezos", [1000.00, 10.00, 500.00]),
+    Donor("Mark Zuckerberg", [200.00, 20.00, 50.00]),
+    Donor("Warren Buffet", [600.00, 300.00]),
+    Donor("Paul Allen", [1000.00])
+    ])
 
 
 def send_thankyou():
@@ -110,69 +124,32 @@ def send_thankyou():
                             '\tEnter full name of donor: ')
 
         if donor_name.lower() == 'list':
-            print(donors_list('list'))
-
+            print(donors.donors_list('list'))
         elif donor_name.lower() == 'db':
-            print(donors_list('db'))
-
-        elif donor_name.lower() == 'exit':
+            print(donors.donors_list('db'))
+        elif donor_name.lower() == 'exit' or donor_name.lower() == '4':
             break
-
         else:
-            add_donation(donor_name.title())
+            donor = donors.add_donor(donor_name.title())
 
-# Create a Report
-# If the user (you) selected “Create a Report,” print a list of your donors, sorted by total historical donation amount.
-# Include Donor Name, total donated, number of donations, and average donation amount as values in each row. You do not need to print out all of each donor’s donations, just the summary info.
-# Using string formatting, format the output rows as nicely as possible. The end result should be tabular (values in each column should align with those above and below).
-# After printing this report, return to the original prompt.
-# At any point, the user should be able to quit their current task and return to the original prompt.
-# From the original prompt, the user should be able to quit the script cleanly.
-# Your report should look something like this:
-#
-# Donor Name                | Total Given | Num Gifts | Average Gift
-# ------------------------------------------------------------------
-# William Gates, III         $  653784.49           2  $   326892.24
-# Mark Zuckerberg            $   16396.10           3  $     5465.37
-# Jeff Bezos                 $     877.33           1  $      877.33
-# Paul Allen                 $     708.42           3  $      236.14
+            amount = verify_donation()
+            print('verify_donation amount: ', amount)
 
-def create_report():
-    report = 'Donor Name                | Total Given | Num Gifts | Average Gift\n'
-    report += '-'*66 + '\n'
+            donor.donations.append(amount)
 
-    donors_stats = {k: [sum(v), len(v), sum(v)/len(v) ] for k, v in donors.items()}
+def verify_donation():
+    amount_input = input('Enter donation amount: $')
 
-    for donor, stat in sorted(donors_stats.items(), key=lambda d: d[1][0], reverse=True):
-        report += f'{donor:26} ${stat[0]:>11.2f} {stat[1]:>11.0f}  ${stat[2]:>12.2f}\n'
+    try:
+        amount = round(float(amount_input),2)
 
-    print(report)
-    return report
+        print('return amount: ', amount)
+        return amount
 
-# Update mailroom with file writing.
-# Goal: Write a full set of letters to all donors to individual files on disk.
-# In the first version of mailroom, you generated a letter to a donor who had just made a new donation, and printed it to the screen.
-# In this version of your program, add a function (and a menu item to invoke it), that goes through all the donors in your donor data structure, generates a thank you letter for each donor, and writes each letter to disk as a text file.
-# The letters should each get a unique file name – you can keep it really simple and just use the donor’s name or add a date timestamp for additional uniqueness.
-# You want to avoid specifying a hardcoded file path when creating the files
+    except ValueError:
+        print('Error. Enter a valid donation amount.')
+        verify_donation()
 
-def send_letters():
-    now = datetime.datetime.now()
-    now = str(now)[:-7].replace(' ','_').replace(':','')
-
-    location = tempfile.gettempdir()
-    files = []
-
-    for name, value in donors.items():
-        file = location + '\\' + name.replace(' ','-') + '__' + str(now) + '.txt'
-        files.append(file)
-
-        with open(file, 'w') as letter:
-            letter.write(create_email(name, value[-1]))
-
-    print(f'Letters created and located at: {location}')
-
-    return files
 
 def quit():
     print('Enjoy! :)')
@@ -181,14 +158,15 @@ def quit():
 def invalid_menu():
     print('Invalid option. Select 1, 2, 3, or 4.')
 
-# send_thankyou()
+send_thankyou()
 # create_report()
 # send_letters()
 
 if __name__ == '__main__':
 
     option = None
-    main_menu = {'1': send_thankyou, '2': create_report, '3': send_letters, '4': quit }
+    # main_menu = {'1': send_thankyou, '2': create_report, '3': send_letters, '4': quit }
+    main_menu = {'1': send_thankyou, '2': quit, '3': quit, '4': quit }
 
     while option != '4':
         option = input('Main Menu:\n'
