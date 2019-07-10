aa94ad3e52a02f5e01276bf50f0ee323a6389b5a
Bryan Coates <bcoates@halowerks.com>
2019-07-09 17:18:30 -0700

    HTML Render Steps 1 - 7

---
+++ students/bcoates/lesson07/html_render.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element(object):
+
+    tag = "html"
+
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            self.contents = [content]
+        else:
+            self.contents = []
+
+        if kwargs is not None:
+            self.attributes = kwargs
+        else:
+            self.attributes = {}
+
+    def _open_tag(self):
+        open_tag = ["<{}".format(self.tag)]
+        for key, value in self.attributes.items():
+            open_tag.append(" {}=\"{}\"".format(key, value))
+        open_tag.append(">")
+        return "".join(open_tag)
+
+    def _close_tag(self):
+        close_tag = "</{}>".format(self.tag)
+        return close_tag
+
+    def append(self, new_content):
+        self.contents.append(new_content)
+
+    def render(self, out_file):
+        out_file.write(self._open_tag())
+        out_file.write("\n")
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except:
+                out_file.write(content)
+            out_file.write("\n")
+        out_file.write(self._close_tag())
+
+class Html(Element):
+    tag = "html"
+
+class Body(Element):
+    tag = "body"
+
+class P(Element):
+    tag = "p"
+
+class Head(Element):
+    tag = "head"
+
+class OneLineTag(Element):    
+    def render(self, out_file):
+        #out_file.write("<{}>".format(self.tag))
+        out_file.write(self._open_tag())
+        out_file.write(self.contents[0])
+        #out_file.write("</{}>\n".format(self.tag))
+        out_file.write(self._close_tag())
+
+    def append(self, content):
+        raise NotImplementedError
+
+class Title(OneLineTag):
+    tag = "title"
+
+class SelfClosingTag(Element):
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError("SelfClosingTag can not contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, out_file):
+        out_file.write(self._open_tag()[:-1] + " />\n")
+
+    def append(self, *args):
+        raise TypeError("You can not add content to a SelfClosingTag")
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+class Br(SelfClosingTag):
+    tag = "br"
+
+class A(OneLineTag):
+    tag = "a"
+
+    def __init__(self, link, content=None, **kwargs):
+        kwargs['href'] = link
+        super().__init__(content, **kwargs)
+
+class Ul(Element):
+    tag = "ul"
+
+class Li(Element):
+    tag = "li"
+
+class H(OneLineTag):
+
+    def __init__(self, level, content=None, **kwargs):
+        self.tag = "h{}".format(level)
+        super().__init__(content, **kwargs)
