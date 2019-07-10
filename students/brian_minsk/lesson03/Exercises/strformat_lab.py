d77a36692bd45825a020151862e6ea5b96e8a204
Brian Minsk <brian.minsk@gmail.com>
2019-07-09 17:28:50 -0700

    Completed Task 4 of String Formatting Lab

--- students/brian_minsk/lesson03/Exercises/strformat_lab.py
+++ students/brian_minsk/lesson03/Exercises/strformat_lab.py
@@ -4,15 +4,15 @@ def main():
     """ Call the other functions which do the actual work per
     https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/string_formatting.html
     """
-    formatted_string = task1(( 2, 123.4567, 10000, 12345.67))
-    assert formatted_string == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+    assert task1(( 2, 123.4567, 10000, 12345.67)) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
     
-    formatted_string2 = task2(( 2, 123.4567, 10000, 12345.67))
-    assert formatted_string2 == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+    assert task2(( 2, 123.4567, 10000, 12345.67)) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
 
     # Task 3
     assert formatter((1,23,4)) == "the 3 numbers are: 1, 23, 4"
     assert formatter((5, 5, 3, 3, 1)) == "the 5 numbers are: 5, 5, 3, 3, 1"
+
+    assert task4((4, 30, 2017, 2, 27)) == '02 27 2017 04 30'
 
 
 def task1(a_tuple):
@@ -42,29 +42,30 @@ def formatter(a_tuple):
     Keyword arguments:
     a_tuple -- a tuple of arbitrary size, each element is an integer
     """
-    a_string =  "The {:d} numbers are:".format(len(a_tuple))
+    a_string =  "the {:d} numbers are:".format(len(a_tuple))
 
-    form_string = ""
+    temp_string = ""
     
     for i in range(0, len(a_tuple)):
-        form_string += " {:d},"
+        temp_string += " {:d},"
 
-    form_string = form_string[:-1] #remove the extra comma
+    temp_string = temp_string[:-1] #remove the extra comma
 
-    print(form_string)
-    print(a_tuple)
-
-    #t = (1, 23, 4)
-
-    form_string.format(*a_tuple)
-
-    print(form_string)
-
-    print(f"{a_string}{form_string}")
+    form_string = temp_string.format(*a_tuple)
 
     return f"{a_string}{form_string}"
 
-        
+def task4(a_tuple):
+    """ Given a 5 element tuple:
+    ( 4, 30, 2017, 2, 27)
+    use string formating to print:
+    '02 27 2017 04 30'
+
+    Keyword arguments:
+    a_tuple: a tuple of 5 integers
+    """
+    return "{:0>2d} {:d} {:d} {:0>2d} {:d}".format(a_tuple[3], a_tuple[4], a_tuple[2], a_tuple[0], a_tuple[1])
+
 def create_file_name(file_number):
     if file_number < 10:
         return "file_{:0>3d}".format(file_number)
