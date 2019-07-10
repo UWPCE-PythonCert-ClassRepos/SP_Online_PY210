35db71fed9850b34769bc1b610d6ccd845562c6f
Zach Thomson <thomsonzachary@gmail.com>
2019-07-09 21:02:01 -0700

    basic trigram seems to be working

--- students/zach_thomson/lesson04/trigram.py
+++ students/zach_thomson/lesson04/trigram.py
@@ -2,8 +2,10 @@
 
 import random
 import string
+import sys
 
 test_words = 'I wish I may I wish I might'.split()
+longer_test_words = 'I would like to go to the store to get groceries for the week. I would also like to go to the park to play fetch with my dog.'.split()
 
 def build_trigrams(words):
     """
@@ -23,36 +25,43 @@ def build_trigrams(words):
             trigrams[pair_key] = [follower]
     return trigrams
 
-trigram = build_trigrams(test_words)
-# pick a random item from a sequence
-#print(trigram.keys())
-#print(trigram[('wish','I')][1])
-test = random.choice(list(trigram.keys()))
-#print(trigram)
 
 def build_text(word_dict):
-    text = []
     start_key = random.choice(list(word_dict.keys()))
-    text.append(start_key[0])
-    text.append(start_key[1])
-    if len(word_dict[start_key]) > 1:
-        text.append(word_dict[start_key][random.randint(0,1)])
-    else:
-        text.append(str(word_dict[start_key][0]))
-    for i in range(10):
+    text = [start_key[0], start_key[1]]
+    text.append(word_dict[start_key][random.randint(0, len(word_dict[start_key])-1)])
+    for i in range(300):
         key = (text[i+ 1], text[i + 2])
         if key not in word_dict:
             break
-        elif len(word_dict[key]) > 1:
-            text.append(word_dict[key][random.randint(0,1)])
         else:
-            text.append(word_dict[key][0])
+            text.append(word_dict[key][random.randint(0, len(word_dict[key])-1)])
         i += 1
     return " ".join(text)
 
-#build_text(trigram)
-print(build_text(trigram))
+def make_words(text):
+    clean_text = ""
+    with open(text, 'r') as f:
+        for line in f:
+            if '*** START OF THIS PROJECT GUTENBERG EBOOK' in line:
+                for line in f:
+                    if 'End of the Project Gutenberg EBook' in line:
+                        break
+                    else:
+                        clean_text = clean_text + line
+    return clean_text.split()
 
-#if __name__ == "__main__":
-#    trigram = build_trigrams(test_words)
-#    print(trigram)
+if __name__ == "__main__":
+    #get the filename from the command line
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
+
+
+    words = make_words(filename)
+    word_pairs = build_trigrams(words)
+    new_text = build_text(word_pairs)
+
+    print(new_text)
