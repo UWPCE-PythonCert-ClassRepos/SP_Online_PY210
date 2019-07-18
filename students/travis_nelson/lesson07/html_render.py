9e9f5462b93cc7a30e6166208aa27e5a2454dae7
Travis Nelson <travisjamesnelson@gmail.com>
2019-07-18 06:45:25 -0700

    progress...

--- students/travis_nelson/lesson07/html_render.py
+++ students/travis_nelson/lesson07/html_render.py
@@ -8,11 +8,131 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
-    def __init__(self, content=None):
-        pass
+    tag = "html"
+    indent = "   "
+
+    def __init__(self, content=None, **element_attribute):
+        
+        if content is not None:
+            self.contents = [content]
+        else:
+            self.contents = [""]
+        if element_attribute:
+            self.attributes = element_attribute
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
-    def render(self, out_file):
-        out_file.write("just something as a place holder...")
+    def _open_tag(self):
+        try:
+            open_tag = [f"<{self.tag}"]
+            for k, v in self.attributes.items():
+                open_tag.append(f' {k}="{v}"')
+            open_tag.append(">")
+            return "".join(open_tag)
+        except AttributeError:
+            return f"<{self.tag}>"
+
+    def _close_tag(self):
+        return f"</{self.tag}>"
+
+    def render(self, out_file, cur_ind=""):
+        out_file.write(self.indent)
+        out_file.write(self._open_tag())
+        out_file.write("\n")
+        for content in self.contents:
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write("\n")
+        out_file.write(self._close_tag())
+        out_file.write("\n")
+
+
+class Html(Element):
+    tag = "html"
+
+
+class Body(Element):
+    tag = "body"
+
+
+class P(Element):
+    tag = "p"
+    indent = "    "
+
+class Head(Element):
+    tag = "head"
+
+
+class OneLineTag(Element):
+    tag = "title"
+    indent = "    "
+
+    def render(self, out_file, cur_ind=""):
+        out_file.write(f"{self._open_tag()}")
+        out_file.write(self.contents[0])
+        out_file.write(f"{self._close_tag()}")
+
+    def append(self, content):
+        raise NotImplementedError
+
+
+class Title(OneLineTag):
+    tag = "title"
+    indent = "    "
+
+
+class SelfClosingTag(Element):
+    tag = "hr"
+    indent = "    "
+
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            raise TypeError("SelfClosingTag can not contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, out_file, cur_ind=""):
+        out_file.write(self._open_tag()[:-1] + " />\n")
+
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+
+class Br(SelfClosingTag):
+    tag = "br"
+
+    def append(self, *args):
+        raise TypeError("You can not add content to a SelfClosingTag")
+
+
+class A(Element):
+
+    tag = "a"
+
+    def __init__(self, link, content=None, **kwargs):
+        kwargs['href'] = link
+        super().__init__(content, **kwargs)
+
+
+class Ul(Element):
+
+    tag = "ul"
+
+
+class Li(Element):
+
+    tag = "li"
+
+
+class H(OneLineTag):
+    indent = "    "
+
+    def __init__(self, level, content=None, **kwargs):
+        self.tag = f"h{level}"
+        super().__init__(content, **kwargs)
+
+    def append(self, new_content):
+        self.contents.append(new_content)
--- students/travis_nelson/lesson07/run_html_render.py
+++ students/travis_nelson/lesson07/run_html_render.py
@@ -33,23 +33,23 @@ def render_page(page, filename, indent=None):
         outfile.write(f.getvalue())
 
 
-# Step 1
-#########
+# # Step 1
+# #########
 
-page = hr.Element()
+# page = hr.Element()
 
-page.append("Here is a paragraph of text -- there could be more of them, "
-            "but this is enough  to show that we can do some text")
+# page.append("Here is a paragraph of text -- there could be more of them, "
+#             "but this is enough  to show that we can do some text")
 
-page.append("And here is another piece of text -- you should be able to add any number")
+# page.append("And here is another piece of text -- you should be able to add any number")
 
-render_page(page, "test_html_output1.html")
+# render_page(page, "test_html_output1.html")
 
-# The rest of the steps have been commented out.
-#  Uncomment them as you move along with the assignment.
+# # The rest of the steps have been commented out.
+# #  Uncomment them as you move along with the assignment.
 
-# ## Step 2
-# ##########
+# # ## Step 2
+# # ##########
 
 # page = hr.Html()
 
@@ -64,8 +64,8 @@ render_page(page, "test_html_output1.html")
 
 # render_page(page, "test_html_output2.html")
 
-# # Step 3
-# ##########
+# # # Step 3
+# # ##########
 
 # page = hr.Html()
 
@@ -152,80 +152,80 @@ render_page(page, "test_html_output1.html")
 
 # render_page(page, "test_html_output6.html")
 
-# # Step 7
-# #########
+# Step 7
+#########
 
-# page = hr.Html()
+page = hr.Html()
 
-# head = hr.Head()
-# head.append(hr.Title("PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append(hr.Title("PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append( hr.H(2, "PythonClass - Class 6 example") )
+body.append( hr.H(2, "PythonClass - Class 6 example") )
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
-#                  "but this is enough  to show that we can do some text",
-#               style="text-align: center; font-style: oblique;"))
+body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+                 "but this is enough  to show that we can do some text",
+              style="text-align: center; font-style: oblique;"))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# list = hr.Ul(id="TheList", style="line-height:200%")
+list = hr.Ul(id="TheList", style="line-height:200%")
 
-# list.append( hr.Li("The first item in a list") )
-# list.append( hr.Li("This is the second item", style="color: red") )
+list.append( hr.Li("The first item in a list") )
+list.append( hr.Li("This is the second item", style="color: red") )
 
-# item = hr.Li()
-# item.append("And this is a ")
-# item.append( hr.A("http://google.com", "link") )
-# item.append("to google")
+item = hr.Li()
+item.append("And this is a ")
+item.append( hr.A("http://google.com", "link") )
+item.append("to google")
 
-# list.append(item)
+list.append(item)
 
-# body.append(list)
+body.append(list)
 
-# page.append(body)
+page.append(body)
 
-# render_page(page, "test_html_output7.html")
+render_page(page, "test_html_output7.html")
 
-# # Step 8 and 9
-# ##############
+# Step 8 and 9
+##############
 
-# page = hr.Html()
+page = hr.Html()
 
 
-# head = hr.Head()
-# head.append( hr.Meta(charset="UTF-8") )
-# head.append(hr.Title("PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append( hr.Meta(charset="UTF-8") )
+head.append(hr.Title("PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append( hr.H(2, "PythonClass - Example") )
+body.append( hr.H(2, "PythonClass - Example") )
 
-# body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
-#                  "but this is enough  to show that we can do some text",
-#                  style="text-align: center; font-style: oblique;"))
+body.append(hr.P("Here is a paragraph of text -- there could be more of them, "
+                 "but this is enough  to show that we can do some text",
+                 style="text-align: center; font-style: oblique;"))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# list = hr.Ul(id="TheList", style="line-height:200%")
+list = hr.Ul(id="TheList", style="line-height:200%")
 
-# list.append( hr.Li("The first item in a list") )
-# list.append( hr.Li("This is the second item", style="color: red") )
+list.append( hr.Li("The first item in a list") )
+list.append( hr.Li("This is the second item", style="color: red") )
 
-# item = hr.Li()
-# item.append("And this is a ")
-# item.append( hr.A("http://google.com", "link") )
-# item.append("to google")
+item = hr.Li()
+item.append("And this is a ")
+item.append( hr.A("http://google.com", "link") )
+item.append("to google")
 
-# list.append(item)
+list.append(item)
 
-# body.append(list)
+body.append(list)
 
-# page.append(body)
+page.append(body)
 
-# render_page(page, "test_html_output8.html")
+render_page(page, "test_html_output8.html")
