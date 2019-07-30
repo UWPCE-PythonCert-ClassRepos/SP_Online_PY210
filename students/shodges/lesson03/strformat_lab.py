5bdd0bc11910c2d7d3e0c77686bd73374e3df786
Sean Hodges <sthodges@gmail.com>
2019-07-30 05:19:13 -0700

    add task 4

--- students/shodges/lesson03/strformat_lab.py
+++ students/shodges/lesson03/strformat_lab.py
@@ -11,3 +11,7 @@ def formatter(nums):
     l = len(nums)
 
     return ('The {:d} numbers are: ' + ', '.join(['{:d}']*l)).format(l, *nums)
+
+#task 4
+date = (4, 30, 2017, 2, 27)
+print('{3:02d} {4} {2} {0:02d} {1}'.format(*date))
