4fac3c36d85ebfd4a4af2c2d25161bf75ae4da67
Sean Hodges <sthodges@gmail.com>
2019-07-30 03:21:19 -0700

    Add student directory, commit lesson1 exercises

---
+++ students/shodges/lesson01/break_me.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def ne():
+	print(test)
+
+def te():
+	a=1
+	b='2'
+	print (a+b)
+
+def se():
+	print (eval('a+'))
+
+def ae():
+	val='attribute error'
+	print (val.doesnotexist)
