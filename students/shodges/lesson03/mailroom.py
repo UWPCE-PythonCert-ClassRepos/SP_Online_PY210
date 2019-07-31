f8790dc4e5e1434dde15ca04651250bee14e7ff1
Sean Hodges <sthodges@gmail.com>
2019-07-31 08:57:35 -0700

    Initial commit

---
+++ students/shodges/lesson03/mailroom.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+donors = [['William Henry Harrison', 806.25, 423.10],
+          ['James K. Polk', 37.67, 127.65, 1004.29],
+          ['Martin van Buren', 126.47],
+          ['Millard Fillmore', 476.21, 2376.21],
+          ['Chester A. Arthur', 10236.91]]
