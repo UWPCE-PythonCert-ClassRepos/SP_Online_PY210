57faa0a25e7874c854663d07d3133199abdca6db
Sean Hodges <sthodges@gmail.com>
2019-07-31 09:19:13 -0700

    Add base main block

--- students/shodges/lesson03/mailroom.py
+++ students/shodges/lesson03/mailroom.py
@@ -4,3 +4,31 @@ donors = [['William Henry Harrison', 806.25, 423.10],
           ['Martin van Buren', 126.47],
           ['Millard Fillmore', 476.21, 2376.21],
           ['Chester A. Arthur', 10236.91]]
+
+def send_thank_you():
+    print('Placeholder for thank you letter')
+    return
+
+def generate_report():
+    print('Placeholder for report')
+    return
+
+if __name__ == '__main__':
+    while True:
+        print('')
+        print('Mailroom -- Main Menu')
+        print('')
+        print('Options:')
+        print('  1 Send a Thank You')
+        print('  2 Generate a Report')
+        print('  3 Quit')
+        print('')
+        option = input('Please select an option (1, 2, 3): ')
+        if option == '1':
+            send_thank_you()
+        elif option == '2':
+            generate_report()
+        elif option == '3':
+            quit()
+        else:
+            continue
