51ac21a8d2dfae23a9c668b91c4d6e2686522090
Sean Hodges <sthodges@gmail.com>
2019-08-02 17:45:23 -0700

    Added base functionality to send_thank_you() and generate_report()

--- students/shodges/lesson03/mailroom.py
+++ students/shodges/lesson03/mailroom.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
-donors = [['William Henry Harrison', 806.25, 423.10],
-          ['James K. Polk', 37.67, 127.65, 1004.29],
-          ['Martin van Buren', 126.47],
-          ['Millard Fillmore', 476.21, 2376.21],
-          ['Chester A. Arthur', 10236.91]]
+donors = {'William Henry Harrison' : [806.25, 423.10],
+          'James K. Polk' : [37.67, 127.65, 1004.29],
+          'Martin van Buren' : [126.47],
+          'Millard Fillmore' : [476.21, 2376.21],
+          'Chester A. Arthur' : [10236.91]}
 
 def send_thank_you():
-    print('Placeholder for thank you letter')
+    while True:
+        donor = input('Please enter a donor name: ')
+        if donor == 'quit':
+            break
+        elif donor == 'list':
+            for item in donors.keys():
+                print(item)
+        else:
+            if not donor in donors.keys(): donors[donor] = []
+            amount = input('Please enter a donation amount: ')
+            try:
+                donors[donor].append(float(amount))
+            except ValueError:
+                break
+            print("""Dear {},
+Thank you for your generous gift of ${:.2f}.  Your donation will be put to excellent use doing thing.
+
+Something something something.
+
+Warmest regards,
+
+Sean Hodges""".format(donor, float(amount)))
+            break
     return
 
 def generate_report():
-    print('Placeholder for report')
+    print('{:24} | {:10} | {:10} | {:12}'.format('Donor Name', 'Total Given', 'Num Gifts', 'Average Gift'))
+    print('-'*68)
+    for donor in donors.keys():
+        total = 0
+        for amount in donors[donor]:
+            total += amount
+        print('{:24}  ${:10.2f}   {:10d}   ${:12.2f}'.format(donor, total, len(donors[donor]), total/len(donors[donor])))
     return
 
 if __name__ == '__main__':
