7fb219978dcb4ba2e5fe832e48d8796be0dbd740
Sean Hodges <sthodges@gmail.com>
2019-08-05 17:09:38 -0700

    Got a bit ahead of myself and used a dictionary, redid with tuples nested in a list :)

--- students/shodges/lesson03/mailroom.py
+++ students/shodges/lesson03/mailroom.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-donors = {'William Henry Harrison' : [806.25, 423.10],
-          'James K. Polk' : [37.67, 127.65, 1004.29],
-          'Martin van Buren' : [126.47],
-          'Millard Fillmore' : [476.21, 2376.21],
-          'Chester A. Arthur' : [10236.91]}
+donors = [('William Henry Harrison', [806.25, 423.10]),
+          ('James K. Polk', [37.67, 127.65, 1004.29]),
+          ('Martin van Buren', [126.47]),
+          ('Millard Fillmore', [476.21, 2376.21]),
+          ('Chester A. Arthur', [10236.91])]
 
 def send_thank_you():
     while True:
@@ -11,17 +11,23 @@ def send_thank_you():
         if donor == 'quit':
             break
         elif donor == 'list':
-            for item in donors.keys():
-                print(item)
+            for item in donors:
+                print(item[0])
         else:
-            if not donor in donors.keys(): donors[donor] = []
+            for index, item in enumerate(donors):
+                if item[0] == donor:
+                    donorindex = index
+                    break
+            else:
+                donors.append((donor, []))
+                donorindex = len(donors) - 1
             amount = input('Please enter a donation amount: ')
             try:
-                donors[donor].append(float(amount))
+                donors[donorindex][1].append(float(amount))
             except ValueError:
                 break
             print("""Dear {},
-Thank you for your generous gift of ${:.2f}.  Your donation will be put to excellent use doing thing.
+Thank you for your generous gift of ${:.2f}.  When it comes to ensuring marmots have loving homes, every dollar goes a long way.
 
 Something something something.
 
@@ -34,11 +40,11 @@ Sean Hodges""".format(donor, float(amount)))
 def generate_report():
     print('{:24} | {:10} | {:10} | {:12}'.format('Donor Name', 'Total Given', 'Num Gifts', 'Average Gift'))
     print('-'*68)
-    for donor in donors.keys():
+    for item in donors:
         total = 0
-        for amount in donors[donor]:
+        for amount in item[1]:
             total += amount
-        print('{:24}  ${:10.2f}   {:10d}   ${:12.2f}'.format(donor, total, len(donors[donor]), total/len(donors[donor])))
+        print('{:24}  ${:10.2f}   {:10d}   ${:12.2f}'.format(item[0], total, len(item[1]), total/len(item[1])))
     return
 
 if __name__ == '__main__':
