1ff137cd730651f5db84d1d24b0c657a162b6e7e
Sean Hodges <sthodges@gmail.com>
2019-08-06 16:50:59 -0700

    Initial cut at sum_series

--- students/shodges/lesson02/series.py
+++ students/shodges/lesson02/series.py
@@ -29,3 +29,17 @@ def lucas(n):
             li2 = li1
             li1 = li
         return li
+
+def sum_series(n, a = 0, b = 1):
+    """Returns the nth element (note: n is 0-based) of the fibonacci series.
+
+    Specifying a and b will override the default start points of the series.
+    """
+    if n == 0: return a
+    elif n == 1: return b
+    else:
+        for i in range(2,n+1):
+            sum = a + b
+            a = b
+            b = sum
+        return sum
