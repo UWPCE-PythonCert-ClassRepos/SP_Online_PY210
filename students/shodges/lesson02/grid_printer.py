8a17b280601a7e85d5455560a1f9ce0e7d6d53bc
Sean Hodges <sthodges@gmail.com>
2019-08-06 14:11:09 -0700

    add part3

--- students/shodges/lesson02/grid_printer.py
+++ students/shodges/lesson02/grid_printer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import math
 
 #part 1
@@ -23,5 +22,19 @@ def print_grid2(n):
     for h in range(0,2):
         print(horizontal_border)
         for v in range(0,math.floor(n/2)):
+            print(vertical_border)
+    print(horizontal_border)
+
+#part 3
+# ed. note -- there was a change between part 2 and 3 in how the grid was calculated. In parts 1 and 2, the "columns" were measured by spaces and minuses
+# in part 3, columns only measures minuses.  I'm not sure if that's the spirit of this but I stuck with the change in counting methodology as the examples
+# given reflected that change.  Sorry if wrong here!
+def print_grid3(n, s):
+    horizontal_border = (('+ ' + ('- ' * s)) * n) + '+'
+    vertical_border = (('|' + (' ' * ((2*s)+1))) * n) + '|'
+
+    for h in range(0,n):
+        print(horizontal_border)
+        for v in range(0,s):
             print(vertical_border)
     print(horizontal_border)
