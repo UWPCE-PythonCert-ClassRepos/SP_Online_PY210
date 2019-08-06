e0ce9bb98e086e797fa3cfbed94f729284da82d7
Sean Hodges <sthodges@gmail.com>
2019-08-06 16:52:20 -0700

    Reworking fibonacci() and lucass() to take advantage of sum_series()

--- students/shodges/lesson02/series.py
+++ students/shodges/lesson02/series.py
@@ -2,33 +2,11 @@
 
 def fibonacci(n):
     """Returns the nth element of the fibonacci sequence.  Note that n is 0-based."""
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        fi2 = 0 # fibonacci(i-2)
-        fi1 = 1 # fibonacci(i-1)
-        for i in range(2,n+1):
-            fi = fi2 + fi1
-            fi2 = fi1
-            fi1 = fi
-        return fi
+    return sum_series(n)
 
 def lucas(n):
     """Returns the nth element (0-based) of the lucas numbers."""
-    if n == 0:
-        return 2
-    elif n == 1:
-        return 1
-    else:
-        li2 = 2 # lucas(i-2)
-        li1 = 1 # lucas(i-1)
-        for i in range(2,n+1):
-            li = li2 + li1
-            li2 = li1
-            li1 = li
-        return li
+    return sum_series(n, 2, 1)
 
 def sum_series(n, a = 0, b = 1):
     """Returns the nth element (note: n is 0-based) of the fibonacci series.
