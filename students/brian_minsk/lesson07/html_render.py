044a90d13083e761ad97f97b8fce2a7485b2b0ef
Brian Minsk <brian.minsk@gmail.com>
2019-08-07 11:45:01 -0700

    passed HTML Renderer part 2 tests

--- students/brian_minsk/lesson07/html_render.py
+++ students/brian_minsk/lesson07/html_render.py
@@ -11,23 +11,46 @@ class Element(object):
     tag_name = "html"
 
     def __init__(self, content=None):
+        self.content = []
         if content:
-            self.content = [content]
-        else:
-            self.content = []
+            self.append(content)        
 
     def append(self, new_content):
-        self.content.append(new_content)
+        if hasattr(new_content, 'render'):
+            self.content.append(new_content)
+        else:
+            self.content.append(TextWrapper(str(new_content)))
 
     def render(self, out_file):
-        print(self.content)
         opening_tag = self.create_opening_tag(self.tag_name)
         closing_tag = self.create_closing_tag(self.tag_name)
-        body = "\n".join([opening_tag, "\n".join(self.content), closing_tag])
-        out_file.write(body)
+        out_file.write(opening_tag)
+        for item in self.content:
+            item.render(out_file)
+        out_file.write(closing_tag)
 
     def create_opening_tag(self, tag_name):
-        return "".join(["<", tag_name, ">"])
+        return "".join(["<", tag_name, ">"]) + "\n"
 
     def create_closing_tag(self, tag_name):
-        return "".join(["</", tag_name, ">"])
+        return "".join(["</", tag_name, ">"]) + "\n"
+
+class Html(Element):
+    tag_name = "html"
+
+class Body(Element):
+    tag_name = "body"
+
+class P(Element):
+    tag_name = "p"
+
+class TextWrapper():
+    """
+    A simple wrapper that creates a class with a render method
+    for simple text
+    """
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, file_out):
+        file_out.write(self.text + "\n")
