59f01da49bb23f807a498f1de79631aa4676b369
Brian Minsk <brian.minsk@gmail.com>
2019-08-06 17:56:12 -0700

    HTML renderer: passed 1a tests

--- students/brian_minsk/lesson07/html_render.py
+++ students/brian_minsk/lesson07/html_render.py
@@ -8,11 +8,19 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
+    tag_name = "html"
+
+    open_tag = {"start": "<", "end": ">"}
+    close_tag = {"start": "</", "end": ">"}
+
     def __init__(self, content=None):
-        pass
+        self.content = content
 
     def append(self, new_content):
-        pass
+        self.content = " ".join([self.content, (new_content)])
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write("".join([self.open_tag["start"], self.tag_name,
+                                self.open_tag["end"], self.content,
+                                self.close_tag["start"], self.tag_name,
+                                self.close_tag["end"]]))
