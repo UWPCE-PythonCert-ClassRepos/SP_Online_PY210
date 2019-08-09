069adbb95962332ac3dc425fd643f0bceb63440d
Zach Thomson <thomsonzachary@gmail.com>
2019-08-09 14:07:16 -0700

    completed through step 6

--- students/zach_thomson/lesson08/circle_exercise.py
+++ students/zach_thomson/lesson08/circle_exercise.py
@@ -18,3 +18,14 @@ class Circle():
     @property
     def area(self):
         return (self.radius ** 2) * pi
+
+    @classmethod
+    def from_diameter(cls, value):
+        diameter = value / 2
+        return cls(diameter)
+
+    def __str__(self):
+        return 'Circle with radius: {:.6f}'.format(float(self.radius))
+
+    def __repr__(self):
+        return "Circle({})".format(self.radius)
--- students/zach_thomson/lesson08/test_circle_exercise.py
+++ students/zach_thomson/lesson08/test_circle_exercise.py
@@ -21,3 +21,15 @@ def test_area():
     assert c.area == 12.566370614359172
     with pytest.raises(AttributeError):
         c.area = 42
+
+def test_circle_from_diameter():
+    c = Circle.from_diameter(8)
+    assert c.diameter == 8
+    assert c.radius == 4
+
+def test_circle(capsys):
+    c = Circle(4)
+    print(str(c))
+    captured = capsys.readouterr()
+    assert captured.out == 'Circle with radius: 4.000000\n'
+    assert repr(c) == "Circle(4)"
