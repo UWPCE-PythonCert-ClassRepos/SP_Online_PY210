a9926522180a0e053219370f565e5c1711085333
Sean Hodges <sthodges@gmail.com>
2019-08-09 15:32:05 -0700

    Build trigrams dict

--- students/shodges/lesson04/trigrams.py
+++ students/shodges/lesson04/trigrams.py
@@ -5,3 +5,9 @@ from pathlib import Path
 textfile = Path("./sherlock_small.txt")
 
 with textfile.open() as fileio: words = fileio.read().split()
+
+trigrams = {}
+for k, w in enumerate(words):
+    if k + 2 < len(words):
+        if not (w, words[k+1]) in trigrams.keys(): trigrams[(w, words[k+1])] = []
+        trigrams[(w, words[k+1])].append(words[k+2])
