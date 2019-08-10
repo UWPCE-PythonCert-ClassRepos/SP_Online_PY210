f82506e0378f35239dba485d808e5e5deb7dd99c
Zach Thomson <thomsonzachary@gmail.com>
2019-08-09 20:35:00 -0700

    circle exercise finished

--- students/zach_thomson/lesson08/circle_exercise.py
+++ students/zach_thomson/lesson08/circle_exercise.py
@@ -10,6 +10,7 @@ class Circle():
     @property
     def diameter(self):
         return self.radius * 2
+
     @diameter.setter
     def diameter(self, value):
         self._diameter = value
@@ -29,3 +30,53 @@ class Circle():
 
     def __repr__(self):
         return "Circle({})".format(self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __iadd__(self, other):
+        if type(other) == int or type(other) == float:
+            return Circle(self.radius + other)
+        else:
+            return Circle(self.radius + other.radius)
+
+    def __imul__(self, other):
+        return Circle(self.radius * other)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        return self.radius / other
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def sort(self, other):
+        return self.radius < other.radius
+
+
+class Sphere(Circle):
+
+    @property
+    def area(self):
+        return 4 * pi * (self.radius ** 2)
+
+    @property
+    def volume(self):
+        return 4/3 * pi * (self.radius ** 3)
+
+    def __str__(self):
+        return 'Sphere with radius: {:.6f}'.format(float(self.radius))
+
+    def __repr__(self):
+        return "Sphere({})".format(self.radius)
--- students/zach_thomson/lesson08/test_circle_exercise.py
+++ students/zach_thomson/lesson08/test_circle_exercise.py
@@ -33,3 +33,57 @@ def test_circle(capsys):
     captured = capsys.readouterr()
     assert captured.out == 'Circle with radius: 4.000000\n'
     assert repr(c) == "Circle(4)"
+
+def test_numerics():
+    c1 = Circle(2)
+    c2 = Circle(4)
+    d = c1 + c2
+    e = c2 * 3
+    f = 3 * c2
+    assert repr(d) == "Circle(6)"
+    assert repr(e) == "Circle(12)"
+    assert repr(f) == "Circle(12)"
+    a_circle = Circle(3)
+    assert a_circle * 3 == 3 * a_circle
+    assert c2 / 2 == 2
+    c3 = Circle(2)
+    c3 += 2
+    c1 += c1
+    assert c3 == Circle(4)
+    assert c1 == Circle(4)
+    c2 *= 2
+    assert c2 == Circle(8)
+
+def test_compare_circles():
+    c1 = Circle(2)
+    c2 = Circle(4)
+    assert (c1 > c2) == False
+    assert (c1 < c2) == True
+    assert (c1 == c2) == False
+    c3 = Circle(4)
+    assert c2 == c3
+
+def test_sort():
+    circle_list = [Circle(7), Circle(5), Circle(6), Circle(3), Circle(4), Circle(1)]
+    circle_list.sort()
+    assert circle_list == [Circle(1), Circle(3), Circle(4), Circle(5), Circle(6), Circle(7)]
+
+def test_sphere():
+    s = Sphere(2)
+    assert s.radius == 2
+    assert s.diameter == 4
+    assert s.area == 50.26548245743669
+    assert s.volume == 33.510321638291124
+    s8 = Sphere.from_diameter(8)
+    assert s8.diameter == 8
+    assert s8.radius == 4
+    with pytest.raises(TypeError):
+        s2 = Sphere()
+
+
+def test_sphere_output(capsys):
+    s = Sphere(2)
+    print(str(s))
+    captured = capsys.readouterr()
+    assert captured.out == 'Sphere with radius: 2.000000\n'
+    assert repr(s) == "Sphere(2)"
