52a7062f34136f35bde45ecdfdfd34408a695bec
mattcasari <mjcasari@gmail.com>
2019-08-12 11:46:27 -0700

    Reformatting mailroom with black

--- students/mattcasari/lesson04/mailroom.py
+++ students/mattcasari/lesson04/mailroom.py
@@ -15,59 +15,56 @@ Description:
 
 """
 
-# data list structure is [Name, [Amount1, Amount 2,...], Sum, Len, Average]
 DONORS = {}
 
-def prompt_user():
-
-    switch_func_dict = {1: thank_you_email,
-                        2: generate_report,
-                        3: create_file,
-                        4: quit_program}
-
-    """ Prompts the user for menu option """
-    PROMPT_TEXT = ("\nSelect an option:\n"
+PROMPT_TEXT = (
+    "\nSelect an option:\n"
     "1. Send a Thank You to a single donor.\n"
     "2. Create a Report.\n"
     "3. Send letters to all donors.\n"
     "4. Quit\n"
-    "> ")
+    "> "
+)
 
 
+def prompt_user():
+
+    switch_func_dict = {
+        1: thank_you_email,
+        2: generate_report,
+        3: create_file,
+        4: quit_program,
+    }
+
+    """ Prompts the user for menu option """
 
     result = input(PROMPT_TEXT)
     result = int(result)
     switch_func_dict.get(result)()
-    # if result.isnumeric():
-    #     result = int(result)
-        # if 0 < result < len(PROMPT_TEXT)-2:
-        #     print(result)
-        #     switch_func_dict.get(result)()
-    # if result.isnumeric():
-    #     result = int(result)
-    #     if 0 < result < 4:
-    #         return result
-    # else:
-    #     return False
+
+
 def initialize_donors():
-    DONORS['Neil Armstrong'] =  [15000.00, 15000.00]
-    DONORS['Buzz Aldrin'] = [23021.10, 25020.30, 28999.29]
-    DONORS['Sally Ride'] = [42917.42, 38281.28]
-    DONORS['Al Shepard'] = [2387.00,  2321.42, 3700.00]
-    DONORS['Alan Bean'] = [28477.13, 727.1]
-    DONORS['Chris Hadfield'] = [17325.42, 13823.83, 0.99]
+    DONORS["Neil Armstrong"] = [15000.00, 15000.00]
+    DONORS["Buzz Aldrin"] = [23021.10, 25020.30, 28999.29]
+    DONORS["Sally Ride"] = [42917.42, 38281.28]
+    DONORS["Al Shepard"] = [2387.00, 2321.42, 3700.00]
+    DONORS["Alan Bean"] = [28477.13, 727.1]
+    DONORS["Chris Hadfield"] = [17325.42, 13823.83, 0.99]
+
 
 def calculate_stats(donor):
     """ Calculates the sum, average and number of donations for a donor """
     donor_sum = sum(donor[1])
     donor_num = len(donor[1])
-    donor_average = donor_sum/donor_num
+    donor_average = donor_sum / donor_num
 
     return (donor_sum, donor_num, donor_average)
+
 
 def sort_donors_by_total(donors):
     """ Function used to sort donors by total contributions """
     return donors[2]
+
 
 def quit_program():
     """ 
@@ -76,8 +73,10 @@ def quit_program():
     print("Exiting Program")
     quit()
 
+
 def create_file():
     pass
+
 
 def generate_report():
     values = DONORS
@@ -86,14 +85,14 @@ def generate_report():
     column_donor_length = 0
 
     for idx, value in enumerate(values[:]):
-        column_donor_length = max(len(value[0]),column_donor_length)+5
+        column_donor_length = max(len(value[0]), column_donor_length) + 5
         [values[idx][2], values[idx][3], values[idx][4]] = calculate_stats(value)
 
     f_str = " {" + f":<{column_donor_length}" + "} | {} | {} | {}"
     title_str = f_str.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
     print(title_str)
-    print("-"*len(title_str))
-    
+    print("-" * len(title_str))
+
     values = sorted(values, key=sort_donors_by_total, reverse=True)
 
     for value in values:
@@ -101,25 +100,29 @@ def generate_report():
         v_str = f_str.format(value[0], value[2], value[3], value[4])
         print(v_str)
 
+
 def print_donor_list(values):
     """ Prints the list of donors passed to function"""
     print("\nList of donors:".upper())
     for value in values:
         print(value[0])
 
+
 def thank_you_email(name, amount):
     """ Format the email and print to terminal """
-    txt = f"""\nDear {name},\n""" \
-          f"""Thank you for your recent donation of ${amount:.2f}. """ \
-          f"""Your donation will help us purchase a taxidermied seagull.\n""" \
-          f"""Please consider donating again at your earliest convenience.\n\n""" \
-          f"""Sincerely,\n""" \
-          f"""The Human Fund\n"""
+    txt = (
+        f"""\nDear {name},\n"""
+        f"""Thank you for your recent donation of ${amount:.2f}. """
+        f"""Your donation will help us purchase a taxidermied seagull.\n"""
+        f"""Please consider donating again at your earliest convenience.\n\n"""
+        f"""Sincerely,\n"""
+        f"""The Human Fund\n"""
+    )
 
     print(txt)
 
-def add_donor():
 
+def add_donor():
 
     # trigrams = {}
     # num_words = len(words) - 1
@@ -141,18 +144,17 @@ def add_donor():
             if donor == "list":
                 print_donor_list(DONORS)
                 continue
-            else:   
-                DONORS.append([donor,[]])
-                
+            else:
+                DONORS.append([donor, []])
+
                 idx += 1
                 valid_donor = True
                 break
-            
-    
+
     amount = input("Enter donation amount ($): ")
     amount = float(amount)
-            # break
-    
+    # break
+
     # Add amount to data
     values[idx][1].append(amount)
     values[idx].append([])
@@ -162,6 +164,7 @@ def add_donor():
     thank_you_email(values[idx][0], amount)
 
     return values
+
 
 def main():
     initialize_donors()
@@ -178,7 +181,7 @@ def main():
         #     break
         # else:
         #     print("\nPlease select a valid option")
-    
+
 
 if __name__ == "__main__":
     main()
