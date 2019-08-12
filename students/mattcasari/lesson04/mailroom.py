6aa4d5f73ed0628ced165cad14df558bc9422760
mattcasari <mjcasari@gmail.com>
2019-08-12 14:12:39 -0700

    Cleaned up and working version of mailroom

--- students/mattcasari/lesson04/mailroom.py
+++ students/mattcasari/lesson04/mailroom.py
@@ -14,6 +14,7 @@ Description:
     Also, add file writing.
 
 """
+import pathlib
 
 DONORS = {}
 
@@ -28,15 +29,13 @@ PROMPT_TEXT = (
 
 
 def prompt_user():
-
+    """ Prompts the user for menu option """
     switch_func_dict = {
         1: add_donor,
         2: generate_report,
-        3: create_file,
+        3: create_files,
         4: quit_program,
     }
-
-    """ Prompts the user for menu option """
 
     result = input(PROMPT_TEXT)
     result = int(result)
@@ -44,6 +43,9 @@ def prompt_user():
 
 
 def initialize_donors():
+    """
+    Populate the donor list with the initial donors
+    """
     DONORS["Neil Armstrong"] = [15000.00, 15000.00]
     DONORS["Buzz Aldrin"] = [23021.10, 25020.30, 28999.29]
     DONORS["Sally Ride"] = [42917.42, 38281.28]
@@ -53,8 +55,17 @@ def initialize_donors():
 
 
 def calculate_stats(donations):
-    # print(donor)
-    """ Calculates the sum, average and number of donations for a donor """
+    """
+    Calculates the sum, average and number of donations for a donor 
+
+    Args:
+        donations: list of all donations
+
+    Returns:
+        donor_sum: Sum of all donations
+        donor_num: Number of donations
+        donor_average: Average of all donations    
+    """
     donor_sum = sum(donations)
     donor_num = len(donations)
     donor_average = donor_sum / donor_num
@@ -74,30 +85,58 @@ def quit_program():
     quit()
 
 
-def create_file():
-    pass
+def create_files():
+    """
+    Create file(s) in user specified directory for each donor.
+    """
+    directory = input("Enter save directory name: ")
+    path = pathlib.Path("./")
+    path = path / directory
+    if not path.exists():
+        path.mkdir()
+
+    for donor in DONORS:
+        filename = donor.replace(" ", "_") + ".txt"
+        filename = path / filename
+
+        with open(filename, "w+") as temp:
+            name = donor
+            donation_last = DONORS[donor][-1]
+            donation_sum = sum(DONORS[donor])
+            donation_number = len(DONORS[donor])
+            EMAIL_TEMPLATE = (
+                f"Dear {name},\n\n"
+                f"Thank you for your last donation of ${donation_last:.2f}.\n"
+                f"You have contributed a total of ${donation_sum:.2f} over {donation_number} donation(s).\n"
+                f"Your generosity is appreciated.\n"
+                f"\nSincerely,\n"
+                f"-The Team\n\n"
+            )
+
+            temp.write(EMAIL_TEMPLATE)
+            temp.close()
 
 
 def generate_report():
-    values = DONORS
     """ Generates a formatted report of donor names, total donation, # of donations and average donation """
+    values = DONORS
     print("\n")
     column_donor_length = 0
     for value in values:
-        column_donor_length = max(len(values),column_donor_length) + 5
+        column_donor_length = max(len(values), column_donor_length) + 5
 
     f_str = " {" + f":<{column_donor_length}" + "} | {} | {} | {}"
     title_str = f_str.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
     print(title_str)
     print("-" * len(title_str))
- 
+
     values = sorted(DONORS, key=sort_donors_by_total, reverse=True)
 
     for value in values:
         f_str = " {" + f":<{column_donor_length}" + "}  ${:11.2f}   {:9}  ${:12.2f}"
         (d_sum, d_num, d_ave) = calculate_stats(DONORS[value])
         v_str = f_str.format(value, d_sum, d_num, d_ave)
-        
+
         print(v_str)
 
 
@@ -132,7 +171,6 @@ def thank_you_email(name, amount):
 
 
 def add_donor():
-
     """ Adds new donor or new donation to existing donor """
     valid_donor = False
     while not valid_donor:
@@ -164,11 +202,11 @@ def add_donor():
 
 
 def main():
-    initialize_donors()
     """ Main Run Loop """
+    initialize_donors()
+
     while True:
         prompt_user()
-
 
 
 if __name__ == "__main__":
