a8e25d972518f106600342aad1311bfba3aaf9ab
Sean Hodges <sthodges@gmail.com>
2019-08-13 07:46:49 -0700

    Clean out headers, footers and T's & C's

--- students/shodges/lesson04/trigrams.py
+++ students/shodges/lesson04/trigrams.py
@@ -13,6 +13,12 @@ def build_trigrams(words):
             trigrams[(w, words[k+1])].append(words[k+2])
     return trigrams
 
+def gutenberg_clean(guten_text):
+    guten_text = guten_text.split('*** START OF THIS PROJECT GUTENBERG EBOOK') # Get rid of the first part of the header
+    guten_text = guten_text[1][(guten_text[1].find('***')+3):] # Prior to the next '***' there may be arbitrary text
+    guten_text = guten_text.split('*** END OF THIS PROJECT GUTENBERG EBOOK') # Cut out the footer and T's & C's
+    return guten_text[0]
+
 if __name__ == '__main__':
     if not len(argv) == 2:
         print("Syntax: {} [filename]".format(argv[0]))
@@ -20,7 +26,7 @@ if __name__ == '__main__':
     textfile = Path(argv[1])
     try:
         with textfile.open() as fileio:
-            words = fileio.read().split()
+            words = gutenberg_clean(fileio.read()).split()
     except FileNotFoundError:
         print("Unable to read {}".format(argv[1]))
         quit()
