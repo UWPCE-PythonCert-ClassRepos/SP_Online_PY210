78ecfd7d92f2e64a5a074de4691ff67e2880742f
Sean Hodges <sthodges@gmail.com>
2019-08-13 07:29:15 -0700

    General cleanup, catch KeyError in main body loop

--- students/shodges/lesson04/trigrams.py
+++ students/shodges/lesson04/trigrams.py
@@ -7,18 +7,24 @@ def build_trigrams(words):
     trigrams = {}
     for k, w in enumerate(words):
         if k + 2 < len(words):
-            if not (w, words[k+1]) in trigrams.keys(): trigrams[(w, words[k+1])] = []
+            if not (w, words[k+1]) in trigrams.keys():
+                trigrams[(w, words[k+1])] = []
             trigrams[(w, words[k+1])].append(words[k+2])
     return trigrams
 
 if __name__ == '__main__':
     textfile = Path("./sherlock.txt")
-    with textfile.open() as fileio: words = fileio.read().split()
+    with textfile.open() as fileio:
+        words = fileio.read().split()
     trigrams = build_trigrams(words)
 
     literature = "It is"
 
     for i in range(0,210):
-        next = trigrams[tuple(literature.split()[-2:])]
-        literature = ' '.join((literature, random.choice(next)))
+        try:
+            next = trigrams[tuple(literature.split()[-2:])]
+            literature = ' '.join((literature, random.choice(next)))
+        except KeyError:
+            literature += '.'
+            break
     print(literature)
