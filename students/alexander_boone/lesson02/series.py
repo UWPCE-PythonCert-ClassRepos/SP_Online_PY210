3ca13d1571692d4080578bd7ab756da39c38d1ff
Alexander Boone <alboone@uw.edu>
2019-08-16 09:22:18 -0700

    added sum_series

--- students/alexander_boone/lesson02/series.py
+++ students/alexander_boone/lesson02/series.py
@@ -15,3 +15,12 @@ def lucas(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+
+def sum_series(n, first_value = 0, second_value = 1):
+    """Return nth integer in the Lucas series starting from index 0 and parameters for the first and second series values."""
+    if n == 0:
+        return first_value
+    elif n == 1:
+        return second_value
+    else:
+        return sum_series(n-1) + sum_series(n-2)
