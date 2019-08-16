acd04fd4f43a95259f83f7aad8dd88ed446e6352
Alexander Boone <alboone@uw.edu>
2019-08-16 08:36:22 -0700

    Updated with further comments

--- students/alexander_boone/lesson02/fizz_buzz.py
+++ students/alexander_boone/lesson02/fizz_buzz.py
@@ -1,8 +1,11 @@
 # Fizz Buzz Program
 for i in range(100):
+    
+    # assigns remainder of i divided by 3 and 5 for fizz and buzz, respectively
     fizz = (i+1) % 3
     buzz = (i+1) % 5
 
+    # checks if current i is divisible by 3 and 5
     if fizz == 0 and buzz == 0:
         print("FizzBuzz")
     elif fizz == 0:
