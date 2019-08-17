94ba4738b5b0522a07b0bda785934ceeb8eef298
Alexander Boone <alboone@uw.edu>
2019-08-17 09:30:58 -0700

    added remove_end_fours function

--- students/alexander_boone/lesson03/exercises/slicing.py
+++ students/alexander_boone/lesson03/exercises/slicing.py
@@ -8,13 +8,21 @@ def remove_every_other(seq):
     """Remove every other item in a sequence."""
     return seq[::2]
 
+# 
+def remove_end_fours(seq):
+    """Remove the first and last four items of the sequence and return every other item of remaining sequence"""
+    return seq[4:-4:2]
+
 # test all functions
 if __name__ == "__main__":
     
-    # exchange_first_last
+    # test variables
     a_string = "Monty Python"
     a_list = ["M", "o", "n", "t", "y"]
+    a_long_string = "One more step closer to writing LeetCode"
+    a_long_list = ["all", "i", "do", "is", "code", "code", "code", "no", "matter", "what", "got", "coding", "on", "my", "mind", "I", "can", "never", "get", "enough"]
     
+    # exchange_first_last
     assert exchange_first_last(a_string) == "nonty PythoM"
     assert exchange_first_last(a_list) == ["y", "o", "n", "t", "M"]
     print('exchange_first_last: Success!')
@@ -23,3 +31,11 @@ if __name__ == "__main__":
     assert remove_every_other(a_string) == "MnyPto"
     assert remove_every_other(a_list) == ["M", "n", "y"]
     print('remove_every_other: Success!')
+
+    # remove_end_fours
+    assert remove_end_fours(a_long_string) == "mr tpcoe owiigLe"
+    assert remove_end_fours(a_long_list) == ["code",  "code", "matter", "got", "on", "mind"]
+    print('remove_end_fours: Success!')
+
+    
+
