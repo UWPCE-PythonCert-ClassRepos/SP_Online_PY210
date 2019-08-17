a2aa4ed13d2e2b2e85dc0a852878f0fa9e73252f
Alexander Boone <alboone@uw.edu>
2019-08-17 09:19:22 -0700

    added remove_every_other function

--- students/alexander_boone/lesson03/exercises/slicing.py
+++ students/alexander_boone/lesson03/exercises/slicing.py
@@ -1,13 +1,25 @@
+# define exchange_first_last
 def exchange_first_last(seq):
     """Exchange the first and last items of a sequence."""
     return seq[-1:] + seq[1:-1] + seq[:1]
 
-a_string = "Monty Python"
-a_list = ["M", "o", "n", "t", "y"]
+# define remove_every_other
+def remove_every_other(seq):
+    """Remove every other item in a sequence."""
+    return seq[::2]
 
-
+# test all functions
 if __name__ == "__main__":
+    
+    # exchange_first_last
+    a_string = "Monty Python"
+    a_list = ["M", "o", "n", "t", "y"]
+    
     assert exchange_first_last(a_string) == "nonty PythoM"
     assert exchange_first_last(a_list) == ["y", "o", "n", "t", "M"]
-    
-    print('No assertion failures. Success!')
+    print('exchange_first_last: Success!')
+
+    # remove_every_other
+    assert remove_every_other(a_string) == "MnyPto"
+    assert remove_every_other(a_list) == ["M", "n", "y"]
+    print('remove_every_other: Success!')
