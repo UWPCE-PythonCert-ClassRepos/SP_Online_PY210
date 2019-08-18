6661b175b24616bd73c9b55332db5289f0de6992
Alexander Boone <alboone@uw.edu>
2019-08-17 18:20:42 -0700

    completed task 3 of strformat_lab

--- students/alexander_boone/lesson03/exercises/strformat_lab.py
+++ students/alexander_boone/lesson03/exercises/strformat_lab.py
@@ -14,3 +14,24 @@ t = (2, 123.4567, 10000, 12345.67)
 s2 = "file_{:03d} :   {:.2f}, {:.2E}, {:.2E}".format(t[0], t[1], t[2], t[3])
 print(s2)
 
+# --------------------------------------------------- TASK 3 -----------------------------------------------------
+
+def formatter(tup):
+    """Return string listing out comma-separated values contained within input tuple."""
+    # get length of tuple
+    l = len(tup)
+
+    # create tuple of format specifiers and join into string
+    f_tuple = tuple(['{:d}'] * l)
+    f_string = ', '.join(f_tuple)
+
+    # create formatted strings with correct values
+    f_string2 = "The {:d} numbers are: ".format(l)
+    f_string3 = f_string.format(*tup)
+    
+
+    # concatenate strings and return final string
+    f_string_final = f_string2 + f_string3
+    return f_string_final
+
+# --------------------------------------------------- TASK 4 -----------------------------------------------------
