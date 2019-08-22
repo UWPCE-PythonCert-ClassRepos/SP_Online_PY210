a1791a5e4c37588fa64513e339bd4b45481fa5df
Anthony McKeever <snip3rm00n@gmail.com>
2019-08-22 15:29:33 -0700

    Remove enddate from mailroom (for now)

--- students/anthony_mckeever/lesson6/assignment_1/mailroom.py
+++ students/anthony_mckeever/lesson6/assignment_1/mailroom.py
@@ -4,7 +4,7 @@
 Programming In Python - Lesson 6 Assignment 1: Mailroom Part 4
 Code Poet: Anthony McKeever
 Start Date: 08/22/2019
-End Date: 08/15/2019
+End Date: 
 
 Copied from Lesson 5 Assignment 1 (..\\..\\lesson5\\assigment_1)
 """
