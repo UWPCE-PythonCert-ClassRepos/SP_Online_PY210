4b7f5666fa557694c6ead501f99e8478a81d8a54
Zach Thomson <thomsonzachary@gmail.com>
2019-08-23 06:56:19 -0700

    added some user input functionality

--- students/zach_thomson/lesson09/cli_main.py
+++ students/zach_thomson/lesson09/cli_main.py
@@ -30,7 +30,8 @@ def initial_input():
 
 
 def add_donor_prompt(name):
-    add_prompt = input('Do you want to add {} to the donor list? (Yes/No)'.format(name))
+    '''lets user chose if they want to add a donor as entered into the database'''
+    add_prompt = input('Do you want to add a new entry from {} to the donor list? (Yes/No)'.format(name))
     if add_prompt.lower() == 'yes':
         return True
     elif add_prompt.lower() == 'no':
@@ -42,41 +43,65 @@ def add_donor_prompt(name):
 def donation_prompt():
     '''Prompt user to enter a donation amount'''
     donation = input('Please enter a donation amount: ')
+    return donation
+
+
+def int_donation_text():
+    '''Print statement for when user inputs a non-integer'''
+    print('Please enter an integer for the donation amount.')
+
+
+def neg_donation():
+    '''Print statement for when user inputs donation less than zero'''
+    print('Donation should be greater than zero!')
+
+
+def thank_you():
+    '''Main thank you function'''
+    thank_you_logic(initial_input())
+
+
+def donation_logic(name, donation):
+    '''Determines if donation is valid and then creates a new donor'''
     try:
         donation = float(donation)
     except ValueError:
-        print('Please enter an integer for the donation amount.')
+        int_donation_text()
     else:
         try:
             assert donation > 0
         except AssertionError:
-            print('Donation should be greater than zero!')
+            neg_donation()
         else:
-            return donation
-
-
-def thank_you():
-    thank_you_logic(initial_input())
+            donor_db.new_donation(name, donation)
 
 
 def donor_list():
+    '''Simple print function for list of donors'''
     print(donor_db.donors.keys())
 
 
+def donor_not_added():
+    '''Simple print function for not adding a donor'''
+    print('Donor not added!')
+
+
 def thank_you_logic(name):
+    '''determines if donor is in database and either appends donation amount
+    or creates a new donor in the database'''
     if name.lower() == 'list':
         donor_list()
     else:
         if name not in donor_db.donors.keys():
             add = add_donor_prompt(name)
             if add == True:
-                donation_amt = donation_prompt()
-                donor_db.new_donation(name, donation_amt)
+                donation_logic(name, donation_prompt())
             else:
-                print('Donor not added!')
+                donor_not_added()
         else:
-            donation_amt = donation_prompt()
-            donor_db.new_donation(name, donation_amt)
+            donation_logic(name, donation_prompt())
+
+
 
 
 #Create a report tasks
