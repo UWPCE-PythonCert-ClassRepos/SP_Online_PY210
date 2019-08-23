93fea7c58f2a8f321f8d512c3baa538dc87a0355
Sean Hodges <sthodges@gmail.com>
2019-08-23 12:36:49 -0700

    Add contents list to init and append

--- students/shodges/lesson07/html_render.py
+++ students/shodges/lesson07/html_render.py
@@ -8,12 +8,12 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
     tag = 'html'
-    
+
     def __init__(self, content=None):
-        pass
+        self.contents = [content]
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
         out_file.write("just something as a place holder...")
