2a1ed14627a5459071c49348107bca2ebedec3d6
Sean Hodges <sthodges@gmail.com>
2019-08-26 16:40:19 -0700

    Add indent class attribute

--- students/shodges/lesson07/html_render.py
+++ students/shodges/lesson07/html_render.py
@@ -8,6 +8,7 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
     tag = 'html'
+    indent = 0
 
     def __init__(self, content=None, **kwargs):
         self.contents = [content]
