8a046d33d373e123a574115dcbe022d08859d73d
Sean Hodges <sthodges@gmail.com>
2019-08-27 15:18:58 -0700

    Second newline per PEP8

--- students/shodges/lesson07/html_render.py
+++ students/shodges/lesson07/html_render.py
@@ -14,8 +14,10 @@ class Element(object):
         self.contents = [content]
         self.elem_attributes = kwargs
 
+
     def append(self, new_content):
         self.contents.append(new_content)
+
 
     def render(self, out_file, current_indent=''):
         out_file.write(current_indent)
@@ -31,6 +33,7 @@ class Element(object):
                     pass
         out_file.write("{}</{}>\n".format(current_indent, self.tag))
 
+
     def print_open_tag(self, out_file, newline=True):
         out_file.write("<{}".format(self.tag))
         for k, v in self.elem_attributes.items():
@@ -40,6 +43,7 @@ class Element(object):
         out_file.write(">")
         if newline == True:
             out_file.write("\n")
+
 
 class SimpleElement(Element):
     def render(self, out_file, current_indent=''):
@@ -51,38 +55,49 @@ class SimpleElement(Element):
             out_file.write((self.contents[0]))
             out_file.write("</{}>\n".format(self.tag))
 
+
     def append(self, content):
         raise NotImplementedError
+
 
 class SelfClosingElement(SimpleElement):
     def __init__(self, **kwargs):
         self.elem_attributes = kwargs
 
+
 class Html(Element):
     tag = 'html'
     indent = ''
+
 
     def render(self, out_file, current_indent=''):
         out_file.write(current_indent + "<!DOCTYPE html>\n")
         Element.render(self, out_file, current_indent)
 
+
 class Body(Element):
     tag = 'body'
+
 
 class P(Element):
     tag = 'p'
 
+
 class Head(Element):
     tag = 'head'
+
 
 class Title(SimpleElement):
     tag = 'title'
 
+
 class Hr(SelfClosingElement):
     tag = 'hr'
 
+
 class Br(SelfClosingElement):
     tag = 'br'
+
 
 class A(SimpleElement):
     tag = 'a'
@@ -91,11 +106,14 @@ class A(SimpleElement):
         SimpleElement.__init__(self, content)
         self.elem_attributes = {'href': link}
 
+
 class Ul(Element):
     tag = 'ul'
 
+
 class Li(Element):
     tag = 'li'
+
 
 class H(SimpleElement):
     tag = 'h'
@@ -105,6 +123,7 @@ class H(SimpleElement):
             raise TypeError
         self.tag = '{}{}'.format(self.tag, level)
         SimpleElement.__init__(self, content, **kwargs)
+
 
 class Meta(SelfClosingElement):
     tag = 'meta'
