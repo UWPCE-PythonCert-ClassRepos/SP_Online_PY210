2cf88e6d0db96b5f061941524fd2b4dedab5753f
Sean Hodges <sthodges@gmail.com>
2019-08-29 21:56:41 -0700

    Initial implementation of Circle class

--- students/shodges/lesson08/circle.py
+++ students/shodges/lesson08/circle.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+class Circle(object):
+    radius = 0
+
+    def __init__(self, radius):
+        if str(radius).isnumeric is False:
+            raise TypeError
+        self.radius = radius
