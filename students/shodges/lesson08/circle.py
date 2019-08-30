600ed252dcc4f748d8d96920cf1fb487bc953e7d
Sean Hodges <sthodges@gmail.com>
2019-08-29 22:11:26 -0700

    Add diameter init and retrieval

--- students/shodges/lesson08/circle.py
+++ students/shodges/lesson08/circle.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 class Circle(object):
-    radius = 0
+    _radius = None
+    _diameter = None
 
     def __init__(self, radius):
         try:
-            self.radius = float(radius)
+            self._radius = float(radius)
+            self._diameter = float(radius) * 2
         except ValueError:
             raise TypeError("radius expects a float")
+
+
+    @property
+    def radius(self):
+        return self._radius
+
+
+    @property
+    def diameter(self):
+        return self._diameter
