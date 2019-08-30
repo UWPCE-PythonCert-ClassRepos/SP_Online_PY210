d88d1a40bb7e397be9fa8713aabd445d65e15b2c
Sean Hodges <sthodges@gmail.com>
2019-08-29 21:51:18 -0700

    Add step 1 test cases

--- students/shodges/lesson08/test_circle.py
+++ students/shodges/lesson08/test_circle.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
 import pytest
+from circle import *
+
+def test_create_circle():
+    """
+    Test creation of a circle and output of its radius.
+
+    Expected output is 8
+    """
+    c = Circle(8)
+
+    assert c.radius == 8
