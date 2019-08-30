36ea886b1bc865f4c51a0cca44a798677c538eac
Sean Hodges <sthodges@gmail.com>
2019-08-29 21:58:52 -0700

    Missing function parens

--- students/shodges/lesson08/circle.py
+++ students/shodges/lesson08/circle.py
@@ -4,6 +4,6 @@ class Circle(object):
     radius = 0
 
     def __init__(self, radius):
-        if str(radius).isnumeric is False:
+        if str(radius).isnumeric() is False:
             raise TypeError
         self.radius = radius
