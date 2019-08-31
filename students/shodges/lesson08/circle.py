471262717e1b77f2c858d86a000a1c596ad32647
Sean Hodges <sthodges@gmail.com>
2019-08-31 08:15:32 -0700

    Add remaining comparison operators

--- students/shodges/lesson08/circle.py
+++ students/shodges/lesson08/circle.py
@@ -70,6 +70,26 @@ class Circle(object):
         return self._radius < other._radius
 
 
+    def __gt__(self, other):
+        return self._radius > other._radius
+
+
+    def __le__(self, other):
+        return self._radius <= other._radius
+
+
+    def __ge__(self, other):
+        return self._radius >= other._radius
+
+
+    def __eq__(self, other):
+        return self._radius == other._radius
+
+
+    def __ne__(self, other):
+        return self._radius != other._radius
+
+
     @property
     def radius(self):
         return self._radius
