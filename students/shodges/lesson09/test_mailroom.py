3a43c7bc40b2b591bb44493f92c2f69467285505
Sean Hodges <sthodges@gmail.com>
2019-09-04 18:42:48 -0700

    Reinit Abe

--- students/shodges/lesson09/test_mailroom.py
+++ students/shodges/lesson09/test_mailroom.py
@@ -4,7 +4,7 @@ import pytest
 from mailroom import *
 
 def test_donor_structure():
-    honest_abe = Donor('Abraham Lincoln', [87.00, 18.65])
+    honest_abe = Donor('Abraham Lincoln', 87.00, 18.65)
     teddy = Donor('Theodore Roosevelt')
 
     assert honest_abe.count == 2
