f6a77c8e96454708b82de9652302f10fda272f01
Sean Hodges <sthodges@gmail.com>
2019-09-06 09:26:30 -0700

    Initial DonorCollection class

--- students/shodges/lesson09/donor_models.py
+++ students/shodges/lesson09/donor_models.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import shelve
 
 class Donor(object):
     def __init__(self, donor_name, *args):
@@ -20,3 +21,29 @@ class Donor(object):
 
     def process(self, donation):
         self.ledger.append(float(donation))
+
+
+class DonorCollection(object):
+    def __init__(self, dbfile='donors.db'):
+        self.db = shelve.open(dbfile, writeback=True)
+
+
+    def donor(self, donor_name):
+        return self.db[donor_name]
+
+
+    def add_donor(self, donor_name):
+        self.db[donor_name] = Donor(donor_name)
+
+
+    def del_donor(self, donor_name):
+        try:
+            self.db.pop(donor_name)
+        except KeyError:
+            raise
+        else:
+            return True
+
+
+    def db_close(self):
+        self.db.close()
