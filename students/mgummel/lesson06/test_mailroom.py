e09842fa84260533c1f78dcac1c1213534538fe2
Matt Gummel <mgummel@tableau.com>
2019-09-11 18:09:14 -0700

    continued work on more tests

--- students/mgummel/lesson06/test_mailroom.py
+++ students/mgummel/lesson06/test_mailroom.py
@@ -62,3 +62,43 @@ def test_get_donor1():
 def test_get_donor2():
     db_tuple = ("Eric Johnson", [23432.23, 43289.32])
     assert get_donor("Eric Johnson", test_db1) == db_tuple
+
+
+def test_build_template1():
+    email_template = '\n'.join(('\n\nDear Dax Shephard,\n',
+                                'Thank you for your very kind donation of $123.32.\n',
+                                'It will be put to very good use.\n',
+                                '           Sincerely,',
+                                '             -The Team\n'))
+    assert build_template(("Dax Shephard", test_db1["Dax Shephard"])) == email_template
+
+
+def test_build_template2():
+    email_template = '\n'.join(('\n\nDear Marc Benioff,\n',
+                                'Your past donation amount of $45023.15\n',
+                                'has helped our organization tremendously.\n',
+                                'Thank you for your very kind donation of $442.30.\n',
+                                'It will be put to very good use.\n',
+                                '           Sincerely,',
+                                '             -The Team\n'))
+    assert build_template(("Marc Benioff", test_db1["Marc Benioff"])) == email_template
+
+
+def test_build_template3():
+    email_template = '\n'.join(('\n\nDear Gerard Way,\n',
+                                'Thank you for your very kind donation of $76.45.\n',
+                                'It will be put to very good use.\n',
+                                '           Sincerely,',
+                                '             -The Team\n'))
+    assert build_template(("Gerard Way", test_db2["Gerard Way"])) == email_template
+
+
+def test_build_template4():
+    email_template = '\n'.join(('\n\nDear Eddie Vedder,\n',
+                                'Your past donation amount of $475.82\n',
+                                'has helped our organization tremendously.\n',
+                                'Thank you for your very kind donation of $723.40.\n',
+                                'It will be put to very good use.\n',
+                                '           Sincerely,',
+                                '             -The Team\n'))
+    assert build_template(("Eddie Vedder", test_db2["Eddie Vedder"])) == email_template
