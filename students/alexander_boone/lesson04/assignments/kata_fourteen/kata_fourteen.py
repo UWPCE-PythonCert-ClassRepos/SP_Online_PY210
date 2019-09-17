24a80b1b0e0507b9bdf2cad7fc6702637e437841
Alexander Boone <alboone@uw.edu>
2019-09-16 20:46:18 -0700

    trigrams is working on basic text - now to add reading files

--- students/alexander_boone/lesson04/assignments/kata_fourteen/kata_fourteen.py
+++ students/alexander_boone/lesson04/assignments/kata_fourteen/kata_fourteen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import random, sys, string
 
+words = '''Testing 1 2 3 1 3 Testing 2 4 5 Testing 3 Testing 1 4 1 2 2 4 5'''.split()
 
 def build_trigrams(words):
     '''Build trigrams data structure using word list input
@@ -23,8 +25,29 @@ def build_trigrams(words):
     return trigrams
 
 
+def build_text(trigram_dict):
+    key = random.choice(list(trigram_dict.keys()))
+    new_word_list = list(key)
+    new_word_list[0] = new_word_list[0].capitalize()
+    try:
+        for i in range(100):
+            next_follower = trigram_dict[key][random.randint(0,(len(trigram_dict[key]) - 1))]
+            new_word_list.append(next_follower)
+            key = tuple(new_word_list[-2:])
+    except KeyError:
+        print('\nLast key not in dictionary of trigrams. Process aborted.\n')
+    finally:
+        new_text = " ".join(new_word_list) + '.'
+        return new_text
+
+
+
 if __name__ == '__main__':
     
-    words = "I wish I may I wish I might".split()
-    print(build_trigrams(words))
+    trigrams = build_trigrams(words)
+    new_text = build_text(trigrams)
+
+    print(new_text)
+    
+        
 
