185a1d341dbe867e059749fdb7ac1f00feae8acf
CalvinFannin <calvin.fannin@gmail.com>
2019-09-16 20:04:12 -0700

    implemented dict switch

--- students/calvin_fannin/mailroom/mailroom.py
+++ students/calvin_fannin/mailroom/mailroom.py
@@ -14,6 +14,7 @@ prompt = "\n".join (("Welcome to Mailroom","Choose an option:",
           "3 - Quit Mailroom",
           " >>> "))
 
+
 def search_fullname(donor_name):
     #search to see if donor already exits
     for i,name in enumerate(donations):
@@ -21,32 +22,39 @@ def search_fullname(donor_name):
             return True
     return False
 
+
 def get_name_index(donor_name):
     #find donors name and return the index in the list
     for i,name in enumerate(donations):
         if donor_name == (donations[i][0]):
             return i
 
+
 def create_email(donor_name, amount):
     #Print thank you message
     print(f"{donor_name.title()}, thank you for your generous donation of ${amount:.2f}.")
+
 
 def add_donation_new_donor(donor_name, amount):
     #add donor and donation to dontaion list
     donations.append([donor_name, [amount]])
 
+
 def add_donation(name_index, amount):
     #add to the donation list of lists when donor exists
     donations[name_index][1].append(amount)
+
 
 def print_donors():
     #[x[0] for x in donations]
     for i,name in enumerate(donations):
         print (donations[i][0])
 
+
 def sort_key(donation_list):
     #return the value to sort on
     return donation_list[1]
+
 
 def create_summary():
     summ_values =[]
@@ -54,6 +62,7 @@ def create_summary():
         #aggregate dontations and get averages
         summ_values.append([name[0],sum(name[1][:]),len(name[1]),sum(name[1][:])/len(name[1])])
     return summ_values
+
 
 def send_thankyou():
     name = input("Enter Donors Full name <First> <Last>")
@@ -76,6 +85,7 @@ def send_thankyou():
             #compose email and print to terninal
             create_email(name, response_amount)
 
+
 def create_report():
     report_values = create_summary()
     print('{:<20}{:>12}{:>10}{:>20}'.format("Donor Name","| Total Given ","| Num Gifts ","| Average Gift "))
@@ -84,20 +94,17 @@ def create_report():
     for name in (sorted(report_values,key=sort_key,reverse=True)):
         #print('{:<20}${:>12,.2f}{:^20d}${:>12,.2f}'.format(name[0],sum(name[1][:]),len(name[1]),sum(name[1][:])/len(name[1])))
         print('{:<20}${:>12,.2f}{:^20d}${:>12,.2f}'.format(name[0],name[1],name[2],name[3]))
+
+
 def quit_program():
     sys.exit()
 
+
 def main():
+    main_menu = {1:send_thankyou, 2:create_report, 3:quit_program}
     while True:
-        response = input(prompt)
-        if response == "1":
-            send_thankyou()
-        elif response == "2":
-            create_report()
-        elif response == "3":
-            quit_program()
-        else:
-            print("Not a valid option!")
+        response = int(input(prompt))
+        main_menu.get(response, "Invalid option")()
 
 if __name__ == "__main__":
     main()
