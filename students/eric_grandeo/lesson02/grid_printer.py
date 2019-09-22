0396d8037fd1a494129cd1187308b98ca1f46f3a
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-22 12:09:08 -0400

    added function

--- students/eric_grandeo/lesson02/grid_printer.py
+++ students/eric_grandeo/lesson02/grid_printer.py
@@ -1,8 +1,24 @@
 #Grid Printer
 
+'''
 for i in range(11):
     if i == 0 or i == 5 or i == 10:
         print('+' + ('-' * 4) + '+' + ('-' * 4) + '+')
     else:
         print("|" + '    ' + "|" + '    ' + '|')
+'''
+
+def print_grid(n):
+    for i in range(n+1):
+        if i == 0 or i == (n+1)/2:
+            print('+' + '-' * (n//2) + '+' + '-' * (n//2) + '+')
+            
+        else:
+            print("|" + ' ' * (n//2) + "|" + ' ' * (n//2) + '|')
+    print('+' + '-' * (n//2) + '+' + '-' * (n//2) + '+')
+    
+
+print_grid(3)
+print_grid(9)
+print_grid(15)
 
