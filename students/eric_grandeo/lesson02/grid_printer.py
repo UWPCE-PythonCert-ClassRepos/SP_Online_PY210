ad7eb1b81d23b48a03b454af12856ade7ca37d52
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-22 15:00:27 -0400

    finished function with 2 arguments

--- students/eric_grandeo/lesson02/grid_printer.py
+++ students/eric_grandeo/lesson02/grid_printer.py
@@ -1,5 +1,6 @@
 #Grid Printer
 
+#first part of the exercise
 '''
 for i in range(11):
     if i == 0 or i == 5 or i == 10:
@@ -7,7 +8,8 @@ for i in range(11):
     else:
         print("|" + '    ' + "|" + '    ' + '|')
 '''
-
+'''
+#function that takes one argument
 def print_grid(n):
     for i in range(n+1):
         if i == 0 or i == (n+1)/2:
@@ -21,4 +23,28 @@ def print_grid(n):
 print_grid(3)
 print_grid(9)
 print_grid(15)
+'''
+
+
+#function that takes 2 arguments
+
+def print_grid2(a,b):
+    plus = '+'
+    lines = (b * '-') + plus
+    col = '|'
+    rows = (b * ' ') + col
+
+    #print(plus + lines * (a+1))
+    #print(col + rows * (a+1))
+    
+    for i in range(a):
+        print(plus + lines * (a)) 
+        for i in range(b):
+            print(col + rows * (a)) 
+    print(plus + lines * (a)) 
+
+
+print_grid2(3,4) 
+print_grid2(5,3)
+
 
