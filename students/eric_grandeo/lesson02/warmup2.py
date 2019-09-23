31088e921b22bef329967a07c29c34d85464f3c9
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-22 22:27:49 -0400

    updated last2

--- students/eric_grandeo/lesson02/warmup2.py
+++ students/eric_grandeo/lesson02/warmup2.py
@@ -42,12 +42,23 @@ print('-' * 25)
 
 #last2
 def last2(str):
-    count = str.count(str[-2:])
-    return count -1
+    last2 = str[-2:]
+    
+    count = 0
+    for i in range(len(str)-2):
+        sub = str[i:i+2]
+        #print(sub)
+        if sub == last2:
+            #print(sub)
+            count += 1
+           
+    return count
+
 
 print(last2('hixxhi'))
 print(last2('xaxxaxaxx'))
 print(last2('axxxaaxx'))
 print('-' * 25)
 
+#array_count9
 
