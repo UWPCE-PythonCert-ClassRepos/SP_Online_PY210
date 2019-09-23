923e0009a7610b1450ae5269b51f9644495b1cf0
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-22 20:26:55 -0400

    added lucas numbers

--- students/eric_grandeo/lesson02/series.py
+++ students/eric_grandeo/lesson02/series.py
@@ -1,13 +1,41 @@
+#fibonacci sequence
 def fibonacci(n):
+    '''
+    Return the nth value in the Fibonacci sequence
+
+    :param n: The number position of the value in the Fibonacci sequence (starting with zero index)
+    '''
+
     fib = [0, 1]
 
     for i in range(2,n):
         new_num = fib[i-2] + fib[i-1]
         fib.append(new_num)
 
-    return fib
+    return fib[n-1]
 
-print(fibonacci(10))
+print(fibonacci(8))
+
+#lucas numbers
+def lucas(n):
+    '''
+    Return the nth value in the lucas sequence
+
+    :param n: The number position of the value in the lucas sequence (starting with zero index)
+    '''
+
+    luc = [2, 1]
+
+    for i in range(2,n):
+        new_num = luc[i-2] + luc[i-1]
+        luc.append(new_num)
+
+    return luc[n-1]
+
+print(lucas(6))
+
+
+
 
 
 
