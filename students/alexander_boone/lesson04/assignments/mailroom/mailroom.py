087ea7720f73f4fa812d6c97b1da5adff994d953
Alexander Boone <alboone@uw.edu>
2019-09-22 20:44:14 -0700

    made primary data structure a dict and made code snake case

--- students/alexander_boone/lesson04/assignments/mailroom/mailroom.py
+++ students/alexander_boone/lesson04/assignments/mailroom/mailroom.py
@@ -2,23 +2,17 @@
 import os
 import sys
 
-# prepare initial data structure for donor data
-donors = [{'first_name': 'Arnold', 'last_name': 'Schwarzenegger',
-           'donation_total': 100000, 'donation_count': 1},
-          {'first_name': 'Lebron', 'last_name': 'James',
-           'donation_total': 1000000, 'donation_count': 1},
-          {'first_name': 'Elon', 'last_name': 'Musk',
-           'donation_total': 2000000, 'donation_count': 1},
-          {'first_name': 'Walter', 'last_name': 'White',
-           'donation_total': 500000, 'donation_count': 1},
-          {'first_name': 'Gordon', 'last_name': 'Ramsay',
-           'donation_total': 1280000, 'donation_count': 1}]
+donors = {'Arnold Schwarzenegger': [100000, 1],
+          'Lebron James': [1000000, 1],
+          'Elon Musk': [2000000, 1],
+          'Walter White': [500000, 1],
+          'Gordon Ramsay': [1280000, 1]}
 
 # functions
 
 
 def thank_you():
-    """Send a thank you email to the donor."""
+    """Send a thank you email to the specified donor."""
 
     print("Who would you like to send a thank you to?")
 
@@ -29,48 +23,36 @@ def thank_you():
 
         # run if user inputs 'list'
         if name == 'list':
-            list_names = list()
-            for item in donors:
-                list_names.append(item['first_name'] + ' ' + item['last_name'])
+            list_names = []
+            for item in donors.keys():
+                list_names.append(item)
             print(list_names)
+
         # run if name that user inputs is not already in the donor list
-        elif not any(donor['first_name'] == name.split(' ')[0] and
-                     donor['last_name'] == name.split(' ')[1] for
-                     donor in donors):
-            firstName = name.split(' ')[0]
-            lastName = name.split(' ')[1]
-            donors.append({'first_name': firstName, 'last_name': lastName,
-                           'donation_total': 0, 'donation_count': 0})
-            index = len(donors) - 1
-        # run if name that user inputs is already in the donor list
-        else:
-            firstName = name.split(' ')[0]
-            lastName = name.split(' ')[1]
-            for i in range(len(donors)):
-                if (donors[i]['first_name'] == firstName and
-                        donors[i]['last_name'] == lastName):
-                    index = i
-                    break
+        elif name not in donors.keys():
+            donors[name] = [0, 0]
 
     # enter donation amount
     donation = float(input("Enter a donation amount: "))
-    donors[index]['donation_total'] += donation
-    donors[index]['donation_count'] += 1
+    donors[name][0] += donation
+    donors[name][1] += 1
 
     # write email (using formatted dict values)
-    email = ('\nDear {first_name} {last_name},\n\nThank you for '
-             + 'your generous donations, totaling ${donation_total}.'
+    email = ('\nDear {0},\n\nThank you for '
+             + 'your generous donations, totaling ${1:,.2f}.'
              + ' We are very grateful.\n\nBest, \n\nLocal Charity\n'
-             ).format(**donors[index])
+             ).format(name, donors[name][0])
     print(email)
 
 
 def create_report():
     '''Create a report of donor data, including total donated, number of
-    donations, and average donation amount.'''
+    donations, and average donation amount.
+    '''
 
     h = ['Donor Name', '|', 'Total Given', '|', 'Num Gifts',
          '|', 'Average Gift']
+
     # create table heading
     report_headers = '{:<25}{:^5}{:<15}{:^5}{:<10}{:^5}{:<15}'.format(*h)
     table_divider = '-' * 71
@@ -79,26 +61,24 @@ def create_report():
 
     # sort donor data by donation_total
     def sort_key(donors_data):
-        return donors_data['donation_total']
+        return donors_data[1][0]
 
-    donors_sorted = sorted(donors, key=sort_key, reverse=True)
+    donors_sorted = sorted(donors.items(), key=sort_key, reverse=True)
 
-    # print donor data
-    for i in range(len(donors_sorted)):
-        first_name = donors_sorted[i]['first_name']
-        last_name = donors_sorted[i]['last_name']
-        donation_avg = (donors_sorted[i]['donation_total'] /
-                        donors_sorted[i]['donation_count'])
+    # print donor data row-by-row
+    for donor in donors_sorted:
+        donation_avg = (donor[1][0] /
+                        donor[1][1])
         print(('{:<25}{:^5}${:>14,.2f}{:^5}{:>10}{:^5}${:>14,.2f}'
-               ).format(first_name + ' ' + last_name, ' ',
-                        donors_sorted[i]['donation_total'], ' ',
-                        donors_sorted[i]['donation_count'], ' ',
+               ).format(donor[0], ' ',
+                        donor[1][0], ' ',
+                        donor[1][1], ' ',
                         donation_avg
                         )
               )
 
     # remain on page until user decides to return to main menu
-    q = 'Z'
+    q = ''
     while q != 'Q' and q != 'q':
         q = input("\nEnter Q to Quit and return to the main menu: ")
 
@@ -107,19 +87,26 @@ def letters_to_all():
     '''Write a letter to every donor and save each one to a
     file on the disk.'''
 
+    script_dir = os.path.dirname(__file__)
+
     # write a letter to a text file for each donor
-    for donor in donors:
-        with open("{first_name}_{last_name}.txt".format(**donor),
+    for donor in donors.keys():
+        first_name = donor.split()[0]
+        last_name = donor.split()[1]
+        relative_path = "letters/{0}_{1}.txt".format(first_name, last_name)
+        abs_file_path = os.path.join(script_dir, relative_path)
+        with open(abs_file_path,
                   'w+') as new_file:
-            letter_body = ('{first_name} {last_name},\n\nThank you'
-                           + ' for donating ${donation_total}. You '
+            letter_body = ('{0} {1},\n\nThank you'
+                           + ' for donating ${2:,.2f}. You '
                            + 'are so kind.\n\nBest,\n\nLocal Charity'
-                           ).format(**donor)
+                           ).format(first_name, last_name, donors[donor][0])
             new_file.write(letter_body)
 
 
 def quit_program():
     '''Print exit message and quit the program'''
+
     exit_message = "Closing the mailroom for the day..."
     print(exit_message)
     sys.exit()
@@ -133,6 +120,7 @@ if __name__ == '__main__':
     response = 0
     while response != 4:
         response = 0
+
         # display main menu with options
         options = ["1. Send a Thank You to a single donor", "2. Create a"
                    + " Report", "3. Send letters to all donors",
@@ -140,8 +128,7 @@ if __name__ == '__main__':
         print(f"----- Main Menu -----\n{options[0]}\n{options[1]}"
               + f"\n{options[2]}\n{options[3]}")
 
-        # get and run user response
-        while int(response) not in [1, 2, 3, 4]:
+        # ask for and run user response
+        while int(response) not in response_dict:
             response = int(input("Enter a number: "))
-        if response == 4:
-            response_dict.get(response)()
+        response_dict.get(response)()
