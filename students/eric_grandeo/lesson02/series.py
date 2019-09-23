3e4d3c3741f3e8b58ff3f013e3a3066cc582f007
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-22 20:45:30 -0400

    added the sum_series

--- students/eric_grandeo/lesson02/series.py
+++ students/eric_grandeo/lesson02/series.py
@@ -14,7 +14,7 @@ def fibonacci(n):
 
     return fib[n-1]
 
-print(fibonacci(8))
+print(fibonacci(9))
 
 #lucas numbers
 def lucas(n):
@@ -32,10 +32,27 @@ def lucas(n):
 
     return luc[n-1]
 
-print(lucas(6))
+print(lucas(7))
 
+#sum series
+def sum_series(x, y=0, z=1):
+    '''
+    Return the nth value in the series from 2 starting numbers
 
+    :param x: The number position of the value in the lucas sequence
+              (starting with zero index)
 
+    :param y=0: The starting number of the series, default is 0
 
+    :param z=0: The second number of the series, the default is 1
+    '''
+    series = [y, z]
 
+    for i in range(2,x):
+        new_num = series[i-2] + series[i-1]
+        series.append(new_num)
+
+    return series[x-1]
+
+print(sum_series(7,2,1))
 
