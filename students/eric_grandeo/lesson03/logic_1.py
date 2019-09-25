ee7be996f6bdc0c8b44024d1d91d94a08cc1161e
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-24 21:41:22 -0400

    added date_fashon exercise

--- students/eric_grandeo/lesson03/logic_1.py
+++ students/eric_grandeo/lesson03/logic_1.py
@@ -13,4 +13,24 @@ print("Cigar party tests passed")
 
 
 
+#date_fashion
+
+def date_fashion(you, date):
+    if you <= 2 or date <= 2: 
+        return 0
+    elif you >= 8 or date >= 8:
+        return 2
+    else:
+        return 1
+
+assert date_fashion(5, 10) == 2
+assert date_fashion(5, 2) == 0
+assert date_fashion(5, 5) == 1
+print("date fashion tests passed")
+
+#squirrel_play
+
+
+
+
 
