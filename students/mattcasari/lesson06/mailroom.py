c46a31db5c348b1dabb89b2e4ff33e4e725f20a6
mattcasari <mjcasari@gmail.com>
2019-09-26 14:35:31 -0700

    Finalizing tests for mailbox

--- students/mattcasari/lesson06/mailroom.py
+++ students/mattcasari/lesson06/mailroom.py
@@ -56,7 +56,6 @@ def initialize_donors():
     donors["Alan Bean"] = [28477.13, 727.1]
     donors["Chris Hadfield"] = [17325.42, 13823.83, 0.99]
 
-    # print(donors)
 
 def update_donation_dict(name, amount):
     """
@@ -70,6 +69,7 @@ def update_donation_dict(name, amount):
         None
     """
     donors.setdefault(name, []).append(amount)
+
 
 def calculate_stats(donations):
     """
@@ -102,12 +102,23 @@ def thank_you_email(donor_dict):
     """
 
     return THANK_YOU_TEMPLATE.format(**donor_dict)
-    
+
+
 def sort_donors_by_total(name):
     """ Function used to sort donors by total contributions """
     return sum(donors[name])
 
+
 def create_report_header(max_name_length):
+    """
+    Create the report header fields
+
+    Args:
+        max_name_length: Length of largest name in data
+    Returns:
+        result: Report header string
+
+    """
     result = []
     f_str = " {" + f":<{max_name_length}" + "} | {} | {} | {}"
     title_str = f_str.format("Donor Name", "Total Given", "Num Gifts", "Average Gift")
@@ -117,34 +128,61 @@ def create_report_header(max_name_length):
 
     return result
 
+
 def create_report_body(name_length, donors):
+    """ 
+    Creates the body of the report
+
+    Args:
+        name_length: Number of names
+        donors: dictionary of donors
+    Return:
+        result: Report body of all donors and associated data
+    """
     result = []
-    f_str = " {name" + f":<{name_length}" + "}  ${sum:11.2f}   {len:9}  ${average:12.2f}"
+    f_str = (
+        " {name" + f":<{name_length}" + "}  ${sum:11.2f}   {len:9}  ${average:12.2f}"
+    )
 
     names = sorted(donors, key=sort_donors_by_total, reverse=True)
 
     for name in names:
         stats = calculate_stats(donors[name])
-        stats['name']=name
+        stats["name"] = name
         v_str = f_str.format(**stats)
         result.append(v_str)
 
     result = "\n".join(result)
     return result
 
+
 def generate_report(donors=donors):
+    """
+    Generate a full report of donors
+    
+    Args:
+        donors: Dictionary of donors and donation amounts
+    Returns:
+        report: Full report of donors and donations totals
+    """
     names = donors.keys()
-    column_donor_length = max([len(name) for name in names])+5
+    column_donor_length = max([len(name) for name in names]) + 5
 
     report = create_report_header(column_donor_length)
-    report += '\n'
+    report += "\n"
     report += create_report_body(column_donor_length, donors)
     print(report)
     return report
 
 
 def create_directory(directory):
-    
+    """
+    Create a directory to save letters to.
+    Args:
+        directory: Path to create
+    Returns:
+        directory: Path created
+    """
     path = pathlib.Path("./")
     path = path / directory
     try:
@@ -153,14 +191,22 @@ def create_directory(directory):
     except OSError:
         print("Error creating directory")
 
-    return directory
+    return path
 
-def save_files(donors, path):
+
+def save_letters(donors, path):
+    """
+    Generate and save letters to donors
+
+    Args: 
+        donors: Dictionary of donors and donations
+        path: Directory to save letters to
+    """
     path = pathlib.Path("./") / path
     for donor in donors:
         filename = donor.replace(" ", "_") + ".txt"
         filename = path / filename
-        
+
         try:
             with open(filename, "w+") as temp:
                 donor_dict = {
@@ -174,33 +220,6 @@ def save_files(donors, path):
                 temp.close()
         except OSError:
             print("File failure")
-
-def generate_report2(donors):
-    """ Generates a formatted report of donor names, total donation, # of donations and average donation """
-    names = donors.keys()
-    print("donors=", donors)
-    print("names=", names)
-    result = []
-
-    column_donor_length = max([len(name) + 5 for name in names])
-    report_header = create_report_header(column_donor_length)
-    report_body = create_report_body()
-    
-
-    # for name in names:
-    # names = sorted(donors.values(), key=sort_donors_by_total, reverse=True)
-    # names = sorted(sum(donors.iterkeys()))
-    print(names)
-
-    # for name in names:
-    #     f_str = " {" + f":<{column_donor_length}" + "}  ${:11.2f}   {:9}  ${:12.2f}"
-    #     (d_sum, d_num, d_ave) = calculate_stats(donors[name])
-    #     v_str = f_str.format(name, d_sum, d_num, d_ave)
-    #     result.append(v_str)
-    #     # print(v_str)
-
-    # return result
-
 
 
 """
@@ -225,7 +244,7 @@ def add_donor():
             print("Invalid entry")
     try:
         update_donation_dict(donor, amount)
-        
+
     except ValueError:
         print("\nInvalid amount entered")
         return
@@ -233,12 +252,13 @@ def add_donor():
     txt = thank_you_email(donor)
     print(txt)
 
+
 def prompt_user():
     """ Prompts the user for menu option """
     switch_func_dict = {
         1: add_donor,
         2: generate_report,
-        3: create_files,
+        3: send_letters,
         4: quit_program,
     }
     try:
@@ -265,35 +285,13 @@ def print_donor_list(values):
         print(value)
 
 
-
-
-
-def create_files():
+def send_letters():
     """
     Create file(s) in user specified directory for each donor.
     """
     directory = input("Enter save directory name: ")
     path = create_directory(directory)
-    save_files(donors, path)
-    # path = input("Enter save directory name: ")
-
-    # for donor in donors:
-    #     filename = donor.replace(" ", "_") + ".txt"
-    #     filename = path / filename
-        
-    #     try:
-    #         with open(filename, "w+") as temp:
-    #             donor_dict = {
-    #                 "name": donor,
-    #                 "last": donors[donor][-1],
-    #                 "sum": sum(donors[donor]),
-    #                 "number": len(donors[donor]),
-    #             }
-
-    #             temp.write(EMAIL_TEMPLATE.format(**donor_dict))
-    #             temp.close()
-    #     except OSError:
-    #         print("File failure")
+    save_letters(donors, path)
 
 
 def main():
--- students/mattcasari/lesson06/test_mailroom.py
+++ students/mattcasari/lesson06/test_mailroom.py
@@ -13,16 +13,18 @@ from mailroom import THANK_YOU_TEMPLATE
 from mailroom import EMAIL_TEMPLATE
 from mailroom import donors
 from mailroom import create_directory
-from mailroom import save_files
+from mailroom import save_letters
 
 import os
 import pathlib
 import shutil
 
+
 def test_initialize_donors_creates_donor_list():
     initialize_donors()
     assert donors["Neil Armstrong"] == [15000.00, 15000.00]
     assert donors["Chris Hadfield"] == [17325.42, 13823.83, 0.99]
+
 
 def test_update_donation_dict():
     initialize_donors()
@@ -32,6 +34,7 @@ def test_update_donation_dict():
 
     assert donors["Matt C"] == [9876.54]
     assert donors["R2 D2"] == [777.66, 44.23]
+
 
 def test_calculate_stats_generates_valid_answers():
     values = [1000, 2000, 3000]
@@ -52,61 +55,63 @@ def test_thank_you_email():
     )
     assert test_email == thank_you_email(donor)
 
+
 def test_update_donation_histor():
     initialize_donors()
     update_donation_dict("Alan Bean", 111.11)
 
     assert donors["Alan Bean"] == [28477.13, 727.10, 111.11]
 
+
 def test_create_report_header():
     name_length = 15
-    
+
     expected_str = []
     expected_str.append(" Donor Name      | Total Given | Num Gifts | Average Gift")
-    expected_str.append("---------------------------------------------------------")  
+    expected_str.append("---------------------------------------------------------")
     expected_str = "\n".join(expected_str)
     assert expected_str == create_report_header(name_length)
 
-def test_create_report_body():
-    donor_test = {'Matt C':[100.00, 200.00], 
-                'R2 D2': [50.00, 100.00, 150.00]}
 
-    length = len('Matt C') + 5
+def test_create_report_body():
+    donor_test = {"Matt C": [100.00, 200.00], "R2 D2": [50.00, 100.00, 150.00]}
+
+    length = len("Matt C") + 5
     expected_str = []
     expected_str.append(" Matt C       $     300.00           2  $      150.00")
     expected_str.append(" R2 D2        $     300.00           3  $      100.00")
     expected_str = "\n".join(expected_str)
-    
+
     result = create_report_body(length, donor_test)
-    # assert False
     assert expected_str == result
 
-def test_sort_donors_by_total(): 
-    temp = {"Matt C":[200.0], "R2 D2": [150]}
+
+def test_sort_donors_by_total():
+    temp = {"Matt C": [200.0], "R2 D2": [150]}
     names = sorted(temp, key=sort_donors_by_total, reverse=True)
     names = "\n".join(names)
-    expected_names = 'Matt C\nR2 D2'
-    
+    expected_names = "Matt C\nR2 D2"
+
     assert expected_names == names
 
+
 def test_generate_report():
-    test_donors = {'Matt C':[5000.00], 'R2 D2':[100.00, 100.00]}
+    test_donors = {"Matt C": [5000.00], "R2 D2": [100.00, 100.00]}
 
     expected_str = []
     expected_str.append(" Donor Name  | Total Given | Num Gifts | Average Gift")
-    expected_str.append("-----------------------------------------------------")  
+    expected_str.append("-----------------------------------------------------")
     expected_str.append(" Matt C       $    5000.00           1  $     5000.00")
     expected_str.append(" R2 D2        $     200.00           2  $      100.00")
     expected_str = "\n".join(expected_str)
 
     report = generate_report(test_donors)
 
-    
     assert expected_str == report
 
 
 def test_create_directory():
-    directory = 'temp'
+    directory = "temp"
 
     path = pathlib.Path("./")
     path = path / directory
@@ -117,15 +122,15 @@ def test_create_directory():
     shutil.rmtree(path)
 
 
-def test_save_files():
-    directory = 'temp'
+def test_save_letters():
+    directory = "temp"
     path = pathlib.Path("./")
     path = path / directory
     create_directory(directory)
 
-    dlist = {"Matt C":[100.00], "R2 D2":[40.00, 140.00]}
+    dlist = {"Matt C": [100.00], "R2 D2": [40.00, 140.00]}
 
-    save_files(dlist, path)
+    save_letters(dlist, path)
 
     m_path = path / "Matt_C.txt"
     r_path = path / "R2_D2.txt"
@@ -133,17 +138,3 @@ def test_save_files():
     assert r_path.exists()
     assert m_path.exists()
     shutil.rmtree(path)
-
-# def test_send_letters():
-#     filenames = os.path.
-#     # filenames = ["Neil_Armstrong.txt", 
-#     #             "Buzz_Aldrin.txt", 
-#     #             "Sally_Ride.txt",
-#     #             "Al_Shephard.txt",
-#     #             "Alan_Bean.txt",
-#     #             "Chris_Hadfield.txt"]
-
-#     for name in filenames:
-#         temp_path = path / name
-#         assert temp_path.exists
-#     assert False
