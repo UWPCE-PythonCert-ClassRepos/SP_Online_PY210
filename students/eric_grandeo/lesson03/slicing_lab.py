9876b8eaf6c0d3d149939da4c14f33563af28737
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-29 14:16:16 -0400

    added first4_last4_every_other

--- students/eric_grandeo/lesson03/slicing_lab.py
+++ students/eric_grandeo/lesson03/slicing_lab.py
@@ -22,4 +22,17 @@ assert every_other_item(a_string) == "ti sasrn"
 assert every_other_item(a_tuple) == (2, 13, 5)
 print("every_other_item tests passed")
 
+#with the first 4 and the last 4 items removed,
+#and then every other item in the remaining sequence.
+
+a_tuple_2 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+a_string_2 = "This is a really long string for a test"
+
+
+def first4_last4_every_other(seq):
+    return seq[4:-4:2]
+
+assert first4_last4_every_other(a_tuple_2) == (5, 7, 9, 11)
+assert first4_last4_every_other(a_string_2) == ' saral ogsrn o  '
+print("first4_last4 tests passed")
 
