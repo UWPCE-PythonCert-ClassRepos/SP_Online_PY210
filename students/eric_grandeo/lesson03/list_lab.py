261e152e1b7d972113b0a18e044deb49055d2ca8
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-29 16:14:12 -0400

    series 1 in progress

--- students/eric_grandeo/lesson03/list_lab.py
+++ students/eric_grandeo/lesson03/list_lab.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+print(fruits)
+
+response = input("Please enter in a new fruit > ")
+fruits.append(response)
+
+print(fruits)
+
+num = int(input("Please enter a number > "))
+print("You selected number {}, which is {}".format(num, fruits[num-1]))
+
+
+
+
+
+    
