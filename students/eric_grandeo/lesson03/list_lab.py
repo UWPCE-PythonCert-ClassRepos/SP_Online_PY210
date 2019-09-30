46ab6682193c05a0b962968a44b94296d58276c8
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-29 20:32:47 -0400

    Completed Series 1

--- students/eric_grandeo/lesson03/list_lab.py
+++ students/eric_grandeo/lesson03/list_lab.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
+#Series 1
 fruits = ["Apples", "Pears", "Oranges", "Peaches"]
 print(fruits)
 
+#Ask the user for another fruit and add it to the end of the list.
 response = input("Please enter in a new fruit > ")
 fruits.append(response)
 
 print(fruits)
 
+#Ask the user for a number and display the number back to the user
+#and the fruit corresponding to that number (on a 1-is-first basis)
 num = int(input("Please enter a number > "))
 print("You selected number {}, which is {}".format(num, fruits[num-1]))
 
+#Add another fruit to the beginning of the list using “+” and display the list.
+strawberry = ['Strawberry']
+fruits = strawberry + fruits
+
+print("Added new fruit with +: {}".format(fruits))
+
+#Add another fruit to the beginning of the
+#list using insert() and display the list.
+
+fruits.insert(0, 'Banana')
+print("Added new fruit with insert: {}".format(fruits))
+
+#Display all the fruits that begin with “P”, using a for loop.
+letter_p = []
+for i in fruits:
+    if i[0] == "P":
+        letter_p.append(i)
+
+print("Fruits that begin with 'P':.{}".format(letter_p))
+
+#Series 2
 
 
 
