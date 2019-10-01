ed9af804e7e229a5e3c9fcfd61dd7a631f5c8dc3
David Baylor <dbaylor1222@gmail.com>
2019-10-01 10:56:42 -0700

    added fizzBuzz.py

---
+++ students/david_baylor/lesson2/fizzBuzz.py
@@ -0,0 +1,23 @@
+"""
+fizzBuzz.py
+by David Baylor
+uses python 3
+
+loops through numbers 1 to 100 and for multipuls of 3 it prints fizz, for multipuls of 5 it prints buzz nad for multipuls of 3 and
+5 it prints FizzBuzz.
+"""
+
+def detFactor(n):
+    """determin if the number is a factor of 3, 5 or both"""
+    if n%3 == 0 and n%5 == 0:
+        return "FizzBuzz"
+    elif n%3 == 0:
+        return "Fizz"
+    elif n%5 == 0:
+        return "Buzz"
+    else:
+        return n
+
+for i in range (1,101):
+    print(detFactor(i))
+
