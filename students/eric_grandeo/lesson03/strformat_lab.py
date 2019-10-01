90c77facd1a7c453d09b417087a39e61bb29af83
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-09-30 21:52:58 -0400

    Added task two

--- students/eric_grandeo/lesson03/strformat_lab.py
+++ students/eric_grandeo/lesson03/strformat_lab.py
@@ -6,3 +6,12 @@ s = "file_{:0>3d} :  {:.2f}, {:.2e}, {:.2e}"
 
 print(s.format( 2, 123.4567, 10000, 12345.67))
 
+#Task Two
+file = 2
+num1 = 123.4567
+num2 = 10000
+num3 = 12345.67
+
+print(f"file_{file:0>3d} :  {num1:.2f}, {num2:.2e}, {num3:.2e}")
+
+
