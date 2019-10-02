be3dc1403c746d998e2b915e41131c9db3ab1194
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-01 22:11:15 -0400

    finished task 3

--- students/eric_grandeo/lesson03/strformat_lab.py
+++ students/eric_grandeo/lesson03/strformat_lab.py
@@ -15,4 +15,16 @@ num3 = 12345.67
 print(f"file_{file:0>3d} :  {num1:.2f}, {num2:.2e}, {num3:.2e}")
 
 #Task Three
+#"the 3 numbers are: {:d}, {:d}, {:d}".format(1,2,3)
+
+def formatter(in_tuple):
+    l = len(in_tuple)
+    form_string = "the {} numbers are: " + ",".join(["{}"]*l)
+    return form_string.format(l,*in_tuple)
+
+
+print(formatter((2,3,5)))
+print(formatter((2,3,5,7,9)))
+
+
 
