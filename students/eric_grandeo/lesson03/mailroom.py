c7c21f819aea6425934eac793f6084381bbbf724
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-03 21:54:10 -0400

    putting together script framework

---
+++ students/eric_grandeo/lesson03/mailroom.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+donors = [("Bill Gates", [653772.32, 12.17]),
+          ("Jeff Bezos", [877.33]),
+          ("Paul Allen", [663.23, 43.87, 1.32]),
+          ("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]),
+          ("Tim Cook", [1563.32, 8976.54])]
+
+prompt = "\n".join(("Welcome to the mailroom!",
+          "Please choose from below options:",
+          "1 - Send a thank you",
+          "2 - Create a report",
+          "3 - Quit",
+          ">>> "))
+
