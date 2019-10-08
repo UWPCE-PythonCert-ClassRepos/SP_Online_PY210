f58efe7290b436d5a6426672e7dd7326da5f1535
Tim <jvmt0115@gmail.com>
2019-10-08 12:59:16 +0800

    modified the program fizz_buzz.py

--- students/shumin/lesson02/fizz_buzz.py
+++ students/shumin/lesson02/fizz_buzz.py
@@ -3,12 +3,15 @@
 # Multiples of 5 prints Buzz
 # Multiples of 15 prints FizzBuzz
 
-for i in range(1, 101):
-    if i % 15 == 0:
-        print("FizzBuzz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    else:
-        print(i)
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+
+fizz_buzz()
