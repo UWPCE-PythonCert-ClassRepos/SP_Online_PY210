430f422a6ecaec826e93de98bfb70338d165e685
CalvinFannin <calvin.fannin@gmail.com>
2019-10-11 16:22:43 -0700

    updates to test_ult_circle

--- students/calvin_fannin/lesson08/test_ultimate_circle.py
+++ students/calvin_fannin/lesson08/test_ultimate_circle.py
@@ -8,21 +8,25 @@ import pytest
 from ultimate_circle import *
 
 def test_init_circle():
+    """ Test Circle is created """
     c = Circle(7)
     assert c.radius == 7
 
 
 def test_init_no_radius():
+    """ Test that you can not create circle with radius """
     with pytest.raises(TypeError):
         c = Circle()
 
 
 def test_diameter():
+    """ Test that you can get the diameter of a circle """
     c = Circle(7)
     assert c.diameter  == 14
 
 
 def test_set_diameter():
+    """ Test that you can set the diameter of a circle """
     c =Circle(7)
     c.diameter = 20
     assert c.diameter == 20
@@ -30,6 +34,7 @@ def test_set_diameter():
 
 
 def test_area():
+    """ Test that you can get the area of a circle """
     c =Circle(7)
     with pytest.raises(AttributeError):
         c.area = 34
@@ -37,16 +42,19 @@ def test_area():
 
 
 def test_from_diameter():
+    """ Test that you can create circle with diameter alt construct"""
     c = Circle.from_diameter(30)
 
 
 def test_print_string():
+    """ Test the string output of the circle"""
     c =Circle(7)
     d = eval(repr(c))
     assert 'Circle(7)' == d
 
 
 def test_add_circle():
+    """ Test adding circles """
     c = Circle(7)
     c2 = Circle(7)
     csum = c + c2
@@ -54,6 +62,7 @@ def test_add_circle():
 
 
 def test_mulitply_circle():
+    """ Test multipling circles"""
     c = Circle(7)
     c2 = Circle(7)
     cvalue = c * c2
@@ -63,6 +72,7 @@ def test_mulitply_circle():
 
 
 def test_create_sphere():
+    """ Test that you can crate a sphere"""
     sp = Sphere(4)
     assert sp.volume == 268.082573106329
     assert sp.area == 201.06192982974676
