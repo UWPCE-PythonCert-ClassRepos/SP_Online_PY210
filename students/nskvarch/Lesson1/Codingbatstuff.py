b3cf062ea91881b5a531461d207ae8ee1e647e65
nskvarch <nielsk01@noa.nintendo.com>
2019-10-16 15:03:11 -0700

    adding lesson1 excersizes

---
+++ students/nskvarch/Lesson1/Codingbatstuff.py
@@ -0,0 +1,15 @@
+#dif21
+def diff21(n):
+  if n > 21:
+    return 2 * (n - 21)
+  return 21 - n
+
+#sleep_in
+
+def sleep_in(weekday, vacation)
+	if vacation:
+		return True
+	if not weekday:
+		return False
+
+		
