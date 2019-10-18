71508a4308bb5fc2e4642be9f217bb2de35979fc
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-17 21:50:33 -0400

    dictionary 1 exercise

--- students/eric_grandeo/lesson04/dict_lab.py
+++ students/eric_grandeo/lesson04/dict_lab.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 
+#Dictionary 1
+dict_1 = {"name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+
+#Display the dict
+print(dict_1)
+
+#Delete the entry for cake, display the dict
+dict_1.pop("cake")
+print(dict_1)
+
+#add fruit, display dict
+dict_1["fruit"] = "Mango"
+print(dict_1)
+
+#display dict keys
+print(dict_1.keys())
+
+#display the dict values
+print(dict_1.values())
+
+
+
+
