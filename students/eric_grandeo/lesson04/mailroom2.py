ad34999964b0e4a56dcf420ede86b20c2bf1c891
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-18 21:51:14 -0400

    working on dispatch dict

--- students/eric_grandeo/lesson04/mailroom2.py
+++ students/eric_grandeo/lesson04/mailroom2.py
@@ -7,19 +7,36 @@ donors = {"Bill Gates": [653772.32, 12.17],
           "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
           "Tim Cook": [1563.32, 8976.54]}
 
+main_prompt = "\n".join(("Welcome to the mailroom!",
+          "Please choose from below options:",
+          "1 - Send a thank you",
+          "2 - Create a report",
+          "3 - Quit",
+          ">>> "))
 
-
+main_dispatch = {"1" : thank_you,
+                "2" : create_report,
+                "3" : quit_program,}
 
 #switch between users selections
 def menu_selection(prompt, dispatch_dict):
     pass
 
 
-#may not be necessary
-def main():
+def thank_you():
     pass
+
+def create_report():
+    pass
+
+def quit_program():
+    pass
+
+
+
 
 
 #may need to change this to menu selection
 if __name__ == "__main__":
-    main()
+    menu_selection(main_prompt, main_dispatch)
+
