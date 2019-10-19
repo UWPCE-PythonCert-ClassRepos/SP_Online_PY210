579b74ce104ee17a2b8b38b0025e1b4770b787c4
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-18 21:32:30 -0400

    creating structure for mailroom using dicts

---
+++ students/eric_grandeo/lesson04/mailroom2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#donors as a dict
+donors = {"Bill Gates": [653772.32, 12.17],
+          "Jeff Bezos": [877.33],
+          "Paul Allen": [663.23, 43.87, 1.32],
+          "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
+          "Tim Cook": [1563.32, 8976.54]}
+
+
+
+
+#switch between users selections
+def menu_selection(prompt, dispatch_dict):
+    pass
+
+
+#may not be necessary
+def main():
+    pass
+
+
+#may need to change this to menu selection
+if __name__ == "__main__":
+    main()
