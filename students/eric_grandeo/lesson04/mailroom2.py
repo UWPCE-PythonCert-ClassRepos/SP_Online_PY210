025796cfebc8281cee7e30c4a2db14e4822cb80f
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-10-19 19:56:45 -0400

    organize the code a bit

--- students/eric_grandeo/lesson04/mailroom2.py
+++ students/eric_grandeo/lesson04/mailroom2.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 
-#donors as a dict
-donors = {"Bill Gates": [653772.32, 12.17],
-          "Jeff Bezos": [877.33],
-          "Paul Allen": [663.23, 43.87, 1.32],
-          "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
-          "Tim Cook": [1563.32, 8976.54]}
-
-main_prompt = "\n".join(("Welcome to the mailroom!",
-          "Please choose from below options:",
-          "1 - Send a thank you",
-          "2 - Create a report",
-          "3 - Quit",
-          ">>> "))
-
 
 #switch between users selections
 def menu_selection(prompt, dispatch_dict):
@@ -33,12 +19,26 @@ def quit_program():
     return "exit menu"
 
 
+
+#donors as a dict
+donors = {"Bill Gates": [653772.32, 12.17],
+          "Jeff Bezos": [877.33],
+          "Paul Allen": [663.23, 43.87, 1.32],
+          "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
+          "Tim Cook": [1563.32, 8976.54]}
+
+main_prompt = "\n".join(("Welcome to the mailroom!",
+          "Please choose from below options:",
+          "1 - Send a thank you",
+          "2 - Create a report",
+          "3 - Quit",
+          ">>> "))
+
 main_dispatch = {"1" : thank_you,
                 "2" : create_report,
                 "3" : quit_program,}
 
-
-
+sub_prompt = 
 
 
 #may need to change this to menu selection
