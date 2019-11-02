45794b18ee75c0dac77a7fc33f6303511dedff74
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2019-11-01 23:46:13 -0400

    Create trigrams dictionary

--- students/eric_grandeo/lesson04/trigrams.py
+++ students/eric_grandeo/lesson04/trigrams.py
@@ -2,7 +2,8 @@
 
 words = "I wish I may I wish I might".split()
 
-#print(words)
+print(words)
+print()
 
 def build_trigrams(words):
     """
@@ -12,12 +13,21 @@ def build_trigrams(words):
        keys: word pairs
        values: list of followers
     """
-    for word in range(len(words)-2):
-        pair = words[word:word+2]
-        print(pair)
 
-    trigrams = {}
     # build up the dict here!
+    trigrams = {}
+    for word in range(len(words)-2):
+        pair = tuple(words[word:word+2])
+        follower = words[word+2]
+        #print(pair,follower)
+
+        '''
+        if pair in trigrams:
+            trigrams[pair].append(follower)
+        else:
+            trigrams[pair] = [follower]
+        '''
+        trigrams.setdefault(pair,[]).append(follower)
 
     return trigrams
 
