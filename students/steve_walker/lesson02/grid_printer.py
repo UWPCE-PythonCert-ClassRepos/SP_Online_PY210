197a03505763b7d364283bdef37e45d043be3ff6
stevetwalker <29964517+stevetwalker@users.noreply.github.com>
2019-11-16 13:40:14 -0800

    Complete Grid Printer assignment

---
+++ students/steve_walker/lesson02/grid_printer.py
@@ -0,0 +1,29 @@
+def print_grid(x,y):
+	
+	# create the horizontal (cross) lines
+	segment = " -"
+	str = segment*x
+	section = "+"
+	
+	for i in range(x):
+		section =  section + str[i]
+	
+	cross = section*(y-1) + "+"
+
+	# create the vertical lines
+	vertrow = ("|"+" "*x)*(y-1) + "|"
+	vertline = ""
+	
+	for i in range(round(x/2)):
+		vertline = vertline + vertrow + "\n"
+	
+	vertline = vertline[:-1]
+	
+	# print the grid
+	for i in range(y-1):
+		print(cross)
+		print(vertline)
+	print(cross)
+
+print_grid(9,5)
+	
