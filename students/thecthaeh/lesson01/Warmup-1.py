c52fbb019019e0d83a1b5772494211534686c282
thecthaeh <vdesmor@uw.edu>
2019-12-03 10:35:49 -0500

    added part 1 of exercise 2.2

--- students/thecthaeh/lesson01/Warmup-1.py
+++ students/thecthaeh/lesson01/Warmup-1.py
@@ -1,10 +1,10 @@
-		
+        
 #diff21
 def diff21(n):
     if abs(n - 21) <= 21:
-	    return abs(n - 21)
-	else:
-	    return 2 * (abs(n - 21))
+        return abs(n - 21)
+    else:
+        return 2 * (abs(n - 21))
 
 
 #near_hundred
@@ -13,4 +13,24 @@ def near_hundred(n):
         return True
     else:
         return False
+        
+#monkey_trouble
+def monkey_trouble(a_smile, b_smile):
+    if a_smile == b_smile:
+        return True
+    else:
+        return False
+ 
+#parrot_trouble
+def parrot_trouble(talking, hour):
+    if talking == True:
+        if 7 > hour or hour > 20:
+            return True
+        else:
+            return False
+    else:
+        return False
 
+#pos_neg
+def pos_neg(a, b, negative):
+   
