74c2019b8019f3ad89ea8a8f8e4a30cf9420e1b9
Nick Miller <nichx@uw.edu>
2019-12-06 13:42:34 -0800

    more pushups

--- students/nick_miller/working_dir/lesson02/codingbat-pushups/last2.py
+++ students/nick_miller/working_dir/lesson02/codingbat-pushups/last2.py
@@ -2,4 +2,15 @@
 
 
 def last2(str):
-    
+    if len(str) < 2:
+        return 0
+
+    last2 = str[len(str) - 2:]
+    count = 0
+
+    for i in range(len(str) - 2):
+        sub = str[i:i + 2]
+        if sub == last2:
+            count = count + 1
+
+    return count
