236f169f1d096a62b89bdc73eab1e42d3681892b
Asemlef <asemlef@users.noreply.github.com>
2019-12-10 11:03:19 -0800

    Add lesson02 assignments for choltzman

---
+++ students/choltzman/lesson02/fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def main():
+    fizzbuzz()
+
+
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+
+
+if __name__ == "__main__":
+    main()
