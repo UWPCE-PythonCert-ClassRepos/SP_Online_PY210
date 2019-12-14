3fc9e88791e9e6f1cdccff5365a65e4e6aa32db5
N0vA4 <jimalbright4@yahoo.com>
2019-12-14 10:08:46 -0800

    Added 3 part of print_grid exercise to file.  I found this last one pretty challenging to get going.

--- students/N0vA/lesson_02/grid_printer.py
+++ students/N0vA/lesson_02/grid_printer.py
@@ -19,7 +19,8 @@ def part_1_grid():
     print_second()
     print_second()
     print_top()
-    
+
+# Run    
 part_1_grid()
 
 
@@ -49,5 +50,34 @@ def print_grid(n):
 
     print('+', top, '+', top, '+')
 
+# Run
 print_grid()
 
+
+# Part 3 - Make function to print grid with columns and rows args
+
+def print_grid_2(grid, units):
+
+# units - size of grid squares
+# grid - number of rows (and columns) of our grid
+    
+    tab = '- '
+    space = ' '
+    
+    # Make two types of rows for grid based on inputs
+    top_row = grid * ('+ ' + tab * units) + '+'
+    col_row = grid * ('| ' + space * units * 2) + '|'
+
+    # Print over rows/columns
+    for i in range(grid):
+        
+        print(top_row)
+
+        for i in range(units):
+            print(col_row)
+    
+    # Print last row
+    print(top_row)
+
+# Run
+print_grid_2(3,4)
