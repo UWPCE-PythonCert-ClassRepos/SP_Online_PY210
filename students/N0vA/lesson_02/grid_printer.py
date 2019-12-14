e9845cc518a444255d5c1a369410a951abf98ad4
N0vA4 <jimalbright4@yahoo.com>
2019-12-13 22:18:50 -0800

    Added part 1 solution for example grid.

---
+++ students/N0vA/lesson_02/grid_printer.py
@@ -0,0 +1,30 @@
+#### Grid Printer Exercises ####
+
+# Set global variables
+plus = "+ "
+minus = "- "
+t = "|"
+space = " "
+
+# Part 1
+def print_top():
+    print("+ - - - - + - - - - +")
+
+def print_second():
+    print("|         |         |")
+
+def part_1_grid():
+    print_top()
+    print_second()
+    print_second()
+    print_second()
+    print_second()
+    print_top()
+    print_second()
+    print_second()
+    print_second()
+    print_second()
+    print_top()
+    
+part_1_grid()
+
