25b57261a0f2d4feb2232feefe34262d22a60f11
N0vA4 <jimalbright4@yahoo.com>
2019-12-13 23:56:47 -0800

    Added part 2 of exercise.  Single parameter function to print grid.

--- students/N0vA/lesson_02/grid_printer.py
+++ students/N0vA/lesson_02/grid_printer.py
@@ -1,12 +1,6 @@
 #### Grid Printer Exercises ####
 
-# Set global variables
-plus = "+ "
-minus = "- "
-t = "|"
-space = " "
-
-# Part 1
+# Part 1 
 def print_top():
     print("+ - - - - + - - - - +")
 
@@ -27,4 +21,33 @@ def part_1_grid():
     print_top()
     
 part_1_grid()
+
+
+# Part 2 - Make a more general grid function with one input.
+
+def print_grid(n):
+
+    # Find units of grid
+    u = (n-1)//2
+
+    # Set up grid size
+    top = u * '-' 
+    space = u * ' ' 
+
+    # Print grid
+    # Top line
+    print('+', top, '+', top, '+')
+
+    for i in range(u):
+        print('|', space, '|', space, '|')
+
+    # Repeat for second half of grid
+    print('+', top, '+', top, '+')
+
+    for i in range(u):
+        print('|', space, '|', space, '|')
+
+    print('+', top, '+', top, '+')
+
+print_grid()
 
