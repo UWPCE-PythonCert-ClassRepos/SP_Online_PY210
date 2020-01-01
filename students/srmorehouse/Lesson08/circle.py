d8134e10199c2e3caee6b0f3a0a69f4f942c4a5c
S. Morehouse <srm@startmail.com>
2019-12-31 17:52:06 -0800

    Step 9

--- students/srmorehouse/Lesson08/circle.py
+++ students/srmorehouse/Lesson08/circle.py
@@ -15,27 +15,34 @@ class Circle (object):
         else:
             self.radius = float (radius)
 
+
     @property
     def diameter (self):
         return self.radius * 2
+
 
     @property
     def area (self):
         return (self.radius**2) * math.pi
 
+
     @diameter.setter
     def diameter (self, diameter):
         self.radius = diameter / 2
+
 
     @classmethod
     def using_diameter (cir, diameter):
         return cir (diameter / 2)
 
+
     def __str__ (self):
         return f'Circle with radius: {self.radius}'
 
+
     def __repr__ (self):
         return f'Circle({self.radius})'
+
 
     def __add__(self, other):
         if type (other) is int:
@@ -43,11 +50,14 @@ class Circle (object):
         else:
             return Circle(self.radius + other.radius)
 
+
     def __iadd__(self, other):
         return self.__add__(other)
 
+
     def __radd__(self, other):
         return self.__add__(other)
+
 
     def __sub__(self, other):
         if type (other) is int:
@@ -55,8 +65,10 @@ class Circle (object):
         else:
             return Circle(self.radius - other.radius)
 
+
     def __isub__(self, other):
         return self.__sub__(other)
+
 
     def __rsub__(self, other):
         if type (other) is int:
@@ -64,27 +76,34 @@ class Circle (object):
         else:
             return Circle(other.radius - self.radius)
 
+
     def __mul__(self, other):
         if type (other) is int:
             return Circle(self.radius * other)
         else:
             return Circle(self.radius * other.radius)
 
+
     def __imul__(self, other):
         return self.__mul__(other)
+
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
+
     def __lt__(self, other):
         return True if self.radius < other.radius else False
+
 
     def __gt__(self, other):
         return True if self.radius > other.radius else False
 
+
     def __eq__(self, other):
         return True if self.radius == other.radius else False
 
+
     def sort_key(self):
         return self.radius       
- 
+
--- students/srmorehouse/Lesson08/test_sphere.py
+++ students/srmorehouse/Lesson08/test_sphere.py
@@ -2,151 +2,156 @@
 
 import math
 import pytest
-from circle import Circle
+from sphere import Sphere
 
 def test_constructor():
-    c = Circle(1)
+    c = Sphere(1)
     assert c.radius == 1
 
 
 def test_alt_constructor():
-    c = Circle.using_diameter (2)
+    c = Sphere.using_diameter (2)
     assert c.radius == 1
     assert c.diameter == 2
 
 
 def test_unhappy_set_zero():
     with pytest.raises(ValueError) as e:
-        c = Circle(0)
+        c = Sphere(0)
 
 
 def test_unhappy_set_neg():
     with pytest.raises(ValueError) as e:
-        c = Circle(-1)
+        c = Sphere(-1)
 
 
 def test_set_diameter():
     radius = 1
-    c = Circle(radius)
+    c = Sphere(radius)
     c.diameter = radius * 2
     assert c.diameter == radius * 2
 
 
 def test_get_area():
-    c = Circle(1)
-    assert c.area == pytest.approx (3.14,0.1)
-
-    c = Circle(2)
+    c = Sphere(1)
     assert c.area == pytest.approx (12.56,0.1)
+
+    c = Sphere(2)
+    assert c.area == pytest.approx (50.26,0.1)
+
+
+def test_volume():
+    s = Sphere(1)
+    assert s.volume == pytest.approx (4.18,0.1)
 
 
 def test_unhappy_set_area():
-    c = Circle(1)
+    c = Sphere(1)
     with pytest.raises(AttributeError) as e:
         c.area = 1
 
 
 def test_str():
-    c = Circle(1)
-    assert str(c).startswith('Circle with radius:')
+    c = Sphere(1)
+    assert str(c).startswith('Sphere with radius:')
     assert '1.0' in str(c)
 
 
 def test_repr():
-    c = Circle(2)
+    c = Sphere(2)
     d = eval(repr(c))
     assert d.radius == 2
 
 
 def test_add():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c3 = c1 + c2
     assert c3.radius == 3
     assert c3 == c1 + c2
 
 
 def test_iadd():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c2 += c1
     assert c2.radius == 3
 
 
 def test_radd():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c3 = c1 + c2
     assert c3 == 1 + c2
 
 
 def test_sub():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c3 = c2 - c1
     assert c3.radius == 1
 
 
 def test_isub():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c2 -= c1
     assert c2.radius == 1
 
 
 def test_msub():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     c2 -= c1
     assert c1 == 2 - c2
 
 
 def test_mul():
-    c1 = Circle(3)
-    c2 = Circle(4)
+    c1 = Sphere(3)
+    c2 = Sphere(4)
     c3 = c1 * c2
     assert c3.radius == 12
     assert c3 == 3 * c2
 
 
 def test_imul():
-    c1 = Circle(3)
+    c1 = Sphere(3)
     c1 *= 2
     assert c1.radius == 6
 
 
 def test_rmul():
-    c1 = Circle(3)
+    c1 = Sphere(3)
     c1 *= 2
-    c2 = Circle(18)
+    c2 = Sphere(18)
     assert c2 == 3 * c1
 
 
 def test_lt():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     assert c1 < c2
 
 
 def test_gt():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     assert c2 > c1
 
 
 def test_eq():
-    c1 = Circle(1)
-    assert c1 == Circle(1)
+    c1 = Sphere(1)
+    assert c1 == Sphere(1)
 
 
 def test_neq():
-    c1 = Circle(1)
-    c2 = Circle(2)
+    c1 = Sphere(1)
+    c2 = Sphere(2)
     assert c1 != c2
 
 
 def test_sort():
-    l = [Circle(6), Circle(7), Circle(8), Circle(4), Circle(2), Circle(3), Circle(5), Circle(9), Circle(1)]
-    l.sort(key=Circle.sort_key)
-    assert l == [Circle(1), Circle(2), Circle(3), Circle(4), Circle(5), Circle(6), Circle(7), Circle(8), Circle(9)]
+    l = [Sphere(6), Sphere(7), Sphere(8), Sphere(4), Sphere(2), Sphere(3), Sphere(5), Sphere(9), Sphere(1)]
+    l.sort(key=Sphere.sort_key)
+    assert l == [Sphere(1), Sphere(2), Sphere(3), Sphere(4), Sphere(5), Sphere(6), Sphere(7), Sphere(8), Sphere(9)]
 
