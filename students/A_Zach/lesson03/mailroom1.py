b90e3ae0e6c8525889854208a366911cc50e8073
Andzak10 <andzak10@gmail.com>
2020-01-12 12:53:05 -0800

    updated mailroom1 and mailroom2 assignments

--- students/A_Zach/lesson03/mailroom1.py
+++ students/A_Zach/lesson03/mailroom1.py
@@ -1,42 +1,30 @@
 import sys
 
 #Functions
-#Add a donation to a person on the list
-def new_donation(inp,n,d):
-    response_donation = input("Please enter an amount to donate >")
-    quit_prog(response_donation)
-    
-    i = 0
-    
-    #iterate through the database until the requested name is found
-    while i < len(n):
-        #Add the donation to the name provided
-        if n[i] == inp.title():
-            d[i].append(float(response_donation))        
-            break
-        #Increase iteration if name has not been reached
-        i += 1
-    return
-
 #Show list of names
-def show_list(inp,n):
+def show_list(inp):
     #print names of donors
-    for i in n:
-        print(i)
+    for name in names:
+        print(name)
     return
     
 #Add a new name to the list
-def search_name(inp,n,d):
-    for i in n:
-        #if the name is in the list add a new donation. If not add the name and a new donation.
-        if inp.title() == i:
-            new_donation(inp,n,d)
-            #leave loop if name has been found and donation added
+def search_name(inp):
+    response_donation = input("Please enter an amount to donate >")
+    if response_donation.lower == 'quit':
+        return
+    i = 0
+    #iterate through the database until the requested name is found
+    while i < len(names):
+        #Add the donation to the name provided
+        if names[i] == inp.title():
+            donations[i].append(float(response_donation))        
             return
-    n.append(inp.title())
-    d.append([])
-    new_donation(inp,n,d)
-    return
+        #Increase iteration if name has not been reached
+        i += 1
+    names.append(inp.title())
+    donations.append([float(response_donation)])
+    return response_donation
 
 #make the program quit
 def quit_prog(p):
@@ -45,70 +33,51 @@ def quit_prog(p):
         sys.exit()
 
 #generate an email1
-def email(inp,n,d):
+def email(inp):
     #find the index of n in the database
-    i = n.index(inp.title())
-    #format email. n is name. format the donation amount (d[i])
+    i = names.index(inp.title())
+    #format email. format the donation amount (d[i])
     #into a floating point with 2 decimal places.
-    print(f"\n\nDear {inp},\n\n\tWe appreciate your generous donations totalling ${sum(d[i]):.2f}.\n\nThank you,\nAndrew\n\n")
+    print(f"\n\nDear {inp},\n\n\tWe appreciate your generous donations totalling ${sum(donations[i]):.2f}.\n\nThank you,\nAndrew\n\n")
     
    
     
-def send_thank_you(n,d):
+def send_thank_you():
     inp = input("Who whould you like to thank?>")
-    
-    #call quit_prog function to end program
-    quit_prog(inp)
-    
+    if inp.lower == 'quit':
+        return
     #show the list of names using show_list function
     while inp.title() == 'List':
-        show_list(inp,n)
+        show_list(inp)
         inp = input("Who whould you like to thank?>")
-        quit_prog(inp)
+        if inp.lower == 'quit':
+            return
     
     #Call the search_name function
-    search_name(inp,n,d)
+    donation = search_name(inp)
     
     #call the email function
-    email(inp,n,d)
-    return
+    email(inp)
 
-#sum up the total donations for each person    
-def tot_don(d):
+#creat report inputs    
+def report_results():
     tots = []
-    i = 0
-    while i < len(d):        
-        tots.append(sum(d[i]))
-        i += 1
-    return tots
-
-#average the donations for each person
-def ave_don(d):
     aves = []
-    i = 0
-    while i < len(d):        
-        aves.append(sum(d[i])/len(d[i]))
-        i += 1
-    return aves
-
-#return the total number of donations for each person
-def num_don(d):
     nums = []
-    i = 0
-    while i < len(d):        
-        nums.append(len(d[i]))
-        i += 1
-    return nums
+    for donation_list in donations:        
+        tots.append(sum(donation_list))
+        aves.append(sum(donation_list)/len(donation_list))
+        nums.append(len(donation_list))
+    return (tots, aves, nums)
+
 
 #Create a report
-def write_report(n,d):
-    tot = tot_don(d)
-    num = num_don(d)
-    ave = ave_don(d)
-    #sort by the totaled values
-    sort_by_tot = sorted(tot, reverse=True)
-    print(tot)
-    print(sort_by_tot)
+def write_report():
+    results = report_results()
+    tots = results[0]
+    aves = results[1]
+    nums = results[2]
+    sort_by_tot = sorted(tots, reverse=True)
     #create a variable as the length of each column other than  names. For ease of updating
     l = 15
     #same but for name column
@@ -119,16 +88,16 @@ def write_report(n,d):
     print(f"\n{Headers[0]:<{ln}}|{Headers[1]:^{l}}|{Headers[2]:^{l}}|{Headers[3]:^{l}}")
     print("- "*int((l*3+3+ln)/2))
     #index each entry of sort_by_tot in tot and use that index to generate report
-    for i in sort_by_tot:
-        ind = tot.index(i)
-        print(f"{n[ind]:<{ln}} ${tot[ind]:>{l-1}}{num[ind]:>{l+1}} ${ave[ind]:>{l-3}.2f}")
+    for entry in sort_by_tot:
+        ind = tots.index(entry)
+        print(f"{names[ind]:<{ln}} ${tots[ind]:>{l-1}}{nums[ind]:>{l+1}} ${aves[ind]:>{l-3}.2f}")
     print(f"\n")
     
 #Mailroom part 1
 #Generate a database of donors
 
 
-def main():
+if __name__ == '__main__':
     #Create a database
     Jen = ['Jenny Jones', [272.00, 8700.11, 4.22]]
     Ste = ['Steve Martin', [85000.00, 12345.67, 54321.99]]
@@ -136,26 +105,25 @@ def main():
     Aba = ['Aba Ababa', [101.01, 10101.01, 1100.10]]
     Wan = ['Wan Do Nation', [1.00]]   
     #Combine donors into a database
-    Database = [Jen, Ste, Che, Aba, Wan]
+    database = [Jen, Ste, Che, Aba, Wan]
     #Initialize a new list for just names and just donations
-    Names = []
-    Donations = []
+    names = []
+    donations = []
     #Fill the names and donations lists
-    for i,j in Database:
-       Names.append(i)
-       Donations.append(j)
+    for i,j in database:
+       names.append(i)
+       donations.append(j)
     #iterate until 1, 2, or quit is chosen. then call the appropriate function
     while True:
         Prompt = input("Choose:\n1) Send a Thank you\n2) Create a "
                        "Report\n3) Quit \nRespond with 1, 2 or 'quit'>")    
-        if Prompt is '1':
-            send_thank_you(Names,Donations)
-        elif Prompt is '2':
-            write_report(Names,Donations)
-        elif Prompt.lower() == 'quit':
-            quit_prog(Prompt)
+        if Prompt == '1':
+            send_thank_you()
+        elif Prompt == '2':
+            write_report()
         else:
+            quit_prog(Prompt)
             print(f"'{Prompt}' is an invalid entry")
             
         
-main()
+
--- students/A_Zach/lesson04/mailroom2.py
+++ students/A_Zach/lesson04/mailroom2.py
@@ -1,18 +1,21 @@
+#Mailroom part2 
+#update initial code to include dictionaries
+
 import sys
 
 #Functions
 
 #Show list of names
-def show_list(dict):
+def show_list():
     #print names of donors
-    names = list(dict)
+    names = list(donor_dict)
     name_string = "\n".join(names)
     print(f"{name_string}")
 
 #Add a new name to the list
-def search_name(name,dict):
-    Donation = input("Please enter an amount to donate >")
-    dict.setdefault(name,[]).append(float(Donation))
+def search_name(name):
+    donation = input("Please enter an amount to donate >")
+    donor_dict.setdefault(name.title,[]).append(float(donation))
     return
 
 #make the program quit
@@ -21,10 +24,10 @@ def quit_prog():
     sys.exit()
 
 #generate an email1
-def email(name,dict):
-    Email = f"\n\nDear {name},\n\n\tWe appreciate your generous donations totalling ${sum(dict[name]):.2f}.\n\nThank you,\nAndrew\n\n" 
+def email(name):
+    email = f"\n\nDear {name},\n\n\tWe appreciate your generous donations totalling ${sum(donor_dict[name]):.2f}.\n\nThank you,\nAndrew\n\n" 
     with open(f"Thank_You_to_{name}.txt", 'w+') as f:
-        f.write(Email)
+        f.write(email)
     return
 
 
@@ -35,55 +38,43 @@ def nothing():
     
 def send_thank_you():
     name = input("Who would you like to thank?>")
-    quit_request = ('q', 'quit')
-    #call quit_prog function to end program
-    if name.lower() in quit_request:
-        quit_prog()
+    if name.lower() == 'quit':
+        return
     
     #show the list of names using show_list function
     while name.title() == 'List':
-        show_list(Donor_dict)
+        show_list()
         name = input("Who whould you like to thank?>")
-        if name.lower() in quit_request:
-            quit_prog()
+        if name.lower() == 'quit':
+            return
     
     #Call the search_name function
-    search_name(name,Donor_dict)
+    search_name(name)
     
     #call the email function
-    Email = email(name,Donor_dict)
-    return
+    email = email(name)
 
 def Thank_you_All():
-    for name in Donor_dict:
-        email(name,Donor_dict)
+    for name in donor_dict:
+        email(name)
 
-#sum up the total donations for each person    
-def tot_don(dict):
+#create report inputs   
+def report_results():
     tots = []
-    for name in dict:
-        tots.append(sum(dict[name]))
-    return tots
-
-#average the donations for each person
-def ave_don(dict):
     aves = []
-    for name in dict:
-        aves.append(sum(dict[name])/len(dict[name]))
-    return aves
-
-#return the total number of donations for each person
-def num_don(dict):
     nums = []
-    for name in dict:
-        nums.append(len(dict[name]))
-    return nums
+    for donation_list in donations:        
+        tots.append(sum(donation_list))
+        aves.append(sum(donation_list)/len(donation_list))
+        nums.append(len(donation_list))
+    return (tots, aves, nums)
 
 #Create a report
 def write_report():
-    tot = tot_don(Donor_dict)
-    num = num_don(Donor_dict)
-    ave = ave_don(Donor_dict)
+    results = report_results()
+    tots = results[0]
+    aves = results[1]
+    nums = results[2]
     #sort by the totaled values
     sort_by_tot = sorted(tot, reverse=True)
     #create a variable as the length of each column other than  names. For ease of updating
@@ -101,10 +92,6 @@ def write_report():
         print(f"{list(Donor_dict.keys())[ind]:<{ln}} ${tot[ind]:>{l-1}}{num[ind]:>{l+1}} ${ave[ind]:>{l-3}.2f}")
     print(f"\n")
     
-#Mailroom part2 
-#update initial code to include dictionaries
-
-
 if __name__ ==  '__main__':
     #Create a database
     Jen = ['Jenny Jones', [272.00, 8700.11, 4.22]]
@@ -113,27 +100,25 @@ if __name__ ==  '__main__':
     Aba = ['Aba Ababa', [101.01, 10101.01, 1100.10]]
     Wan = ['Wan Do Nation', [1.00]]   
     #Combine donors into a database
-    Database = [Jen, Ste, Che, Aba, Wan]
+    database = [Jen, Ste, Che, Aba, Wan]
     #create a dictionary of donor information
-    Donor_dict = {}
-    for name in Database:
-        Donor_dict[name[0]] = name[1]
-    print(Donor_dict)    
+    donor_dict = {}
+    for name in database:
+        donor_dict[name[0]] = name[1]    
     #Create a dictionary to switch between input options
     switch_dict = {
         '1': send_thank_you,
         '2': write_report,
-        '3': Thank_you_All,
+        '3': thank_you_all,
         '4': quit_prog,
         'quit': quit_prog
         }
     
     while True:
-        Prompt = input("Choose:\n"
+        prompt = input("Choose:\n"
                        "1) Send a Thank you\n"
                        "2) Create a Report\n"
                        "3) Send letters to all donors\n"
                        "4) Quit \n"
                        "Respond with 1, 2, 3 or 'quit'>")                       
-        switch_dict.get(Prompt,"nothing")()
-       
+        switch_dict.get(prompt,"nothing")()
