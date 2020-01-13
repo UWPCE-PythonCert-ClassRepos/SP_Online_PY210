2f1db10dae8ec3a76db8d9535aeed8d142fb5c67
Anthony Ganyo <ganyoa@gmail.com>
2020-01-13 23:22:03 +0300

    completed task 6 in strformat_lab

--- students/ganyoa/lesson03/strformat_lab.py
+++ students/ganyoa/lesson03/strformat_lab.py
@@ -16,3 +16,21 @@ def formatter(in_tuple):
     return form_string.format(*in_tuple)
 
 print("\n" + "--- Task04 ---")
+taskfour = (4, 30, 2017, 2, 27)
+print(taskfour)
+print('{3:02d}, {4:02d}, {2:02d}, {0:02d}, {1:02d}'.format(*taskfour))
+
+print("\n" + "--- Task05 ---")
+t = ['oranges', 1.3, 'lemons', 1.1]
+print(f'The weight of an {t[0][:-1]} is {t[1]} and the weight of a {t[2][:-1]} is {t[3]}.')
+print(f'The weight of an {t[0][:-1].upper()} is {t[1]*1.2} and the weight of a {t[2][:-1].upper()} is {t[3]*1.2}.')
+
+print("\n" + "--- Task06 ---")
+print('{:<15}{:<10}{:<9}'.format('name', 'age', 'cost'))
+print('{:<15}{:<10}{:<7.2f}'.format('Sylvester', 49, 584.84))
+print('{:<15}{:<10}{:<7.2f}'.format('Forest', 6, 435537.9145))
+print('{:<15}{:<10}{:<7.2f}'.format('Park Bench', 34, 3814.11))
+
+print("\n" + "--- Task06_extra_task ---")
+tups = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(('{:<5}' * len(tups)).format(*tups))
