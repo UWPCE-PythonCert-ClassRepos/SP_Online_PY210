c7a9babd64fd109558ebdd97926e78acef419b92
Andzak10 <andzak10@gmail.com>
2020-01-14 22:04:14 -0800

    adding mailroom3 lesson05 with updates

--- students/A_Zach/lesson05/mailroom3.py
+++ students/A_Zach/lesson05/mailroom3.py
@@ -1,29 +1,14 @@
 import sys
-
+from operator import itemgetter
 #Functions
 
 #Show list of names
-def show_list(dict):
+def show_list():
     #print names of donors
-    names = list(dict)
+    names = list(donor_dict)
     name_string = "\n".join(names)
     print(f"{name_string}")
 
-#Add a new name to the list
-def search_name(name,dict):
-    Donation = input("Please enter an amount to donate >")
-    quit_request = ('q', 'quit')
-    while True:
-        #call quit_prog function to end program
-        if Donation.lower() in quit_request:
-            quit_prog()
-        try:
-            dict.setdefault(name,[]).append(float(Donation))
-        except ValueError:
-            Donation = input(f"\nInvalid response.\n\nEnter a number.\n\nPlease enter an amount to donate >")
-        else:
-            break
-    return
 
 #make the program quit
 def quit_prog():
@@ -31,64 +16,57 @@ def quit_prog():
     sys.exit()
 
 #generate an email1
-def email(name,dict):
-    Email = f"\n\nDear {name},\n\n\tWe appreciate your generous donations totalling ${sum(dict[name]):.2f}.\n\nThank you,\nAndrew\n\n" 
-    with open(f"Thank_You_to_{name}.txt", 'w+') as f:
-        f.write(Email)
-    return
+def write_email(name):
+    email = f"\n\nDear {name.title()},\n\n\tWe appreciate your generous donations totalling ${sum(donor_dict[name.title()]):.2f}.\n\nThank you,\nAndrew\n\n" 
+    with open(f"Thank_You_to_{name.title()}.txt", 'w+') as f:
+        f.write(email)
 
-
-def nothing():
-    print("Invalid response. Please use an appropriate response.")
-    return
-   
     
 def send_thank_you():
     name = input("Who would you like to thank?>")
-    quit_request = ('q', 'quit')
-    #call quit_prog function to end program
-    if name.lower() in quit_request:
-        quit_prog()
+    if name.lower() == 'quit':
+        return
     
     #show the list of names using show_list function
     while name.title() == 'List':
-        show_list(Donor_dict)
+        show_list()
         name = input("Who whould you like to thank?>")
-        if name.lower() in quit_request:
-            quit_prog()
+        if name.lower() == 'quit':
+            return
     
-    #Call the search_name function
-    search_name(name,Donor_dict)
-    
+    donation = input("Please enter an amount to donate >")
+    while True:
+        #call quit_prog function to end program
+        if donation.lower() == 'quit':
+            return
+        try:
+            donor_dict.setdefault(name.title(),[]).append(float(donation))
+        except ValueError:
+            donation = input(f"\nInvalid response.\n\nEnter a number.\n\nPlease enter an amount to donate >")
+        else:
+            break
     #call the email function
-    Email = email(name,Donor_dict)
+    write_email(name)
     return
 
-def Thank_you_All():
-    Email = [email(name, Donor_dict) for name in Donor_dict]
+def thank_you_all():
+    [write_email(name) for name in donor_dict]
 
-#sum up the total donations for each person    
-def tot_don(dict):
-    tots = [sum(dict[name]) for name in dict]
-    return tots
 
-#average the donations for each person
-def ave_don(dict):
-    aves = [sum(dict[name])/len(dict[name]) for name in dict]
-    return aves
-
-#return the total number of donations for each person
-def num_don(dict):
-    nums = [len(dict[name]) for name in dict]
-    return nums
-
+def report_data(name):
+    """function generates the number of donations, total amount, and average amount of each donor"""
+    names = name
+    tots = sum(donor_dict[name])
+    nums = len(donor_dict[name])
+    aves = tots/nums
+    return (name,tots,nums,aves)
+    
 #Create a report
 def write_report():
-    tot = tot_don(Donor_dict)
-    num = num_don(Donor_dict)
-    ave = ave_don(Donor_dict)
+    report = []
+    [report.append(report_data(name)) for name in donor_dict.keys()]
     #sort by the totaled values
-    sort_by_tot = sorted(tot, reverse=True)
+    sort_by_tot = sorted(report, key = itemgetter(1), reverse=True)
     #create a variable as the length of each column other than  names. For ease of updating
     l = 15
     #same but for name column
@@ -99,9 +77,8 @@ def write_report():
     print(f"\n{Headers[0]:<{ln}}|{Headers[1]:^{l}}|{Headers[2]:^{l}}|{Headers[3]:^{l}}")
     print("- "*int((l*3+3+ln)/2))
     #index each entry of sort_by_tot in tot and use that index to generate report
-    for i in sort_by_tot:
-        ind = tot.index(i)
-        print(f"{list(Donor_dict.keys())[ind]:<{ln}} ${tot[ind]:>{l-1}}{num[ind]:>{l+1}} ${ave[ind]:>{l-3}.2f}")
+    for entry in sort_by_tot:
+        print(f"{entry[0]:<{ln}} ${entry[1]:>{l-1}.2f}{entry[2]:>{l+1}} ${entry[3]:>{l-3}.2f}")
     print(f"\n")
     
 #Mailroom part3 
@@ -110,33 +87,32 @@ def write_report():
 
 if __name__ ==  '__main__':
     #Create a database
-    Jen = ['Jenny Jones', [272.00, 8700.11, 4.22]]
-    Ste = ['Steve Martin', [85000.00, 12345.67, 54321.99]]
-    Che = ['Cher', [0.01, 0.01]]
-    Aba = ['Aba Ababa', [101.01, 10101.01, 1100.10]]
-    Wan = ['Wan Do Nation', [1.00]]   
+    jen = ['Jenny Jones', [272.00, 8700.11, 4.22]]
+    ste = ['Steve Martin', [85000.00, 12345.67, 54321.99]]
+    che = ['Cher', [0.01, 0.01]]
+    aba = ['Aba Ababa', [101.01, 10101.01, 1100.10]]
+    juan = ['Juan Doe Nation', [1.00]]   
     #Combine donors into a database
-    Database = [Jen, Ste, Che, Aba, Wan]
+    database = [jen, ste, che, aba, juan]
     #create a dictionary of donor information
-    Donor_dict = {name[0]: name[1] for name in Database}
-    print(Donor_dict)    
+    donor_dict = {name[0]: name[1] for name in database}    
     #Create a dictionary to switch between input options
     switch_dict = {
         '1': send_thank_you,
         '2': write_report,
-        '3': Thank_you_All,
+        '3': thank_you_all,
         '4': quit_prog,
         'quit': quit_prog
         }
     
     while True:
         try:
-            Prompt = input("Choose:\n"
+            prompt = input("Choose:\n"
                            "1) Send a Thank you\n"
                            "2) Create a Report\n"
                            "3) Send letters to all donors\n"
                            "4) Quit \n"
                            "Respond with 1, 2, 3 or 'quit'>")                       
-            switch_dict.get(Prompt,"nothing")()
+            switch_dict.get(prompt)()
         except TypeError:
             print(f"\nInvalid response.\n\n")
