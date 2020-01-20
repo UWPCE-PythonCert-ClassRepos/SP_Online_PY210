243c38d0d0ce3d4d7ef1b3938f91d907c15d2a21
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-01-20 10:26:53 -0500

    setting things up...

---
+++ students/eric_grandeo/lesson09/cli_main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from mailroom_oo import *
+
+
+if __name__ == "__main__":
+    pass
+
--- students/eric_grandeo/lesson09/mailroom_oo.py
+++ students/eric_grandeo/lesson09/mailroom_oo.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 
+#class donor
+#class report(sub of donor?)
+#class thank_you(sub of donor?)
+
+#hold all information about a single donor, including thank you letter
+class Donor:
+    pass
+
+
+#hold all of the donor bjection method to add a new donor, search for a donor, 
+#save and reload data, generate reports
+class DonorCollection:
+    pass
+
+
