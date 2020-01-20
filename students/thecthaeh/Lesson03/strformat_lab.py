b52ec99f66af0360fb69f25644d0ade413b8208b
thecthaeh <vdesmor@uw.edu>
2020-01-19 19:02:02 -0500

    added docstrings to strformat_lab.py

--- students/thecthaeh/Lesson03/strformat_lab.py
+++ students/thecthaeh/Lesson03/strformat_lab.py
@@ -1,4 +1,6 @@
-# Task 1
+#!/usr/bin/env python3
+
+"""Task One: Use a given tuple to produce a string using string formatting operators."""
 a_tuple = (2, 123.4567, 10000, 12345.67)
 
 file_name = a_tuple[0]
@@ -8,10 +10,10 @@ ele4 = a_tuple[3]
 
 print(f"file_{file_name:03} :    {ele2:.2f}, {ele3:.2e}, {ele4:.3g}")
 
-# Task 2
+"""Task Two: Use the result from Task One to produce the same string using a different string format type (string.format() in this case)."""
 print("file_{:03} :    {:.2f}, {:.2e}, {:.3g}".format(file_name, ele2, ele3, ele4))
 
-# Task 3
+"""Task Three: Produce a string that takes an arbitrary number of values."""
 def formatter(in_tuple):
     tup_length = len(in_tuple)
 
@@ -19,7 +21,7 @@ def formatter(in_tuple):
 
     return build_string.format(tup_length, *in_tuple[:])
 
-# Task 4
+"""Task Four: Use a given tuple to produce a formatted string."""
 tup_5 = (4, 30, 2017, 2, 27)
 
 zero = tup_5[0]
@@ -30,7 +32,7 @@ four = tup_5[4]
 
 print(f"{three:02} {four} {two} {zero:02} {one}")
 
-# Task 5
+"""Task Five: Use a given list to produce specific f-strings."""
 list4 = ['oranges', 1.3, 'lemons', 1.1]
 
 fruit = list4[0]
@@ -40,9 +42,14 @@ weight2 = list4[3]
 
 print(f"The weight of an {fruit[:-1]} is {weight} and the weight of a {fruit2[:-1]} is {weight2}")
 
+# produce the same string as above with the weight values (indexes 1 and 3) 20% greater.
 print(f"The weight of an {fruit[:-1].upper()} is {weight * 1.2} and the weight of a {fruit2[:-1].upper()} is {weight2 * 1.2}")
 
-# Task 6
+"""Task Six: Use string formatting to display data in columns.
+
+Use alignment specifers to ensure the columns are properly aligned. Then,
+display a tuple of 10 consecutive numbers in columns of 5 characters wide.
+"""
 row1 = ['Larry', 23, 190.00]
 row2 = ['Sabine', 12, 4500.00]
 row3 = ['Katy', 89, 42.50]
