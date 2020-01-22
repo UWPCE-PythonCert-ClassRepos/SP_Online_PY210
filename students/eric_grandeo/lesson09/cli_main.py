e6dc14db01d0087571bb4a83055eb1ed52310797
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-01-21 22:55:52 -0500

    removing functions that are now in classes

--- students/eric_grandeo/lesson09/cli_main.py
+++ students/eric_grandeo/lesson09/cli_main.py
@@ -1,8 +1,99 @@
 #!/usr/bin/env python3
 
 from mailroom_oo import *
+import sys
+from collections import OrderedDict
+import os
+
+donors = {"Bill Gates": [653772.32, 12.17],
+          "Jeff Bezos": [877.33],
+          "Paul Allen": [663.23, 43.87, 1.32],
+          "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
+          "Tim Cook": [1563.32, 8976.54]}
+
+
+#switch between users selections, using a dict
+def menu_selection(prompt, dispatch_dict):
+    while True:
+        #handle KeyError
+        try:
+            response = input(prompt)
+            dispatch_dict[response](donors)
+        except KeyError:
+            print("Please select 1 through 4. \n")
+
+#this function is for the submenu
+def sub_menu_selection(prompt, dispatch_dict):
+    while True:
+        try:
+            response = input(sub_prompt).title()
+            #removes spaces and checks if there is a number
+            if not response.replace(" ", "").isalpha():
+                raise ValueError
+        except ValueError:
+            print("Please enter a name, list, or q to quit \n")
+        
+        else:
+            if response in dispatch_dict:
+                dispatch_dict[response](donors) == "exit menu"
+                break
+            else:
+                try:
+                    donation = input("Please enter in a donation, or 'q' to quit: ")
+                    if donation == "q":
+                        break
+                    elif donation.isalpha():
+                        raise ValueError
+                    else:
+                        add_donation(response, donation, donors)
+                        thankyou_email(response, donation)    
+                        break 
+                except ValueError:
+                    print("Please enter a number \n")        
+
+                
+
+#calls the sub menu function when thank you is selected from main menu
+def thank_you(donors):
+    sub_menu_selection(sub_prompt, sub_dispatch)
+
+
+
+def quit_submenu(donors):
+    return "exit menu"
+
+def quit_program(donors):
+    print("Good Bye")
+    sys.exit()
+
+
+
+main_prompt = "\n".join(("Welcome to the mailroom!",
+          "Please choose from below options:",
+          "1 - Send a thank you",
+          "2 - Create a report",
+          "3 - Send letters to all donors",
+          "4 - Quit",
+          ">>> "))
+
+#change quit program to just break the loop
+main_dispatch = {"1" : thank_you,
+                "2" : create_report,
+                "3" : send_letters,
+                "4": quit_program}
+
+sub_prompt = "\n".join(("Please enter one of the following",
+            "A full name",
+            "Type list to see all name",
+            "Enter 'q' to quit",
+            ">>> "))
+
+sub_dispatch = {"List" : display_donors,
+                "Q" : quit_submenu}
 
 
 if __name__ == "__main__":
-    pass
+    menu_selection(main_prompt, main_dispatch)
+
+
 
