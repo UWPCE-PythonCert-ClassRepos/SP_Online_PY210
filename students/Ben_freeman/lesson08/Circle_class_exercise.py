15902a2985d09c85b10b08fa2cb29d7c014d97c3
Benjamin Freeman <bfreema1@mail.sfsu.edu>
2020-01-23 21:00:08 -0800

    Still don't know what is going on with __str__, but the rest work fine.

--- students/Ben_freeman/lesson08/Circle_class_exercise.py
+++ students/Ben_freeman/lesson08/Circle_class_exercise.py
@@ -6,6 +6,39 @@ class Circle(object):
     def __init__(self,radius_value):
         self.radius = radius_value
 
+    def __repr__(self):
+        return f"Circle({self.radius})"
+
+    def __str__(self):
+        return f"Circle with radius: {self.radius}"
+
+    def __add__(self, other):
+        new_radius=self.radius + other.radius
+        return Circle(new_radius)
+
+    def __mul__(self, other):
+        new_radius=self.radius*other
+        return Circle(new_radius)
+
+    def __rmul__(self, other):
+        new_radius=self.radius*other
+        return Circle(new_radius)
+
+    def __eq__(self, other):
+        return self.radius==other.radius
+
+    def __le__(self, other):
+        return self.radius<=other.radius
+
+    def __ge__(self, other):
+        return self.radius>=other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
     @property
     def diameter(self):
         return 2*self.radius
@@ -26,9 +59,20 @@ class Circle(object):
     def from_diameter(cls,value):
         return Circle(value/2)
 
-    def __repr__(cls):
-        return f"Circle({cls.radius})"
 
-    def __str__(cls):
-        return f"Circle with radius: {cls.radius}"
 
+class Sphere(Circle):
+
+    def __repr__(self):
+        return f"Sphere({self.radius})"
+
+    def __str__(self):
+        return f"Sphere with radius: {self.radius}"
+
+    @property
+    def volume(self):
+        return 4/3 * self.radius ** 3 * math.pi
+
+    @property
+    def area(self):
+        return 4 * self.radius**2*math.pi
