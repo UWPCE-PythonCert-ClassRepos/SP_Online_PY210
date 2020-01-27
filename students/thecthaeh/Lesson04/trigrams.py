c8ca9f3bf0da1a7354bf90c90d4f84e302c98ca8
thecthaeh <vdesmor@uw.edu>
2020-01-26 22:22:46 -0500

    added and tested the text generating function to trigrams.py

--- students/thecthaeh/Lesson04/trigrams.py
+++ students/thecthaeh/Lesson04/trigrams.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python3
+import random
 
 """Produce a trigram using The Adventures of Sherlock Holmes.
 
 """
 #testing the script using a small string to get the code correct
 words = "I wish I may I wish I might".split()
-more_words = "I am trying to come up with a longer string than words".split()
-
-"""more_words should look like:
-
-"I am" = 'trying'
-'am trying' = 'to'
-'trying to' = 'come'
-'to come' = 'up'
-'come up' = 'with'
-'up with' = 'a'
-'with a' = 'longer'
-a longer = string
-longer string = than
-string than = words
-"""
-
-"""#create pairs of 2 adjacents words (will be tuples and keys) that return the next adjacent word/s (will be lists and values)
-"I wish" = ["I", "I"]
-"wish I" = ["may", "might"]
-"I may" = ["I"]
-"may I" = ["wish"]"""
 
 """example of how the dict should look:
 
@@ -34,32 +14,51 @@ trigrams =
 ("wish I"): ["may", "might"],
 ("I may"): ["I"],
 ("may I"): ["wish"],}
-"""
 
-def build_trigrams(more_words):
+Examples of generated text to expect if using words as the original text:
+[I wish I may I wish I might]
+[I wish I might]
+[I wish I may I wish I may I wish I might] #and so on...
+"""
+trigrams = {}
+
+def build_trigrams(a_seq):
     """Build up the trigrams dict from the list of words above.
     
     returns a dict of:
         keys: adjacent word pairs
         values: the word that immediately follows the last word of the key
     """
-    trigrams = {}
-    
-    #build the dict
-    for x in range(len(more_words) - 2):
-        pair = tuple(more_words[x:x + 2]) # converts to tuple; will be the keys in trigrams
-        follower = more_words[x + 2] # will be the values in trigrams
+    for x in range(len(a_seq) - 2):
+        pair = tuple(a_seq[x:x + 2]) # will be the keys in trigrams. try frozen sets instead of tuples
+        follower = a_seq[x + 2] # will be the values in trigrams
 
-        #assigns pair and follower to trigrams as keys and values
-        #if the pair is already in trigrams, follower is appended to that pair instead
-        #update this to use defaultdict(list) instead
+        #try to update this to use defaultdict(list) instead
         if pair in trigrams:
             trigrams[pair].append(follower)
         else:
             trigrams[pair] = [follower]
-    
+
     return trigrams
 
+def generator():
+    #TESTING: update first word pair in new_text to be determined randomly & test with longer string
+    new_text = ["I", "wish"]
+
+    while tuple(new_text[-2:]) in trigrams:
+        new_text.append(random.choice(trigrams[tuple(new_text[-2:])]))
+    
+    return new_text
+
+def main():
+
+
 if __name__ == "__main__":
-    trigrams = build_trigrams(more_words)
+    trigrams = build_trigrams(words)
     print(trigrams)
+    
+    #test that a longer string works
+    more_words = "I am trying to come up with a longer string than words".split()
+
+    assert build_trigrams(more_words)
+    print("test passed")
