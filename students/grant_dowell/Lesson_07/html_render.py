baba8c6ebc68d535981ddeff90ebf365fe97187e
Grant Dowell <grant.r.dowell@gmail.com>
2020-02-09 17:33:46 -0800

    Update to use the class convention on giving the render method d and optional indent

--- students/grant_dowell/Lesson_07/html_render.py
+++ students/grant_dowell/Lesson_07/html_render.py
@@ -10,7 +10,7 @@ class Element(object):
     
     tag = "html"
     tag_attributes = dict()
-    indent = 0
+    indent = '    '
 
     def __init__(self, content=None, **kwargs):
         if content is None:
@@ -23,38 +23,39 @@ class Element(object):
     def append(self, new_content):
         self.contents.append(new_content)
 
-    def render(self, out_file):
-        out_file.write(self._open_tag())
+    def render(self, out_file, curr_ind=""):
+        out_file.write(self._open_tag(curr_ind))
         out_file.write("\n")
         for content in self.contents:
             if isinstance(content,str):
-                out_file.write((self.indent + 1) * '    ') #Add indent to content
+                out_file.write(curr_ind+self.indent)
+#                out_file.write((self.indent + 1) * '    ') #Add indent to content
                 out_file.write(content)
                 out_file.write("\n")
             else:
-                content.indent = self.indent + 1 #Set next indent level
-                content.render(out_file)
-        out_file.write(self._close_tag())
+#                content.indent = self.indent + 1 #Set next indent level
+                content.render(out_file, curr_ind+self.indent)
+        out_file.write(self._close_tag(curr_ind))
 
-    def _open_tag(self):
-        open_tag = [self.indent * '    ']
+    def _open_tag(self, curr_ind=""):
+        open_tag = [curr_ind]
         open_tag.append("<{}".format(self.tag))
         for key, value in self.tag_attributes.items():
             open_tag.append(' {}="{}"'.format(key, value))
         open_tag.append(">")
         return "".join(open_tag)
     
-    def _close_tag(self):
-        close_tag = [self.indent * '    ']
+    def _close_tag(self, curr_ind=""):
+        close_tag = [curr_ind]
         close_tag.append("</{}>\n".format(self.tag))
         return "".join(close_tag)
         
 class Html(Element):
     tag = "html"
     
-    def render(self, out_file):
-        out_file.write('<!DOCTYPE html>\n')
-        super().render(out_file)
+    def render(self, out_file, curr_ind=""):
+        out_file.write(curr_ind + '<!DOCTYPE html>\n')
+        super().render(out_file, curr_ind)
 
 class Body(Element):
     tag = "body"
@@ -70,21 +71,21 @@ class OneLineTag(Element):
     def append(self, new_content):
         raise NotImplementedError
         
-    def render(self, out_file):
-        out_file.write(self._open_tag())
+    def render(self, out_file, curr_ind=""):
+        out_file.write(self._open_tag(curr_ind))
         for content in self.contents:
             if isinstance(content,str):
                 out_file.write(content)
             else:
                 content.render(out_file)
-        out_file.write(self._close_tag().lstrip()) #pull out the indents
+        out_file.write(self._close_tag()) #Don't give the indents
         
 class Title(OneLineTag):
     tag = "title"
     
 class SelfClosingTag(Element):
-    def render(self, out_file):
-        out_file.write(self._open_tag()[:-1] + " />\n")
+    def render(self, out_file, curr_ind=""):
+        out_file.write(self._open_tag(curr_ind)[:-1] + " />\n")
 
 class Hr(SelfClosingTag):
     tag = 'hr'
