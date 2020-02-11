15d4f7b7ff251a1d928300ae6dd96d2acc7f235e
Level11Jack@bitbucket.org <jackanderson@level11.com>
2020-02-11 15:41:47 -0800

    Added more functions for the list_lab.py file

--- students/jack_anderson/lesson03/list_lab.py
+++ students/jack_anderson/lesson03/list_lab.py
@@ -53,6 +53,7 @@ def add_more_fruit(fruit_list):
     sort_fruit(fruit_list)
 
 
+
 def sort_fruit(fruit_list):
     ask = input("Please enter a single letter from A - Z: ")
     if ask.isalpha():
@@ -70,16 +71,53 @@ def sort_fruit(fruit_list):
 
             if sorted_fruit_list is not None:
                 print_item(sorted_fruit_list)
+                series_2_start(fruit_list)
             else:
                 print_item("There are no fruits that start with " + q + " or " + z)
+                series_2_start(fruit_list)
     else:
         sort_fruit(fruit_list)
 
 
 
 
+def series_2_start(fruit_list):
+    print("Starting series 2.....")
+    print(fruit_list)
+    fruit_list.pop()
+    print_item(fruit_list)
+    ask = input("Please enter a fruit to remove from this list: ")
+    fruit_list.remove("{}".format(ask))
+    print_item(fruit_list)
+    series_3_start(fruit_list)
+
+
+def series_3_start(fruit_list):
+    items_to_delete = []
+
+    for item in fruit_list:
+
+        ask = input("Do you like {}?   ".format(item.lower()))
+
+        check_ask = ("no" in ask) or ("yes" in ask)
+
+        while check_ask is False:
+            print("Please enter 'yes' or 'no' ")
+            ask = input("Do you like {}?   ".format(item.lower()))
+            check_ask = ('no' in ask) or ('yes' in ask)
+
+        if ask == 'no':
+            items_to_delete.append(item)
+
+    for items in items_to_delete:
+        fruit_list.remove(items)
+
+    print_item(fruit_list)
+
+
+
+
 def print_item(x):
-    print()
     print(x)
     print()
 
