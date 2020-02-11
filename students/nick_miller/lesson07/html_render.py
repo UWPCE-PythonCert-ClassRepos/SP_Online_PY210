c7cd0f0cd7367adc6048a042c3d6826728187e85
Nick Miller <nick.a.miller@oracle.com>
2020-02-10 17:42:49 -0800

    Sync'ing work done on html_render.py and test_html_render.py - neither of those two are done yet

--- students/nick_miller/lesson07/html_render.py
+++ students/nick_miller/lesson07/html_render.py
@@ -8,11 +8,57 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
-    def __init__(self, content=None):
-        pass
+    tag = "html"
+    indent = "   "
+
+    def __init__(self, content=None, **kwargs):
+        if content is None:
+            self.content = []
+        else:
+            self.content = [content]
+        self.kwargs = kwargs
 
     def append(self, new_content):
-        pass
+        self.content.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        # loop through the list of contents:
+        for content in self.content:
+            out_file.write("<{}>\n".format(self.tag))
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write("\n")
+            out_file.write("</{}>\n".format(self.tag))
+
+
+class Html(Element):
+    tag = 'html'
+
+
+class Body(Element):
+    tag = 'body'
+
+
+class P(Element):
+    tag = 'p'
+
+
+class Head(Element):
+    tag = 'head'
+
+
+class OneLineTag(Element):
+
+    def render(self, out_file):
+        out_file.write("<{}>".format(self.tag))
+        out_file.write(self.content[0])
+        out_file.write("</{}>\n".format(self.tag))
+
+    def append(self, content):
+        raise NotImplementedError
+
+
+class Title(OneLineTag):
+    tag = 'title'
