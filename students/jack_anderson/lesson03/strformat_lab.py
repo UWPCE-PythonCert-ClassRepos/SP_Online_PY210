5bcfd0d2e6957afd809107ebe56038f07b634109
Level11Jack@bitbucket.org <jackanderson@level11.com>
2020-02-13 10:55:02 -0800

    Added documentation and tests for strformat.py and some cleanup

--- students/jack_anderson/lesson03/strformat_lab.py
+++ students/jack_anderson/lesson03/strformat_lab.py
@@ -1,50 +1,31 @@
 #!/usr/bin/env python3
 
-
-"""
-Task One
-Write a format string that will take the following four element tuple: ( 2, 123.4567, 10000, 12345.67)
-and produce: 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-
-Task Two
-Using your results from Task One, repeat the exercise, but this time using an alternate type of format string
-(Hint: Think about alternative ways to use '.format(), and also consider f-strings if you've not used them already
-
-Task Three
-Rewrite "the 3 numbers are: {:d}, {:d}, {:d}".format(1,2,3)" to take an arbitrary number of values
-
-Task Four
-Given a 5 element tuple of ( 4, 30, 2017, 2, 27), use string formatting to print '02 27 2017 04 30'
-
-Task Five
-Given the following four elemental list ['oranges', 1.3, 'lemons', 1.1] , write an f-string that will display:
-The weight of an orange is 1.3 and the weight of a lemon is 1.1
-Then, change the f-string so that it displays the names of the fruit in upper case,
-and the weight 20% higher (that is 1.2 times higher).
-
-Task Six
-Write some Python code to print a table of several rows, each with a name, an age and a cost.
-Make sure some of the costs are in the hundreds and thousands to test your alignment specifiers.
-For an extra task, given a tuple with 10 consecutive numbers, can you work how to quickly print the tuple
-in columns that are 5 charaters wide? It can be done on one short line!
-"""
-
-turp = ( 2, 123.4567, 10000, 12345.67)
-turp2 = ( 1, 2, 3, 4, 5, 6, 7)
-turp3 = ( 4, 30, 2017, 2, 27)
-t3_result = '02 27 2017 04 30'
-turp_result = 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+# Test Variables
+turp1 = ( 2, 123.4567, 10000, 12345.67)
+turp3 = ( 1, 2, 3, 4, 5, 6, 7)
+turp4 = ( 4, 30, 2017, 2, 27)
 list_5 = ['oranges', 1.3, 'lemons', 1.1]
+
 
 
 # Task One
 def task_one(x):
+    """
+    Action to take a 4 element tuple and format using f-string
+    :param x: 4 element tuple
+    :return: formats as 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+    """
     converted = f"file_{(x[0]):03d} :{(x[1]):9.2f}, {(x[2]):2.2e}, {(x[3]):2.2e}"
     return(converted)
 
 
 # Task Two
 def task_two(x):
+    """
+    Action to take a 4 element tuple and format using format()
+    :param x: 4 element tuple
+    :return: formats as 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+    """
     fname = 'file_{:03d} :'.format(x[0])
     float_num1 = '{:9.2f},'.format(x[1])
     int_num1 = ' {:2.2e},'.format(x[2])
@@ -54,6 +35,11 @@ def task_two(x):
 
 
 def task_three(x):
+    """
+    Action to take an arbitrary number of values and display the values as well as the total count of values
+    :param x: Tuple or list containing n number of values
+    :return: values and the total count of values passed
+    """
     count = len(x)
 
     if count > 1:
@@ -69,6 +55,11 @@ def task_three(x):
 
 
 def task_four(x):
+    """
+    Action to take a 5 element tuple and format it to display in a date-time format
+    :param x: tuple containing 5 elements
+    :return: month / day / year / hour / minute
+    """
     month = x[3]
     day = x[4]
     year = x[2]
@@ -82,6 +73,11 @@ def task_four(x):
 
 
 def task_five(x):
+    """
+    Action to format a 4 element list, print the 1 & 3 elements in Uppercase and multiply the 2 & 4th elements by 1.2
+    :param x: List containing 4 elements
+    :return: Message to user containing the name and weight of fruit in the provided list
+    """
     orange = (x[0])[:-1]
     lemon = (x[2])[:-1]
     o_weight = x[1]
@@ -93,6 +89,10 @@ def task_five(x):
 
 
 def task_six():
+    """
+    Action to print a table of several rows, each with a name, an age and a cost. All items in rows are aligned
+    :return: 4 rows with columns showing Name, Age, and Cost
+    """
     x = ('NAME', 'AGE', 'COST')
     a = ('Jack', 42, '$100,000' )
     b = ('Stephanie', 9, '$1,200.97')
@@ -112,9 +112,43 @@ def task_six():
 
 
 
+######################################################################################
+#   TESTING - uncomment the below code to run a test of the functions in this file   #
+######################################################################################
 
-def test():
-    return(task_six())
+
+# if __name__ == "__main__":
+#     # Test tasks 1 - 5 and then run task 6
+#     # Task One Test
+#     assert task_one(turp1) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+#     print("Task 1 test passed")
+#
+#
+#     # Task Two test
+#     assert task_two(turp1) == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
+#     print("Task 2 test passed")
+#
+#
+#     # Task Three test
+#     assert task_three(turp3) == 'the 7 numbers are: 1, 2, 3, 4, 5, 6, 7'
+#     print("Task 3 test passed")
+#     print()
+#
+#
+#     # Task Four test
+#     print("Print task 4: ")
+#     task_four(turp4)
+#     print()
+#
+#     # Task Five Test
+#     print("Print task 5: ")
+#     task_five(list_5)
+#     print()
+#
+#     # Task Six test
+#     print("Print task 6: ")
+#     task_six()
+#     print()
 
 
-test()
+
