75715157451a0c5e3b3b5ef9916f0b838830f768
stellie82 <kims82@gmail.com>
2020-02-19 00:14:14 -0800

    add fizz buzz exercise to lesson folder

---
+++ students/stellie/lesson02/fizz_buzz.py
@@ -0,0 +1,24 @@
+"""
+Write a program that prints the numbers from 1 to 100 inclusive.
+But for multiples of three print “Fizz” instead of the number.
+For the multiples of five print “Buzz” instead of the number.
+For numbers which are multiples of both three and five print “FizzBuzz”
+instead.
+"""
+
+
+def fizz_buzz(x):
+    for i in range(x):
+        if i == 0:
+            pass
+        elif i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
+fizz_buzz(101)
