c4f29b667435f3be9ae6ded74d9dfd8b0efc7ca2
Clifford Butler <cliff002@msn.com>
2020-02-20 17:40:47 -0800

    added the second part to the excercise

--- students/clifford_butler/lesson02/grid_printer.py
+++ students/clifford_butler/lesson02/grid_printer.py
@@ -5,3 +5,14 @@ def print_grid(n):
         print(("+ " + "- " ) * (n - 1) + "+")
         
 print_grid(15)
+
+# grid_printer exercise part 2
+
+def print_grid(n):
+    for i in range(n):
+        print(("+ " + "- " ) * (n - 1) + "+")
+        for j in range(n):
+            print(("| " + "  ") * (n - 1) + "|") 
+    print(("+ " + "- ") * (n - 1) + "+")    
+        
+print_grid(8)
