a514baa906b463b3b8e8a8bcff8fd4cc4a7503c3
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-02-21 15:14:18 -0800

    comprehensions, better except hands, pep8

--- students/scotchwsplenda/Lesson_5/MailRoom_3.py
+++ students/scotchwsplenda/Lesson_5/MailRoom_3.py
@@ -9,15 +9,14 @@ donators = {"Gordian": [30.0, 45.0], "Tiberius": [60.0],
             "Commodus": [43.0, 11.0]}
 
 
-# Opening menu
-def main():
+def mainy():
     print('''
-      Welcome to the MAILROOM main menu
-      Please choose from below options:
-      1 - Initiate Grovel Sequence
-      2 - Data Metrics 3000
-      3 - Quit
-      4 - Send thank you notes to all donors
+  Welcome to the MAILROOM main menu
+  Please choose from below options:
+  1 - Initiate Grovel Sequence
+  2 - Data Metrics 3000
+  3 - Quit
+  4 - Send thank you notes to all donors
               ''')
     while True:
         try:
@@ -26,80 +25,87 @@ def main():
                         3: exit_program, 4: mass_mail}
             swit_dic[choice]()
             break
+        except KeyError:
+            print('You have entered a non-choice'
+                  ' , please get your shit together')
         except ValueError:
-            print('You have entered a non-number where a number'
-                  ' clearly goes, please get your shit together')
+            print('You have entered a non-number'
+                  ' , please get your shit together')
             continue
 
 
 # Option 1: Content of Thank you note
 def send_note():
-    done = False
-    while not done:
-        respondy = input(
-            "\n"+"Please select from the below Thank You Note options:"
-            "\n"+"1 - Print list of extant donors"
-            "\n"+"2 - Enter donor name and donation"
-            "\n"+"3 - Return to the MAILROOM main menu"
-            "\n"+"Indicate your choice:  ")
-        if respondy == "1":
-            for k in donators:
-                print(k)
-        if respondy == "2":
-            donor_inp = input("Input donor: ")
-            if donor_inp in donators.keys():
-                while True:
-                    try:
-                        don_amount = input("That's a known donor,"
-                                           " input donation amount: ")
-                        don_amount = float(don_amount)
-                        donators[donor_inp].append(don_amount)
-                        send_thanks(donor_inp, don_amount)
-                        break
-                    except ValueError:
-                        print('You have entered a non-number where a number'
-                              ' clearly goes, please get your shit together')
-                        continue
-            else:
-                while True:
-                    try:
-                        new_don = input(
-                            f"{donor_inp}'s an unknown donor, input donation"
-                            " amount: ")
-                        new_don = float(new_don)
-                        donators.update({donor_inp: [new_don]})
-                        send_thanks(donor_inp, new_don)
-                        break
-                    except ValueError:
-                        print('You have entered a non-number where a number'
-                              ' clearly goes, please get your shit together')
-                        continue
-
-        elif respondy == "3":
-            main()
+    print('''
+Please select from the below Thank You Note options:
+1 - Print list of extant donors
+2 - Enter donor name and donation
+3 - Return to the MAILROOM main menu
+    ''')
+    while True:
+        try:
+            respondy = int(input("Indicate your choice:"))
+            if respondy == 1:
+                for k in donators:
+                    print(k)
+                continue
+            if respondy == 2:
+                donor_inp = input("Input donor: ")
+                if donor_inp in donators.keys():
+                    don_amount = input("That's a known donor,"
+                                       " input donation amount: ")
+                    don_amount = float(don_amount)
+                    donators[donor_inp].append(don_amount)
+                    send_thanks(donor_inp, don_amount)
+                else:
+                    new_don = input(
+                        f"{donor_inp}'s an unknown donor, input donation"
+                        " amount: ")
+                    new_don = float(new_don)
+                    donators.update({donor_inp: [new_don]})
+                    send_thanks(donor_inp, new_don)
+            if respondy == 3:
+                mainy()
+            if respondy > 3:
+                print('You have entered a non-choice'
+                      ' , please get your shit together')
+                continue
+            if respondy < 1:
+                print('You have entered a non-choice'
+                      ' , please get your shit together')
+                continue
+            break
+        except ValueError:
+            print('You have entered a non-number'
+                  ' , please get your shit together')
+            continue
+# break - exits program entirely
+# continue - stays in send_note() [have to quit terminal]
+# exit() - exits program entirely
+# False - stays in send_note() [have to quit terminal]
+# done - stays in send_note() [have to quit terminal]
+# return - exits program entirely
+# True - stays in send_note() [have to quit terminal]
 
 
 # Option 1: sub-function
 def send_thanks(a, b):
     print("\n"f'Wow {a}, only ${b}?'"\n" +
           'Give til it hurts you capitalist swine')
+    send_note()
 
 
 # Option 2: create report
 def data_metrics():
-    report = []
-    for k, v in donators.items():
-        total_Given = sum(v)
-        number_Gifts = len(v)
-        average_Gift = sum(v)/len(v)
-        report.append((k, total_Given, number_Gifts, round(average_Gift, 1)))
-    ranked_dons = sorted(report, key=itemgetter(1), reverse=True)
-    # https://stackoverflow.com/questions/46490541/how-print-list-of-tuples-side-by-side/46490575
-    # for x in ranked_dons:
+    total_giv = [(name, sum(donat), len(donat),
+                 round((sum(donat)/len(donat)), 1))
+                 for (name, donat) in donators.items()]
+    ranked_d = sorted(total_giv, key=itemgetter(1), reverse=True)
     print('Name'+'-'*30+'Sum'+'-'*28+'Count'+'-'*30+'Avg')
-    for a, b, c, d in ranked_dons:
+    for a, b, c, d in ranked_d:
         print(f'{a:<33}{b:<33}{c:<33}{d:<33}')
-    main()
+    mainy()
+# https://www.youtube.com/watch?v=AhSvKGTh28Q
 
 
 # Option 3: get out of this program
@@ -115,10 +121,10 @@ def mass_mail():
             sumy = str(sum(value))
             f.write(f'Thanks {key} for donating ${sumy}.'
                     + "\n"+'Your mother would be so proud.')
-    main()
+    mainy()
 
 
 if __name__ == "__main__":
     # don't forget this block to guard against your code running automatically
     # if this module is imported
-    main()
+    mainy()
