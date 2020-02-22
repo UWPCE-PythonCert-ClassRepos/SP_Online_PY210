38d8caa3be79f5d5e41010f4cac890aeddb4f53a
Clifford Butler <cliff002@msn.com>
2020-02-22 10:26:43 -0800

    Function for removing the first 4 and last 4 created.

--- students/clifford_butler/lesson03/slicing_lab.py
+++ students/clifford_butler/lesson03/slicing_lab.py
@@ -35,13 +35,25 @@ def every_other(seq):
     """
     return seq[::2]
 
+def remove_last4_first4_every_other(seq):
+    """
+    Takes a sequence as an argument and returns a copy of the sequence,
+    with the first 4 and the last 4 items removed, 
+    and then every other item in the remaining sequence.
+    """
+    return seq[4:-4:2]
+
 if __name__ == "__main__":
-    # run some tests for exchanging the first and last items
+    # run some tests for exchange_fist_last
     assert exchange_first_last(a_string) == "ghis is a strint"
     assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
     
-    # run some tests for exchanging the first and last items
+    # run some tests for every_otherseq()
     assert every_other(a_string) == "ti sasrn"
     assert every_other(a_tuple) == (2, 13, 5)
+    
+    # run some tests for remove_last4_first4_every_other()
+    assert remove_last4_first4_every_other(a_string) == " sas"
+    assert remove_last4_first4_every_other(a_tuple) == ()
     
     print("tests passed")
