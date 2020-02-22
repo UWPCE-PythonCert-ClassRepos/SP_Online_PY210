9ae55795dfa7d27da05a0cd350a5d94cc5fd9aa4
Clifford Butler <cliff002@msn.com>
2020-02-22 07:21:06 -0800

    made changes to the fibonacci function

--- students/clifford_butler/lesson02/series.py
+++ students/clifford_butler/lesson02/series.py
@@ -1,15 +1,36 @@
-def fibonacci(n):
-    """Computes the fibonacci series starting with zero"""
-    nth = 0
-    b = 1
-    for i in range(0, n):
-        print(nth)
-        
-        a = nth
-        nth = b
-        b = a + b
-    return nth
-        
-fibonacci(25)
+#!/usr/bin/env python3
 
+"""
+Fibonacci Series Exercise 
+Computing the Fibonacci and Lucas Series
+Author: Clifford Butler
+"""
+
+def fibonacci(n):
+    """compute the nth Fibonacci number"""
+    if (n < 0):
+        return None
+    
+    elif (n == 0):
+        return 0
+    
+    elif (n == 1):
+        return 1
+    
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+if __name__ == "__main__":
+    # run some tests for fibonacci(n)
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+
+
+    print("tests passed")
 
