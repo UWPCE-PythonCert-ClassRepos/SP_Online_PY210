746336c0080f737617f2a1af9f9388a39143f570
Clifford Butler <cliff002@msn.com>
2020-02-22 10:13:20 -0800

    Task 1 of the slicing lab added. The function exchanges the first and last items

---
+++ students/clifford_butler/lesson03/slicing_lab.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Slicing Lab
+
+TASKS
+Write some functions that take a sequence as an argument, and return a copy of that sequence:
+    
+    -with the first and last items exchanged.
+    -with every other item removed.
+    -with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
+    -with the elements reversed (just with slicing).
+    
+    -with the last third, then first third, then the middle third in the new order.
+Author: Clifford Butler
+"""
+
+#sequences as an argument utilized for this module. 
+a_string = "this is a string"
+a_tuple = (2, 54, 13, 12, 5, 32)
+
+def exchange_first_last(seq):
+    """ 
+    Takes a sequence as an argument and returns a copy of the sequence,
+    with the first and last items exchanged
+    """
+    first_part = seq[:1]
+    last_part = seq[-1:]
+    return last_part + seq[1:-1] + first_part
+
+if __name__ == "__main__":
+    # run some tests for exchanging the first and last items
+    assert exchange_first_last(a_string) == "ghis is a strint"
+    assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+    
+    print("tests passed")
