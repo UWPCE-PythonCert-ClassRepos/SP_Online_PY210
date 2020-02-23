031168e91d77e4886a90771b501ec4b86061f3e0
Clifford Butler <cliff002@msn.com>
2020-02-22 17:36:30 -0800

    The for loop now prints the index number with each item in the list.

--- students/clifford_butler/lesson03/list_lab.py
+++ students/clifford_butler/lesson03/list_lab.py
@@ -6,8 +6,8 @@ Goal - Learn the basic ins and outs of Python lists.
 Author: Clifford Butler
 """
 
-list_of_fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
 
-for item in list_of_fruits:
-    # list the items in list_of_fruits
-    print(item)
+for i, item in enumerate(fruit_list):
+    """Print each item in the list and the corresponding index number """
+    print(i, item)
