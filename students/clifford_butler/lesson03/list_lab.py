1119a957a534c5979cc4c1803ef28247dc7fa815
Clifford Butler <cliff002@msn.com>
2020-02-22 17:30:59 -0800

    Generate a list of fruits with a for loop listing the items in the list.

---
+++ students/clifford_butler/lesson03/list_lab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+List Lab 
+Goal - Learn the basic ins and outs of Python lists.
+Author: Clifford Butler
+"""
+
+list_of_fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+
+for item in list_of_fruits:
+    # list the items in list_of_fruits
+    print(item)
