0ba4bf53dfb7a15ac8e5eccf5d4091dc883d91be
Clifford Butler <cliff002@msn.com>
2020-02-22 23:37:48 -0800

    All tasks for series 1 completed. Started series 2 tasks which starts with displaying the list.

--- students/clifford_butler/lesson03/list_lab.py
+++ students/clifford_butler/lesson03/list_lab.py
@@ -18,27 +18,56 @@ Series 1
     Display all the fruits that begin with “P”, using a for loop.
 """
 
-fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
+print ("series 1 exercise" + "\n")
 
+# Create a list and display the list
+fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
 for item in (fruit_list):
-    """Display each item in the list """
     print(item)
 
-#ask the user for another fruit 
+# Ask the user for another fruit and add it to the end of the list
 response = input("Enter the fruit you would like to add to the list: ")
-
 if response not in fruit_list:
-    """Add user input to the end of fruit_list """
     fruit_list.append(response)
 else: print("Already in list!")
-    
 print(fruit_list)
 
-#ask the user for a number
-response2 = input("Enter a number: ")
 
+# Ask the user for a number and display the number back to the user 
+# and the fruit corresponding to that number (on a 1-is-first basis)
+response2 = int(input("Enter a number: "))
 for i, item in enumerate(fruit_list):
-    """Display the number the user input and the fruit corresonding to that number """
-    i += 1
-    print(response2, fruit_list[int(response2) - 1])
-    break    
+    if str(response2) in str(i):
+        i += 1
+        print(str(response2), fruit_list[int(response2) - 1])
+        break
+        
+# Add another fruit to the beginning of the list using “+” and display the list
+fruit_list = ["Mangos"] + fruit_list
+print(fruit_list)
+    
+# Add another fruit to the beginning of the list using insert() and display the list
+fruit_list.insert(0, "Watermelons")
+print(fruit_list)
+
+# Display all the fruits that begin with “P”, using a for loop
+for fruit in fruit_list:
+    if fruit[0].upper() == "P":
+        print(fruit)
+        
+
+"""
+Series 2
+Using the list created in series 1 above:
+
+    Display the list.
+    Remove the last fruit from the list.
+    Display the list.
+    Ask the user for a fruit to delete, find it and delete it.
+    (Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.)
+"""
+
+#print(" ")
+print ("\n" + "series 2 exercise" + "\n")
+for item in (fruit_list):
+    print(item)
