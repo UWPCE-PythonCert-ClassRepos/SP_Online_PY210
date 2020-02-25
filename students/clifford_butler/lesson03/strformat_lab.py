44a32e976c97765ce97ce6c997b1bdc1a7b19c2b
Clifford Butler <cliff002@msn.com>
2020-02-24 22:09:47 -0800

    Task 1-3 are completed.

--- students/clifford_butler/lesson03/strformat_lab.py
+++ students/clifford_butler/lesson03/strformat_lab.py
@@ -9,8 +9,8 @@ concepts of string formatting, so that these
 start to become second nature!
 '''
 
-# task 1: format string
-a_tuple = (22, 123.4567, 10000, 12345.67)
+# Task 1: format string
+a_tuple = (2, 123.4567, 10000, 12345.67)
 
 def file_number():
     # Format index 0 file number
@@ -30,7 +30,29 @@ def sci_not():
 def sci_not_three():
     # Format index 3, scientific notation with 3 significant numbers
     return(format((a_tuple[3]), '.2e'))
+    
+if __name__ == "__main__" :
+    
+    # Print task one with formatted numbers.
+    print ('{} :   {}, {}, {}'.format(file_number(),round_num(),sci_not(),sci_not_three()))
+    
+    # Task two, 
+    a = "file_00" + str(a_tuple[0])
+    b = "{:.2f}".format(a_tuple[1])
+    c = "{:.2e}".format(a_tuple[2])
+    d = "{:.2e}".format(a_tuple[3])
+
+    print('{} :   {}, {}, {}'.format(a,b,c,d))
+    
+    # Task 3, re written format string
+    
+    form_string = "The three numbers are: {:d}, {:d}, {:d}"
+    numb = (1,2,3)
+    
+    print(form_string.format(*numb))
+    
+    # Task 4, use string formatiing to display given numbers
+ 
 
 
-if __name__ == "__main__":
-    print(sci_not_three())
+
