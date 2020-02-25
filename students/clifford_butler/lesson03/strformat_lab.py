5f72b26b1af6387403d5f20cb040c5fcaaf73227
Clifford Butler <cliff002@msn.com>
2020-02-24 19:28:01 -0800

    Added a function for step four - display the number in scientific notation with three significant figures.

--- students/clifford_butler/lesson03/strformat_lab.py
+++ students/clifford_butler/lesson03/strformat_lab.py
@@ -27,6 +27,10 @@ def sci_not():
     # Format index 2, scientific notation
     return(format((a_tuple[2]), '.2e'))
 
+def sci_not_three():
+    # Format index 3, scientific notation with 3 significant numbers
+    return(format((a_tuple[3]), '.2e'))
+
 
 if __name__ == "__main__":
-    print(sci_not())
+    print(sci_not_three())
