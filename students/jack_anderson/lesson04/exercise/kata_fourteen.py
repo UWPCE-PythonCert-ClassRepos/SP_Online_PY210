9026c271d47285082bd4362cae3d2ae48062780f
Level11Jack@bitbucket.org <jackanderson@level11.com>
2020-02-26 15:58:50 -0800

    Completed more work on kata_fourteen.py functions. TODO: Add try catches, documentation, and error checks

--- students/jack_anderson/lesson04/exercise/kata_fourteen.py
+++ students/jack_anderson/lesson04/exercise/kata_fourteen.py
@@ -24,25 +24,31 @@ def build_trigrams(words):
     trigrams = {}
 
     for i in range(len(words) - 2):
-        word_pairs = words[i:i + 2]
-        value = words[i + 2]
-        key = tuple(word_pairs)
+        pairs = words[i:i + 2]
+        follower = words[i + 2]
+        key = tuple(pairs)
         if key not in trigrams.keys():
-            trigrams[key] = [value]
+            trigrams[key] = [follower]
         else:
-            trigrams[key].append(value)
+            trigrams[key].append(follower)
     return trigrams
+
 
 def read_in_data(filename):
     start_of_text = get_doc_start()
     end_of_text = get_doc_end()
-    in_data = []
+    in_data = list()
+    invalid_chars = str.maketrans(".,()!?;", "       ")
     with open(filename, "r") as f:
         for num,line in enumerate(f, 1):
             if num > start_of_text and num < end_of_text:
+                line = line.replace('"', ' ')
+                line = line.replace('--', ' ')
+                line = line.translate(invalid_chars)
                 in_data.append(line)
-    f.close()
-    return in_data
+
+        return in_data
+
 
 
 def get_doc_start():
@@ -55,9 +61,10 @@ def get_doc_start():
                 f.close()
                 return start_line
             else:
-                return 1
+                return 0
 
 def get_doc_end():
+    count = 0
     end = "*** END OF THIS PROJECT GUTENBERG EBOOK THE ADVENTURES OF SHERLOCK HOLMES ***"
     with open(filename, "r") as f:
         for num, line in enumerate(f, 1):
@@ -67,31 +74,84 @@ def get_doc_end():
                 f.close()
                 return end_line
             else:
-                return sum(num)
-
-
+                count += 1
+        return count
 
 
 def make_words(in_data):
-    new_words = []
-    invalid_chars = ['.', ',', '(', ')', '/']
+    new_words = list()
     for line in in_data:
-        line = line.replace('--', ' ')
-        for x in invalid_chars:
-            line = line.replace(x, '')
         line = line.split()
         for item in line:
-            if item == 'I' or item[0].isupper():
+            if item == "I":
                 new_words.append(item)
             else:
                 new_words.append(item.lower())
-
-        new_words.append(line)
     return (new_words)
 
 
-def build_text(word_pairs):
-    return
+
+def get_sentance_start(word_pairs):
+    sentence_start = random.choice(list(word_pairs.keys()))
+    first_word = sentence_start[0]
+    next_word = sentence_start[1]
+    start_of_sentence = (first_word, next_word)
+    return start_of_sentence
+
+
+def create_sentence(word_pairs):
+    sentence = list()
+    start = random.choice(list(word_pairs.keys()))
+    start_follower = word_pairs[start]
+    sentence.append(start[0].title())
+    sentence.append(start[1])
+    sentence.append(start_follower[0])
+
+    for i in range(random.randint(12, 20)):
+        firstKey = sentence[-2]
+        secondKey = sentence[-1]
+        next_word = word_pairs[firstKey, secondKey]
+        sentence.append(next_word[0])
+
+        # try:
+        #     next_word = word_pairs[sentence[]]
+        #     next_set = word_pairs[firstKey, nextKey]
+        #     sentence.append(next_set[0])
+        #     i = i + 1
+        # except KeyError:
+        #     print("No Key matches made")
+        #     sys.exit()
+
+    a = " ".join(sentence)
+    end = "."
+    return (a + end)
+
+def build_text(filename):
+    story = " "
+    in_data = read_in_data(filename)
+    words = make_words(in_data)
+    word_pairs = build_trigrams(words)
+    prompt = input("Please enter a number of sentences for the story: \n")
+    line_start = 0
+
+    if len(prompt) == 0:
+        line_end = 100
+    else:
+        line_end = int(prompt)
+
+    while line_start < line_end:
+        new_text = create_sentence(word_pairs)
+        if line_start % 7 == 0:
+            story = (story + " " + new_text + '\n\n')
+            line_start += 1
+
+        else:
+            story = (story + " " + new_text)
+            line_start += 1
+
+    return story
+
+
 
 if __name__ == "__main__":
     try:
@@ -100,12 +160,6 @@ if __name__ == "__main__":
         print("you muster pass in a filename")
         sys.exit(1)
 
-    print(read_in_data(filename))
+    story = build_text(filename)
 
-
-    # in_data = read_in_data(filename)
-    # words = make_words(in_data)
-    # word_pairs = build_trigrams(words)
-    # new_text = build_text(word_pairs)
-    #
-    # print(new_text)
+    print(story)
