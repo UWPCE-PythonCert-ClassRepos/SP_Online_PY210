08a48301a978e071a36f3036636d5ef283dc81e1
Level11Jack@bitbucket.org <jackanderson@level11.com>
2020-02-27 16:05:36 -0800

    Started on the test_html.render.py testing. Still need to wrap up end of step 2

--- students/jack_anderson/lesson07/html_render.py
+++ students/jack_anderson/lesson07/html_render.py
@@ -8,11 +8,28 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
+    tag = "html"
+
     def __init__(self, content=None):
-        pass
+        self.contents = [content]
+
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write(f"<{self.tag}>\n")
+        for content in self.contents:
+            if content is not None:
+                out_file.write(content)
+                out_file.write("\n")
+        out_file.write(f"</{self.tag}>\n")
+
+class Body(Element):
+    tag = 'body'
+
+class Html(Element):
+    tag = 'html'
+
+class P(Element):
+    tag = 'p'
