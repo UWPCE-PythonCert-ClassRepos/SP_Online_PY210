38b46e6f61ffe1096231162229530cc68109932a
Clifford Butler <cliff002@msn.com>
2020-03-01 08:43:10 -0800

    Cleaned up the syntax for Dictionaries 1.

--- students/clifford_butler/lesson04/dict_lab.py
+++ students/clifford_butler/lesson04/dict_lab.py
@@ -6,7 +6,9 @@ Learn the basic ins and outs of Python dictionaries and sets.
 
 Dictionaries 1
 
-Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate” (so the keys should be: “name”, etc, and values: “Chris”, etc.)
+Create a dictionary containing “name”, “city”, and “cake” for “Chris” from 
+“Seattle” who likes “Chocolate” (so the keys should be: “name”, etc, and 
+values: “Chris”, etc.)
 Display the dictionary.
 Delete the entry for “cake”.
 Display the dictionary.
@@ -15,65 +17,73 @@ Add an entry for “fruit” with “Mango” and display the dictionary.
     Display the dictionary values.
     Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
     Display whether or not “Mango” is a value in the dictionary (i.e. True).
-'''
 
+
+Dictionaries 2
+
+Using the dictionary from item 1: Make a dictionary using the same keys but with 
+the number of ‘t’s in each value as the value (consider upper and lower case?).
+
+Sets
+
+Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible by 2, 3 and 4 (figure out a way to compute those – don’t just type them in).
+Display the sets.
+Display if s3 is a subset of s2 (False)
+and if s4 is a subset of s2 (True).
+
+Sets 2
+
+Create a set with the letters in ‘Python’ and add ‘i’ to the set.
+Create a frozenset with the letters in ‘marathon’.
+Display the union and intersection of the two sets.
+'''
 # Create a dictionary, add specified keys and values
 d = {}
+d = {'name': "Chris", 'city': "Seattle", 'cake': "Chocolate"}
 
-def add_to_d():
-    d['name'] = 'Chris'
-    d['city'] = 'Seattle'
-    d['cake'] = 'Chocolate'
-
-def check_for_key(n):
+def d_one(d):
+    # Create a dictionary, remove cake and add fruit
+    dict_one = d.copy()
+    print(dict_one)
+    dict_one.pop('cake')
+    print(dict_one)
+    dict_one['fruit'] = 'Mango'
+    print(dict_one)
+    print(dict_one.keys())
+    print(dict_one.values())
+    
     # Dsiplay whether or not the key is in the dictionary
     while True:
-        if n in d.keys():
-            print('True! {} is a key in the dictionary'.format(n))
+        i = 'cake'
+        if i in dict_one.keys():
+            print('True! {} is a key in the dictionary'.format(i))
             break
         
-        print('False! {} is not a key in the dictionary.'.format(n))
+        print('False! {} is not a key in the dictionary.'.format(i))
         break
     
-def check_for_value(n):
     # Dsiplay whether or not the key is in the dictionary
     while True:
-        if n in d.values():
+        n = 'Mango'
+        if n in dict_one.values():
             print('True! {} is a value in the dictionary'.format(n))
             break
         
         print('False! {} is not a value in the dictionary.'.format(n))
         break    
+     
 
 def main():
     
-    # Add keys and values to the dictionary
-    add_to_d()
+    print ('Dictionaries 1: \n')
     
-    # Display the dictionary
-    print(d)
-
-    # Delete the entry cake
-    d.pop('cake')
-
-    # Display the dictionary
-    print(d)
-
-     # Add an entry for “fruit” with “Mango” and display the dictionary
-    d['fruit'] = 'Mango'
-
-    # Display the dictionary keys
-    print(d.keys())
-
-    # Display the dictionary values
-    print(d.values())
+    d_one(d)
+   
+    print ('\n' + 'Dictionaries 2: \n')
     
-    # check if n is a key in the dictionary
-    check_for_key('cake')
-    
-    # check if n is a value in the dictionary
-    check_for_value('Mango')
-    
+    print ('\n' + 'Sets: \n')
+   
+    print ('\n' + 'Sets 2: \n')
     
 if __name__ == '__main__':
    main()
