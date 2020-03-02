ef88f9572e20dd26abddabb3b61f1e9ae51703cc
mmanc125uw <mmanc125@uw.edu>
2020-03-02 15:42:16 -0500

    strformat_lab added task 1 and 2

--- students/mmancini/lesson03/strformat_lab.py
+++ students/mmancini/lesson03/strformat_lab.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3
 
+
+
+
+def task1(tup):
+    print("task 1 output without padding ==> " + 'file{:03d} : {:.2f}, {:.2E} , {:.2E}'.format(*tup))
+    print("task 1 output with padding ==> " + 'file{:>03d} : {:.2f}, {:.2E} , {:.2E}'.format(*tup))
+
+
+def task2(tup):
+    print("task 2 output ==> " + 'file{:>03d} : {:.2f}, {:.2E} , {:.2E}'.format(*tup))
+    print("task 2 output with f string ==> " + f'file00{tup[0],2} : {tup[1]:.2f}, {tup[2]:.2E}, {tup[3]:.2E}')
+
+
+
+
+####################################
+
+
+# main, test funcs
+
+if __name__ == "__main__":
+    # run some tests
+    xTup = (2, 123.4567, 10000, 12345.67)
+    task1(xTup)
+    task2(xTup)
