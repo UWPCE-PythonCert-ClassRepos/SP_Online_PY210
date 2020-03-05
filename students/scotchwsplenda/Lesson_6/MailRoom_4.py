7477fcd66f7a22a1248532eabd42d07f3bcb3440
Oliver Lockwood (CBRE) <v-ollock@microsoft.com>
2020-03-04 22:54:37 -0800

    except block revisions

--- students/scotchwsplenda/Lesson_6/MailRoom_4.py
+++ students/scotchwsplenda/Lesson_6/MailRoom_4.py
@@ -10,27 +10,24 @@ donators = {"Gordian": [30.0, 45.0], "Tiberius": [60.0],
 
 
 def mainy():
-    print('''
-  Welcome to the MAILROOM main menu
-  Please choose from below options:
-  1 - Initiate Grovel Sequence
-  2 - Data Metrics 3000
-  3 - Quit
-  4 - Send thank you notes to all donors
-              ''')
     while True:
         try:
-            choice = int(input('Main Indicate your choice: '))
+            choice = int(input('''
+Welcome to the main MAILROOM option menu:
+1 - Initiate Grovel Sequence
+2 - Data Metrics 3000
+3 - Quit
+4 - Send thank you notes to all donors
+Indicate your choice:'''))
             swit_dic = {1: send_note, 2: data_metrics,
                         3: exit_program, 4: mass_mail}
             swit_dic[choice]()
         except KeyError:
             print('You have entered a non-choice'
-                  ' , please get your shit together')
+                  ', please get your life together')
         except ValueError:
             print('You have entered a non-number'
-                  ' , please get your shit together')
-            continue
+                  ', please get your life together')
 
 
 # Option 1: Content of Thank you note
@@ -39,44 +36,19 @@ def send_note():
 Please select from the below Thank You Note options:
 1 - Print list of extant donors
 2 - Enter donor name and donation
-3 - Exit Program
+3 - Retur to main menu
     ''')
-    while True:
-        try:
-            respondy = int(input("send_note: Indicate your choice:"))
-            if respondy == 1:
-                print_donors()
-                continue
-            if respondy == 2:
-                try:
-                    donor_inp = input("Input donor: ")
-                    if donor_inp in donators.keys():
-                        don_amount = input("That's a known donor,"
-                                           " input donation amount: ")
-                    else:
-                        don_amount = input(f"{donor_inp}'s an unknown donor,"
-                                           " input donation amount: ")
-                    don_amount = float(don_amount)
-                    update_dons(donor_inp, don_amount)
-                    send_thanks(donor_inp, don_amount)
-                    continue
-                except ValueError:
-                    print('You have entered a non-number'
-                          ' , for the donation amount start over')
-            if respondy == 3:
-                mainy()
-            if respondy > 3:
-                print('You have entered a non-choice'
-                      ' , please get your shit together')
-                continue
-            if respondy < 1:
-                print('You have entered a non-choice'
-                      ' , please get your shit together')
-                continue
-        except ValueError:
-            print('You have entered a non-number'
-                  ' , please get your shit together')
-            continue
+    respondy = int(input("send_note: Indicate your choice:"))
+    if respondy == 1:
+        print_donors()
+        return
+    elif respondy == 2:
+        input_donor()
+    elif respondy == 3:
+        return
+    else:
+        print('You have entered a non-choice'
+              ' , please get your life together')
 # break - exits program entirely
 # continue - stays in send_note() [have to quit terminal]
 # exit() - exits program entirely
@@ -84,6 +56,25 @@ Please select from the below Thank You Note options:
 # done - stays in send_note() [have to quit terminal]
 # return - exits program entirely
 # True - stays in send_note() [have to quit terminal]
+
+
+def input_donor():
+    donor_inp = input("Input donor: ")
+    if donor_inp in donators.keys():
+        print("That's a known donor")
+    else:
+        print(f"{donor_inp}'s an unknown donor")
+    while True:
+        try:
+            don_amount = float(input(f"how much is {donor_inp} donating?:"))
+            update_dons(donor_inp, don_amount)
+            send_thanks(donor_inp, don_amount)
+            return
+        except ValueError:
+            print('You have entered a non-number, try again')
+# continue - infinite loop
+# break - goes back to main menu
+# return - goes back to main menu
 
 
 # option 1d: update donor database
@@ -102,11 +93,9 @@ def send_thanks(donor_inp, don_amount):
 
 # option 1c: print list:
 def print_donors():
-    butt = list()
-    for k in donators:
-        butt.append(k)
-    print(butt)
-    return butt
+    alldons = [k for k in donators]
+    print(alldons)
+    return alldons
 
 
 # Option 2: create report
