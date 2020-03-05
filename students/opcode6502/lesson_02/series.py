99222b0a1687cd3716caf77f6f1efd093533338c
Mark Hanschen <mark@markhanschen.com>
2020-03-05 01:27:01 -0800

    series.py: Added.

---
+++ students/opcode6502/lesson_02/series.py
@@ -0,0 +1,13 @@
+# FIBONACCI(n):
+#
+# REQ-01: Create a new module series.py in the lesson02 folder in your student folder.
+# [ note: This file. ]
+# REQ-02: In it, add a function called fibonacci.
+# REQ-03: The function should have one parameter n.
+
+
+def fibonacci(n):
+    print(n)
+
+
+fibonacci(5)
