f0818309fce0b5d148b7dbe21c19ddc2c5b224b4
mmanc125uw <mmanc125@uw.edu>
2020-03-06 15:25:49 -0500

    mailer.py populate stubs new and existing donor

--- students/mmancini/lesson03/mailroom.py
+++ students/mmancini/lesson03/mailroom.py
@@ -3,9 +3,22 @@
 
 db_donors = [("Jane Smith", [25, 50]),
              ("Tom Adams", [100]),
+             ("Helen Smalls", [10,20,30]),
+             ("Ming Chan", [50]),
              ("Mary Jones", [5, 10, 15])]
 
 ####################################
+
+
+def menu_donation_amount():
+    msg = ""
+    msg += "Please enter a donation amount::\n"
+    msg += ".....>>"
+
+    # donation_amount_entry = input(msg)
+    donation_amount_entry = 55      # ***MMM
+
+    return donation_amount_entry
 
 
 def show_donors():
@@ -17,11 +30,31 @@ def show_donors():
 
 def process_new_donor(name):
     print(f"process a new donor ==> ", name)
+    amount_ary = []
+
+    amount = menu_donation_amount()
+    amount_ary.append(amount)
+
+    db_donors.append((name,amount_ary))
+    #donor_donations.append((user_action_TY, [donation]))
+
+    print(f"***MMM diag , donations >=> ", db_donors)
 
 
 def process_existing_donor(name):
     print(f"process existing donor ==> ", name)
 
+    donor_names_lst = [x[0] for x in db_donors]
+    x = donor_names_lst.index(name)
+
+    amount = menu_donation_amount()
+    #for d in db_donors:
+    #    d[1].append(3)
+    db_donors[x][1].append(amount)
+    print(f"existing donor {name} donated {amount}")
+    #print(db_donors)
+
+    print(x)
 
 def process_donor(in_name):
 
@@ -34,8 +67,8 @@ def process_donor(in_name):
 
     ix = -1
     for donor in db_donors:
-        print(f"diag spin donors, donor ==> ", donor[0])
-
+        #print(f"diag spin donors, donor ==> ", donor[0])
+        i=1
 
         #if donors[0] == user_action_TY:
         #        donors[1].append(donation)
@@ -53,8 +86,8 @@ def send_thankyou():
     while need_entry:
         # entry = input(msg)
 
-        # entry = 'Phil Mason'
-        entry = 'Tom Adams'
+        entry = 'Phil Mason'
+        # entry = 'Tom Adams'
         #entry = 'list'
         print(f"user entered ==> ", entry)
 
