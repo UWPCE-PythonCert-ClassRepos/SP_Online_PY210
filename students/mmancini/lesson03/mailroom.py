da29a192e686a77c7d849e0a8ffe10f2d96f3dfe
mmanc125uw <mmanc125@uw.edu>
2020-03-06 13:33:14 -0500

    mailer.py initial with main menu and stubs

---
+++ students/mmancini/lesson03/mailroom.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+
+db_donors = [("Jane Smith", [25, 50]),
+             ("Tom Adams", [100]),
+             ("Mary Jones", [5, 10, 15])]
+
+####################################
+
+
+def show_donors():
+    print(f"List of Donors:")
+    for donor in db_donors:
+        # print(f"diag spin donors, donor ==> ", donor[0])
+        print(f" ", donor[0])
+
+
+def process_new_donor(name):
+    print(f"process a new donor ==> ", name)
+
+
+def process_existing_donor(name):
+    print(f"process existing donor ==> ", name)
+
+
+def process_donor(in_name):
+
+    donor_names_lst = [x[0] for x in db_donors]
+
+    if in_name in donor_names_lst:
+        process_existing_donor(in_name)
+    else:
+        process_new_donor(in_name)
+
+    ix = -1
+    for donor in db_donors:
+        print(f"diag spin donors, donor ==> ", donor[0])
+
+
+        #if donors[0] == user_action_TY:
+        #        donors[1].append(donation)
+
+        i = 1
+
+
+def send_thankyou():
+    msg = ""
+    msg += "Please enter donor name or 'list' for list of donors:\n"
+    msg += ".....>>"
+
+    entry = ""
+    need_entry = True
+    while need_entry:
+        # entry = input(msg)
+
+        # entry = 'Phil Mason'
+        entry = 'Tom Adams'
+        #entry = 'list'
+        print(f"user entered ==> ", entry)
+
+        if entry.lower() == 'list':
+            show_donors()
+        else:
+            # have a donor name
+            need_entry = False
+
+        break   # ***MMM test only
+
+    process_donor(entry)
+
+
+def main_menu():
+    msg = ""
+    msg += "Please enter option from below:\n"
+    msg += " S: Send thank you note\n"
+    msg += " C: Create report\n"
+    msg += " Q: Quit\n"
+    msg += ".....>>"
+
+    # entry = input(msg)
+
+    entry = 'S'
+    print(f"user entered ==> ", entry)
+
+    if entry == 'S':
+        send_thankyou()
+
+    """
+    if user_action == 'S'
+        create_a_report()
+    elif user_action == '3':
+        quit()
+    else:
+        print('Invalid input. Please choose 1, 2 or 3.')
+
+
+    """
+    return entry
+
+###################################
+
+
+def mailroom():
+    print("mailroom begin")
+
+    # exit()
+
+    is_done = False
+    while True:
+        operation = main_menu()
+
+        if operation == 'Q':
+            is_done = True
+
+        is_done = True  # ***MMM
+
+        if is_done:
+            break
+
+    print("mailroom end")
+
+
+###################################
+
+
+# main, test funcs
+
+if __name__ == "__main__":
+    mailroom()
+
