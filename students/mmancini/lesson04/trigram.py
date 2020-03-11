6a4fd297771a7994df4ca53494d580fa8b7d5878
mmanc125uw <mmanc125@uw.edu>
2020-03-11 11:25:08 -0400

    trigram.py, added file handling

--- students/mmancini/lesson04/trigram.py
+++ students/mmancini/lesson04/trigram.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 
+import sys
+
 
 ###################################
+
 
 def get_file_data(in_filename):
     out_data = ""
 
+    with open(in_filename, 'r') as fil:
+        out_data = fil.read()
+    fil.close()
+    out_data = out_data.lower()
+
     return out_data
+
+
+def filter_data(in_data):
+    # filter out punctuation
+    out_data = in_data
+    out_data = out_data.replace(',', '')
+    out_data = out_data.replace('.', '')
+    out_data = out_data.replace(')', '')
+    out_data = out_data.replace('(', '')
+    out_data = out_data.replace('--', ' ')
+
+    return out_data
+
 
 ###################################
 
@@ -16,6 +37,6 @@ def get_file_data(in_filename):
 if __name__ == "__main__":
 
     data = ""
-    filename="c:\Temp\Sherlock_small.txt"
+    filename="Sherlock_small.txt"
     data = get_file_data(filename)
-
+    data = filter_data(data)
