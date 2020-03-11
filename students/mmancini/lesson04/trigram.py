f78c426f23a70098645f24eb98510ce727d728a0
mmanc125uw <mmanc125@uw.edu>
2020-03-11 06:55:24 -0400

    trigram.py, lesson 04 lab initial template created

---
+++ students/mmancini/lesson04/trigram.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+###################################
+
+def get_file_data(in_filename):
+    out_data = ""
+
+    return out_data
+
+###################################
+
+
+# main, test funcs
+
+if __name__ == "__main__":
+
+    data = ""
+    filename="c:\Temp\Sherlock_small.txt"
+    data = get_file_data(filename)
+
