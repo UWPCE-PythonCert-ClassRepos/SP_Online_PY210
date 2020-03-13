1d913e24d76a0be7e479e87d0de620bf6ee661c0
lisabelle <ferlis21@gmail.com>
2020-03-13 12:04:53 -0700

    lesson 06 files

---
+++ students/lisa_ferrier/lesson06/test_mailroom.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# mailroom_pt4.py, Python 210, Lesson 06
+
+import mailroom
+
+def test_create_report():
+    report = create_report()
+    assert "Bill Gates" in list(donors)
+    pass
+
+
+def test_add_donor():
+    pass
+
+
+def test_update_donor():
+    pass
+
+
+def add_donation():
+    pass
+
+
+def test_list_donor():
+    pass
+
+
+def test_send_thank_you_all_donors():
+    pass
+
+
+def test_send_thank_you_single_donor():
+    pass
