6474a91f1dbc70bfdb0b431b85e2c7218b0ec15a
Mark Hanschen <mark@markhanschen.com>
2020-03-18 23:17:33 -0700

    series.py: Added fibonacci(n) requirements

--- students/opcode6502/lesson_02/series.py
+++ students/opcode6502/lesson_02/series.py
@@ -1,13 +1,17 @@
-# FIBONACCI(n):
+# series.py
+# opcode6502
 #
 # REQ-01: Create a new module series.py in the lesson02 folder in your student folder.
 # [ note: This file. ]
+
+
 # REQ-02: In it, add a function called fibonacci.
 # REQ-03: The function should have one parameter n.
-
-
 def fibonacci(n):
-    print(n)
 
+    # REQ-04: The function should return the nth value in the fibonacci series
+    # (starting with zero index).
 
-fibonacci(5)
+    # REQ-05: Docstring for fibonacci().
+    # TODO: Add docstring
+
