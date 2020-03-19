1d70e5c30a2f79bf8722069a7aa88a54de6a3730
Mark Hanschen <mark@markhanschen.com>
2020-03-19 00:11:34 -0700

    series.py: Added fibonacci(n) assertion tests.

--- students/opcode6502/lesson_02/series.py
+++ students/opcode6502/lesson_02/series.py
@@ -19,3 +19,20 @@ def fibonacci(n):
 
     # REQ-05: Docstring for fibonacci().
     # TODO: Add docstring
+
+
+# REQ-10: Add a block of code to the end of your series.py module.
+# Use the block to write a series of assert statements that demonstrate that
+# your three functions work properly.
+if __name__ == "__main__":
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    assert fibonacci(8) == 21
+    assert fibonacci(9) == 34
+    assert fibonacci(10) == 55
