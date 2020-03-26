18b561fb98889758cebe709d343d817124a6a789
Zhen Yang <wwxyzd@hotmail.com>
2020-03-25 19:34:13 -0700

    finished dict_lab.py

--- students/zhen_yang/lesson04/dict_lab.py
+++ students/zhen_yang/lesson04/dict_lab.py
@@ -44,5 +44,41 @@ for key,val in dic_1.items():
     # get the number of 't's in value
     num = val.lower().count('t')
     dic_2[key] = num 
-
 print(f"New dic: {dic_2}")
+
+##########
+# Sets   #
+##########
+# Create sets s2, s3 and s4 that contain numbers from zero through twenty,
+# divisible by 2, 3, and 4 (figure out a way to compute those)
+# set s2 divisible by 2
+s2 = set(range(0,21,2))
+print(f"Set s2: {s2}")
+s3 = set(range(0,21,3))
+print(f"Set s3: {s3}")
+s4 = set(range(0,21,4))
+print(f"Set s4: {s4}")
+
+# Display if s3 is a subset of s2
+print(f"Is set s3 a subset of s2? {s3.issubset(s2)}")
+# Display if s4 is a subset of s2
+print(f"Is set s4 a subset of s2? {s4.issubset(s2)}")
+
+
+
+###########
+# Sets  2 #
+###########
+# Create a set with the letters in 'Python' and add 'i' to the set
+my_set = {'Python'}
+my_set.add('i')
+print(f"my_set:{my_set}")
+# create a frozenset with letters in 'marathon'
+frozenset = {'marathon'}
+print(f"frozenset:{frozenset}")
+# Display the union and intersection of the two sets
+print(f"Union of my_set and frozenset: {my_set.union(frozenset)}")
+print(f"Intersection of my_set and frozenset: {my_set.intersection(frozenset)}")
+
+
+
