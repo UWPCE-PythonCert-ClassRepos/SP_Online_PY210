0e7462d2cadc2329ee09b81eee1cd111cf0d1d20
Zhen Yang <wwxyzd@hotmail.com>
2020-03-28 12:49:19 -0700

    fix the python style errors

--- students/zhen_yang/lesson04/file_exercise.py
+++ students/zhen_yang/lesson04/file_exercise.py
@@ -7,73 +7,68 @@
 #############################
 import os
 
-# Write a program which prints the full path for 
+# Write a program which prints the full path for
 # all files in the current directory
-
+print("-------  Paths and File Processing  Exerceise  --------")
 # get the current directory
 cur_dir = os.getcwd()
 print(cur_dir)
 # list the names of the files
 file_list = os.listdir(cur_dir)
 for i in file_list:
-    print(f"{cur_dir}\{i}")
-
+    print(f"{cur_dir}\\{i}")
 
 # Write a program which copies a file from a source to a destination.
-source = cur_dir + '\my_file.txt'
-dest = cur_dir + '\my_copy.txt'
+source = cur_dir + '\\my_file.txt'
+dest = cur_dir + '\\my_copy.txt'
 file_size = 4000
-# use with as block to open files, then don't need to close the files.
+# use 'with as' block to open files, then don't need to close the files.
 with open(source, 'rb') as in_file, open(dest, 'wb') as out_file:
-        # read part of a file into memeory
-        out_file.write(in_file.read(file_size))
-        # read the rest part of a file into memeory
-        out_file.write(in_file.read())
+    # read part of a file into memeory
+    out_file.write(in_file.read(file_size))
+    # read the rest part of a file into memeory
+    out_file.write(in_file.read())
 
 
 #############################
 # File reading and parsing  #
-# Write a little script that reads that file and generates 
+# Write a little script that reads that file and generates
 # a list of all the languages that have been used.
 #############################
-# use dictionary data sturcture 
-# key is the difference language, and value is the 
+# use dictionary data sturcture
+# key is the difference language, and value is the
 # number of studets use the language.
-language_dic = {} 
+language_dic = {}
 
+print("-------  File reading and parsing Exerceise  --------")
 def create_language_list(the_list):
     for i in the_list:
-        # remove the whitespace in each string
-        new_str = str(i).strip() 
-        if len(new_str)!=0:
-            # if the language is already on the dic, then add the value by one
+        new_str = str(i).strip() # remove whitespaces
+        if len(new_str) != 0:
             if new_str in language_dic:
                 language_dic[new_str] = language_dic[new_str] + 1
-            else:
-                # if the language is not on the dic, then add the item
-                language_dic[new_str] =  1
-        
+            else:# new language
+                language_dic[new_str] = 1
 
-source = cur_dir + '\students.txt'
-with open(source,'r') as in_file:
-    # read in each line
-    for line in in_file:
-        # remove the newlines from each line
-        line = line.rstrip("\n") 
+source = cur_dir + '\\students.txt'
+with open(source, 'r') as in_file:
+    for line in in_file: # read in one line
+        line = line.rstrip("\n") # remove newlines character
         new_list_1 = line.split(": ")
-        # skip the first title line and the line without nick name or language specification
+        # skip the first title line and
+        # the line without nick name or language specification
         if new_list_1[0] != 'Name' and len(new_list_1) == 2:
             new_list_2 = new_list_1[1].split(',')
             # Note: check nothing is in the list using 'in' not '=='
-            if 'nothing' in new_list_2 :
-                #print("2. Nothing is here. {new_list_2}") 
+            if 'nothing' in new_list_2:
+                #print("2. Nothing is here. {new_list_2}")
                 pass
-            # nick name has the first uppercase letter 
+            # nick name has the first letter uppercase
             elif not new_list_2[0].islower():
                 # remove nick name from the list
                 #print(f"before nick {new_list_2}")
                 new_list_3 = new_list_2[0].split(" ")
-                if len (new_list_3) ==1:
+                if len(new_list_3) == 1:
                     new_list_2.pop(0)
                 else:
                     new_list_2.pop(0)
@@ -84,7 +79,5 @@ with open(source,'r') as in_file:
             else:
                 create_language_list(new_list_2)
 
-
     for key, val in language_dic.items():
-        print(f"Language: {key}, student speficed: {val}")
-
+        print(f"Language: {key}, num of student used: {val}")
