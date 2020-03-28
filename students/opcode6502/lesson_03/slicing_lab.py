027f3d03e7420813b0fc592c6ba492652b46603b
Mark Hanschen <mark@markhanschen.com>
2020-03-28 01:18:24 -0700

    zz_snk_test.py: Removed.

---
+++ students/opcode6502/lesson_03/slicing_lab.py
@@ -0,0 +1,10 @@
+# slicing_lab.py
+# opcode6502: SP_Online_PY210
+
+# Write some functions that take a sequence as an argument, and return a copy of that sequence:
+# REQ-01: with the first and last items exchanged.
+# REQ-02: with every other item removed.
+# REQ-03: with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
+# REQ-04: with the elements reversed (just with slicing).
+# REQ-05: with the last third, then first third, then the middle third in the new order.
+# REQ-06: Write a test or two like that for each of the above functions.
--- students/opcode6502/lesson_03/zz_snk_test.py
+++
@@ -1,7 +0,0 @@
-# Lesson 03 notes
-
-# Slicing
-
-example_string_01 = "Hello there, this is a test"
-
-print(example_string_01[:4])
