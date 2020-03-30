73eda0c4b685c9e63c48820f628b08b953e96b90
Mark Hanschen <mark@markhanschen.com>
2020-03-30 02:08:02 -0700

    slicing_lab.py: Added basic code layout.

--- students/opcode6502/lesson_03/slicing_lab.py
+++ students/opcode6502/lesson_03/slicing_lab.py
@@ -2,10 +2,43 @@
 # opcode6502: SP_Online_PY210
 
 
-# Write some functions that take a sequence as an argument, and return a copy of that sequence:
-# REQ-01: with the first and last items exchanged.
-# REQ-02: with every other item removed.
-# REQ-03: with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
-# REQ-04: with the elements reversed (just with slicing).
-# REQ-05: with the last third, then first third, then the middle third in the new order.
+# REQ-01: Write a function that take a sequence as an argument, and
+# return a copy of that sequence with the first and last items exchanged.
+def seq_req_01(arg):
+    print(arg)
+
+
+# REQ-02: Write a function that take a sequence as an argument, and
+# return a copy of that sequence with every other item removed.
+def seq_req_02(arg):
+    print(arg)
+
+
+# REQ-03: Write a function that take a sequence as an argument, and
+# return a copy of that sequence with the first 4 and the last 4 items removed,
+# and then every other item in the remaining sequence.
+def seq_req_03(arg):
+    print(arg)
+
+
+# REQ-04: Write a function that take a sequence as an argument, and
+# return a copy of that sequence with the elements reversed (just with slicing).
+def seq_req_04(arg):
+    print(arg)
+
+
+# REQ-05: Write a function that take a sequence as an argument, and
+# return a copy of that sequence with the last third, then first third,
+# then the middle third in the new order.
+def seq_req_05(arg):
+    print(arg)
+
+
 # REQ-06: Write a test or two like that for each of the above functions.
+
+
+seq_req_01("seq_req_01")
+seq_req_02("seq_req_02")
+seq_req_03("seq_req_03")
+seq_req_04("seq_req_04")
+seq_req_05("seq_req_05")
