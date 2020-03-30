480a36a62bcca82f613233499c0e412a5da31862
Mark Hanschen <mark@markhanschen.com>
2020-03-30 04:40:13 -0700

    slicing_lab.py: Updated remove_four_print_alternate(seq).

--- students/opcode6502/lesson_03/slicing_lab.py
+++ students/opcode6502/lesson_03/slicing_lab.py
@@ -18,18 +18,19 @@ def exchange_first_last(seq):
     """
 
     #
-    first_element = arg[:1]
-    last_element = arg[-1:]
-    final_sequence = last_element + arg[1:-1] + first_element
+    first_element = seq[:1]
+    last_element = seq[-1:]
+    final_sequence = last_element + seq[1:-1] + first_element
 
     # DEBUG statements for developer testing.
     if debug_flag:
-        print("- - - - - - - - - -  - - - - - - - - - - - - - - -")
+        print("--------------------------------------------------")
         print("[ EXEC  ]: exchange_first_last(seq): called!")
-        print("[ DEBUG ]: arg: " + arg)
-        print("[ DEBUG ]: first_element: " + first_element)
-        print("[ DEBUG ]: last_element: " + last_element)
-        print("[ DEBUG ]: final_sequence: " + final_sequence)
+        print("--------------------------------------------------")
+        print("[ DEBUG ]: seq             : " + seq)
+        print("[ DEBUG ]: first_element   : " + first_element)
+        print("[ DEBUG ]: last_element    : " + last_element)
+        print("[ DEBUG ]: final_sequence  : " + final_sequence)
 
     return final_sequence
 
@@ -51,22 +52,59 @@ def remove_alternate_items(seq):
 
     # DEBUG statements for developer testing.
     if debug_flag:
-        print("- - - - - - - - - -  - - - - - - - - - - - - - - -")
+        print("--------------------------------------------------")
         print("[ EXEC  ]: remove_alternate_items(seq): called!")
-        print("[ DEBUG ]: arg: " + arg)
-        print("[ DEBUG ]: arg[0] " + arg[0])
-        print("[ DEBUG ]: arg[0:] " + arg[0:])
-        print("[ DEBUG ]: arg[:2] " + arg[:2])
-        print("[ DEBUG ]: arg[::2] " + arg[::2])
+        print("--------------------------------------------------")
+        print("[ DEBUG ]: seq             : " + seq)
+        print("[ DEBUG ]: seq[0]          : " + seq[0])
+        print("[ DEBUG ]: seq[0:]         : " + seq[0:])
+        print("[ DEBUG ]: seq[:2]         : " + seq[:2])
+        print("[ DEBUG ]: seq[::2]        : " + seq[::2])
 
-    return arg[::2]
+    return seq[::2]
 
 
 # REQ-03: Write a function that take a sequence as an argument, and
 # return a copy of that sequence with the first 4 and the last 4 items removed,
 # and then every other item in the remaining sequence.
-def seq_req_03(arg):
-    print(arg)
+def remove_four_print_alternate(seq):
+
+    # Python Docstring
+    """
+    Returns a copy of a sequence with the first 4 and the last 4 items removed,
+    and then every other item in the remaining sequence.
+
+    Args:
+        param1: The sequence to be copied and changed.
+
+    Returns:
+        A copy of the sequence with the first 4 and the last 4 items removed,
+        and then every other item in the remaining sequence.
+    """
+
+    # DEBUG statements for developer testing.
+    if debug_flag:
+        print("--------------------------------------------------")
+        print("[ EXEC  ]: seq_req_03(seq): called!")
+        print("--------------------------------------------------")
+        print("[ DEBUG ]: seq             : " + seq)
+        #
+        # The first four items.
+        print("[ DEBUG ]: seq[4]          : " + seq[4])
+        print("[ DEBUG ]: seq[4:]         : " + seq[4:])
+        #
+        # The last four items.
+        print("[ DEBUG ]: seq[-4]         : " + seq[-4])
+        print("[ DEBUG ]: seq[:-4]        : " + seq[:-4])
+        #
+        # Every other item.
+        print("[ DEBUG ]: seq[::2]        : " + seq[::2])
+        #
+        # The final sliced sequence.
+        print("[ DEBUG ]: seq[4:-4:2]     : " + seq[4:-4:2])
+
+    return seq[4:-4:2]
+
 
 # REQ-04: Write a function that take a sequence as an argument, and
 # return a copy of that sequence with the elements reversed (just with slicing).
@@ -91,17 +129,19 @@ def seq_req_05(arg):
 # DEBUG MESSAGES key:
 # [ EXEC  ]: Informs which function is printing debug statements.
 # [ DEBUG ]: A debug statement.
-debug_flag = 0
+debug_flag = 1
 
 
 # The test data we will use for this source file.
 a_string = "The quick brown fox jumps over the lazy dogs"
 
 
+# Execute the code.
 exchange_first_last(a_string)
-seq_req_02(a_string)
-seq_req_03(a_string)
+remove_alternate_items(a_string)
+remove_four_print_alternate_items(a_string)
 seq_req_04(a_string)
 seq_req_05(a_string)
+
 
 # REQ-06: Write a test or two like that for each of the above functions.
