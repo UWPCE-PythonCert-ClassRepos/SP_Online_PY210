d77272786ebca290b97c568ebcd9a52f2bf3b456
mmanc125uw <mmanc125@uw.edu>
2020-03-31 14:00:35 -0400

    test_mailroom4.py, added test_send_thankyou

--- students/mmancini/lesson06/test_mailroom4.py
+++ students/mmancini/lesson06/test_mailroom4.py
@@ -14,7 +14,12 @@ def test_create_report():
     assert report == expected_report
 
 
-####################################
+def test_send_thankyou_email():
+    expected_thankyou_email = mailroom.read_file(mailroom.canned_thankyou_email)
+    test_name = "Phil Sims"
+    test_amount = 35
+    thankyou_email = mailroom.process_send_thankyou_email(test_name, test_amount)
+    assert expected_thankyou_email == thankyou_email
 
 
 def test_suite():
@@ -22,6 +27,7 @@ def test_suite():
     # run test suite tests
     print(f"Mailroom Test Suite Started")
     test_create_report()
+	test_send_thankyou_email()
     print(f"Mailroom Test Suite Completed")
 
 ####################################
