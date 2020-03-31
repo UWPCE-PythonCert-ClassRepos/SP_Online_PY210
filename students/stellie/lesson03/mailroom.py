30dfd70aa43e4cfba309a58cf3c9ae5ee6d87848
stellie <kims82@gmail.com>
2020-03-31 13:50:01 -0700

    add logic for sending thank you notes, adding a new user or adding a donation amount to an existing user

--- students/stellie/lesson03/mailroom.py
+++ students/stellie/lesson03/mailroom.py
@@ -11,11 +11,11 @@ choose from a menu of 3 actions: send a thank you, create a report or quit.
 import sys
 
 # Lists donors and their contributions
-donor_db = [('John Smith', [10000, 5000, 1000]),
-            ('Mary Johnson', [50000]),
-            ('David Carlton', [2500, 500]),
-            ('James Wright', [500, 500, 500]),
-            ('Caroline Baker', [1000])
+donor_db = [['John Smith', [10000, 5000, 1000]],
+            ['Mary Johnson', [50000]],
+            ['David Carlton', [2500, 500]],
+            ['James Wright', [500, 500, 500]],
+            ['Caroline Baker', [1000]]
             ]
 
 # Displays options to user
@@ -27,16 +27,47 @@ prompt = '\n'.join(('Welcome to the mail room.',
           '>>> '))
 
 def send_thank_you():
-    user_response = input('Please enter your full name: ')
+    user_response = input('Please enter your full name or type "list" to view the list of donors: ')
     if user_response == 'list':
         view_donors()
-    #elif user_response == :
-
-
-#def create_report():
+        send_thank_you()
+    else:
+        donor = search_db(user_response)
+        if donor:
+            print('This donor already exists in our database.')
+            donation = donation_amount()
+            donor[1].append(donation)
+            # print(donor_db)
+            thank_you_email(donor[0], donation)
+        else:
+            print('This is a NEW donor and will be added to the database.')
+            donation = donation_amount()
+            new_donor = [user_response, [donation]]
+            donor_db.append(new_donor)
+            # print(donor_db)
+            thank_you_email(new_donor[0], donation)
 
 def view_donors():
+    print('\nThe following is the list of donors:')
+    for donor in donor_db:
+        print(donor[0])
     print(donor_db)
+
+def search_db(donor_name):
+    for record in donor_db:
+        if record[0] == donor_name:
+            return record
+    return None
+
+def donation_amount():
+    user_donation = input('Please enter the amount you would like to donate: $')
+    return float(user_donation)
+
+def thank_you_email(donor_name, amount):
+    print(f'Thank you {donor_name} for your generous donation amount of ${amount:.2f}.  We hope to see you again soon.')
+    return
+
+#def create_report():
 
 def exit_program():
     print('Thank you for visiting!')
