2b1e8594f102a26584f25e1630137f794e0d6ef5
mmanc125uw <mmanc125@uw.edu>
2020-03-31 15:49:33 -0400

    test_mailroom4.py, included test_write_letters, fin refactor

--- students/mmancini/lesson06/test_mailroom4.py
+++ students/mmancini/lesson06/test_mailroom4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-
 import mailroom4 as mailroom
+
 
 ####################################
 
@@ -28,13 +28,44 @@ def test_all_donors_listed():
     assert expected_all_donors_listed == all_donors_listed
 
 
+def test_write_letters():
+
+    # first write all the letters
+    mailroom.write_letters_to_all()
+
+    # now test the letters were created correctly
+    for key, value in mailroom.db_donors.items():
+
+        # prepare the expected test data
+        donor_name = key
+        donations_ary = value
+        donations_total = sum(donations_ary)
+
+        # test the created letter
+        dict_data_line = {"donor_name": donor_name, "donation_amount": float(donations_total)}
+        expected_thank_you_letter = ""
+        expected_thank_you_letter += f"Dear {donor_name} Thank You for your generous donations " \
+                                     f"totaling ${donations_total}:\n".format(**dict_data_line)
+        thank_you_letter = mailroom.create_letter(donor_name)
+        assert thank_you_letter == expected_thank_you_letter
+
+        # test the persisted letter data
+        filename = donor_name + ".txt"
+        with open(filename, "r") as f:
+            thank_you_letter_data = f.read()
+            assert thank_you_letter_data == expected_thank_you_letter
+
+####################################
+
+
 def test_suite():
 
     # run test suite tests
     print(f"Mailroom Test Suite Started")
     test_create_report()
-	test_send_thankyou_email()
-	test_all_donors_listed()
+    test_send_thankyou_email()
+    test_all_donors_listed()
+    test_write_letters()
     print(f"Mailroom Test Suite Completed")
 
 ####################################
@@ -43,7 +74,9 @@ def test_suite():
 
 # tests for mailroom business logic
 
+
 test_suite()
+
 
 
 
