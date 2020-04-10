1d68a81d31b8b2100bd5a84f7d1751331937daf1
“tianxie812” <xietian628@yahoo.com>
2020-04-10 09:39:59 -0700

    Completed task 1.

---
+++ students/Tianx/Lesson3/strformat_lab.py
@@ -0,0 +1,20 @@
+# ------------------------------------------#
+# !/usr/bin/env python3
+# Title: strformat_lab.py
+# Desc: In this exercise we will reinforce the important concepts of string formatting, so that these start to become second nature!
+# Tian Xie, 2020-04-10, Created File
+# ------------------------------------------#
+
+# Task 1
+print("------Task 1------")
+my_tuple = (2, 123.4567, 10000, 12345.67)
+print(f"file_00{my_tuple[0]} :",'   {:.2f},'.format(my_tuple[1]), '{:.2e},'.format(my_tuple[2]),'{:.2e},'.format(my_tuple[3]))
+
+# Task 2
+print("------Task 2------")
+
+# Task 3
+print("------Task 3------")
+
+# Task 3
+print("------Task 3------")
