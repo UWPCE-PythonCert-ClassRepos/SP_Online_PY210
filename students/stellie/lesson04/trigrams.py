e53ee8d2c01c83cf94afc89f459e08d5202a7c39
stellie <kims82@gmail.com>
2020-04-14 14:24:39 -0700

    create functions to read file, parse, and randomly pick one line to choose to make a trigram with

--- students/stellie/lesson04/trigrams.py
+++ students/stellie/lesson04/trigrams.py
@@ -1,33 +1,69 @@
 #!/usr/bin/env python3
 
+# Stella Kim
+# Assignment 3: Kata Fourteen
+
 import sys
 import re
+import random
 
-def read_file():
-    with open('ion.txt', 'r') as book_file:
-        book_text = book_file.read()
+def read_file(filename):
+    with open(filename, 'r') as book_file:
+        book_text = book_file.readlines()  # reads text file by line
         book_file.close()
-    words = re.split(r'\W+', book_text.lower())
-    print(words)
+    # print(book_text)
+    state = 0
+    data = []
+    start_text = '*** START OF THIS PROJECT GUTENBERG EBOOK THE WAR OF THE WORLDS ***'
+    end_text = '*** END OF THIS PROJECT GUTENBERG EBOOK THE WAR OF THE WORLDS ***'
+    for line in book_text:
+        if state == 0:
+            if start_text in line:
+                state = 1
+            else:
+                pass
+        elif state == 1:
+            if end_text in line:
+                state = 0
+            else:
+                line = re.sub(r'\W+', ' ', line.lower())
+                data.append(line)
+    return(data)
 
-def build_trigrams(words):
-    trigrams = {}
+def make_words(data):
+    clean_data = []
+    for i in data:
+        if i == ' ':
+            pass
+        else:
+            clean_data.append(i.strip())
+    num = random.randint(0, len(clean_data))
+    words = clean_data[num].split(' ')
+    return(words)
+
+def build_trigram(words):
+    print(words)
+    trigram = {}
     for item in range(len(words) - 2):
         pair = (words[item], words[item + 1])
+        # print(pair)
         follower = words[item + 2]
-        if pair not in trigrams:
-            trigrams[(pair)] = [follower]
-    print(trigrams)
+        # print(follower)
+        if pair not in trigram:
+            trigram[(pair)] = [follower]
+    return(trigram)
 
-    for item in trigrams.items():
-        print(item)
-    #return trigrams
+def create_story(trigram):
+    print(trigram)
 
-# Exits the program
-def exit_program():
-    print('\nThank you for visiting.')
-    sys.exit()
-
-if __name__ == "__main__":
-    #build_trigrams(words)'
-    read_file()
+if __name__ == '__main__':
+    # try:
+    #     filename = sys.argv[1]
+    # except IndexError:
+    #     print('Please pass in a filename')
+    #     sys.exit(1)
+    data = read_file('wotw.txt')
+    words = make_words(data)
+    word_pairs = build_trigram(words)
+    new_text = create_story(word_pairs)
+    # print(new_text)
