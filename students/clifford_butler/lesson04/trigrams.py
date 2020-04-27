2f2c74b97ee78d8b55863fc5374b920ced9fe7c7
Clifford Butler <cliff002@msn.com>
2020-04-26 17:36:49 -0700

    buidling the Trigrams dict.

---
+++ students/clifford_butler/lesson04/trigrams.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+'''
+Objectives
+Katas are about trying something many times. In this one, what we’re experimenting with is not just the code, but the heuristics of processing the text. What do we do with punctuation? Paragraphs? Do we have to implement backtracking if we chose a next word that turns out to be a dead end?
+
+I’ll fire the signal and the fun will commence…
+'''
+
+#!/usr/bin/env python3
+
+words = "I wish I may I wish I might".split()
+
+
+def build_trigrams(words):
+    """
+    build up the trigrams dict from the list of words
+
+    returns a dict with:
+       keys: word pairs
+       values: list of followers
+    """
+    trigrams = {}
+
+    # build up the dict here!
+
+    return trigrams
+
+
+if __name__ == "__main__":
+    trigrams = build_trigrams(words)
+    print(trigrams)
