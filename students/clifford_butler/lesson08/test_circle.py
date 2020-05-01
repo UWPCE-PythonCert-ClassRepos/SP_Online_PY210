dd7a901a19b9c3cffae1cc01dc9aa140f75c92bb
Clifford Butler <cliff002@msn.com>
2020-04-30 21:24:17 -0700

    Area
    area test passed.

--- students/clifford_butler/lesson08/test_circle.py
+++ students/clifford_butler/lesson08/test_circle.py
@@ -13,28 +13,34 @@ import pytest
 
 
 def test_circle():
-    # test circle class is working
+    # test if circle class is working
     c = Circle(8)
     assert c.radius == 8
     print("test_circle passed")
      
 def test_diameter():
-    #test diameter() function is working
+    #test if diameter() function is working
     c = Circle(1)
     assert c.radius == 1
     print("test_diameter passed")
     
 def test_diameter2():
-    ''' test diameter2() function is working'''
+    #test if diameter2() function is working
     c = Circle(4)
     c.diameter = 2
     assert c.diameter == 2
-    assert c.radius == 4
+    assert c.radius == 1
     print("test_diameter2 passed")
+    
+def test_area():
+    # test if area() function is working  
+    c = Circle(2)
+    assert c.area == 12.566370614359172
+    
     
 if __name__ == "__main__":
     test_circle()
     test_diameter()
     test_diameter2()
-    
-    print('All tests passed!!')
+    test_area()
+    print("All tests passed!!")
