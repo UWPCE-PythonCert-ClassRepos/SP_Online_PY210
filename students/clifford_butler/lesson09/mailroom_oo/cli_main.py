fee47ec5d2f8a62566268dde22b1f5aed882bc04
Clifford Butler <cliff002@msn.com>
2020-05-02 06:30:13 -0700

    Files
    Starting the clif main and test files with prev. mailroom syntex.

--- students/clifford_butler/lesson09/mailroom_oo/cli_main.py
+++ students/clifford_butler/lesson09/mailroom_oo/cli_main.py
@@ -4,8 +4,16 @@
 User interaction functions and main program flow
 """
 
-from donor_models import DonorCollection
+import donor_models as dm
 import sys
+import operator
+
+# dictionary with donor names and donation amounts
+donor_list = {"William Gates, III": [653772.32, 12.17],
+              "Jeff Bezos":  [877.33],
+              "Paul Allen": [663.23, 43.87, 1.32],
+              "Mark Zuckerberg": [1663.23, 4300.87, 10432.0],
+              "Alexandra Butler": [777.77, 44.44]}
 
 def main_menu():
     # display the main menu
@@ -18,30 +26,73 @@ def main_menu():
         ">>> ")))
     return input()
 
-def send_thank_you(donors):
-    # send thank you email based on user input information
-    try: 
-        name = input("Type the donors full name or type list to display donor names.")
-        if name == 'list':
-            for key in donors.name:
+def prompt_amount(full_name):
+    # request user input for donation amount
+    try:    
+        amount = input("What's the donation amount? \n >>")
+        amount = int(amount)
+        add_amount(full_name,amount) 
+    except ValueError:
+        print("Input must be a number. Donor information not entered. Try again! ")
+    return amount
+
+def add_amount(full_name,amount):
+    # add donation amount to the dictionary
+    donor_list[full_name].append(amount)
+
+def prompt_name():
+    # request user to input a full name
+    try:
+        full_name = input("Type the donors full name or type list to display donor names.")
+        if full_name == 'list':
+            for key in donor_list:
                 print(key)
-            print('')
-        elif name == "":
+            prompt_name()
+        elif full_name == "":
             raise TypeError
         else:
-            donors.add_donor(name)
+            add_name(full_name)
     except TypeError:
-        print("not valid name")
-    return name
-    print(donors)
+            print("\nNot a valid answer. Please enter a name.\n>>>")
+    return full_name
+
+def add_name(full_name):
+    # update add name to the dictionary
+    for donor in donor_list:
+        if full_name == donor:
+            break
+    else:
+        donor_list[full_name] = []
+
+def thank_you_text(full_name,amount):
+    # display thank you letter with donor name and donation amount
+    print ("\nHi {}:\n\nThank you for the generous donation of ${:2d}, Sincerely, \n\nClifford Butler".format(full_name,amount))
+
+def send_thank_you():
+    # send thank you email based on user input information
+    full_name = prompt_name()
+    amount = prompt_amount(full_name)
+    thank_you_text(full_name,amount)
+    main()
 
 def create_report():
-    # generate and display a report of the donors in donor_dict
-    pass
+    # Generate and display a report of the donors in donor_dict
+    report = []
+    print("\n{:<18}{:<6}{:<20}{}{:<25}{}{:<15}".format(*('Donor Name','|','Total Given','|','Num Gifts','|','Average Gift')))
+    print ('-'*90)
 
+    for donor, value in sorted(donor_list.items(), key=operator.itemgetter(1)):
+        if len(value) != 0:
+            report.append((donor, round(sum(value),2), len(value),round(sum(value)/len(value))))
+            print ("{:<20} {:>2} {:>12} {:>17}{:>17}{:>12}".format(*(donor, '$', round(sum(value),2), len(value), '$',round(sum(value)/len(value),1))))
+    return report
+    
 def letter_to_all():
-    # send thank you letter to all donors 
-    pass
+    # send thank you letter to all donors
+    for full_name in donor_list:
+        with open(f"{full_name}.txt","w+") as donor_letter:
+            donor_letter.write(f"Hi {full_name},\n\nThank you for the generous donation of ${sum(donor_list[full_name]):.2f}.\n\nSincerely,\nClifford Butler")
+    print("Thank you letters sent!")
 
 def exit_program():
     # exit the interactive script
@@ -49,24 +100,20 @@ def exit_program():
     sys.exit() 
 
 def main():
-    # prompt user to select option
-    donors = DonorCollection()
-    
     #dict with the user options and the functions
     switch_dict = {
         '1': send_thank_you,
         '2': create_report,
         '3': letter_to_all,
         '4': exit_program
-    } 
+    }
 
     while True:
         try:
             response = main_menu()
-            switch_dict[response](donors)
+            switch_dict[response]()
         except KeyError:
             print("\n'{}'  is not a valid option, please enter 1, 2, 3, or 4!. \n >> ".format(response))
 
 if __name__ == "__main__":
-    # runs the main function
-    main()
+   main()
