ca1da31e0ff6a73d5db6aab4ea04bfdf5741dac0
Clifford Butler <cliff002@msn.com>
2020-05-01 18:19:22 -0700

    Update donor_models.py
    Forming the initial classes in donor_models file.

--- students/clifford_butler/lesson09/mailroom_oo/donor_models.py
+++ students/clifford_butler/lesson09/mailroom_oo/donor_models.py
@@ -3,3 +3,11 @@
 """
 A class-based system for donor data encapsulation.
 """
+
+class Donor(object):
+    # Class responsible for donor data encapsulation
+    pass
+
+class DonorCollection(object):
+    # Class responsible for donor collection data encapsulation
+    pass
