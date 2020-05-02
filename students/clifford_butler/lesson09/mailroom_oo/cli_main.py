98717e6c3caa12ea1e24ac2bc103a781f37065b0
Clifford Butler <cliff002@msn.com>
2020-05-01 18:17:30 -0700

    Files
    Setting up the .py files.

---
+++ students/clifford_butler/lesson09/mailroom_oo/cli_main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""
+User interaction functions and main program flow
+"""
---
+++ students/clifford_butler/lesson09/mailroom_oo/donor_models.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for donor data encapsulation.
+"""
