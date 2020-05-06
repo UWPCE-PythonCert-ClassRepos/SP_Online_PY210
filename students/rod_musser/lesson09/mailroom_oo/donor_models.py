dd221e5bc634b1c03d2b3edee52588180af8d1e9
Rod Musser <rmusser@yahoo.com>
2020-05-05 20:37:31 -0700

    Added properties to Donor class

--- students/rod_musser/lesson09/mailroom_oo/donor_models.py
+++ students/rod_musser/lesson09/mailroom_oo/donor_models.py
@@ -3,6 +3,51 @@ class Donor:
     Represents a single donor.  Provides information about an individual donor including their
      donation history.
     """
+    def __init__(self, full_name):
+        """
+        Create a new donor
+
+        :param full_name: The first and last name of the donor.
+        """
+        self._name = full_name
+        self._donations = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, full_name):
+        self._name = full_name
+
+    @property
+    def num_of_donations(self):
+        """
+        Returns the number of donations this donor has made.
+        """
+        return len(self._donations)
+
+    @property
+    def sum_of_donations(self):
+        """
+        Returns the total number of donations this donor has made"""
+        return round(sum(self._donations[:]), 2)
+
+    @property
+    def average_donation(self):
+        """
+        Returns the average number of donations this donor has made
+        """
+        return round(self.sum_of_donations / self.num_of_donations, 2)
+
+    def add_donation(self, amount):
+        """
+        Add a donation to this donor's donation history
+
+        :param amount: The amount of the donation
+        """
+        amount = float(amount)
+        self._donations.append(round(amount, 2))
 
 
 class DonorCollection:
--- students/rod_musser/lesson09/mailroom_oo/test_mailroom_oo.py
+++ students/rod_musser/lesson09/mailroom_oo/test_mailroom_oo.py
@@ -1,5 +1,34 @@
-import donor_models
+from donor_models import Donor
 
 
-def test_add_donor():
-    assert False
+def test_new_donor():
+    d = Donor('John Smith')
+    assert d.name == 'John Smith'
+
+    d.name = 'Jane Doe'
+    assert d.name == 'Jane Doe'
+
+
+def test_add_donation():
+    d = Donor('Chuck Norris')
+    d.add_donation(50.05)
+    assert d.num_of_donations == 1
+    assert d.sum_of_donations == 50.05
+    assert d.average_donation == 50.05
+
+    d.add_donation(49.95)
+    assert d.num_of_donations == 2
+    assert d.sum_of_donations == 100.00
+    assert d.average_donation == 50.00
+
+    d.add_donation(20.211234)
+    assert d.num_of_donations == 3
+    assert d.sum_of_donations == 120.21
+    assert d.average_donation == 40.07
+
+    d.add_donation("39.79")
+    assert d.num_of_donations == 4
+    assert d.sum_of_donations == 160.00
+    assert d.average_donation == 40.00
+
+
