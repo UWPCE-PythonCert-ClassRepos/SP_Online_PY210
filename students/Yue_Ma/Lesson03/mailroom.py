e603a7721a4cc757399b238cc7d242b07aedf2c8
Yue Ma <mayue3688@gmail.com>
2020-05-09 20:24:35 -0700

    Revise Mailroom

--- students/Yue_Ma/Lesson03/mailroom.py
+++ students/Yue_Ma/Lesson03/mailroom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 donor_list = {'Yue Ma': [100000, 1561132],
               'Yanan Ma': [1000, 5645, 6161, 27],
@@ -17,13 +18,10 @@ def main():
         user_main = input(main_prompt)
         if user_main == '1':
             send_a_thank_you_note()
-            break
         elif user_main == '2':
             create_a_report()
-            break
         elif user_main == '3':
-            print('Thank you for using Mailroom! See you next time!')
-            break
+            exit()
         else:
             print("Not a valid option!!!")
 
@@ -32,15 +30,13 @@ def send_a_thank_you_note():
     send_a_thank_you_note_prompt = "\n".join(("Please choose from below options:",
                                               "Enter: 'list' - Print a list of donors",
                                               "Enter: '1' - Print the donation history of individual donors",
-                                              "Enter: '2' - Back to the main menu or Quit"))
+                                              "Enter: '2' - Back to the main menu"))
     while True:
         user_send_a_thank_you_note = input(send_a_thank_you_note_prompt)
         if user_send_a_thank_you_note == 'list':
             print(' ')
             print(list(donor_list.keys()))
             print(' ')
-            back_to_main()
-            break
         elif user_send_a_thank_you_note == '1':
             user_name = input("Please enter the full name that you want to search with a space in between"
                               "(Ex. 'Tom James'):")
@@ -54,20 +50,17 @@ def send_a_thank_you_note():
                 donor_list[user_name] = list_history
                 print('Updated the info to our database.')
                 print(email(user_name))
-                back_to_main()
             else:
                 print('')
                 print('This name is NOT in our database!')
                 donor_list[user_name] = [float(user_amount)]
                 print('Added the name to our database.')
                 print(email(user_name))
-                back_to_main()
-            break
         elif user_send_a_thank_you_note == '2':
-            back_to_main()
             break
         else:
             print("Not a valid option!!!")
+    return main()
 
 
 def create_a_report():
@@ -75,11 +68,15 @@ def create_a_report():
     value_title = list(title.values())
     key_title = list(title.keys())
 
+    # sort
+    donor_list_sort = dict(sorted(donor_list.items(), key=lambda t: t[0]))
+    print(donor_list_sort)
+
     # calculate total donation
     donation_matrix = []
-    for j in range(0, len(donor_list)):
-        donation_matrix.append([list(donor_list.keys())[j], sum(list(donor_list.values())[j]),
-                                len(list(donor_list.values())[j])])
+    for j in range(0, len(donor_list_sort)):
+        donation_matrix.append([list(donor_list_sort.keys())[j], sum(list(donor_list_sort.values())[j]),
+                                len(list(donor_list_sort.values())[j])])
 
     line_title = f'| {key_title[0]:<20}| {value_title[0][0]:<15} | {value_title[0][1]:<10} | {value_title[0][2]:<15}|'
     line_x = '|' + '-' * 69 + '|'
@@ -89,28 +86,17 @@ def create_a_report():
     print(line_1)
     print(line_title)
     print(line_x)
-    for i in range(0, len(donor_list)):
+    for i in range(0, len(donation_matrix)):
         main_lines = f'| {donation_matrix[i][0]:<20}| {donation_matrix[i][1]:<15.2f} | {donation_matrix[i][2]:<10} ' \
                      f'| {donation_matrix[i][1] / donation_matrix[i][2]:<15.2f}|'
         print(main_lines)
     print(line_1)
-    back_to_main()
+    return main()
 
 
-def back_to_main():
-    back_to_main_prompt = "\n".join(("Please choose from below options:",
-                                     "Enter: '1' - Back to the main menu",
-                                     "Enter: '2' - Quit"))
-    while True:
-        user_back_to_mean = input(back_to_main_prompt)
-        if user_back_to_mean == '1':
-            main()
-            break
-        elif user_back_to_mean == '2':
-            print('Thank you for using Mailroom! See you next time!')
-            break
-        else:
-            print("Not a valid option!!!")
+def exit():
+    print('Thank you for using Mailroom! See you next time!')
+    sys.exit()
 
 
 def email(x):
