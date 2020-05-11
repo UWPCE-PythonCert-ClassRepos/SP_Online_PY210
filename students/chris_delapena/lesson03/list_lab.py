c6007b999aa45f36cd2ef4538c1facc78e3293d1
Christopher Dela Pena <delapc@gmail.com>
2020-05-11 08:54:57 -0700

    Update to list_lab.py

--- students/chris_delapena/lesson03/list_lab.py
+++ students/chris_delapena/lesson03/list_lab.py
@@ -3,3 +3,11 @@
 #Chris Dela Pena
 #UW PCE PY210
 #Lesson 3.2 List Lab
+
+fruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
+print(fruit)
+
+#Ask user for another fruit and add to list
+addFruit = input("Enter a new fruit to the list: ")
+fruit.append(addFruit)
+print(fruit)
