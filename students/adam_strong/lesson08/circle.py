f34e223b956b39cff67e2f4858a45b7936951108
Adam Strong <str.adam.81@gmail.com>
2020-05-11 18:15:20 -0700

    Updated lesson08 assignment and mailroom_oo package for review

--- students/adam_strong/lesson08/circle.py
+++ students/adam_strong/lesson08/circle.py
@@ -11,6 +11,7 @@ class Circle(object):
 
     def __init__(self, radius):
         self.radius = radius
+        self._radius = radius
 
     @classmethod
     def from_diameter(cls, _diameter):
@@ -29,54 +30,54 @@ class Circle(object):
     
     @property
     def diameter(self):
-        return 2*self.radius
+        return 2*self._radius
     @diameter.setter
     def diameter(self, val):
-        self.radius = val / 2
+        self._radius = val / 2
     
     @property
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * (self._radius ** 2)
 
 
 
     def __str__(self):
         '''For use in print()'''
-        return 'This circle has a radius of {} and a diameter of {}'.format(self.radius, self.diameter)
+        return 'This circle has a radius of {} and a diameter of {}'.format(self._radius, self.diameter)
 
     def __repr__(self):
         '''For use when object itself is represented'''
-        return 'Circle({})'.format(self.radius)
+        return 'Circle({})'.format(self._radius)
 
     def __add__(self, other):
-        return Circle(self.radius + other.radius)
+        return Circle(self._radius + other.radius)
 
     def __mul__(self, other):
-        return Circle(self.radius * other)
+        return Circle(self._radius * other)
 
     __rmul__ = __mul__
 
     def __lt__(self, other):
-        return (self.radius < other.radius)
+        return (self._radius < other.radius)
 
     def __eq__(self, other):
-        return (self.radius == other.radius)
+        return (self._radius == other.radius)
 
 
 class Sphere(Circle):
     
     def __str__(self):
         '''For use in print()'''
-        return 'This sphere has a radius of {} and a diameter of {}'.format(self.radius, self.diameter)
+        return 'This sphere has a radius of {} and a diameter of {}'.format(self._radius, self.diameter)
 
     def __repr__(self):
         '''For use when object itself is represented'''
-        return 'Sphere({})'.format(self.radius)
+        return 'Sphere({})'.format(self._radius)
 
     @property
     def area(self):
-        return 4 * math.pi * (self.radius ** 2)
+        return 4 * math.pi * (self._radius ** 2)
 
     @property
     def volume(self):
-        return (4/3) * math.pi * (self.radius ** 3)
+        return (4/3) * math.pi * (self._radius ** 3)
