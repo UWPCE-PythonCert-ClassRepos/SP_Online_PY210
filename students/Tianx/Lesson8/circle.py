d4eaf480135b03e1406469e6c20d38688bf0bc89
“tianxie812” <xietian628@yahoo.com>
2020-05-16 15:03:40 -0700

    adding subclass, magic methods and more tests.

--- students/Tianx/Lesson8/circle.py
+++ students/Tianx/Lesson8/circle.py
@@ -13,33 +13,81 @@ class Circle(object):
 	"""
 	def __init__(self, radius):
 		self.radius = radius
-
+	# print it out nicely
 	def __str__(self):
-		return f'The radius is {self.radius}'
+		return f'Circle with radius: {self.radius}'
 
 	def __repr__(self):
 		return f'Circle({self.radius})'
 
+	# numeric protocols
+	def __eq__(self, other):
+		return self.radius == other.radius
+
+	def __lt__(self, other):
+		return self.radius < other.radius
+
+	def __le__(self, other):
+		return self.radius <= other.radius
+
+	def __ge__(self, other):
+		return self.radius >= other.radius
+
+	def __gt__(self, other):
+		return self.radius > other.radius
+
+	def __ne__(self, other):
+		return self.radius != other.radius
+
+	# numeric protocols
+	def __add__(self, other):
+		return Circle(self.radius + other.radius)
+
+	# used in a numeric context on the left side of the multiplication operator
+	def __mul__(self, other):
+		return Circle(self.radius * other)
+
+	# optional feature to reflect
+	def __rmul__(self, other):
+		return Circle(self.radius * other)
+
+	# diameter property
 	@property
 	def diameter(self):
 		return self.radius * 2
 
+	# diameter setter
 	@diameter.setter
 	def diameter(self, diameter):
 		self.radius = diameter / 2
 
+	# area property
 	@property
 	def area(self):
 		return math.pi * self.radius * self.radius
 
+	# alternate constructor
 	@classmethod
 	def from_diameter(cls, diameter):
-		self = cls()
-		self.radius = diameter / 2
-		return self
+		return cls(diameter/2)
 
+# Subclass
+class Sphere(Circle):
+	def __str__(self):
+		return f'Sphere with radius: {self.radius}'
 
+	def __repr__(self):
+		return f'Sphere({self.radius})'
 
+	# volume property
+	@property
+	def volume(self):
+		return (4 / 3) * math.pi * self.radius ** 3
+
+	# area property
+	@property
+	def area(self):
+		return 4 * math.pi * self.radius ** 2
 
 
 
--- students/Tianx/Lesson8/test_circle.py
+++ students/Tianx/Lesson8/test_circle.py
@@ -1,22 +1,93 @@
-from circle import Circle
-import pytest
 import math
+from circle import Circle, Sphere
+
 
 def test_radius():
 	c = Circle(4)
 	assert c.radius == 4
 
+
 def test_diameter():
 	c = Circle(5)
 	assert c.diameter == 10
 
-def test_setDiameter():
+
+def test_setdiameter():
 	c = Circle(6)
 	c.diameter = 3
 	assert c.diameter == 3
 	assert c.radius == 1.5
 
+
 def test_area():
 	c = Circle(3)
 	assert c.area == 28.274333882308138
 
+
+def test__repr__():
+	c = Circle(4)
+	assert repr(c) == 'Circle(4)'
+
+
+def test__str__():
+	c = Circle(4)
+	assert str(c) == 'Circle with radius: 4'
+	assert eval(repr(c)) == Sphere(4)
+
+
+def test_from_diameter():
+	c = Circle.from_diameter(8)
+	assert c.diameter == 8
+	assert c.radius == 4
+
+
+def test_compare_circles():
+	c1 = Circle(4)
+	c2 = Circle(8)
+	c3 = Circle(8)
+	assert (c1 > c2) is False
+	assert (c1 < c2) is True
+	assert (c1 == c2) is False
+	assert (c2 == c3) is True
+	circles = [Circle(6), Circle(7), Circle(8), Circle(4), Circle(0), Circle(2), Circle(3), Circle(5), Circle(9), Circle(1)]
+	circles.sort()
+	assert circles == [Circle(0), Circle(1), Circle(2), Circle(3), Circle(4), Circle(5), Circle(6), Circle(7), Circle(8), Circle(9)]
+
+
+def test_circle_numeric_protocol():
+	c1 = Circle(4)
+	c2 = Circle(8)
+	assert c1 + c2 == Circle(12)
+	assert c1 * 2 == Circle(8)
+
+
+def test_reflected():
+	c1 = Circle(4)
+	assert c1 * 3 == 3 * c1
+
+
+def test_sphere__repr__():
+	s = Sphere(4)
+	assert repr(s) == 'Sphere(4)'
+
+
+def test_sphere__str__():
+	s = Sphere(4)
+	assert str(s) == 'Sphere with radius: 4'
+	assert eval(repr(s)) == Sphere(4)
+
+
+def test_sphere_volume():
+	s = Sphere(4)
+	assert s.volume == (4 / 3 * math.pi * 4 ** 3)
+
+
+def test_sphere_from_diameter():
+	s = Sphere.from_diameter(4)
+	assert s.diameter == 4
+	assert s.radius == 2
+
+
+def test_sphere_area():
+	s = Sphere(4)
+	assert s.area == (4 * math.pi * (4 ** 2))
