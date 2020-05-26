8daac9173e1587f5ff866613cde56d520379e4ad
stellie <kims82@gmail.com>
2020-05-26 13:39:02 -0700

    setup files to begin assignment

---
+++ students/stellie/lesson09/donor_models.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Stella Kim
+# Assignment 8: Object Oriented Mailroom
+
+"""Donor Models"""
+
+
+class Donor(object):
+    # Class responsible for donor data encapsulation
+
+    # This class will hold all the information about a single donor, and have
+    # attributes, properties, and methods to provide access to the donor-
+    # specific information that is needed. Any code that only accesses
+    # information about a single donor should be part of this class.
+    pass
+
+
+class DonorCollection(object):
+    # Class responsible for donor collection data encapsulation
+
+    # This class will hold all of the donor objects, as well as methods to add
+    # a new donor, search for a given donor, etc. If you want a way to save and
+    # re-load your data, this class would hold that method, too.
+
+    # Your class for the collection of donors will also hold the code that
+    # generates reports about multiple donors.
+    pass
---
+++ students/stellie/lesson09/test_mailroom_oo.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import cli_main
+import os.path
+
+"""Test code for Object Oriented Mailroom"""
+
+
+def test_search_db():
+    """Test to see if existing donor in database returns True value"""
+    assert mailroom4.search_db('John Smith') is True
+    assert mailroom4.search_db('Mary Johnson') is True
+    assert mailroom4.search_db('David Carlton') is True
+    assert mailroom4.search_db('James Wright') is True
+    assert mailroom4.search_db('Caroline Baker') is True
+
+
+def test_search_db_unregistered():
+    """Test to see if unexisting donor in database returns False value"""
+    assert mailroom4.search_db('Jane Doe') is False
+
+
+def test_add_donation():
+    """Test expected result when adding donation amount to existing user"""
+    mailroom4.donor_db['John Smith'].append(300)
+    expected = [10000, 5000, 1000, 300]
+    assert mailroom4.donor_db['John Smith'] == expected
+
+
+def test_add_new_donor():
+    """Test expected result when adding new donor and donation to database"""
+    mailroom4.donor_db['Ryan Doe'] = [2000]
+    expected = [2000]
+    assert mailroom4.donor_db['Ryan Doe'] == expected
+
+
+def test_thank_you_email():
+    """Test expected thank you messsage after a donation is made"""
+    expected = ('\nThank you Ryan Doe for your generous donation amount of $300.00!')
+    assert mailroom4.thank_you_email('Ryan Doe', 300) == expected
+
+
+def test_create_report():
+    """Test expected donor data output after adding new donors and donations"""
+    donor_db = {'Ryan Doe': [2000, 2000], 'Jane Doe': [10000]}
+    donor_list = list(donor_db.items())
+    expected_donor1 = 'Jane Doe             | 10000.00     | 1          | 10000.00       '
+    expected_donor2 = 'Ryan Doe             | 4000.00      | 2          | 2000.00        '
+    assert mailroom4.create_report(donor_list)[0] == expected_donor1
+    assert mailroom4.create_report(donor_list)[1] == expected_donor2
+
+
+def test_create_file():
+    """Test that files were created for each donor"""
+    record = ('Ryan Doe', [2000, 2000])
+    mailroom4.create_file(record)
+    expected = 'Ryan Doe.txt'
+    assert os.path.exists(expected) is True
+
+
+def test_compose_letter():
+    """Test that donor letters are properly formatted for each donor"""
+    record = ('Ryan Doe', [2000, 2000])
+    expected = ('Dear Ryan Doe,'
+                '\n\nThank you for your 2 donations that total $4000.00.'
+                '\nIt will be put to very good use.'
+                '\n\n\tSincerely,\n\t-The Team')
+    assert mailroom4.compose_letter(record) == expected
