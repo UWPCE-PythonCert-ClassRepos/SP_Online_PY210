d16bec8c80918830051835724c2c43f7105dd187
Deniss Semcovs <deniss01@uw.edu>
2020-06-01 21:51:30 -0700

    Updated slicing exercise

--- students/Deniss_Semcovs/Lesson03/slicing_lab.py
+++ students/Deniss_Semcovs/Lesson03/slicing_lab.py
@@ -2,7 +2,12 @@
 a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
 def exchange_first_last(seq):
-    return seq[-1:]+seq[1:-1]+seq[:1]
+    return(seq[-1:]+seq[1:-1]+seq[:1]) #with the first and last items exchanged.
+    return(seq[::2]) #with every other item removed.
+    return(seq[4:-4:2]) #with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
+    return(seq[::-1]) #with the elements reversed (just with slicing).
+# I don't understand what is teh "the middle third"
+    return(seq[-3:-2]+seq[2:3]) #with the last third, then first third, then the middle third in the new order
 
 if __name__ == "__main__":    
     assert exchange_first_last(a_string) == "ghis is a strint"
