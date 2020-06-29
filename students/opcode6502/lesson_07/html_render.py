d6205db4b7ecc12b5304cd047078287669d07d71
Mark Hanschen <mark@markhanschen.com>
2020-06-28 22:52:17 -0700

    Updated class OneLineTag(Element).

--- students/opcode6502/lesson_07/html_render.py
+++ students/opcode6502/lesson_07/html_render.py
@@ -48,14 +48,12 @@ class Html(Element):
 class OneLineTag(Element):
 
     def render(self, out_file):
-        # loop through the list of contents:
-        for content in self.contents:
-            out_file.write("<{}>".format(self.tag))
-            try:
-                content.render(out_file)
-            except AttributeError:
-                out_file.write(content)
-            out_file.write("</{}>\n".format(self.tag))
+        out_file.write("<{}>".format(self.tag))
+        out_file.write(self.contents[0])
+        out_file.write("</{}>\n".format(self.tag))
+
+    def append(self, content):
+        raise NotImplementedError
 
 
 class P(Element):
