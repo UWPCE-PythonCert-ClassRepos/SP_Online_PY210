d06b3b261d2ccc42fabf85924a54049fa560a563
Olyskoop <snap@olymega.org>
2020-07-01 14:13:55 -0700

    Fixed exercise 3, refactored exercise 1

--- students/luftsmeerflier/lesson03/slicing_lab.py
+++ students/luftsmeerflier/lesson03/slicing_lab.py
@@ -3,48 +3,29 @@
 # with the first and last items exchanged
 
 def exchange_first_last(seq):	
-	def switch(input_):
-		output = input_
+    def switch(input_):
+	    output = input_
 		reverse = input_[::-1]
-		output[0:1] = reverse[0:1]
-		output[-1] = reverse[-1]
-		return output
-		
-	if type(seq) == tuple:
-		return tuple(switch(list(seq)))
-	elif type(seq) == str:
-		return ''.join(switch(list(seq)))
-	else:
-		print('Please input a string or tuple')
+		if type(input_) == str:
+			return reverse[0:1] + output[1:-1] + reverse[0-1]
+		else:
+			output = list(output)
+			output[0:1] = reverse[0:1]
+			output[-1] = reverse[-1]
+			return tuple(output)
+
+	return switch(seq)
 
 def every_other_removed(seq):
-	if type(seq) == tuple:
-		output = list(seq)
-		return tuple(output[::2])
-	elif type(seq) == str:
-		return seq[::2]
-	else:
-		print('Please input a string or tuple')
+    return seq[::2]
 
 # with the first 4 and the last 4 items removed, and the nevery other item in the remaning sequence
 def first_and_last_four_removed(seq):
-	if type(seq) == str:
-		return seq[4:-4]
-	elif type(seq) == tuple:
-		output = list(seq)
-		return tuple(output[4:-4])
-	else:
-		print('Please input a string or tuple')
+	return seq[4:-4:2]
 
 #with the elements reversed (just with slicing)
 def elements_reversed(input):
-	if type(input == str):
-		# return reverse(input)
-		return input[::-1]
-
-	elif type(seq) == tuple:
-		output = list(seq)
-		return tuple(output[::-1])
+	return input[::-1]
 
 # Pass
 assert exchange_first_last("hello") == "oellh"
@@ -55,8 +36,8 @@ assert every_other_removed("hello") == "hlo"
 assert every_other_removed((1,2,3,4,5)) == (1,3,5)
 
 # Pass 
-assert first_and_last_four_removed("helloworld") == "ow"
-assert first_and_last_four_removed((1,2,3,4,5,6,4,3,2,1)) == (5,6) 
+assert first_and_last_four_removed("helloworld") == "o"
+assert first_and_last_four_removed((1,2,3,4,5,6,7,4,3,2,1)) == (5,7) 
 
 # Pass
 assert elements_reversed("hello") == "olleh"
