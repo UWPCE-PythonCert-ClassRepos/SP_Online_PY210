86eb4437f1000f6725ed683535b7115fb6d8c98d
Mark Hanschen <mark@markhanschen.com>
2020-07-01 00:22:22 -0700

    Update test_circle_area_setter().

--- students/opcode6502/lesson_08/test_circle.py
+++ students/opcode6502/lesson_08/test_circle.py
@@ -1,12 +1,15 @@
 # test_circle.py
 # opcode6502: SP_Online_PY210
 
+
 from circle import *
+
 
 def debug_print_attributes(c):
     print("c.raidus:   " + str(c.radius))
     print("c.diameter: " + str(c.diameter))
     print("c.area:     " + str(c.area))
+
 
 def test_circle_radius():
     #
@@ -19,6 +22,7 @@ def test_circle_radius():
     # Assertion.
     assert c.radius == 4
 
+
 def test_circle_diameter():
     #
     # Test setup.
@@ -29,6 +33,7 @@ def test_circle_diameter():
     #
     # Assertion.
     assert c.diameter == 8
+
 
 def test_set_circle_diameter():
     #
@@ -42,6 +47,7 @@ def test_set_circle_diameter():
     # Assertion.
     assert c.radius == 1
     assert c.diameter == 2
+
 
 def test_circle_area():
     #
@@ -62,3 +68,23 @@ def test_circle_area():
     #
     # Assertion.
     assert c.area == 78.53981633974483
+
+
+def test_circle_area_setter():
+    c = Circle(2)
+    try:
+        c.area = 5
+        #
+        # If this succeeds, assert False.
+        debug_print_attributes(c)
+        assert False
+    except AttributeError:
+        #
+        # Since we have an AttributeError, assert True.
+        assert True
+    except Exception as e:
+        #
+        # Anything else, print the debug attributes and assert False.
+        debug_print_attributes(c)
+        print(e)
+        assert False
