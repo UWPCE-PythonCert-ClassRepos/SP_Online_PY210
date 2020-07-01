a14b00e176a2378b3f627f98686af1a120bfb1c6
Mark Hanschen <mark@markhanschen.com>
2020-07-01 00:15:20 -0700

    Updated tests, added debug helper function.

--- students/opcode6502/lesson_08/test_circle.py
+++ students/opcode6502/lesson_08/test_circle.py
@@ -9,27 +9,56 @@ def debug_print_attributes(c):
     print("c.area:     " + str(c.area))
 
 def test_circle_radius():
+    #
+    # Test setup.
     c = Circle(4)
+    #
+    # Print debug statements (in case of failure).
     debug_print_attributes(c)
+    #
+    # Assertion.
     assert c.radius == 4
 
 def test_circle_diameter():
+    #
+    # Test setup.
     c = Circle(4)
+    #
+    # Print debug statements (in case of failure).
     debug_print_attributes(c)
+    #
+    # Assertion.
     assert c.diameter == 8
 
 def test_set_circle_diameter():
+    #
+    # Test setup.
     c = Circle(4)
     c.diameter = 2
+    #
+    # Print debug statements (in case of failure).
     debug_print_attributes(c)
+    #
+    # Assertion.
     assert c.radius == 1
     assert c.diameter == 2
 
 def test_circle_area():
+    #
+    # Test setup.
     c = Circle(2)
+    #
+    # Print debug statements (in case of failure).
     debug_print_attributes(c)
+    #
+    # Assertion.
     assert c.area == 12.566370614359172
-
+    #
+    # Test setup.
     c = Circle(5)
+    #
+    # Print debug statements (in case of failure).
     debug_print_attributes(c)
+    #
+    # Assertion.
     assert c.area == 78.53981633974483
