311c693deb106d75bb41f46f0d577e272dc8eaea
Mark Hanschen <mark@markhanschen.com>
2020-06-30 18:29:25 -0700

    Added else in try block.

--- students/opcode6502/lesson_05/except_exercise.py
+++ students/opcode6502/lesson_05/except_exercise.py
@@ -25,6 +25,8 @@ try:
     not_joke = fun(first_try[2])
 except SyntaxError:
     print('Run Away!')
+else:
+    print(not_joke)
 
 # What did that do? You can think of else in this context, as well as in
 # loops as meaning: "else if nothing went wrong"
