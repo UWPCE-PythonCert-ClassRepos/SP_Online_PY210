d506403ca68adcef6754c21c0412faf56e1248b5
Mark Hanschen <mark@markhanschen.com>
2020-07-02 15:21:50 -0700

    Test case updates.

--- students/opcode6502/lesson_08/test_circle.py
+++ students/opcode6502/lesson_08/test_circle.py
@@ -256,13 +256,149 @@ def test_sphere_area():
     #
     # Print debug statements (in case of failure).
     debug_print_attributes(s1)
-    debug_print_attributes(s2)
     #
     # Assertion.
     assert s1.area == 50.26548245743669
 
 
+def test_sphere_add():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    s2 = Sphere(4)
+    result = s1 + s2
+    #
+    # Print debug statements (in case of failure).
+    print(str(result))
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert result == Sphere(6)
+
+
+def test_sphere_area():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert s1.area == 50.26548245743669
+    #
+    # Test setup.
+    s1 = Sphere(5)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert s1.area == 314.1592653589793
+
+
+def test_sphere_area_setter():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    #
+    #
+    try:
+        #
+        # Test setup.
+        s1.area = 5
+        #
+        # If this succeeds, assert False.
+        debug_print_attributes(s1)
+        #
+        # Assertion.
+        assert False
+    except AttributeError:
+        #
+        # Since we have an AttributeError, assert True.
+        #
+        # Assertion.
+        assert True
+    except Exception as e:
+        #
+        # Anything else, print the debug attributes and assert False.
+        debug_print_attributes(s1)
+        print(e)
+        #
+        # Assertion.
+        assert False
+
+
 def test_sphere_diameter():
+    #
+    # Test setup.
+    s1 = Sphere(4)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert s1.diameter == 8
+
+
+def test_sphere_diameter_setter():
+    #
+    # Test setup.
+    s1 = Sphere(4)
+    s1.diameter = 2
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert s1.radius == 1
+    assert s1.diameter == 2
+
+
+def test_sphere_eq_01():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    s2 = Sphere(2)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert (s1 == s2) is True
+
+
+def test_sphere_eq_02():
+    #
+    # Test setup.
+    s1 = Sphere(4)
+    s2 = Sphere(1)
+    s2 = s1
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert (s1 == s2) is True
+
+
+def test_from_diameter():
+    #
+    # Test setup.
+    s1 = Sphere.from_diameter(4)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert s1.diameter == 4
+    assert s1.radius == 2
+
+
+def test_sphere_ge():
     #
     # Test setup.
     s1 = Sphere(2)
@@ -273,23 +409,92 @@ def test_sphere_diameter():
     debug_print_attributes(s2)
     #
     # Assertion.
-    assert s1.diameter == 4
-    assert s2.diameter == 8
+    assert (s2 >= s1) is True
+
+
+def test_sphere_gt():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    s2 = Sphere(4)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert (s2 > s1) is True
+
+
+def test_sphere_le():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    s2 = Sphere(4)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert (s1 <= s2) is True
+
+
+def test_sphere_lt():
+    #
+    # Test setup.
+    s1 = Sphere(2)
+    s2 = Sphere(4)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    debug_print_attributes(s2)
+    #
+    # Assertion.
+    assert (s1 < s2) is True
+
+
+def test_sphere_mul():
+    #
+    # Test setup.
+    s1 = Sphere(5)
+    #
+    # Print debug statements (in case of failure).
+    debug_print_attributes(s1)
+    #
+    # Assertion.
+    assert repr(s1 * 5) == repr(Sphere(25))
 
 
 def test_sphere_radius():
     #
     # Test setup.
-    s1 = Sphere(2)
-    s2 = Sphere(4)
+    s1 = Sphere(4)
     #
     # Print debug statements (in case of failure).
     debug_print_attributes(s1)
-    debug_print_attributes(s2)
     #
     # Assertion.
-    assert s1.radius == 2
-    assert s2.radius == 4
+    assert s1.radius == 4
+
+
+def test_sphere_sort_key():
+    #
+    # Test setup.
+    spheres = [Sphere(5), Sphere(1), Sphere(4), Sphere(3), Sphere(2)]
+    spheres.sort(key=Sphere.sort_key)
+    #
+    # Print debug statements (in case of failure).
+    print(spheres)
+    #
+    # Assertion.
+    assert spheres == [Sphere(1), Sphere(2), Sphere(3), Sphere(4), Sphere(5)]
+
+
+def test_sphere_str():
+    #
+    # Assertion.
+    assert str(Sphere(100)) == "Sphere: Radius: 100"
 
 
 def test_sphere_volume():
