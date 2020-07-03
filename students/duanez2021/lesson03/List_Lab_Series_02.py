2b90a153e01a729cc1a948f6b792f5e98e264385
duanez2015 <duane.j.mccollum@boeing.com>
2020-07-02 17:53:22 -0700

    List_Lab_Series_01_REV03.py List_Lab_Series_02.py List_Lab_Series_03.py List_Lab_Series_04.py mailroom_rev01.py Slice_Lab_01_REV03.py

--- students/duanez2021/lesson03/List_Lab_Series_02.py
+++ students/duanez2021/lesson03/List_Lab_Series_02.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
+
 ##############################################################
 # 20200616    djm list lab series 2
+# 20200701    djm   resub, updated file  for list lab series 02
 #
 # Duane McCollum Python self-paced winter 2020
 #
@@ -20,18 +22,22 @@ import sys
 #############################################################
 
 # Series 2
-fruit = list(('Apples', 'Pears', 'Oranges' ,'Peaches'))
-#s = "I can't tell the difference between Whizzo butter and this dead crab."
-# find a fruit by a name, return T/F
-def find_a_fruit (x, seq):
+fruit = list(('Apples', 'Pears', 'Oranges', 'Peaches'))
+
+
+def find_a_fruit(x, seq):
     b = False
     x = x.lower()
     for i, j in enumerate(seq):
         if x in j.lower():
             b = True
-    return  b
+    return b
+
+
 # find a fruit by name, return the index
-def find_a_fruit_index (x, seq):
+
+
+def find_a_fruit_index(x, seq):
     idx = False
     x = x.lower()
     for i, j in enumerate(seq):
@@ -39,11 +45,11 @@ def find_a_fruit_index (x, seq):
             idx = i
     return idx
 
+
 def remove_last_list_item(seq):
-    x=seq[::-1]
-    x=x[1:]
-    x=x[::-1]
-    #seq=x
+    x = seq[::-1]
+    x = x[1:]
+    x = x[::-1]
     return x
 
 
@@ -55,7 +61,7 @@ def remove_last_list_item(seq):
 
 def fruit_loops_delete(seq):
     x = seq[:]
-    print (x)
+    print(x)
     pop_item = input("Do you want to remove the last item from the list? (y/n) > ")
     if pop_item.lower() == 'y':
         x = remove_last_list_item(x)
@@ -64,21 +70,25 @@ def fruit_loops_delete(seq):
         print(x)
         remove_item = input("Remove a fruit (by name) > ")
         print('your entered ' + remove_item + ' to be removed.')
-        if not find_a_fruit (remove_item, x):
+        if not find_a_fruit(remove_item, x):
             print(remove_item + ' was not found.')
-        while find_a_fruit (remove_item, x):
-            #print(remove_item + ' will be removed.')
+        while find_a_fruit(remove_item, x):
+            # print(remove_item + ' will be removed.')
             i = find_a_fruit_index(remove_item, x)
-            sRemove=x[i]
-            x.remove(sRemove)
-            #print(seq)
+            sremove = x[i]
+            x.remove(sremove)
+            # print(seq)
         print(x)
         status = input("Continue? (y/n) > ")
-        if status == 'n': exit()
+        if status == 'n':
+            exit()
         if not len(x):
-            print ("List is empty!")
+            print("List is empty!")
             exit()
 
-fruit = list(('Apples', 'Pears', 'Oranges' ,'Peaches'))*2
+
+fruit = list(('Apples', 'Pears', 'Oranges', 'Peaches')) * 2
+
 fruit_loops_delete(fruit)
+
 find_a_fruit('Apples', fruit[1:4])
