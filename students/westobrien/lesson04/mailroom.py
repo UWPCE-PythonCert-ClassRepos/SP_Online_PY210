c2fc0ff4dd6aa194fcac0c59cc5864d2dd38c12d
West Obrien <westobrien@gmail.com>
2020-07-03 11:42:55 -0700

    Finished mailroom pt2

--- students/westobrien/lesson04/mailroom.py
+++ students/westobrien/lesson04/mailroom.py
@@ -3,39 +3,36 @@
 import sys
 from operator import itemgetter
 
-donor_db = [('Bob', [5.00, 10.00, 20.00, 150000.00]), ('Kathy', [20.00]),
-            ('Sherry', [50.00, 100.00]), ('Sophia', [1000.00]),
-            ('Chet', [10000.00, 10000.00])]
+donor_db = {'Bob': [5.00, 10.00, 20.00, 15000.00],
+            'Kathy': [20, 00],
+            'Sherry': [50.00, 100.00],
+            'Sophia': [1000.00],
+            'Chet': [10000.00, 10000.00]}
 
-prompt = "\n".join(("\nDonation Database Menu\n",
-                    "Please choose from below options:",
-                    "1 - Send a Thank You",
-                    "2 - Create a Report",
-                    "3 - Quit",
-                    ">>> "))
+main_prompt = "\n".join(("\nDonation Database Menu\n",
+                         "Please choose from below options:",
+                         "1 - Send a Thank You to an single Donor",
+                         "2 - Create a Report",
+                         "3 - Send a letter to all Donors",
+                         "4 - Quit",
+                         ">>> "))
 
 
 def thank_you():
     name = input("\n".join(("\nPlease type a Full Name:",
                             ">>> ")))
-    name_list = []
-    for names in donor_db:
-        name_list.append(names[0])
-    # change to while name == "list"
     if name == "list":
-        print(name_list)
+        print(donor_db.keys())
         thank_you()
-    elif name in name_list:
+    elif name in donor_db:
         amount = input("\n".join(("\nPlease type a donation amount:",
                                   ">>> ")))
 
-        donor_db[name_list.index(name)][1].append(float(amount))
-        print(donor_db)
+        donor_db.get(name).append(float(amount))
     else:
         amount = input("\n".join(("\nPlease type a donation amount: ",
                                   ">>> ")))
-        new_donor = (name, [float(amount)])
-        donor_db.append(new_donor)
+        donor_db[name] = [float(amount)]
         print(donor_db)
 
     print("\n Thank you {} For your generous donation of {} dollars! We appreciate your generous support"
@@ -44,18 +41,15 @@ def thank_you():
 
 def create_report():
     donor_db_sum = []
-    len_name = 0
-    for donor in donor_db[:]:
+    for donor in donor_db.copy():
         sum_donation = 0
         num_gifts = 0
         avg_gift = 0
-        if len(donor[0]) > len_name:
-            len_name = len(donor[0])
-        for donation in donor[1]:
+        for donation in donor_db.get(donor):
             sum_donation += donation
             num_gifts += 1
         avg_gift = sum_donation / num_gifts
-        donor_db_sum_object = (donor[0], sum_donation, num_gifts, avg_gift)
+        donor_db_sum_object = (donor, sum_donation, num_gifts, avg_gift)
         donor_db_sum.append(donor_db_sum_object)
 
     donor_db_sum = sorted(donor_db_sum, key=itemgetter(1), reverse=True)
@@ -76,19 +70,28 @@ def exit_program():
     sys.exit()
 
 
-def main():
-    while True:
-        response = input(prompt)
+def letters():
+    for donor in donor_db.copy():
+        with open(donor + ".txt", "w") as f:
+            f.write("Dear {}".format(donor))
+            f.write("\nThank you for your very kind donation of ${:.2f}".format(sum(donor_db.get(donor))))
+            f.write("\n\nIt will be put to very good use")
+            f.write("\nSincerely, \n\t The Team")
 
-        if response == "1":
-            thank_you()
-        elif response == "2":
-            create_report()
-        elif response == "3":
-            exit_program()
-        else:
-            print("Not a valid option!")
+main_dispatch={
+    1: thank_you,
+    2: create_report,
+    3: letters,
+    4: exit_program
+}
+
+
+def menu_selection(prompt, dispatch_dict):
+    while True:
+        response=input(prompt)
+        print(response)
+        dispatch_dict.get(int(response))()
 
 
 if __name__ == "__main__":
-    main()
+    menu_selection(main_prompt, main_dispatch)
