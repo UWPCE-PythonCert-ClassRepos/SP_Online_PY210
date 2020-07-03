868381704a2739375872f501394d01f2786c4ba2
Mark Hanschen <mark@markhanschen.com>
2020-07-03 00:56:08 -0700

    Added def test_donor().

--- students/opcode6502/lesson_09/test_mailroom_oo.py
+++ students/opcode6502/lesson_09/test_mailroom_oo.py
@@ -1,2 +1,9 @@
 # test_mailroom.py
 # opcode6502: SP_Online_PY210
+
+import pytest
+from donor_models import *
+
+def test_donor():
+    donor = Donor('Archie Adams')
+    assert donor.name == 'Archie Adams'
