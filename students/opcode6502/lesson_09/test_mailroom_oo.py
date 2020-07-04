6247f58d2a9b1228f8394ceb9304a887555ec5e7
Mark Hanschen <mark@markhanschen.com>
2020-07-03 20:23:01 -0700

    Added def test_donor_init_donations().

--- students/opcode6502/lesson_09/test_mailroom_oo.py
+++ students/opcode6502/lesson_09/test_mailroom_oo.py
@@ -1,9 +1,24 @@
 # test_mailroom.py
 # opcode6502: SP_Online_PY210
 
+
 import pytest
 from donor_models import *
 
-def test_donor():
-    donor = Donor('Archie Adams')
-    assert donor.name == 'Archie Adams'
+
+def test_donor_init_donations():
+    #
+    # Test setup.
+    d = Donor('Archie Adams', [123.45])
+    #
+    # Assertion.
+    assert d.donations == [123.45]
+
+
+def test_donor_init_name():
+    #
+    # Test setup.
+    d = Donor('Archie Adams')
+    #
+    # Assertion.
+    assert d.name == 'Archie Adams'
