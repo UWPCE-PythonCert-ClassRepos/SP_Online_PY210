78e98e775ed893bcfb0d54cd2ce5b0db51ab0c51
Mark Hanschen <mark@markhanschen.com>
2020-07-03 23:21:10 -0700

    Added def test_sum_donations().

--- students/opcode6502/lesson_09/test_mailroom_oo.py
+++ students/opcode6502/lesson_09/test_mailroom_oo.py
@@ -13,6 +13,12 @@ def print_debug_statement(d):
 
 def test_donor_init_donations():
     #
+    # Docstring.
+    """
+    This will test creating a Donor; specifically that the 'donations' property
+    is properly created and set.
+    """
+    #
     # Test setup.
     d = Donor('Archie Adams', [123.45])
     #
@@ -24,6 +30,12 @@ def test_donor_init_donations():
 
 
 def test_donor_init_name():
+    #
+    # Docstring.
+    """
+    This will test creating a Donor; specifically that the 'name' property
+    is properly created and set.
+    """
     #
     # Test setup.
     d = Donor('Archie Adams')
@@ -37,6 +49,11 @@ def test_donor_init_name():
 
 def test_add_donation():
     #
+    # Docstring.
+    """
+    This will test adding a donation for a given Donor.
+    """
+    #
     # Test setup.
     d = Donor('Archie Adams')
     d.add_donation([123.45])
@@ -46,3 +63,20 @@ def test_add_donation():
     #
     # Assertion.
     assert d.donations == [123.45]
+
+
+def test_sum_donations():
+    #
+    # Docstring.
+    """
+    This will test summing a set of donations for a given Donor.
+    """
+    #
+    # Test setup.
+    d = Donor('Archie Adams', [123.45, 678.90, 99.99])
+    #
+    # Debug statement; print data if we fail the assert.
+    print_debug_statement(d)
+    #
+    # Assertion.
+    assert d.sum_donations == 902.34
