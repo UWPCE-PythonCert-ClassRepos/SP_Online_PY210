45d76ebefeede3622ccefa3e7450bc4da988074e
Mark Hanschen <mark@markhanschen.com>
2020-07-03 20:33:02 -0700

    Added def test_add_donation().

--- students/opcode6502/lesson_09/test_mailroom_oo.py
+++ students/opcode6502/lesson_09/test_mailroom_oo.py
@@ -6,10 +6,18 @@ import pytest
 from donor_models import *
 
 
+def print_debug_statement(d):
+    print('d.name:            ' + str(d.name))
+    print('d.donations:       ' + str(d.donations))
+
+
 def test_donor_init_donations():
     #
     # Test setup.
     d = Donor('Archie Adams', [123.45])
+    #
+    # Debug statement; print data if we fail the assert.
+    print_debug_statement(d)
     #
     # Assertion.
     assert d.donations == [123.45]
@@ -20,5 +28,21 @@ def test_donor_init_name():
     # Test setup.
     d = Donor('Archie Adams')
     #
+    # Debug statement; print data if we fail the assert.
+    print_debug_statement(d)
+    #
     # Assertion.
     assert d.name == 'Archie Adams'
+
+
+def test_add_donation():
+    #
+    # Test setup.
+    d = Donor('Archie Adams')
+    d.add_donation([123.45])
+    #
+    # Debug statement; print data if we fail the assert.
+    print_debug_statement(d)
+    #
+    # Assertion.
+    assert d.donations == [123.45]
