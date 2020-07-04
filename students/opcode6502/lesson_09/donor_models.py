8ad066b38265909360eea143595861d7fbdb512f
Mark Hanschen <mark@markhanschen.com>
2020-07-03 20:22:02 -0700

    Added def test_donor_init_donations().

--- students/opcode6502/lesson_09/donor_models.py
+++ students/opcode6502/lesson_09/donor_models.py
@@ -1,11 +1,13 @@
 # donor_models.py
 # opcode6502: SP_Online_PY210
 
+
 class Donor:
 
     def __init__(self, name, donations=[]):
         self.name = name
         self.donations = donations
+
 
 class DonorCollection:
     pass
