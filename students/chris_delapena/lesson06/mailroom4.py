3356b61491162063edac765101e69c82888643d6
Christopher Dela Pena <delapc@gmail.com>
2020-07-12 14:35:32 -0700

    Completed lesson 6 assignment

--- students/chris_delapena/lesson06/mailroom4.py
+++ students/chris_delapena/lesson06/mailroom4.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 #Chris Dela Pena
 #UW PCE PY210
-#Assignment 5 - Mailroom Part 3
-#2020/7/5
+#Assignment 5 - Mailroom Part 4
+#2020/7/10
 
-#Use exceptions for main menu entry and donation amount.
-#Use comprehension for list of donor names
+#Refactor for unit testing
+#Include add donor
 
 import sys
 
@@ -14,7 +16,7 @@ donor_db = {"William Gates, III": [653772.32, 12.17],
             "Mark Zuckerberg": [1663.23, 4300.87, 10432.0]
             }
 
-prompt = "\n".join(("Mail Room Program version 2.0",
+prompt = "\n".join(("Mail Room Program version 4.0",
             "Please choose from below options:",
             "1 - Send a Thank you",
             "2 - Send all Donors a Thank You",
@@ -30,53 +32,9 @@ prompt_name = "\n".join(("Please enter the full name of the donor",
 prompt_amount = "\n".join(("What was the donation amount?",
                 ">>> "))
 
-def thank_you():
-    donor_name = input(prompt_name)
-    if donor_name.upper() == 'Q':
-        return
-    elif donor_name.upper() == "LIST":
-        donor_list = [donor for donor in donor_db.keys()]
-        print(donor_list)
-    else:
-        #if donor is found in db
-        if donor_name in donor_db:
-            donation_amount = donation()
-            donor_db[donor_name].append(donation_amount)
-            print('-'*60)
-            print(f"Dear {donor_name}, thank you for your generous donation of ${donation_amount}."
-            " Regards, the Club Owners")
-            print('-'*60)
-            return
-        else:
-            print('-'*60)
-            print(f"Adding new donor {donor_name}")
-            donor_db[donor_name] = []
-            donation_amount = donation()
-            donor_db[donor_name].append(donation_amount)
-            print('-'*60)
-            #Use update method
-            return
-
-def thank_you_bulk():
-    for i in donor_db.items():
-        with open(f"{i[0]}.txt", "w") as note:
-            note.write(f"Dear {i[0]}, thank you for your generous donations totaling ${sum(i[1]):.2f} \n\n Regards, the club owners")
-
-def donor_report():
-    print('-'*60)
-    print('-'*60)
-    header = ["Donor", "Total Donated", "Times Donated", "Average Donation"]
-    print(f"{header[0]:<20}{header[1]:<15}{header[2]:<15}{header[3]:<7}")
-    print('-'*60)
-    #Create sorted list. use sorted(donor_db.items())
-    donors_sorted = sorted(donor_db.items(), key=sort_key,reverse=True)
-
-    for i in donors_sorted:
-        print(f"{i[0]:<20}{sum(i[1]):<15.2f}{len(i[1]):<15}{sum(i[1])/len(i[1]):<7.2f}")
-    print('-'*60)
-
-def sort_key(donor_db):
-    return donor_db[1]
+def add_donor(donor_name):
+    print(f"Adding new donor {donor_name}")
+    donor_db[donor_name] = []
 
 def donation():
         donation_amount = input(prompt_amount)
@@ -88,6 +46,59 @@ def donation():
                 return donation_amount
         except ValueError:
                 print("Not a dollar value")
+
+def add_donation(donor_name, donation_amount):
+    donor_db[donor_name].append(donation_amount)
+
+def list_donors():
+    donor_list = [donor for donor in donor_db.keys()]
+    print(donor_list)
+
+def thank_you():
+    donor_name = input(prompt_name)
+    if donor_name.upper() == 'Q':
+        return
+    elif donor_name.upper() == "LIST":
+        list_donors()
+    else:
+        #if donor is found in db
+        if donor_name not in donor_db:
+            add_donor(donor_name)
+        donation_amount = donation()
+        add_donation(donor_name, donation_amount)
+        print('-'*80)
+        print_thank_you(donor_name, donation_amount)
+        print('-'*80)
+        return
+
+def print_thank_you(donor_name, donation_amount):
+    print(f"Dear {donor_name}, thank you for your generous donation of ${donation_amount}. Regards, the Club Owners")
+
+def thank_you_bulk():
+    for i in donor_db.items():
+        with open(f"{i[0]}.txt", "w") as note:
+            note.write(f"Dear {i[0]}, thank you for your generous donations totaling ${sum(i[1]):.2f} \n\n Regards, the club owners")
+
+def donor_report():
+    print('-'*80)
+    print('-'*80)
+    header = ["Donor", "Total Donated", "Times Donated", "Average Donation"]
+    print(f"{header[0]:<20}{header[1]:<15}{header[2]:<15}{header[3]:<7}")
+    print('-'*80)
+    #Create sorted list. use sorted(donor_db.items())
+    donors_sorted = sort_donors()
+    format_report(donors_sorted)
+    print('-'*80)
+
+def sort_donors():
+    return sorted(donor_db.items(), key=sort_key, reverse=True)
+
+def sort_key(donor_db):
+    return donor_db[1]
+
+def format_report(donors_sorted):
+    for i in donors_sorted:
+        print(f"{i[0]:<20}{sum(i[1]):<15.2f}{len(i[1]):<15}{sum(i[1])/len(i[1]):<7.2f}")
 
 def quit():
     print("Goodbye and have a great day!")
