01d7412cfd738bedc90114801bfd0667d32bc8ec
Jason Jenkins <jdj0891@yahoo.com>
2020-07-28 16:51:15 -0700

    Updated Variable and Method names

--- students/jason_jenkins/lesson03/strformat_lab.py
+++ students/jason_jenkins/lesson03/strformat_lab.py
@@ -7,7 +7,7 @@ Author: Jason Jenkins
 """
 
 
-def formatString_1(fileNum, floatNum1, intNum, floatNum2):
+def format_string_1(file_num, float_num1, int_num, float_num2):
     """
     Task 1
     Format the string
@@ -19,12 +19,12 @@ def formatString_1(fileNum, floatNum1, intNum, floatNum2):
     """
 
     text = "file_{:0>3d} :{:9.2f}, {:.2e}, {:.3g}"
-    result = text.format(fileNum, floatNum1, intNum, floatNum2)
+    result = text.format(file_num, float_num1, int_num, float_num2)
     print(result)
     return result
 
 
-def formatString_2(fileNum, floatNum1, intNum, floatNum2):
+def format_string_2(file_num, float_num1, int_num, float_num2):
     """
     Task 2
     Format the string
@@ -35,45 +35,45 @@ def formatString_2(fileNum, floatNum1, intNum, floatNum2):
     :param floatNum2: display in scientific notation with 3 sig fig
     """
 
-    result = f"file_{fileNum:0>3d} :{floatNum1:9.2f}, {intNum:.2e}, {floatNum2:.3g}"
+    result = f"file_{file_num:0>3d} :{float_num1:9.2f}, {int_num:.2e}, {float_num2:.3g}"
     print(result)
     return result
 
 
-def formatString_3(*tmpTuple):
+def format_string_3(*tmp_tuple):
     """
     Rewrite a tumpe to the following
     the {total items} numbers are: {num1}, {num2}, ...
 
-    :param tmpTuple: tuple to be converted to string
+    :param tmp_tuple: tuple to be converted to string
     """
 
-    totalItems = len(tmpTuple)
+    total_items = len(tmp_tuple)
     formated_string = "the {} numbers are: "
-    formated_string += ", ".join(["{}"] * totalItems)
-    result = formated_string.format(totalItems, *tmpTuple)
+    formated_string += ", ".join(["{}"] * total_items)
+    result = formated_string.format(total_items, *tmp_tuple)
 
     print(result)
     return result
 
 
-def formatString_4(*tmpTuple):
+def format_string_4(*tmp_tuple):
     """
     Given a 5 element tuple: ( 4, 30, 2017, 2, 27)
     use string formating to print: '02 27 2017 04 30'
 
-    :param tmpTuple: tuple to be converted to string
+    :param tmp_tuple: tuple to be converted to string
     """
 
-    result = f"{tmpTuple[3]:0>2d} {tmpTuple[4]:0>2d} "
-    result += f"{tmpTuple[2]:0>4d} {tmpTuple[0]:0>2d} "
-    result += f"{tmpTuple[1]:0>2d}"
+    result = f"{tmp_tuple[3]:0>2d} {tmp_tuple[4]:0>2d} "
+    result += f"{tmp_tuple[2]:0>4d} {tmp_tuple[0]:0>2d} "
+    result += f"{tmp_tuple[1]:0>2d}"
 
     print(result)
     return result
 
 
-def formatString_5(tmpList=[]):
+def format_string_5(tmp_list=[]):
     """
     Given the following four element list: ['oranges', 1.3, 'lemons', 1.1]
     Write an f-string that will display:
@@ -82,17 +82,17 @@ def formatString_5(tmpList=[]):
     but change to display the names of the fruit in upper case,
     and the weight 20% higher (that is 1.2 times higher)
 
-    :param tmpList: list to be converted to string
+    :param tmp_list: list to be converted to string
     """
 
-    result = f"The weight of an {tmpList[0][:-1].upper()} is {tmpList[1] * 1.2} and "
-    result += f"the weight of a {tmpList[2][:-1].upper()} is {tmpList[3] * 1.2}"
+    result = f"The weight of an {tmp_list[0][:-1].upper()} is {tmp_list[1] * 1.2} and "
+    result += f"the weight of a {tmp_list[2][:-1].upper()} is {tmp_list[3] * 1.2}"
 
     print(result)
     return result
 
 
-def formatString_6(*tmpTuple):
+def format_string_6(*tmp_tuple):
     """
     Part 1:
     Print a table of several rows, each with a name, an age and a cost.
@@ -104,54 +104,57 @@ def formatString_6(*tmpTuple):
     can you work how to quickly print the tuple in columns that are 5
     charaters wide?
 
-    :param tmpList: list to be converted to string
+    :param tmp_list: list to be converted to string
     """
 
-    Col1 = "Name"
-    Col2 = "Age"
-    Col3 = "Cost"
+    col1 = "Name"
+    col2 = "Age"
+    col3 = "Cost"
 
     # Part 1
-    basicBurbon = ["BasicBurbon", "5", "25"]
-    niceBurbon = ["NiceBurbon", "20", "213"]
-    wowBurbon = ["WowBurbon", "123", "2353"]
-    oldBurbon = ["oldBurbon", "1323", "23135"]
+    basic_bourbon = ["Basicbourbon", "5", "25"]
+    nice_bourbon = ["Nicebourbon", "20", "213"]
+    wow_bourbon = ["Wowbourbon", "123", "2353"]
+    old_bourbon = ["oldbourbon", "1323", "23135"]
 
-    result = f"{Col1:20} {Col2:10} {Col3:10}\n"
-    result += f"{basicBurbon[0]:20} {basicBurbon[1]:10} {basicBurbon[2]:10}\n"
-    result += f"{niceBurbon[0]:20} {niceBurbon[1]:10} {niceBurbon[2]:10}\n"
-    result += f"{wowBurbon[0]:20} {wowBurbon[1]:10} {wowBurbon[2]:10}\n"
-    result += f"{oldBurbon[0]:20} {oldBurbon[1]:10} {oldBurbon[2]:10}\n"
+    result = f"{col1:20} {col2:10} {col3:10}\n"
+    result += f"{basic_bourbon[0]:20} {basic_bourbon[1]:10} {basic_bourbon[2]:10}\n"
+    result += f"{nice_bourbon[0]:20} {nice_bourbon[1]:10} {nice_bourbon[2]:10}\n"
+    result += f"{wow_bourbon[0]:20} {wow_bourbon[1]:10} {wow_bourbon[2]:10}\n"
+    result += f"{old_bourbon[0]:20} {old_bourbon[1]:10} {old_bourbon[2]:10}\n"
 
     print(result)
 
     # Part 2 (Couldn't figure out how to do if using f"")
-    print("".join(["{:5}"] * len(tmpTuple)).format(*tmpTuple))
+    result = "".join(["{:5}"] * len(tmp_tuple)).format(*tmp_tuple)
+    print(result)
+    return(result)
+
 
 
 if __name__ == "__main__":
     # testing
-    result = formatString_1(2, 123.4567, 10000, 12345.67)
+    result = format_string_1(2, 123.4567, 10000, 12345.67)
     assert result == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-    result = formatString_1(53, 1233.464327, 1012432000, 153345.6417)
+    result = format_string_1(53, 1233.464327, 1012432000, 153345.6417)
     assert result == 'file_053 :  1233.46, 1.01e+09, 1.53e+05'
 
-    result = formatString_2(2, 123.4567, 10000, 12345.67)
+    result = format_string_2(2, 123.4567, 10000, 12345.67)
     assert result == 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-    result = formatString_2(53, 1233.464327, 1012432000, 153345.6417)
+    result = format_string_2(53, 1233.464327, 1012432000, 153345.6417)
     assert result == 'file_053 :  1233.46, 1.01e+09, 1.53e+05'
 
-    assert formatString_3(2, 3, 5) == 'the 3 numbers are: 2, 3, 5'
-    assert formatString_3(2, 3, 5, 7, 9) == 'the 5 numbers are: 2, 3, 5, 7, 9'
+    assert format_string_3(2, 3, 5) == 'the 3 numbers are: 2, 3, 5'
+    assert format_string_3(2, 3, 5, 7, 9) == 'the 5 numbers are: 2, 3, 5, 7, 9'
 
-    assert formatString_4(4, 30, 2017, 2, 27) == '02 27 2017 04 30'
+    assert format_string_4(4, 30, 2017, 2, 27) == '02 27 2017 04 30'
 
-    result = formatString_5(['oranges', 1.3, 'lemons', 1.1])
+    result = format_string_5(['oranges', 1.3, 'lemons', 1.1])
     expected = "The weight of an ORANGE is 1.56 and the weight of a LEMON is 1.32"
     assert result == expected
 
-    formatString_6(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    formatString_6(1, 2, 3, 4)
+    format_string_6(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    assert format_string_6(1, 11, 111, 1111) == '    1   11  111 1111'
 
     print()
     print("All Tests Pass")
