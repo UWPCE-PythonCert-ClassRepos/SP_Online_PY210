f834b26bf5d42dc81debbc46aebc20eaac68f4e5
Devon Peterka <dhpeterka@gmail.com>
2020-07-29 06:18:58 -0700

    add partially complete grid code

---
+++ students/Devon_Peterka/Lesson_2/Grid_Print.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+print('Part 1 - Explicit Grid Printing without using function')
+print('+ ' + 4*'- ' + '+ ' + 4*'- ' + '+')
+for i in range(0,4):
+    print('|' + 9*' ' + '|' + 9*' ' + '|')
+print('+ ' + 4*'- ' + '+ ' + 4*'- ' + '+')
+for i in range(0,4):
+    print('|' + 9*' ' + '|' + 9*' ' + '|')
+print('+ ' + 4*'- ' + '+ ' + 4*'- ' + '+')
+print()
+
+
+
+
+#Part 2 - Print grid of arbitrary cell size using a function
+
+def print_grid(n):
+    for i in range (0,n+2):
+        if i in [0,n//2+1,n+1]:
+            for j in range (0,2*n+3):
+                if j in [0,n+1,2*(n+1)]:
+                    print('+', end='')
+                elif j%2 != 0:
+                    print(' ', end='')
+                else:
+                    print('-', end='')
+        else:
+            for j in range (0,2*n+3):
+                print('|', end='') if j in [0,n+1,2*(n+1)] else print(' ', end='')
+        print()
+
+print('Part 2 - Print grid using function for abitrary size')
+print('\n    grid of size 3')
+print_grid(3)
+print('\n    grid of size 15')
+print_grid(15)
+print('\n    grid of size 12')
+print_grid(12)
+
+
+
+#Part 3 - Print square grid of arbitrary size and cells
+def print_grid2(c,n):
+    return none
