627c00d92ab286998a712863f44e41a7ff926edc
Jason Jenkins <jdj0891@yahoo.com>
2020-07-29 20:17:05 -0700

    Created starting template

---
+++ students/jason_jenkins/lesson04/dict_lab.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Lesson 4: dict lab
+Course: UW PY210
+Author: Jason Jenkins
+"""
+
+
+def display_dict():
+    pass
+
+
+def delete_entry():
+    pass
+
+
+def add_entry():
+    pass
+
+
+if __name__ == "__main__":
+    # Create a list
+    print("Program Started")
