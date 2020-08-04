d1a5a6e08bb480785c6fb9ff5fecde773c10dff9
Olyskoop <snap@olymega.org>
2020-08-04 14:16:44 -0700

    final submission

---
+++ students/luftsmeerflier/lesson07/test_html_render.py
@@ -0,0 +1,11 @@
+"""
+test code for html_render.py
+
+This is just a start -- you will need more tests!
+"""
+
+import io
+import pytest
+
+# import * is often bad form, but makes it easier to test everything in a module
+from html_render import *test_re
