2c33c7b3c21fad7e6fdc267c42f00bc67f4a4751
Jason Jenkins <jdj0891@yahoo.com>
2020-08-04 10:46:15 -0700

    Starting Point

--- students/jason_jenkins/lesson04/trigrams.py
+++ students/jason_jenkins/lesson04/trigrams.py
@@ -6,9 +6,9 @@ Course: UW PY210
 Author: Jason Jenkins
 """
 
-
+# Global
 words = "I wish I may I wish I might".split()
-
+trigrams = {}
 
 def build_trigrams(words):
     """
@@ -26,5 +26,19 @@ def build_trigrams(words):
 
 
 if __name__ == "__main__":
-    trigrams = build_trigrams(words)
-    print(trigrams)
+    # get the filename from the command line
+    print("Program Started")
+    """
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
+
+    in_data = read_in_data(filename)
+    words = make_words(in_data)
+    word_pairs = build_trigram(words)
+    new_text = build_text(word_pairs)
+
+    print(new_text)
+    """
