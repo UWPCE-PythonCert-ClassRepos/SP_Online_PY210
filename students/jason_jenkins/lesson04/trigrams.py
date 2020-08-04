510a80b2c030c8b2d0f021899da88b27e7177b6b
Jason Jenkins <jdj0891@yahoo.com>
2020-08-04 13:46:50 -0700

    Basic Functions Complete. Need to add more formating logic

--- students/jason_jenkins/lesson04/trigrams.py
+++ students/jason_jenkins/lesson04/trigrams.py
@@ -6,33 +6,81 @@ Course: UW PY210
 Author: Jason Jenkins
 """
 
-# Global
-words = "I wish I may I wish I might".split()
-trigrams = {}
+import random
+import sys
 
-def build_trigrams(words):
+
+def build_trigram(words):
     """
     build up the trigrams dict from the list of words
-
-    returns a dict with:
-       keys: word pairs
-       values: list of followers
     """
-    trigrams = {}
+
+    trigrams = dict()
+
+    for i in range(len(words) - 2):
+        pair = tuple(words[i:i + 2])
+        follower = words[i + 2]
+        if(pair in trigrams):
+            trigrams[pair].append(follower)
+        else:
+            trigrams[pair] = [follower]
 
     # build up the dict here!
 
     return trigrams
 
 
+def make_words(text):
+    """
+    Splits a long string of text into an list of words
+    """
+
+    return text.split()
+
+
+def build_text(word_pairs, iterations=100000):
+    """
+    Creates new text from a trigram
+    """
+
+    text_list = []
+
+    # Create the start of the text
+    first_two = random.choice(list(word_pairs.keys()))
+
+    text_list.append(first_two[0])
+    text_list.append(first_two[1])
+    text_list.append(random.choice(word_pairs[first_two]))
+
+    # Iterate to create a long text stream
+    for i in range(iterations):
+        last_two = tuple(text_list[-2:])
+        if last_two in word_pairs:
+            text_list.append(random.choice(word_pairs[last_two]))
+
+    return " ".join(text_list)
+
+
+def read_in_data(filename):
+    """
+    Reads in text from a file
+    """
+
+    try:
+        with open(filename, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     # get the filename from the command line
-    print("Program Started")
-    """
     try:
         filename = sys.argv[1]
+
     except IndexError:
-        print("You must pass in a filename")
+        print("You must pass in a valid filename")
         sys.exit(1)
 
     in_data = read_in_data(filename)
@@ -41,4 +89,3 @@ if __name__ == "__main__":
     new_text = build_text(word_pairs)
 
     print(new_text)
-    """
