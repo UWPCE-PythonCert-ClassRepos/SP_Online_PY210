4704aebf5e81a662f0e9c76e3cdf03d70eaaff4a
Ian Mayther <maytheri@gmail.com>
2020-08-07 06:00:05 -0700

    Added brute force method for grid

--- students/IanMayther/Lesson02/Exercise2.2.py
+++ students/IanMayther/Lesson02/Exercise2.2.py
@@ -1,1 +1,14 @@
 #Exercise2.2.py
+
+#Creating Grid the "Hard way"
+print("+ - - - - + - - - - +")
+print("|         |         |")
+print("|         |         |")
+print("|         |         |")
+print("|         |         |")
+print("+ - - - - + - - - - +")
+print("|         |         |")
+print("|         |         |")
+print("|         |         |")
+print("|         |         |")
+print("+ - - - - + - - - - +")
