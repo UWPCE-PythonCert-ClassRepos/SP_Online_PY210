e2a0a5f8db5622aaf001a4e7cbe81cc4a55c3ac9
Ian Mayther <maytheri@gmail.com>
2020-08-07 06:39:11 -0700

    Starting loop to make arbitrary size

--- students/IanMayther/Lesson02/Exercise2.2.py
+++ students/IanMayther/Lesson02/Exercise2.2.py
@@ -12,3 +12,19 @@ print("|         |         |")
 print("|         |         |")
 print("|         |         |")
 print("+ - - - - + - - - - +")
+
+#Creating square grids of various widths
+x = 5
+y = 2
+#header to generate columns
+for w in range(y):
+    #first row
+    for z in range(x+1):
+        line = ""
+        if z == 0:
+            line = line + "+"
+        elif z == w*x:
+            line = line + " +"
+        else:
+            line = line + " -"
+        print(line, end= "")
