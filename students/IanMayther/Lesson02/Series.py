c86d932d4e1e3c2fa5cbc5d729aa2f94f44d9cfe
Ian Mayther <maytheri@gmail.com>
2020-08-09 15:01:09 -0700

    Initalization of Series.py

---
+++ students/IanMayther/Lesson02/Series.py
@@ -0,0 +1,12 @@
+#Exercise2.4.py
+
+def fibonacci(n):
+    if n == 0:
+        return print("0")
+    elif n == 1:
+        return print("1")
+    #elif n > 1:
+    #    for x in range(n):
+    #        if n 
+
+fibonacci(1)
