cee7a65b8f1c9a007fc2e7baeec3680efd6c8c1a
s-mar206 <stevenm8@gmail.com>
2020-08-10 13:20:51 -0700

    adding trigrams script

--- students/Steven/lesson04/trigrams.py
+++ students/Steven/lesson04/trigrams.py
@@ -30,21 +30,21 @@ def make_words(file):
 
 def build_text(db, n):  # params of word list and number of words to read
     book = ""
-    key = random.choice(list(db.keys()))  # word pair key
+    key = random.choice(list(db.keys()))  # select a random key from the sequence
     temp = list(key)
     for number in range(n):
-        nextWord = (db[key][random.randint(0, (len(db[key]) - 1))])
+        nextWord = (db[key][random.randint(0, (len(db[key]) - 1))])  # select the value of the key pair from a random number based on the length of the keys of input seq
         temp.append(nextWord)
         key = tuple(temp[-2:])
-    story = " ".join(temp)
-    story = story.split(".")
+    story = " ".join(temp)  # combine all the words
+    story = story.split(".")  # Split each line when there is a period
     for item in story:
-        book += (item[0].capitalize() + item[1:] + ".")
+        book += (item[0].capitalize() + item[1:] + ".")  # Make each sentence start with a capital letter and end with a period
     return book
 
 if __name__ == "__main__":
     filename = r"C:\Users\smar8\PycharmProjects\Python210\lesson04\sherlock_small.txt"
-    n = 100  # number of words to read from the file
+    n = 10  # number of key pairs of words to read from the file
     words = make_words(filename)
     trigrams = build_trigrams(words)
     new_text = build_text(trigrams, n)
