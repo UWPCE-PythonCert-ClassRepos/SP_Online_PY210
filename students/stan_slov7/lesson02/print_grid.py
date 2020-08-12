b9750f7e930c39e8a7ee780c5993f621fd00ea55
Stan Slov <s.slov7@gmail.com>
2020-08-11 20:11:37 -0400

    added part2 of print_grid exercise, the function with single param n for grid size where printGrid2(n) function added and tested.

--- students/stan_slov7/lesson02/print_grid.py
+++ students/stan_slov7/lesson02/print_grid.py
@@ -38,5 +38,51 @@ print("literal function printGrid1() creating 2x2 grid of size '9':\n")
 printGrid1()
 print()
 
-  
-  
+#----------------------------Part 2----------------------------
+
+def line2(n):
+    print("+",("- " *((n-1)//2)), end="")
+    #force integer division
+   
+def space2(n):
+    print("|", (" " *(n-1)), end="")
+
+
+def linerow2(n):
+    line2(n)
+    line2(n)
+    print("+")
+
+def spacerow2(n):
+    space2(n)
+    space2(n)
+    print("|")
+
+
+def printRow2(n):
+    linerow2(n)
+    for i in range((n-1)//2):
+        spacerow2(n)
+        #force integer division
+
+
+def printGrid2(n):
+    printRow2(n)
+    printRow2(n)
+    linerow2(n)
+
+#test2 
+#with various sizes where param: n is unit size of whole grid length/width
+print("test using printGrid2(n) function creating same 2x2 grid as before but with grid size n.\n")
+
+print("2x2 grid with grid size param n=15:")
+printGrid2(15)
+print()
+
+print("2x2 grid with grid size param n=3:")
+printGrid2(3)
+print()
+
+print("2x2 grid with grid size param n=9: (identical to literal grid created with printGrid1, grid size 9)")
+printGrid2(9)
+print()
