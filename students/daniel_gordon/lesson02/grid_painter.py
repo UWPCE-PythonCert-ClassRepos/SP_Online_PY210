7b8c70afed369017c5f40a717d4066a7e67debf4
djgordon-maker <dragongordon@gmail.com>
2020-08-12 17:00:42 -0700

    Part 2 of Grid Painter

--- students/daniel_gordon/lesson02/grid_painter.py
+++ students/daniel_gordon/lesson02/grid_painter.py
@@ -1,5 +1,17 @@
-print('+','-'*4,'+','-'*4,'+')
-for i in range(2):
-    for j in range(4):
-        print('|',' '*4,'|',' '*4,'|')
-    print('+','-'*4,'+','-'*4,'+')
+def print_grid(n):
+    cellSize = n//2
+    PrintLine(cellSize)
+    for i in range(2):
+        PrintCell(cellSize)
+        PrintLine(cellSize)
+    return True
+    
+def PrintLine(width):
+    print('+','-'*width,'+','-'*width,'+')
+    return True
+
+def PrintCell(size):
+    for i in range(size):
+        print('|',' '*size,'|',' '*size,'|')
+        
+print_grid(21)
