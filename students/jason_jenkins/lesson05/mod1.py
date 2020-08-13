7b9b311fc85147556b7b98485b1d5ad14cd98069
Jason Jenkins <jdj0891@yahoo.com>
2020-08-12 20:21:55 -0700

    Practice during lesson 5

---
+++ students/jason_jenkins/lesson05/mod1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Lesson 5: Mod1
+Course: UW PY210
+Author: Jason Jenkins
+"""
+
+x = 5
---
+++ students/jason_jenkins/lesson05/mod3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Lesson 5: Mod3
+Course: UW PY210
+Author: Jason Jenkins
+"""
+
+import mod1
+
+print("In mod3 -- changing the value of mod1.x")
+
+mod1.x = 555
