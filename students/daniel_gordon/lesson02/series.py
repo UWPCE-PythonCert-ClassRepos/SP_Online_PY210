303f8fde289ee770fa090b05bd30185445d789da
djgordon-maker <dragongordon@gmail.com>
2020-08-13 20:37:06 -0700

    Added generalized series function, and corrected misconception

--- students/daniel_gordon/lesson02/series.py
+++ students/daniel_gordon/lesson02/series.py
@@ -1,12 +1,16 @@
 def fibonacci(n):
     """Calculates the nth value of the fibonacci sequence"""
-    if n == 0 or n == 1:
-        return n
-    else:
-        return fibonacci(n-2) + fibonacci(n-1)
+    return sum_series(n)
 
 def lucas(n):
     """Calculates the nth lucas number"""
-    if n == 1 or n == 2:
-        return n
-    return lucas(n-2) + lucas(n-1)
+    return sum_series(n, 2, 1)
+ 
+ def sum_series(n, n0 = 0, n1 = 1):
+    """Calculates the nth value of an arbitrary summation"""
+    if n == 0:
+        return n0
+    if n == 1:
+        return n1
+    return sum_series(n-2) + sum_series(n-1)
+    
