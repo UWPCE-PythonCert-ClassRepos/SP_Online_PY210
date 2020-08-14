c8a8b5de259b0d49d38ddf680c399304c4d3c14b
djgordon-maker <dragongordon@gmail.com>
2020-08-13 20:51:19 -0700

    Fixed bug: forgets the defined series durring recursion Improved docstring

--- students/daniel_gordon/lesson02/series.py
+++ students/daniel_gordon/lesson02/series.py
@@ -6,11 +6,15 @@ def lucas(n):
     """Calculates the nth lucas number"""
     return sum_series(n, 2, 1)
  
- def sum_series(n, n0 = 0, n1 = 1):
-    """Calculates the nth value of an arbitrary summation"""
+def sum_series(n, n0 = 0, n1 = 1):
+    """
+    Calculates the nth value of an arbitrary summation series
+    :param n0: zeroth element of the series
+    :param n1: first element of the series
+    """
     if n == 0:
         return n0
     if n == 1:
         return n1
-    return sum_series(n-2) + sum_series(n-1)
-    
+    return sum_series(n-2, n0, n1) + sum_series(n-1, n0, n1)
+ 
