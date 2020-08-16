739b2c1da35de057dd96fe546c9b44825e36089d
djgordon-maker <dragongordon@gmail.com>
2020-08-15 22:08:08 -0700

    Series 3 complete

--- students/daniel_gordon/lesson03/list_lab.py
+++ students/daniel_gordon/lesson03/list_lab.py
@@ -1,7 +1,8 @@
 #!/user/bin/env python3
-
+#setup list
 fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 print(fruit)
+#input practice
 fruit.append(input("Add another fruit> "))
 print(fruit)
 num = int(input("Give me a number between 1 and 5> "))
@@ -11,10 +12,11 @@ fruit = [response] + fruit
 print(fruit)
 fruit.insert(0, input("Add yet another fruit> "))
 print(fruit)
-
 for snack in fruit:
     if snack.startswith("P"): print(snack)
 
+#save list as is for parts 3 and 4
+series1 = fruit[:]
 print(fruit)
 fruit.pop()
 print(fruit)
@@ -26,8 +28,23 @@ while delete not in fruit:
     print(f"{delete} not found")
     print(fruit)
     delete = input("What should I remove?> ")
+#remove all instances of element
 count = fruit.count(delete)
 for _ in range(count):
     fruit.remove(delete)
+print(fruit)
+
+#use list from series 1
+fruit = series1[:]
+answer = ["yes", "no"]
+#remove all fruit that user does not like
+for snack in fruit[:]:
+    like = input(f"Do you like {snack}?> ").lower()
+    #keep asking until you understand
+    while like not in answer:
+        print(f"I dont understand {like}")
+        like = input(f"Do you like {snack}?> ")
+    if like == "no":
+        fruit.remove(snack)
 print(fruit)
 
