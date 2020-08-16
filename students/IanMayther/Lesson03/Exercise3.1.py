e43cb2fc7783d51d2e51adac0679a6c2e48d11a2
Ian Mayther <maytheri@gmail.com>
2020-08-15 17:10:05 -0700

    Fifth item and Testing for E3.1#5 - complete

--- students/IanMayther/Lesson03/Exercise3.1.py
+++ students/IanMayther/Lesson03/Exercise3.1.py
@@ -1,4 +1,5 @@
 #Exercise3.1.py
+import math
 
 #Exercise 3.1 Objective:
 """
@@ -30,18 +31,40 @@ def FirstFour_LastFour_EveryOther(seq):
 def reverse_it(seq):
     return seq[::-1]
 
+#5 - with the last third, then first third, then the middle third in the new order.
+def last_first_middle(seq):
+    first = math.ceil(len(seq)/3)
+    middle = math.ceil(len(seq)/3)
+    last = len(seq) - (first + middle)
+    return seq[-last:] + seq[:first] + seq[first:(first+middle)]
+
+
 if __name__ == "__main__":
     # run some tests
     a_string = "this is a string"
     a_tuple = (2, 54, 13, 12, 5, 32) 
 
-    print(swap_first_last(a_string))
-    print(swap_first_last(a_tuple))
-    print(remove_every_other(a_string))
-    print(remove_every_other(a_tuple))
-    print(FirstFour_LastFour_EveryOther(a_string))
-    print(FirstFour_LastFour_EveryOther(a_tuple))
-    print(reverse_it(a_string))
-    print(reverse_it(a_tuple))
-    #assert swap_first_last(a_string) == "ghis is a strint"
-    #assert swap_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+    #Check First Function
+    assert swap_first_last(a_string) == "ghis is a strint"
+    assert swap_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+    print("Swap First and Last - PASSED!")
+
+    #Check Second Function
+    assert remove_every_other(a_string) == "ti sasrn"
+    assert remove_every_other(a_tuple) == (2, 13, 5)
+    print("Remove Every Other - PASSED!")
+
+    #Check Third Function
+    assert FirstFour_LastFour_EveryOther(a_string) == " sas"
+    assert FirstFour_LastFour_EveryOther(a_tuple) == ()
+    print("First 4, Last 4, and Every Other - PASSED!")
+
+    #Check Fourth Function
+    assert reverse_it(a_string) == "gnirts a si siht"
+    assert reverse_it(a_tuple) == (32, 5, 12, 13, 54, 2)
+    print("Reverse It - PASSED!")
+
+    #Check Fifth Function
+    assert last_first_middle(a_string) == "ringthis is a st"
+    assert last_first_middle(a_tuple) == (5, 32, 2, 54, 13, 12)
+    print("Last, First and Middle - PASSED!")
