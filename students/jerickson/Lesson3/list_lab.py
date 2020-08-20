1d454bdfe321799f369f3373e88986e8cbd92a69
Erickson <Jacob.Erickson@bsci.com>
2020-08-20 15:27:54 -0700

    Ex3.4 Finished, Finished list_lab
    Corrected bug in series3, wasn't deleting items
    Moved input mocking to __main__ block to allow running as module
    Finished series4.

--- students/jerickson/Lesson3/list_lab.py
+++ students/jerickson/Lesson3/list_lab.py
@@ -1,18 +1,4 @@
 # windows4life
-
-
-def response_generator(seq):
-    for item in seq:
-        yield item
-
-
-def input(prompt):  # Mocks input function
-    print(prompt, end="")
-    response = mocked_resp_gen.__next__()
-    print(response)
-    return response
-
-
 def series_1():
     print("Let's get rolling with Series 1.")
     print("Here is the original list")
@@ -67,13 +53,50 @@ def series_3(the_list):
     for fruit in the_list[:]:
         liked_response = None
         while liked_response not in ["yes", "y", "no", "n"]:
-            liked_response_raw = input(f"Do you like {fruit}? yes or no: -> ")
+            liked_response_raw = input(f"Do you like {fruit.lower()}? yes or no: -> ")
             liked_response = liked_response_raw.lower()
+        if liked_response in ["no", "n"]:
+            the_list.remove(fruit)
     print("\nI guess this is what you like...hmmm, picky much?")
     print(the_list, end="\n\n")
 
 
+def series_4(the_list):
+    print(
+        """Alright, lets do this one last time. My name is Series 4,
+         and for 2mins I've been the one and only series being worked on."""
+    )
+    bassackwards_list = []
+    for item in the_list:
+        bassackwards_list.append(item[::-1])
+    bassackwards_list.pop()
+    print("Here is the Series 1 Result: ")
+    print(the_list, end="\n\n")
+    print("Here is the final result, I hope you are happy.")
+    print(bassackwards_list, end="\n\n")
+
+
+def main():
+    series1_result = series_1()
+
+    # Use full slice to create copies for each subsequent series
+    series_2(series1_result[:])
+    series_3(series1_result[:])
+    series_4(series1_result[:])
+
+
 if __name__ == "__main__":
+
+    def response_generator(seq):
+        for item in seq:
+            yield item
+
+    def input(prompt):  # Mocks input function
+        print(prompt, end="")
+        response = mocked_resp_gen.__next__()
+        print(response)
+        return response
+
     mocked_responses = [
         "UserFruit",
         "3",
@@ -89,6 +112,5 @@ if __name__ == "__main__":
         "n",
     ]
     mocked_resp_gen = response_generator(mocked_responses)
-    series1_result = series_1()
-    series_2(series1_result)
-    series_3(series1_result)
+
+    main()
