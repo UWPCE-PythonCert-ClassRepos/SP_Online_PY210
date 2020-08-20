8cb294245455adf0ba48ba9132d55447b8222141
Erickson <Jacob.Erickson@bsci.com>
2020-08-19 18:39:16 -0700

    Ex2.4 Progress, Changed to zero-index

--- students/jerickson/Lesson2/Ex2_4/series.py
+++ students/jerickson/Lesson2/Ex2_4/series.py
@@ -1,35 +1,33 @@
 # TODO turn to zero index
 def fibonacci(n):
     """Return n-th value of fib series"""
-    if n == 1:
+    if n == 0:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
         n = fibonacci(n - 1) + fibonacci(n - 2)
-    print(f"{n} ", end=" ")
     return n
 
 
 def lucas(n):
     """Return n-th value of lucas series"""
-    if n == 1:
+    if n == 0:
         return 2
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
         n = lucas(n - 1) + lucas(n - 2)
-    print(f"{n} ", end=" ")
     return n
 
 
 if __name__ == "__main__":
     print("fib")
-    for i in range(1, 10):
+    for i in range(0, 10):
         n = fibonacci(i)
         print(f"{i}:{n}")
 
     print("\nlucas")
-    for i in range(1, 10):
+    for i in range(0, 10):
         n = lucas(i)
         print(f"{i}:{n}")
