1480db6cb23fdccb25891f276eb385d0fdee83da
Erickson <Jacob.Erickson@bsci.com>
2020-08-19 18:37:58 -0700

    Ex2.4 Progress, Finished lucas function.

--- students/jerickson/Lesson2/Ex2_4/series.py
+++ students/jerickson/Lesson2/Ex2_4/series.py
@@ -1,3 +1,4 @@
+# TODO turn to zero index
 def fibonacci(n):
     """Return n-th value of fib series"""
     if n == 1:
@@ -10,7 +11,25 @@ def fibonacci(n):
     return n
 
 
+def lucas(n):
+    """Return n-th value of lucas series"""
+    if n == 1:
+        return 2
+    elif n == 2:
+        return 1
+    else:
+        n = lucas(n - 1) + lucas(n - 2)
+    print(f"{n} ", end=" ")
+    return n
+
+
 if __name__ == "__main__":
+    print("fib")
     for i in range(1, 10):
         n = fibonacci(i)
+        print(f"{i}:{n}")
+
+    print("\nlucas")
+    for i in range(1, 10):
+        n = lucas(i)
         print(f"{i}:{n}")
