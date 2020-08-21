c3545042ee9f2c5c7ec91368fb7370808dca7c48
Nicksama88 <Nicksama88@gmail.com>
2020-08-20 20:43:55 -0700

    add Task 3 to strformat_lab.py

--- students/Nick_Haury/Lesson03/strformat_lab.py
+++ students/Nick_Haury/Lesson03/strformat_lab.py
@@ -13,3 +13,12 @@ formatted_string = "file_{:0>3} :   {:5.2f}, {:3.2e}, {:3.2e}".format(*input_tup
 
 alt_formatted_string = f"""file_{input_tuple[0]:0>3} :   {input_tuple[1]:5.2f}, 
 {input_tuple[2]:3.2e}, {input_tuple[3]:3.2e}""".replace('\n', '')
+
+# Task Three
+
+def formatter(input_tuple):
+    formatter_string = f"the {len(input_tuple)} numbers are: "
+    for tup in input_tuple:
+        formatter_string += f"{tup}, "
+    formatter_string = formatter_string[:-1]
+    return formatter_string
