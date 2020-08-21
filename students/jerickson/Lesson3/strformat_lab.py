95ccea4092d05bf96b2de8efbeb4b90232f876b4
Erickson <Jacob.Erickson@bsci.com>
2020-08-20 19:48:02 -0700

    Ex3.3 Progress, Finished task 3

--- students/jerickson/Lesson3/strformat_lab.py
+++ students/jerickson/Lesson3/strformat_lab.py
@@ -1,11 +1,11 @@
 def task_one():
-    print("Task One")
+    print("\nTask One")
     seq = (2, 123.4567, 10000, 12345.67)
     print(f"file_{seq[0]:03.0f} : {seq[1]:0.2f}, {seq[2]:0.2e}, {seq[3]:0.3g}")
 
 
 def task_two():
-    print("Task Two")
+    print("\nTask Two")
     seq = (2, 123.4567, 10000, 12345.67)
     print(
         "file_{num:03.0f} : {dec:0.2f}, {sci:0.2e}, {sig:0.3g}".format(
@@ -14,6 +14,19 @@ def task_two():
     )
 
 
+def task_three(*args):
+    print("\nTask Three")
+    length = len(args)
+    repeated = " {:d},"
+    full = "The {:d} numbers are:" + repeated * length
+    full = full[:-1]
+    all_args = (length,) + args
+    formatted = full.format(*all_args)
+    print(formatted)
+
+
 if __name__ == "__main__":
     task_one()
     task_two()
+    task_three(1, 2, 3, 4, 5, 6)
+    task_three(42, 42, 42)
