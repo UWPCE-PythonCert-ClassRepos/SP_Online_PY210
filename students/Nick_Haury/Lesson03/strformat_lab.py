a981ad847190e6c68d0d31d4c2416d19b8a0982d
Nicksama88 <Nicksama88@gmail.com>
2020-08-20 20:34:23 -0700

    add task 2 to strformat_lab.py

--- students/Nick_Haury/Lesson03/strformat_lab.py
+++ students/Nick_Haury/Lesson03/strformat_lab.py
@@ -9,3 +9,7 @@
 input_tuple = ( 2, 123.4567, 10000, 12345.67)
 formatted_string = "file_{:0>3} :   {:5.2f}, {:3.2e}, {:3.2e}".format(*input_tuple)
 
+# Task Two
+
+alt_formatted_string = f"""file_{input_tuple[0]:0>3} :   {input_tuple[1]:5.2f}, 
+{input_tuple[2]:3.2e}, {input_tuple[3]:3.2e}""".replace('\n', '')
