d6e501b04f987ec81fe3b249765378460650b944
djgordon-maker <dragongordon@gmail.com>
2020-08-21 10:39:51 -0700

    Turned off debug mode in lesson 3 Made the donor list a dictionary for lesson 4

--- students/daniel_gordon/lesson04/mailroom.py
+++ students/daniel_gordon/lesson04/mailroom.py
@@ -2,74 +2,58 @@
 
 DEBUG = False
 
-def get_names(record):
-    """Extracts the first element from each list in a list of lists"""
-    names = []
-    for item in record:
-        names.append(item[0])
-    return names
-
 def get_total(items):
-    """Adds together all of the elements of a list, except the zeroth element"""
+    """Adds together all of the elements of a list stored in a tuple"""
+    """items is a ("name", [list]) pair"""
     total = 0
-    for num in items[1:]:
+    for num in items[1]:
         total += num
-    if DEBUG: print(f"{items[0]} | {total}")
+    if DEBUG: print(total)
     return total
 
 def send_thank_you():
     """Records a new donation and thanks the donor"""
     #Find out who to thank
-    d_names = get_names(donors)
     name = input("Enter Donors Full Name> ").title()
     if DEBUG: print(f"name: {name}")
     while name == 'List':
-        print(d_names)
+        print(donor.keys())
         #Ensure correct capitalization
         name = input("Enter Doners Full Name> ").title()
         if DEBUG: print(f"name: {name}")
     #Allow user to leave mid task
-    if name == 'Q':
-        return False
-    #Identify existing donor
-    elif name in d_names:
-        send_to = d_names.index(name)
-    #Make new donor
-    else:
-        send_to = len(donors)
-        donors.append([name])
+    if name == 'Q': return False
     #Enter ammount
     money = input("Enter ammount donated> ")
     #Allow user to leave mid task
-    if money.upper() == 'Q':
-        if [name] in donors: donors.remove([name])
-        return False
+    if money.upper() == 'Q': return False
+    #Setup empty list for new donor
+    if name not in donors: donors[name] = []
     #Add donation to records
-    donors[send_to].append(int(money))
+    donors[name].append(float(money))
     #Send thank you
     print(f"Thank you {name} for your generous donation of {money}")
     return True
     
 def create_a_report():
     """Prints report of contributions recived so far"""
-    donors.sort(reverse = True, key = get_total)
     title_str = f"{'Donor Name':<25} | Total Given | Num Gifts | Average Gift"
     print(title_str)
     print('-'*len(title_str))
-    for donor in donors:
+    for donor in sorted(donors.items(), key = get_total, reverse = True):
         total = get_total(donor)
-        num = len(donor) - 1
+        num = len(donor[1])
         average = total / num
         print(f"{donor[0]:<25} $ {total:>11.2f} {num:>11} $ {average:>12.2f}")
     return True
 
 #Generate list of donors
-#I'm inclined to use a dictonary of lists, but we haven't covered that yet
-donors = [["Nick Esen", 1800, 720],
-          ["Sabina", 1500],
-          ["Marceline Theodosia", 30000, 500000, 100],
-          ["Rafat Rein", 1500000],
-          ["Kevin Both", 150, 1200, 750]]
+donors = {"Nick Esen" : [1800, 720],
+          "Sabina" : [1500],
+          "Marceline Theodosia" : [30000, 500000, 100],
+          "Rafat Rein" : [1500000],
+          "Kevin Both" : [150, 1200, 750]}
+
 if __name__ == "__main__":
     #I try to avoid while true
     option = ''
@@ -80,7 +64,7 @@ if __name__ == "__main__":
         option = input(menu).upper()
         #catch unrecognized actions
         while option not in actions:
-            print("That is not an option")
+            print(f"{option} is not an option")
             option = input(menu).upper()
         if DEBUG: print(f"{option} is chosen")
 
