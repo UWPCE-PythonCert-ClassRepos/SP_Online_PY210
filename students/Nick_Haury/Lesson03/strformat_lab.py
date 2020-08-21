5b1ba11a9300c8c5cdb28b7e3c009f82807fd8e0
Nicksama88 <Nicksama88@gmail.com>
2020-08-20 20:20:16 -0700

    add task 1 to strformat_lab.py

---
+++ students/Nick_Haury/Lesson03/strformat_lab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+
+'''
+
+# Task One
+
+input_tuple = ( 2, 123.4567, 10000, 12345.67)
+formatted_string = "file_{:0>3} :   {:5.2f}, {:3.2e}, {:3.2e}".format(*input_tuple)
+
