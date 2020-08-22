c4a8b205600f080b31198ad991c9033c7440e118
Erickson <Jacob.Erickson@bsci.com>
2020-08-22 12:34:45 -0700

    A4.3 Progress, Added File Source Material
    Created from Critical Role transcripts.

--- students/jerickson/Lesson4/trigram.py
+++ students/jerickson/Lesson4/trigram.py
@@ -2,79 +2,12 @@ import re
 import random
 
 
-def get_material(short=True):
-    original_short = "I wish I may I wish I might"
-    # Critical Role
-    original_long = """
-    the city of Zadash. Long in the Dwendalian Empire,
-    you’ve made a small name for yourself in a few
-
-    factions. You’ve done a deed or two for the King’s
-    Hall here in service of the Crown. You’ve done a
-
-    deed or two with the Knights of Requital, that
-    involved getting caught up briefly in a manhunt,
-
-    as you were loosely responsible for the death of
-    the High-Richter, the high judge of the local
-
-    court. Managed to make an introductory trade, a
-    deal if you will, with The Gentleman, this
-
-    subterranean figure of not the most legal of
-    maneuvers. You delved deep into one of the
-
-    subterranean river tunnels beneath the city of
-    Zadash for him, managed to clear out and recover
-
-    materials from a long-buried research facility
-    from the Age of Arcanum.
-
-    Upon returning, the deal was solid. You managed to
-    smuggle out Horris, who needed to be away from the
-
-    town for the time being while this investigation
-    was happening. In doing so, also managed to clear
-
-    your names of the death of the High-Richter. Upon
-    returning back, getting a few deeds done, now
-
-    waiting for this to blow over, and hearing word
-    that there is no longer a manhunt for a number of
-
-    you in cahoots to this, you begin to prepare for
-    the coming Harvest Close Festival. Now we’re
-
-    getting close to the third of the month of
-    Fessuran, you’ve all managed to prepare in the
-
-    ways you want, got a few things on your side.
-    You’ve delved into some of the mysteries of the
-
-    beacon that you maintain. You’ve retrieved your
-    Forever Alcoholic Flask. That’s where we’ll begin
-
-    today. Friends, as the early morning creeps up
-    upon you, the chilled morning air has left a bit
-
-    of ice and mist crawling across areas of the city
-    floor, from outside your window here in the Leaky
-
-    Tap tavern. However, though it is a gloomy
-    morning, there is already a general air of
-
-    celebration around. Glancing across, more people
-    are now shifting out into the street and city
-
-    around you. Music is beginning to creep into the
-    vicinity, and you can hear muffling through the
-
-    nearby walls.
-    """
-    if short:
-        return original_short
-    else:
-        return original_long
+def get_material(speaker):
+    path = r"students\jerickson\Lesson4\speakers"
+    full_file_path = f"{path}\\{speaker}.txt"
+    with open(full_file_path, "r") as f:
+        raw_text = f.readlines()
+    return " ".join(raw_text)
 
 
 def get_words(source_material):
@@ -111,11 +44,12 @@ def generate_multi_grams(words, xgram_seq):
     return multi_gram_structures
 
 
-def generate_new_text(multi_grams):
+def generate_new_text(multi_grams, new_story_length=25):
     # TODO add missing key error correction
     # TODO search multiple x-grams
-    new_story = ["of", "the"]
-    new_words_to_gen = 80
+    # new_story = ["im", "jester", "nice", "to"]
+    new_story = ["hello", "its", "me", "jester"]
+    new_words_to_gen = new_story_length - len(new_story)
 
     xgram_size = 3
     xgram_key_len = xgram_size - 1
@@ -123,17 +57,23 @@ def generate_new_text(multi_grams):
         word_key = tuple(new_story[-xgram_key_len:])
         new_word = random.choice(multi_grams[xgram_size][word_key])
         new_story.append(new_word)
-        print(multi_grams[xgram_size][word_key])
-        print(new_word)
+        # print(multi_grams[xgram_size][word_key])
+        # print(new_word)
     print(" ".join(new_story))
 
 
 if __name__ == "__main__":
-    raw = get_material(False)
+    # TODO user generated seed story
+    # TODO random generated seed story
+    raw = get_material(speaker="LAURA")
     words = get_words(source_material=raw)
-    xgram_list = [2, 3, 4, 5]
+    xgram_list = [2, 3, 4, 5, 6, 7, 8]
     multi_grams = generate_multi_grams(words, xgram_list)
-    generate_new_text(multi_grams)
+    # for k, v in multi_grams[8].items():
+    #     if len(v) > 3:
+    #         print(k, v)
+    for i in range(10):
+        generate_new_text(multi_grams, new_story_length=25)
     # for gram_size, gram_structure in multi_grams.items():
     #     print(gram_size)
     #     for word_set, followers in gram_structure.items():
