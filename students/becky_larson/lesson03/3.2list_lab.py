0d66155442eb762784f4caa89f8cd6970d366587
belarsongit <68929696+belarsongit@users.noreply.github.com>
2020-08-23 13:02:39 -0700

    code done for list_lab.py

--- students/becky_larson/lesson03/3.2list_lab.py
+++ students/becky_larson/lesson03/3.2list_lab.py
@@ -35,6 +35,7 @@ def display_fruits_starting_with(fruit_list, starting_letter):
 
 
 def series1(fruit_list):
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++")
     print('1 series1 starting List: ', fruit_list)
 
     """Ask  user for another fruit and add it to the end of the list."""
@@ -42,24 +43,27 @@ def series1(fruit_list):
     updated_fruit = add_fruit(fruit_list, new_fruit)
 
     """Display the list."""
-    print('2 Updated List: ', updated_fruit)
+    # print('2 Updated List: ', updated_fruit)
 
     """Ask  user for a number """
     """Display number and fruit for  number"""
     num_in = get_number(len(updated_fruit))
+    while int(num_in) > len(updated_fruit):
+        num_in = get_number(len(updated_fruit))
+
     text = "Fruit number {num} is {fruit}"
     the_display = text.format(num=num_in, fruit=updated_fruit[(int(num_in)-1)])
-    print("3 " + the_display)
+    print(the_display)
 
     """Add another fruit to beginning of list using “+” . Display list."""
     updated_with_plus = ['Lemons'] + updated_fruit
-    print("4")
-    print(updated_with_plus)
+    # print("4")
+    # print(updated_with_plus)
 
     """Add another fruit to beginning of list using insert(). Display list"""
     updated_with_insert = add_fruit_with_insert(updated_with_plus, 'Mango')
-    print("5")
-    print(updated_with_insert)
+    # print("5")
+    # print(updated_with_insert)
 
     """Display all fruits that begin with “P”, using a for loop"""
     display_fruits_starting_with(updated_with_insert, 'P')
@@ -79,36 +83,41 @@ def do_series2_bonus(doubledList):
 
 
 def series2(the_list):
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++")
     print('Series 2 Starting List: ', the_list)
     '''Remove last fruit from list.  Display updated list'''
-    the_list.pop()
-    print('After removing the last fruit: ', the_list)
+    new_list = the_list.copy()
+    new_list.pop()
+    print('After removing the last fruit: ', new_list)
 
     '''Ask user for fruit to delete, find and delete it.'''
     deleteFruit = ask_user("Enter name of fruit to delete > ")
     print('Series 2 delete fruit: ', deleteFruit)
-    if deleteFruit in the_list:
+    if deleteFruit in new_list:
         print(deleteFruit, " was found in list and deleted")
-        the_list.remove(deleteFruit)
+        new_list.remove(deleteFruit)
     else:
         print(deleteFruit, " was not found in List")
-    return the_list
+    return new_list
 
 
 def series3(the_list):
-    # print('Series 3 Starting List: ', the_list)
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+    print('Series 3 Starting List: ', the_list)
     '''Ask user if they like each fruit (in lowercase)'''
     cnt = len(the_list)
-    print('cnt ', cnt)
-    
-    for fruit in reversed(the_list):
+    # print('cnt ', cnt)
+
+    new_list = the_list.copy()
+
+    for fruit in reversed(new_list):
         print('fruit ', fruit)
         question = "1 Do you like " + fruit.lower() + " (y or n) > "
         # print(lower(fruit))
         answer = ask_user(question)
-        print('++++++++++++++++== answer ', answer)
-        # For any answer that is not “yes” or “no”, 
-        #    prompt user to answer with one of those two values 
+        # print('++++++++++++++++== answer ', answer)
+        # For any answer that is not “yes” or “no”,
+        #    prompt user to answer with one of those two values
         if not ((answer.lower() == 'n') or (answer.lower() == 'y')):
             valid = False
             while not valid:
@@ -121,15 +130,16 @@ def series3(the_list):
             # if they don't like, delete from list
             if(answer == 'n'):
                 print('Delete ', fruit)
-                the_list.remove(fruit)
+                new_list.remove(fruit)
             else:
                 continue
 
-    return the_list
+    return new_list
 
 
 def series4(the_list):
-    # print('Series 4 Starting List: ', the_list)
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+    print('Series 4 Starting List: ', the_list)
     new_list = []
     for fruit in the_list:
         # print('fruit: ', fruit [::-1])
@@ -154,9 +164,5 @@ print('series2_list: ', series2_list)
 series3_list = series3(series1_list)
 print('series3_list: ', series3_list)
 
-# series4_list = series4(series1_list)
-# print('series3_list: ', series3_list)
-'''
-series3_list = series3(fruits_list)
-print('series3_list: ', series3_list)
-'''
+series4_list = series4(series1_list)
+print('series4_list: ', series4_list)
