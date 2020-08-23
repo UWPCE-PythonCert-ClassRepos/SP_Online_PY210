02a803cdd600c5034dbd8db9326b12198a953ea3
Nicksama88 <Nicksama88@gmail.com>
2020-08-23 16:27:17 -0700

    add Dictionaries 2 exercises to dict_lab.py

--- students/Nick_Haury/Lesson04/dict_lab.py
+++ students/Nick_Haury/Lesson04/dict_lab.py
@@ -5,6 +5,8 @@
 
 # Dictionaries 1
 
+print("Dictionaries 1:\n")
+
 my_dict = {
     "name":"Chris", 
     "city":"Seattle", 
@@ -20,3 +22,11 @@ print(my_dict.values())
 print("Is 'cake' a key in the dictionary?: " + str(("cake" in my_dict)))
 print("Is 'Mango' a value in the dictionary?: " + str(("Mango" in my_dict.values())))
 
+# Dictionaries 2
+
+print("\nDictionaries 2:\n")
+
+dict_t = dict()
+for key in my_dict:
+    dict_t[key] = my_dict[key].count("t")
+print(dict_t)
