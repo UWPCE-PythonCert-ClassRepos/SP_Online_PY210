8b2e1562c856479b8c235629e16bcc64118a65cb
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-08-22 18:17:28 -0700

    added function3(removes first 4 and last 4, then every other from a sequence

--- students/Ajay_Randhawa/lesson03/Slicing.py
+++ students/Ajay_Randhawa/lesson03/Slicing.py
@@ -4,5 +4,13 @@ def firstLast(seq):
 	return seq1
 
 def everyOther(seq):
+	""" using the step function to return
+	every other item in sequence"""
 	seq1 = seq[0:-1:2]
+	return seq1
+
+def function3(seq):
+	"""removes first 4 and last 4 items, and
+	then evey other item"""
+	seq1 = seq[4:-4:2]
 	return seq1
