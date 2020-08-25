f4f9c14d324d8852cee6b84ce7a34e637624a664
Nicksama88 <Nicksama88@gmail.com>
2020-08-24 17:36:48 -0700

    add build_trigrams skeleton structure

--- students/Nick_Haury/Lesson04/trigrams.py
+++ students/Nick_Haury/Lesson04/trigrams.py
@@ -3,3 +3,19 @@
 '''
 '''
 
+words = "I wish I may I wish I might".split()
+
+def build_trigrams(words):
+    '''
+    build up the trigrams dict from the list of words
+
+    returns a dict with:
+        keys: word pairs in tuple format
+        values: list of words that follow key pair
+    '''
+    trigrams = {}
+
+    return trigrams
+
+if __name__ == "__main__":
+    trigrams = build_trigrams(words)
