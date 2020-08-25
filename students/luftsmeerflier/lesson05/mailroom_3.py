d3eb09caa59324e2b5a38877b07744e0935c95bd
Olyskoop <snap@olymega.org>
2020-08-25 15:55:05 -0700

    mailroom_3 is identical to 2 because no fit place for a list comprehension

--- students/luftsmeerflier/lesson05/mailroom_3.py
+++ students/luftsmeerflier/lesson05/mailroom_3.py
@@ -2,32 +2,17 @@
 import sys
 
 donors = {
-	"John Quigley": [
-		("2019-02-04", 5000.00),
-		("2019-02-04", 4250.00),
-		("2019-02-04", 3000.00)
-	],
-	"Sara Smith": [
-		("2020-04-02", 2000.00),
-		("2019-02-04", 42000.00),
-		("2019-02-04", 32000.00)
-	],
-	"Jacob van der Schmidt": [			
-		("2012-04-20", 20.00),
-		("2019-02-04", 2000.00),
-		("2019-02-04", 3400.00)
-	],
-	"Ogden Nash": [
-		("2019-02-04", 5000000.00),
-		("2019-02-04", 500000.00),
-		("2019-02-04", 50000.00)
-	]
+	"John Quigley": {"2019-02-04": 5000.00, "2019-02-04": 4250.00, "2019-02-04": 3000.00},
+	"Sara Smith": {"2020-04-02": 2000.00, "2019-02-04": 42000.00, "2019-02-04": 32000.00},
+	"Jacob van der Schmidt": {"2012-04-20": 20.00, "2019-02-04": 2000.00, "2019-02-04": 3400.00},
+	"Ogden Nash": {"2019-02-04": 5000000.00, "2019-02-04": 500000.00, "2019-02-04": 50000.00}
 }
 
 def send_thank_you(first_name='', last_name=''):
 	names_list = list(donors.keys())
 
 	_list = input("Type `list` for a list of donors or press the carriage key to skip\n")
+
 	if _list == "list":
 		for donor in names_list:
 			print(donor)
@@ -36,15 +21,17 @@ def send_thank_you(first_name='', last_name=''):
 		pass
 
 	name = input('What is the name of the donor?\n').title()
+	donor = donors.get(name)
 
-	if name in names_list:
-		index = names_list.index(name)
-		name = names_list[index]
-		donor = donors[name]
-		date = donor[0][0]
-		amount = donor[0][1]
+	if donor:
+		amount = list(donor.values())[0]
+		date = list(donor.keys())[0]
 		write_letters(name, amount, date)
-	
+	else:
+		amount = input("How much did {} donate?\n".format(name))
+		date = input("When was the donation? YYYY-MM-DD\n")
+		write_letters(name, amount, date)
+
 
 def write_letters(name, amount, date):	
 
@@ -53,37 +40,31 @@ def write_letters(name, amount, date):
 	letterhead = "{}.txt".format(name).replace(" ", "_")
 	# open a letter template
 	formatted = text.format(name, amount, date)
-	thank_you = open("{}".format(letterhead), "w+")
-	thank_you.write(formatted)
-	thank_you.close()
+
+	with open("{}".format(letterhead), "w+") as thank_you:
+		thank_you.write(formatted)
+
 	print('\n')
 	print("Thank you letter has been created")
 	print('\n')
 
 
 def create_report():
-	#print(donor_list[0].ljust(20) + donor_list[1].ljust(10) + donor_list[2].ljust(20) + donor_list[3].ljust(8))
-	# print(donor_list[0].ljust(20))
 	print('\n')
 	print("Donor Name".ljust(28) + '|'.ljust(2) + "Total Given" + '|'.rjust(2) + "Num Gifts".rjust(10) + "|".ljust(2) + "Average Gift")
 	print('-' * 70)
 
-	names_list = list(donors.keys())
-	names_list.sort()
+	def key(item):
+		return sum(item[1].values())
 
-	for name in names_list:
-		index = names_list.index(name)
-		name = names_list[index]
-		donor = donors[name]
-		date = donor[0][0]
-		total = 0
-		num_gifts = 0
-		for donation in donor:
-			total += donation[1]
-			num_gifts += 1
-		average = int(total / num_gifts)
+	# list of tuples
+	sorted_donors = sorted(donors.items(), key=key, reverse=True)
 
-		name_length = len(name)
+	for donor in sorted_donors:
+		name = donor[0]
+		total = sum(list(donor[1].values()))
+		num_gifts = len(donor[1].values())
+		average = total / num_gifts
 		
 		print((name).ljust(30) + '$'.ljust(2) + str(total).ljust(15) + str(num_gifts).ljust(10) + '$'.ljust(2) + str(round(average, 2)))
 
@@ -91,22 +72,17 @@ def create_report():
 
 
 def send_letter_all():
-	text = "Dear {},\n Thank you very much for your very kind donation of ${}.\n It will be put to good use.\n Sincerely,\n-The Team"
+	text = "Dear {},\n Thank you very much for your very kind donation of ${}.\n It will be put to good use.\nSincerely,\n-The Team"
 
 	for donor in donors:
 		name = donor
-		donation = donors[donor]
-		amount = donation[0][1]
-		date = donation[0][0]
+		amount = list(donors[name].values())[0]
+		date = list(donors[name].keys())[0]
 		write_letters(name, amount,date)
 
-
-	# write to the template
-
-	
 def exit_program():
 	print("Adieu, auf wiedersehen, goodbye!")
-	sys.exit()
+	sys.exit()	
 
 arg_dict = {
     1: send_thank_you,
@@ -116,21 +92,18 @@ arg_dict = {
 }
 
 prompt = "\n".join(("Please select from the following options:",
-					"1 - Send a Thank You",
-					"2 - Create a Report",
-					"3 - Send letters to all donors.",
-					"4 - quit"
-					))
+	"1 - Send a Thank You",
+	"2 - Create a Report",
+	"3 - Send letters to all donors.",
+	"4 - quit"
+))
 
 
 def main():
 	while True:
-		try:
-			response = int(input(prompt + "\n")) # continuously colelct user selection
-			# now redirect to feature functions based on the user selection
-			arg_dict.get(response)()
-		except:
-			print("Error")
+		response = int(input(prompt + "\n")) # continuously colelct user selection
+		# now redirect to feature functions based on the user selection
+		arg_dict.get(response)()
 
 
 if __name__ == "__main__":
