aa6c334fd75c72c871220fc67067fcc7616be401
Nicksama88 <Nicksama88@gmail.com>
2020-08-25 19:07:54 -0700

    add docstring. comment on choice of random text if no word pair match found. Add argument for output text word length

--- students/Nick_Haury/Lesson04/trigrams.py
+++ students/Nick_Haury/Lesson04/trigrams.py
@@ -3,9 +3,11 @@ import random
 import sys
 
 '''
+Trigrams program takes command line arguments for a text file to parse
+along with an int.  The program will open up the text file, and then
+parse along the file to create a trigram of the words.  The int argument
+is used to build up a new text, with wordcount equal to the provided int.
 '''
-
-# words = "I wish I may I wish I might".split()
 
 def build_trigrams(words):
     '''
@@ -37,6 +39,8 @@ def build_text(trigram, text_length):
     text = list(random_key())
     for i in range(text_length - 2):
         next_key = (text[i], text[i + 1])
+        # use dictionary to find next word list, but if key doesn't exist then
+        # pick a random word list
         next_word_list = trigram.setdefault(next_key, trigram[random_key()])
         text.append(random.choice(next_word_list))
     return " ".join(text)
@@ -45,13 +49,15 @@ if __name__ == "__main__":
     # get the filename from the command line
     try:
         file_name = sys.argv[1]
+        length = sys.argv[2]
     except IndexError:
-        print("You must pass in a filename in the form of 'filename.txt'")
+        print("You must pass in a filename argument in the form of "
+        "'filename.txt' along with an int argument for the number of pages "
+        "you'd like the created text to be.  ex: trigrams.py text.txt 100")
         sys.exit(1)
     words = read_file(file_name)
     trigrams = build_trigrams(words.split())
-    text = build_text(trigrams, 100)
+    text = build_text(trigrams, int(length))
     print(text)
-    print(len(text))
     # for k, v in trigrams.items():
     #     print(k, v)
