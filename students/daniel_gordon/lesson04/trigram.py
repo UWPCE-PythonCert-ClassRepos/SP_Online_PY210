085483e53e7e0c94d3340d3d831667134ff262fa
djgordon-maker <dragongordon@gmail.com>
2020-08-27 20:58:54 -0700

    Begining trigram analysis

--- students/daniel_gordon/lesson04/trigram.py
+++ students/daniel_gordon/lesson04/trigram.py
@@ -1,9 +1,28 @@
+import random
+
+def read_data(filename = ""):
+    """Processes data from a text file and returns a list of all words it contains"""
+    if filename == "":
+        return "I wish I may I wish I might".split()
+    
+    
 def build_trigrams(words):
+    """Builds a trigram from a list of words"""
+    """Trigram is a dictionary of lists, using a word pair tuple as a key"""
     trigram = {}
     for i in range(len(words)-2):
         trigram.setdefault((words[i], words[i+1]), []).append(words[i+2])
     return trigram
 
 if __name__ == "__main__":
-    test_string = "I wish I may I wish I might"
-    print(build_trigrams(test_string.split()))
+    words = read_data()
+    trigram = build_trigrams()
+    key = random.choice(list(trigram))
+    new_text = list(key)
+    for i in range(20):
+        next_word = random.choice(trigram[key])
+        key = (key[1], next_word)
+        new_text.append(next_word)
+        if key not in trigram: break
+        
+    print(" ".join(new_text))
