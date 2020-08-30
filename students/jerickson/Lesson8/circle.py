20e994bc77bcd036f5846ddebe067146c7836b26
Erickson <Jacob.Erickson@bsci.com>
2020-08-30 14:15:09 -0700

    A8.7 Progress, Steps 2-8
    Added tests then code for each step

--- students/jerickson/Lesson8/circle.py
+++ students/jerickson/Lesson8/circle.py
@@ -1,8 +1,81 @@
 """Implements the Circle class"""
 
+import math
+
 
 class Circle:
     """A circle"""
 
     def __init__(self, radius):
         self.radius = radius
+
+    @property
+    def diameter(self):
+        """Gets the diameter"""
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, new_diam):
+        """
+        Sets the new diameter of the circle
+
+        Parameters
+        ----------
+        new_diam : int|float
+            Diameter to set for the circle
+        """
+        self.radius = new_diam / 2
+
+    @property
+    def area(self):
+        """Gets the area"""
+        return math.pi * self.radius ** 2
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        """
+        Alternate constructor of Circle using diameter
+
+        Parameters
+        ----------
+        diameter : int|float
+            The largest straight measurement accross a circle
+
+        Returns
+        -------
+        Circle
+            The circle class
+        """
+        return cls(diameter / 2)
+
+    def __str__(self):
+        return f"Circle with radius: {self.radius:.2f}"
+
+    def __repr__(self):
+        return f"Circle({self.radius:.2f})"
+
+    def __add__(self, other):
+        radius = self.radius + other.radius
+        return self.__class__(radius=radius)
+
+    def __mul__(self, scalar):
+        radius = self.radius * scalar
+        return self.__class__(radius=radius)
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __truediv__(self, scalar):
+        radius = self.radius / scalar
+        return self.__class__(radius=radius)
+
+    __rmul__ = __mul__
--- students/jerickson/Lesson8/test_circle.py
+++ students/jerickson/Lesson8/test_circle.py
@@ -1,7 +1,9 @@
 """Test code for circle.py"""
+# pylint: disable=wrong-import-order
 
 import pytest  # pylint: disable=unused-import
 
+import math
 import circle
 
 
@@ -11,7 +13,164 @@ import circle
 
 
 def test_init():
-    """
-    This only tests that it can be initialized
-    """
+    """This only tests that it can be initialized"""
     circle.Circle(42)  # pylint: disable=unused-variable
+
+
+########
+# Step 2
+########
+
+
+def test_diam_get():
+    """Tests that diameter can be get"""
+    inst = circle.Circle(42)
+    assert inst.diameter == 84
+
+
+########
+# Step 3
+########
+
+
+def test_diam_set():
+    """Tests that diameter can be get"""
+    inst = circle.Circle(42)
+    inst.diameter = 42
+    assert inst.radius == 21
+    assert inst.diameter == 42
+
+
+########
+# Step 4
+########
+
+
+def test_area_get():
+    """Tests that are can be get"""
+    inst = circle.Circle(42)
+    assert inst.area == math.pi * (inst.radius ** 2)
+
+
+def test_area_set():
+    """Tests that are can not be set"""
+    inst = circle.Circle(42)
+    with pytest.raises(AttributeError):
+        inst.area = 42
+
+
+########
+# Step 5
+########
+
+
+def test_alt_construct_from_diameter():
+    """Tests that a circle can be constructed from the diameter"""
+    inst = circle.Circle.from_diameter(42)
+    assert inst.radius == 21
+
+
+########
+# Step 6
+########
+
+
+def test_circle_str():
+    """Tests the str(Circle) return value"""
+    inst = circle.Circle(42)
+    assert str(inst) == "Circle with radius: 42.00"
+
+
+def test_circle_repr():
+    """Tests the repr(Circle) return value"""
+    inst = circle.Circle(42)
+    assert repr(inst) == "Circle(42.00)"
+
+
+########
+# Step 7
+########
+
+
+def test_circle_add():
+    """Tests that Circles add together by radius"""
+    inst1 = circle.Circle(42)
+    inst2 = circle.Circle(1)
+    inst3 = inst1 + inst2
+    assert repr(inst3) == "Circle(43.00)"
+
+
+def test_circle_mul():
+    """Tests that Circles multiply scalars by radius"""
+    inst1 = circle.Circle(42)
+    inst2 = inst1 * 2
+    assert repr(inst2) == "Circle(84.00)"
+
+
+def test_circle_rmul():
+    """Tests that Circles multiply scalars by radius"""
+    inst1 = circle.Circle(42)
+    inst2 = 2 * inst1
+    assert repr(inst2) == "Circle(84.00)"
+
+
+########
+# Step 8
+########
+
+
+def test_circle_lt():
+    """Tests that Circles can compare using '<' operator"""
+    inst1 = circle.Circle(1)
+    inst2 = circle.Circle(42)
+    assert inst1 < inst2
+    assert inst1 <= inst2
+    assert inst1 != inst2
+
+
+def test_circle_gt():
+    """Tests that Circles can compare using '>' operator"""
+    inst1 = circle.Circle(42)
+    inst2 = circle.Circle(1)
+    assert inst1 > inst2
+    assert inst1 >= inst2
+    assert inst1 != inst2
+
+
+def test_circle_eq():
+    """Tests that Circles can compare using '==' operator"""
+    inst1 = circle.Circle(42)
+    inst2 = circle.Circle(42)
+    assert inst1 == inst2
+    assert inst1 <= inst2
+    assert inst1 >= inst2
+
+
+def test_circle_reflected_math():
+    """Tests the reflected operators behavior"""
+    inst1 = circle.Circle(42)
+    inst2 = circle.Circle(1)
+    assert inst1 + inst2 == inst2 + inst1
+    assert inst1 * 42 == 42 * inst1
+
+
+def test_circle_add_augmented():
+    """Tests that Circles can add to itself"""
+    inst1 = circle.Circle(42)
+    inst2 = circle.Circle(1)
+    inst1 += inst2
+    assert repr(inst1) == "Circle(43.00)"
+
+
+def test_circle_mul_augmented():
+    """Tests that Circles can multiply a scalar to itself"""
+    inst1 = circle.Circle(42)
+    inst1 *= 2
+    assert repr(inst1) == "Circle(84.00)"
+
+
+def test_circle_div_augmented():
+    """Tests that Circles can divide by a scalar to itself"""
+    inst1 = circle.Circle(42)
+    inst1 /= 2
+    assert repr(inst1) == "Circle(21.00)"
