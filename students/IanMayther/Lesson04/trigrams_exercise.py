eb9077cd2ab5d1214a2e9933a0af2c104fde59e2
Ian Mayther <maytheri@gmail.com>
2020-08-29 18:50:17 -0700

    Initial start to Trigrams exercise

---
+++ students/IanMayther/Lesson04/trigrams_exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+words = "I wish I may I wish I might"
+
+def build_trigrams(words):
+    """
+    build up the trigrams dict from the list of words
+
+    returns a dict with:
+       keys: word pairs
+       values: list of followers
+    """
+    trigrams = {}
+
+    # build up the dict here!
+
+    return trigrams
+
+#Main Exicutable
+if __name__ == '__main__':
+    trigrams = build_trigrams(words)
+    print(trigrams)
