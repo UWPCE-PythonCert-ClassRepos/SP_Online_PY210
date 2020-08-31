00a86e6d586abd3678b50c2b4556776e1a7b79b5
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-08-30 17:23:43 -0700

    Added Lesson3, Activity1, Series1

--- students/Ajay_Randhawa/lesson03/list_lab.py
+++ students/Ajay_Randhawa/lesson03/list_lab.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+def Series1():
+	list1 = ["Apples", "Pears", "Oranges", "Peaches"]
+	print(list1)
+	x = input("Name another fruit:")
+	list1.append(x)
+	print(list1)
+	number = input("Type a number:")
+	print(number)
+	print(list1[int(number) -1])
+	list1 = ["Guava"] + list1
+	print(list1)
+	list1.insert(0, "Grapes")
+	print(list1)
+	for i in list1:
+		if i.startswith("P"):
+			print(i)
