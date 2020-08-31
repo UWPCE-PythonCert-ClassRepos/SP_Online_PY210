c9384291e74054a64c176249ae806e791924e900
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-08-30 21:47:04 -0700

    added Lesson3, Activity2, Series4

--- students/Ajay_Randhawa/lesson03/list_lab.py
+++ students/Ajay_Randhawa/lesson03/list_lab.py
@@ -28,29 +28,54 @@ def Series1():
 		if i.startswith("P"):
 			print(i)
 	"""
-
 	Series 2
-
-
 	"""
+def Series2():
+	list2 = ["Apples", "Pears", "Oranges", "Peaches"]
 	#Display the list
-	print(list1)
+	print(list2)
 	#REmove the last fruit from the list
-	list1.pop()
+	list2.pop()
 	#Display the list
-	print(list1)
+	print(list2)
 	#Ask the user for a fruit to delete, find it and delete it
 	#Bonus
 	#only works for when the list is multiplied by less than or equal to 2.
-	list1 = list1*2
-	print(list1)
+	list2 = list2*2
+	print(list2)
 	count = 0
 	while(count<2):
 		delete = input("Name a fruit to be deleted from the list: ")
-		for i in list1:
+		for i in list2:
 			if i == delete:
-				list1.remove(delete)
+				list2.remove(delete)
 				count = count +1
 		if count < 2:
 			print("No match found, try again: ")
-	print(list1)
+	print(list2)
+	"""
+		Series3
+	"""
+def Series3():
+	list3 = ["Apples", "Pears", "Oranges", "Peaches"]
+	list3_copy = list3[0:]
+	for i in list3:
+		while(1):
+			print("Do you like", i,"? (yes/no)")
+			item = str(input())
+			if item == "yes":
+				pass
+				break
+			if item == "no":
+				list3_copy.remove(i)
+				break
+			else:
+				pass
+	print(list3_copy)
+
+def Series4():
+	list4 = ["Apples", "Pears", "Oranges", "Peaches"]
+	list4_new = [i[::-1] for i in list4]
+	list4.pop()
+	print(list4)
+	print(list4_new)
