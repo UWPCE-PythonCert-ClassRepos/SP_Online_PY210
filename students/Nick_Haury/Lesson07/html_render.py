eca9026084827c97365a33318eefa65ecf9589a6
Nicksama88 <Nicksama88@gmail.com>
2020-09-01 17:09:38 -0700

    add code to make html_render pass first two tests.  uncomment first two tests from test_html_render

--- students/Nick_Haury/Lesson07/html_render.py
+++ students/Nick_Haury/Lesson07/html_render.py
@@ -8,11 +8,37 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
+    tag = "html"
+
     def __init__(self, content=None):
-        pass
+        if content is None:
+            self.contents = []
+        else:
+            self.contents = [content]
 
     def append(self, new_content):
-        pass
+        if new_content is not None:
+            self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        # Loop through list of contents, having tags on front and back
+        out_file.write(f"<{self.tag}>\n")
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write("\n")
+        out_file.write(f"</{self.tag}>\n")
+
+
+class Body(Element):
+
+    tag = "body"
+
+
+class P(Element):
+
+    tag = "p"
+
+class Html(Element):
+
+    tag = "html"
+
