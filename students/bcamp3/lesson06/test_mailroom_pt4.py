ad3f3be5ee3bca375b5e50d1f5ae5fba12679f6f
Brian Campbell <bh.campbell@gmail.com>
2020-09-02 23:22:56 -0700

    mailroom_pt4 in-work

---
+++ students/bcamp3/lesson06/test_mailroom_pt4.py
@@ -0,0 +1,5 @@
+
+from mailroom_pt4.py import db, thank_you_tmp
+from mailroom_pt4.py import get_letter_text, msg_vars
+from mailroom_pt4.py import list_of_donors, check_name, add_donation
+from mailroom_pt4.py import get_report
