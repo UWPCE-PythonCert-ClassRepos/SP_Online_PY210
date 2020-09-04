764ee02c76c6cc7e3befdb751d88cf421383824f
Nicksama88 <Nicksama88@gmail.com>
2020-09-03 19:45:30 -0700

    add kwargs to Element init. create render_open_tag for attribues. refactor old render method to work with it new

--- students/Nick_Haury/Lesson07/html_render.py
+++ students/Nick_Haury/Lesson07/html_render.py
@@ -10,7 +10,9 @@ class Element(object):
 
     tag = "html"
 
-    def __init__(self, content=None):
+    # **kwargs are keyword value pairs for HTML style attributes
+    def __init__(self, content=None, **kwargs):
+        self.class_dict = kwargs
         if content is None:
             self.contents = []
         else:
@@ -22,7 +24,8 @@ class Element(object):
 
     def render(self, out_file):
         # Loop through list of contents, having tags on front and back
-        out_file.write(f"<{self.tag}>\n")
+        self.render_open_tag(out_file)
+        out_file.write(">\n")
         for content in self.contents:
             # out_file.write(content)
             try:
@@ -31,6 +34,11 @@ class Element(object):
                 out_file.write(content)
                 out_file.write("\n")
         out_file.write(f"</{self.tag}>\n")
+
+    def render_open_tag(self, out_file):
+        out_file.write(f"<{self.tag}")
+        for key in self.class_dict:
+                out_file.write(f' {key}="{self.class_dict[key]}"')
 
 
 class Body(Element):
@@ -52,13 +60,13 @@ class Head(Element):
 class OneLineTag(Element):
 
     def render(self, out_file):
-        out_file.write(f"<{self.tag}>")
-        for content in self.contents:
-            try:
-                content.render(out_file)
-            except AttributeError:
-                out_file.write(content)
+        self.render_open_tag(out_file)
+        out_file.write(">")
+        out_file.write(self.contents[0])
         out_file.write(f"</{self.tag}>\n")
+    
+    def append(self, content):
+        raise NotImplementedError
 
 class Title(OneLineTag):
 
