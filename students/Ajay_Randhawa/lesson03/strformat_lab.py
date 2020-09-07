469efda37e3dd6fd68b8590b51ca7dca8064d9a2
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-09-07 07:06:04 -0700

    added task2 and task3

--- students/Ajay_Randhawa/lesson03/strformat_lab.py
+++ students/Ajay_Randhawa/lesson03/strformat_lab.py
@@ -8,3 +8,23 @@ def formatString(a,b,c,d):
 	print(C)
 	D = "{:.2e}".format(d)
 	print(D)
+def task2(a,b,c,d):
+	A = f'{a:03d}'
+	print(A)
+	B = f'{b:.2f}'
+	print(B)
+	C = f'{c:.2e}'
+	print(C)
+	d = f'{d:.2e}'
+	print(d)
+def formatter(in_tuple):
+	a = len(in_tuple)
+	format_string = f'the {a} numbers are:'
+	while a>0:
+		if a>1:
+			format_string = format_string + " {},"
+		else:
+			format_string = format_string + " {}"
+		a = a-1
+	print(format_string)
+	return format_string.format(*in_tuple)
