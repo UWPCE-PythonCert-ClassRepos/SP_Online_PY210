9d914b608cc3128a6cf7f5dc75a23f3520ed14e3
bapandaOK44 <barbapy@uw.edu>
2020-09-08 12:30:48 -0500

    codebat warmup2 for ex1

---
+++ students/barb_matthews/lesson01/warmup2.py
@@ -0,0 +1,12 @@
+## Codebat Warump-2 String Times
+## By: barb matthews
+## 9/8/2020
+## Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+def string_times(str, n):
+   mystring = str * n
+   return mystring
+
+#call funcion to test
+print (string_times("Beep ", 5))
+
