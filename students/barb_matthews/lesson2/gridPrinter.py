85c3c03cfd54b0f622be67646a9390f6144f1d52
bapandaOK44 <barbapy@uw.edu>
2020-09-09 18:11:15 -0500

    gridPrinter first version working fixed size grid i

--- students/barb_matthews/lesson2/gridPrinter.py
+++ students/barb_matthews/lesson2/gridPrinter.py
@@ -2,4 +2,27 @@
 ## By: B. Matthews
 ## 9/8/2020
 
+def printGrid(n):
+	while n:			#iterate through the rows
+		if n<0:
+			break
+		n-=1
+
+		count=n
+		print ('+ ', end='')
+		count-=1
+
+		i=n
+		##while (i>0):
+			while count:		#iterate through the columns
+				if count<0:
+					break
+				print ('-', end=' ')
+				count-=1
+			print('+')
+		##i-=1
+	print()			#advance row
+
+printGrid(3)
+
 
