1d8418d7e0dc5f82192c00c0f534a3ba94c3fb72
Nicksama88 <Nicksama88@gmail.com>
2020-09-09 20:01:35 -0700

    refactor donors to top so they can be reused in tests.  make tests for adding donations

--- students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
+++ students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
@@ -7,10 +7,19 @@ import cli_main as cm
 Test for the object oriented version of the mailroom program.
 '''
 
-def test_create_donor():
-    # test if a donor can be successfully created
-    d1 = dm.Donor("Dude1")
-    d2 = dm.Donor("Dude2", [100.00])
-    d3 = dm.Donor("Dude3", [100.00, 200.00])
+d1 = dm.Donor("Dude1")
+d2 = dm.Donor("Dude2", [100.00])
+d3 = dm.Donor("Dude3", [100.00, 200.00])
 
-    assert d1.donor_name == "Dude1"
+def test_create_donor():
+    assert d1.name == "Dude1"
+    assert d1.donations == []
+    assert d2.donations == [100.00]
+    assert d3.donations == [100.00, 200.00]
+
+def test_add_donation():
+    with pytest.raises(TypeError):
+        d1.add_donation("hello")
+    d1.add_donation(10)
+    d1.add_donation(20.0)
+    assert d1.donations == [10, 20.0]
