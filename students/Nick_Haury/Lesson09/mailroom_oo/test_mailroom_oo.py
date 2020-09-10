6487c31b2b105f861010ea737e02cf3fa0d9563c
Nicksama88 <Nicksama88@gmail.com>
2020-09-09 17:22:46 -0700

    add first test for creating a donor

--- students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
+++ students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
+import pytest
+import donor_models as dm
+import cli_main as cm
 
 '''
-Implementation of the mailroom program from the first week, but this time using
-an object oriented approach.  
+Test for the object oriented version of the mailroom program.
+'''
+
+def test_create_donor():
+    # test if a donor can be successfully created
+    d1 = dm.Donor("Dude1")
+    d2 = dm.Donor("Dude2", [100.00])
+    d3 = dm.Donor("Dude3", [100.00, 200.00])
+
+    assert d1.donor_name == "Dude1"
