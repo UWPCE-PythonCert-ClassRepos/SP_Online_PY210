84eadda0bbdb220410f3a309a2c612654a50e371
Jason Jenkins <jdj0891@yahoo.com>
2020-09-09 20:53:02 -0700

    Completed activity

--- students/jason_jenkins/lesson08/SparseArray.py
+++ students/jason_jenkins/lesson08/SparseArray.py
@@ -4,10 +4,34 @@
 A class-based system for a SparseArray
 """
 
-class SparseArray(object):
+class SparseArray(list):
 
-    def __init__(self, tmp_list = None):
-        if tmp_list == None:
-            self.tmp_list = []
+    def __init__(self, l = None):
+        if l == None:
+            self.l = list()
         else:
-            self.tmp_list = tmp_list
+            self.l = list(l)
+
+    def __len__(self):
+        count = 0
+        for item in self.l:
+            if item != 0:
+                count += 1
+        return count
+
+
+    def __getitem__(self, index):
+        return self.l[index]
+
+
+    def __setitem__(self, key, newvalue):
+        if(newvalue != 0):
+            self.l[key] = newvalue
+
+
+    def append(self, val):
+        self.l.append(val)
+
+
+    def __delitem__(self, i):
+        del self.l[i]
