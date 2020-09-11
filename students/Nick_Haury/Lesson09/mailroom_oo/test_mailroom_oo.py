aba8a261b8d6eaf17ad17e08d6ae613b46e1dc84
Nicksama88 <Nicksama88@gmail.com>
2020-09-10 20:44:35 -0700

    add tests for DonorCollection

--- students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
+++ students/Nick_Haury/Lesson09/mailroom_oo/test_mailroom_oo.py
@@ -10,6 +10,11 @@ Test for the object oriented version of the mailroom program.
 d1 = dm.Donor("Dude1")
 d2 = dm.Donor("Dude2", [100.00])
 d3 = dm.Donor("Dude3", [100.00, 200.00])
+dc = dm.DonorCollection({
+    d1.name:d1,
+    d2.name:d2,
+    d3.name:d3
+})
 
 def test_create_donor():
     assert d1.name == "Dude1"
@@ -25,3 +30,15 @@ def test_add_donation():
     d1.add_donation(10)
     d1.add_donation(20.0)
     assert d1.donations == [10, 20.0]
+
+def test_donorcollection():
+    with pytest.raises(TypeError):
+        dm.DonorCollection("invalid")
+    assert dc.get_names() == [d1.name, d2.name, d3.name]
+    assert dc.donors[d1.name] == d1
+    with pytest.raises(TypeError):
+        dc.add_donor("not a Donor")
+    d4 = dm.Donor("Dude4")
+    dc.add_donor(d4)
+    assert d4.name in dc.get_names()
+    
