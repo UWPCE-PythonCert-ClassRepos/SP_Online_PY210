9f1242fa5528723f312cdbb89cbee33cf11cf236
bapandaOK44 <barbapy@uw.edu>
2020-09-11 17:05:34 -0500

    Added series.py for Fibonnaci & Lucas series

--- students/barb_matthews/lesson2/series.py
+++ students/barb_matthews/lesson2/series.py
@@ -1,5 +1,34 @@
-## Lesson 2, Exercise 2.3 Fibonnaci Series
+## Lesson 2, Exercise 2.4 Fibonnaci Series
 ## By: B. Matthews
 ## 9/11/2020
 
-def fibon():
+def fibonnaci(n):
+#"""Generates a Fibonnaci series with length n"""
+
+    if (n == 0):
+        return 0
+    elif (n == 1):
+        return 1
+    else:
+        return fibonnaci(n-1) + fibonnaci(n-2)
+
+def lucas(n):
+#"""Generates a Lucas series with length n"""
+    if (n == 0):
+        return 2
+    elif (n == 1):
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
+
+##Test the function
+n = int(input("Enter a number >> "))
+
+for i in range (n+1):
+    print (lucas(i))
+
+print ("Fibonnaci:", n,"th value is: ", fibonnaci(n))
+
+print ("Lucas series: ", lucas(n))
+
+
