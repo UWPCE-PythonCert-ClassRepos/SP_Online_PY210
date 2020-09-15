c4dbacc60d9ab56da66f557958e8fadd7136fb91
jakedaylong <57467298+FloppyDogs@users.noreply.github.com>
2020-09-15 16:55:24 -0700

    series.py updated with tests and refactored lucas and fibonacci to call series_sum. refactored series_sum to be smaller and more streamlined.
    All tests passed

--- students/jacob_daylong/Fibonacci_Series/series.py
+++ students/jacob_daylong/Fibonacci_Series/series.py
@@ -7,51 +7,93 @@ Created on Tue Sep 15 12:11:54 2020
 series.py
 """
 
-def fibonacci(n):
-    """returns nth fibonacci number.
-    1st 2 numbers are set to 0 and 1"""
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
+def sum_series(n, n0=0, n1=1):
+    """
+    Sums a series of numbers regardless of start point.
+    Check built in for fibonacci, default with only 1 argument, Lucas requires arguments.
+    """
 
-# print(fibonacci(7))
+    if n==0:
+        return n0
+    elif n==1:
+        return n1
+    else:
+        return sum_series(n-1, n0, n1)+sum_series(n-2, n0, n1)
+    pass
+
+def fibonacci(n):
+     """
+     Returns nth fibonacci number.
+     Calls sumseries with default starting values: 0, 1
+     Original logic of fibonacci is retained on block coment below
+     """
+
+    #original logic for fibonacci
+     '''
+     if n==0:
+         return 0
+     elif n==1:
+         return 1
+     else:
+         return fibonacci(n-1)+fibonacci(n-2)
+     '''
+
+     return sum_series(n)
+     pass
 
 def lucas(n):
+    """
+    Returns lucas numbers.
+    Calls sumseries with starting values: 2, 1
+    Original logic of fibonacci is retained on block coment below
+    """
+
+    #original logic for lucas
+    '''
     if n==0:
-        return 2
-    elif n==1:
-        return 1
-    else:
-        return lucas(n-1)+lucas(n-2)
+         return 2
+     elif n==1:
+         return 1
+     else:
+         return lucas(n-1)+lucas(n-2)
+    '''
 
-# print(lucas(4))
+    return sum_series(n, n0=2, n1=1)
+    pass
 
-def sumseries(n, n0=0, n1=1):
-    if n0==0 and n1==1:
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
-        else:
-            return sumseries(n-1)+sumseries(n-2)
-    elif n0==2 and n1==1:
-        if n==0:
-            return 2
-        elif n==1:
-            return 1
-        else:
-            return sumseries(n-1, n0, n1)+sumseries(n-2, n0, n1)
-    else:
-        if n==0:
-            return n0
-        elif n==1:
-            return n1
-        else:
-            return sumseries(n-1, n0, n1)+sumseries(n-2, n0, n1)
+"""
+Test block
+"""
 
-print(sumseries(7))
-print(sumseries(4, 2, 1))
-print(sumseries(4, 3, 2))
+if __name__ == "__main__":
+    # run some tests
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+
+    assert lucas(0) == 2
+    assert lucas(1) == 1
+
+    assert lucas(4) == 7
+
+    # test that sum_series matches fibonacci
+    assert sum_series(5) == fibonacci(5)
+    assert sum_series(7, 0, 1) == fibonacci(7)
+
+    # test if sum_series matched lucas
+    assert sum_series(5, 2, 1) == lucas(5)
+
+    # test if sum_series works for arbitrary initial values
+    assert sum_series(0, 3, 2) == 3
+    assert sum_series(1, 3, 2) == 2
+    assert sum_series(2, 3, 2) == 5
+    assert sum_series(3, 3, 2) == 7
+    assert sum_series(4, 3, 2) == 12
+    assert sum_series(5, 3, 2) == 19
+
+    print("tests passed")
