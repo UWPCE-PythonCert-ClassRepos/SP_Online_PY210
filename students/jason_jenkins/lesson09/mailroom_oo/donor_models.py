a8fb1d3d1346f00137a3c91fade9517a23fd82a1
Jason Jenkins <jdj0891@yahoo.com>
2020-09-19 14:40:50 -0700

    Completed Lesson 9

--- students/jason_jenkins/lesson09/mailroom_oo/donor_models.py
+++ students/jason_jenkins/lesson09/mailroom_oo/donor_models.py
@@ -7,11 +7,13 @@ Author: Jason Jenkins
 """
 
 # Define a multiple inheritance scheme:
+from operator import itemgetter
+
 class Donor(object):
 
     def __init__(self, full_name, donation_amount):
         self.full_name = full_name
-        self.total_given = 0
+        self._total_given = 0
         self.num_gifts = 0
 
         self.give(donation_amount)
@@ -21,17 +23,57 @@ class Donor(object):
         if self.num_gifts < 1:
             return 0
         else:
-            return self.total_given / self.num_gifts
+            return round(self.total_given / self.num_gifts, 2)
+
+    @property
+    def total_given(self):
+        return round(self._total_given, 2)
 
     def give(self, val):
         if(val > 0):
-            self.total_given += val
+            self._total_given += val
             self.num_gifts += 1
+
+    def thanks(self):
+        return f"Thank you {self.full_name} for your donation."
+
+    def __str__(self):
+        return self.full_name
+
+    def __eq__(self, other):
+        if self.full_name.lower() == other.lower():
+            return True
+        else:
+            return False
 
 
 # Define a multiple inheritance scheme:
 class DonorCollection(list):
-    pass
+    def get_list(self):
+        output = list()
+        for item in self:
+            output.append(item.full_name)
 
+        return "\n".join(filter(None, output))
 
+    def get_report(self):
+        output = list()
 
+        for item in self:
+            output.append([item.full_name, item.total_given, item.num_gifts, item.average])
+
+        return sorted(output, key=itemgetter(1), reverse=True)
+
+    def donate(self, person, amount):
+        try:
+            self.lookup(person).give(amount)
+        except AttributeError:
+            new_doner = Donor(person, amount)
+            self.append(new_doner)
+        finally:
+            return self.lookup(person).thanks()
+
+    def lookup(self, person):
+        for item in self:
+            if item == person:
+                return item
