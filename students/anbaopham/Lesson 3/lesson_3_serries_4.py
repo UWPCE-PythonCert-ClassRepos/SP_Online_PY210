c2c81d0c75882479c8241a97e2df12c989aca796
unknown <anbaopham@gmail.com>
2020-09-19 23:47:04 -0700

    fixed PEP8 in lesson_3_serries_4

--- students/anbaopham/Lesson 3/lesson_3_serries_4.py
+++ students/anbaopham/Lesson 3/lesson_3_serries_4.py
@@ -1,10 +1,10 @@
 
-fruit_list = ["Apples", "Pears","Oranges","Peaches"]
+fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
 l = fruit_list[0:-1]
-newList = []
+new_list = []
 for i in l:
-    newList.append(i[::-1])
+    new_list.append(i[::-1])
 
 print("old list: ", fruit_list)
 print("old list after remove last item: ",l)
-print("new list: " ,newList[::-1])
+print("new list: " , new_list[::-1])
