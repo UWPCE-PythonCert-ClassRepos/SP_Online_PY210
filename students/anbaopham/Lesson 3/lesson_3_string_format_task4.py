9910a6b23c0fff58ba0f7dd77190f49dbb78c461
unknown <anbaopham@gmail.com>
2020-09-20 00:02:25 -0700

    fixed PEP8 in lesson_3_string_format_task4

--- students/anbaopham/Lesson 3/lesson_3_string_format_task4.py
+++ students/anbaopham/Lesson 3/lesson_3_string_format_task4.py
@@ -4,8 +4,8 @@ def task_4(seq):
     for i in seq:
         #n = "{:02d}".format(i)
         nums.append("{:02d}".format(i))
-    newNums = nums[-2::] + nums[2:-2] + nums[0:2]
-    print(newNums)
+    new_nums = nums[-2::] + nums[2:-2] + nums[0:2]
+    print(new_nums)
 
-seq=(4,30, 2017, 2,27)
+seq=(4, 30, 2017, 2, 27)
 task_4(seq)
