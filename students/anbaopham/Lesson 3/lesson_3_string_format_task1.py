2b16dcffa03ca2712ee57867ef7c4d21257f07d3
unknown <anbaopham@gmail.com>
2020-09-19 23:51:52 -0700

    fixed PEP8 in lesson_3_string_format_task1

--- students/anbaopham/Lesson 3/lesson_3_string_format_task1.py
+++ students/anbaopham/Lesson 3/lesson_3_string_format_task1.py
@@ -1,19 +1,19 @@
 def task_1(seq):
     l = len(seq)-1
-    x=[]
+    x= []
     for i in seq[1::]:
         n = format(i,'e')
         x.append(n)
-    fileN =[]
+    file_name =[]
     tempL = str(x[0])
-    if len(tempL)==1:
-        fileN = "00" + str(seq[0])
-    elif len(tempL)==2:
-        fileN = "0"+str(seq[0])
+    if len(tempL) == 1:
+        file_name = "00" + str(seq[0])
+    elif len(tempL) == 2:
+        file_name = "0"+str(seq[0])
     else:
-        fileN = seq[0]
-    
-    print(("file_{}: " + ", ".join(["{}"]*l)).format(fileN, *x))
+        file_name = seq[0]
 
-seq = ( 2, 123.4567, 10000, 12345.67)
+    print(("file_{}: " + ", ".join(["{}"]*l)).format(file_name, *x))
+
+seq = (2, 123.4567, 10000, 12345.67)
 task_1(seq)
