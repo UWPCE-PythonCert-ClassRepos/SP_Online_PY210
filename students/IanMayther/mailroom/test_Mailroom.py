ed6335af7aec8e2ecdbe7e1d8159e4a191df1644
Ian Mayther <maytheri@gmail.com>
2020-09-20 15:43:02 -0700

    Added test for mailroom, lacking tests at the moment

---
+++ students/IanMayther/mailroom/test_Mailroom.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+test code for Mailroom
+
+Different scenarios to test:
+Thank you:
+    Old Donor New Gift
+    New Donor New gift
+
+Create report
+    create report
+    print report
+
+Send Letters
+    Body of letters
+    Letters are created
+"""
+
+
+# you can change this import to test different versions
+from Mailroom import gift
+
