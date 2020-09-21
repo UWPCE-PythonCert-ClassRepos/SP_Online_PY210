20f0d0715587727c941ceff2e75ccec092f535cf
belarsongit <68929696+belarsongit@users.noreply.github.com>
2020-09-20 19:59:29 -0700

    updated lesson07 files

--- students/becky_larson/lesson07/html_render.py
+++ students/becky_larson/lesson07/html_render.py
@@ -8,103 +8,137 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
     tag = "html"
+    indent = "    "
 
     def __init__(self, content=None, **kwargs):
+        self.attributes = kwargs
         if content is None:
             self.contents = []
         else:
             self.contents = [content]
-        self.attributes = kwargs
 
     def append(self, new_content):
         self.contents.append(new_content)
 
-    def render(self, out_file):
-        # loop through the list of contents: 
-        open_tag = ["<{}".format(self.tag)]
-        for k, v in self.attributes.items():
-            open_tag.append(' {}="{}"'.format(k,v))
-        open_tag.append(">\n")
-        out_file.write("".join(open_tag))
-        for content in self.contents:
-            out_file.write("<{}>\n".format(self.tag))
-            try:
-                content.render(out_file)
-            except AttributeError:
-                out_file.write(content)
-                out_file.write("</{}>\n".format(self.tag))
-
     def _open_tag(self):
-        open = ['<{}'.format(self.tag)]
-        for k, v in self.attributes.items():
-            open.append(' {}="{}"'.format(k,v))
-        open.append('>')
-        open= ''.join(open)
-        return open
-        
+        open_tag = ["<{}".format(self.tag)]
+        for key, value in self.attributes.items():
+            open_tag.append(' {}="{}"'.format(key, value))
+        open_tag.append(">")
+        return "".join(open_tag)
+
     def _close_tag(self):
-        close = '</{}>'.format(self.tag)
-        return close
+        return "</{}>".format(self.tag)
+
+    def render(self, out_file, cur_ind=""):
+
+        new_indent = cur_ind + self.indent
+        out_file.write(cur_ind+self._open_tag())
+        out_file.write("\n")
+
+        for content in self.contents:
+            try:
+                content.render(out_file, cur_ind=new_indent)
+            except AttributeError:
+                out_file.write(new_indent + content)
+            out_file.write("\n")
+        out_file.write(cur_ind + self._close_tag())
 
 
 class Html(Element):
-
     tag = 'html'
 
-#    def render(self, out_file):
-#        out_file.write("<{}>\n".format(self.tag))
-#        for content in self.contents:
-#            out_file.write(content)
-#        out_file.write("</{}>\n".format(self.tag))
+    def render(self, out_file, cur_ind=""):
+        out_file.write(cur_ind)
+        out_file.write("<!DOCTYPE html>\n")
+        Element.render(self, out_file, cur_ind)
 
 
 class Body(Element):
-
     tag = 'body'
 
-class P(Element):
 
+class P(Element):
     tag = 'p'
 
-class Head(Element):
 
+class Head(Element):
     tag = 'head'
 
 
 class OneLineTag(Element):
-    def render(self, out_file):
-        out_file.write("<{}>".format(self.tag))
+
+    def render(self, out_file, cur_ind=""):
+        out_file.write(cur_ind + self._open_tag())
+        out_file.write(cur_ind+self._open_tag())
         out_file.write(self.contents[0])
-        out_file.write("</{}>\n".format(self.tag))
+        out_file.write(self._close_tag())
 
     def append(self, content):
         raise NotImplementedError
-    
-class Title(OneLineTag):
 
-    tag = 'title'
+
+class Title(OneLineTag):
+    tag = "title"
 
 
 class SelfClosingTag(Element):
-    # belarson -- stopped in step 5. not sure where this goes. 
-    #  https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/html_renderer_tutorial.html#html-renderer-tutorial
-    
-    def render(self, out_file):
-        # loop through the list of contents:
-        out_file.write(self._open_tag())
-        out_file.write("\n")
-        for content in self.contents:
-            try:
-                content.render(out_file)
-            except AttributeError:
-                out_file.write(content)
-                out_file.write("\n")
-        out_file.write(self._close_tag())
-        out_file.write("\n")
+
+    def __init__(self, content=None, **kwargs):
+        if content:
+            raise TypeError("SelfClosingTag can not contain any content")
+        super().__init__(content=content, **kwargs)
+
+    def render(self, outfile, cur_ind=""):
+        tag = self._open_tag()[:-1] + " />\n"
+        outfile.write(cur_ind + tag)
+
+    def append(self, *args):
+        raise TypeError("You can not add content to a SelfClosingTag")
 
 
 class Hr(SelfClosingTag):
     tag = "hr"
 
 
+class Br(SelfClosingTag):
+    tag = "br"
 
+
+class A(OneLineTag):
+    tag = "a"
+
+    def __init__(self, link, content=None, **kwargs):
+        kwargs["href"] = link
+        super().__init__(content, **kwargs)
+
+
+class Ul(Element):
+    tag = "ul"
+
+
+class Li(Element):
+    tag = "li"
+
+
+class H(OneLineTag):
+    base_tag = "h"
+
+    def __init__(self, level, content=None, **kwargs):
+        self.tag = self.base_tag + str(level)
+        super().__init__(content, **kwargs)
+
+
+class Meta(Element):
+    tag = "meta "
+
+    def render(self, out_file, cur_ind=""):
+
+        out_file.write(cur_ind + "<{}".format(self.tag))
+        for content in self.contents:
+            try:
+                content.render(out_file, cur_ind)
+            except AttributeError:
+                out_file.write(content)
+        out_file.write("</>")
+        out_file.write('\n')
