22b0e8bf7def92a0688639e6623cd51a210c40bd
jakedaylong <57467298+FloppyDogs@users.noreply.github.com>
2020-09-22 09:40:31 -0700

    Corrected first_four_last_four to remove every other remaining element.
    Corrected thirds to for with any length of string

--- students/jacob_daylong/Lesson 3/slicing_lab/slicing_lab.py
+++ students/jacob_daylong/Lesson 3/slicing_lab/slicing_lab.py
@@ -12,13 +12,17 @@ with the first 4 and the last 4 items removed, and then every other item in the 
 with the elements reversed (just with slicing).
 with the last third, then first third, then the middle third in the new order.
 """
+import math
+
 
 txt = "This is a string"
+txt2 = "This is a string that has way more letters than the first, but hopefully still works as intended."
 numbers = [1, 2, 3, 4, 5, 10, 11, 99, 43, 78, 1002]
 print()
 print("Unmodified")
 print(txt)
 print(numbers)
+print(txt2)
 print()
 
 def exchange_first_last(seq):
@@ -30,28 +34,32 @@ print()
 print("Exchange first and last, numbers")
 print(exchange_first_last(txt))
 print(exchange_first_last(numbers))
+print(exchange_first_last(txt2))
 print()
 
 def every_other(seq):
     """Remove every other element"""
-    seq = seq[1::2]
+    seq = seq[::2]
     return seq
 
 print()
 print("Remove every other")
 print(every_other(txt))
 print(every_other(numbers))
+print(every_other(txt2))
 print()
 
 def first_four_last_four(seq):
-    """Remove first four and last four elements"""
-    seq = seq[4:-4]
+    """Remove first four and last four and every other of the remaining elements"""
+    seq = seq[4:-4:2]
     return seq
 
 print()
-print("Remove first four and last four")
+print("Remove first four and last four, and every other remaining element")
 print(first_four_last_four(txt))
 print(first_four_last_four(numbers))
+print(first_four_last_four(txt2))
+
 print()
 
 def reversal(seq):
@@ -63,15 +71,18 @@ print()
 print("Reverse")
 print(reversal(txt))
 print(reversal(numbers))
+print(reversal(txt2))
 print()
 
 def thirds(seq):
     """Rearrange thirds of the element"""
-    seq = seq[9:] + seq[0:4] + seq[5:8]
+    third = math.floor(len(seq)/3)
+    seq = seq[third:third*2] + seq[third*2:] + seq[0:third]
     return seq
 
 print()
 print("Thirds Exchanged")
 print(thirds(txt))
 print(thirds(numbers))
+print(thirds(txt2))
 print()
