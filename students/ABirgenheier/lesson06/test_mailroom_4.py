3faad3e95d46a4b67ea774e74b996a8eea010907
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-09-23 10:23:24 -0700

    bump

--- students/ABirgenheier/lesson06/test_mailroom_4.py
+++ students/ABirgenheier/lesson06/test_mailroom_4.py
@@ -8,7 +8,7 @@ mailroom_4.donars_data = mailroom_4.donars()
 
 def test_donars():
     """ Test initial donars list and amounts """
-    donars_data = mailroom4.donars()
+    donars_data = mailroom_4.donars()
     assert donars_data['Mike'] == [200, 150, 50]
     assert donars_data['Sarah'] == [150, 150, 150]
 
@@ -16,13 +16,13 @@ def test_donars():
 def test_list_of_donars():
     """ Test all donor names are returned """
     donars_list = ('Mike\nTony\nSarah')
-    assert mailroom4.list_of_donars() == donars_list
+    assert mailroom_4.list_of_donars() == donars_list
 
 
 def test_donations():
     """ Test donor name and donation amount update """
-    mailroom4.donations('Mike', 200)
-    assert mailroom4.donars_data.get('Mike') == [200]
+    mailroom_4.donations('Mike', 200)
+    assert mailroom_4.donars_data.get('Mike') == [200]
 
 
 def test_thank_you_message():
@@ -31,14 +31,14 @@ def test_thank_you_message():
                f'\n\nThank you for your generous donation of $500.00.'
                '\nWe value your contribution and support.'
                '\n\nSincerely,\n\nNew Horizon Charity Director\n')
-    assert mailroom4.thank_you_message('Mike') == message
+    assert mailroom_4.thank_you_message('Mike') == message
 
 
 def test_send_letters_to_all():
     dirpath = pathlib.Path('./').absolute()
     file1 = os.path.join(dirpath, 'Sarah.txt')
     file2 = os.path.join(dirpath, 'Tony.txt')
-    mailroom4.send_letters_to_all()
+    mailroom_4.send_letters_to_all()
     assert os.path.exists(file1)
     assert os.path.exists(file2)
 
