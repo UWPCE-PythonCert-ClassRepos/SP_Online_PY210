7b57f7fbb6deab0d8b2c61fc084bbadf0696b840
Devon Peterka <dhpeterka@gmail.com>
2020-09-26 21:01:05 -0700

    All tests passed.  All 9 steps complete.

--- students/Devon_Peterka/Lesson_7/html_render.py
+++ students/Devon_Peterka/Lesson_7/html_render.py
@@ -9,6 +9,7 @@ A class-based system for rendering html.
 class Element(object):
     
     tag = 'html'
+    indent = '   '
 
     def __init__(self, content=None, **kwargs):
         self.content = [] if content is None else [content]
@@ -18,27 +19,31 @@ class Element(object):
     def append(self, new_content):
         self.content.append(new_content)
 
-    def _open_tag(self):
-        open_tag = f'<{self.tag}'
+    def _open_tag(self, cur_indent=''):
+        open_tag = f'{cur_indent}<{self.tag}'
         for key, value in self.attributes.items():
             open_tag += f' {key}="{value}"'
         return open_tag + ('>\n')
 
-    def _close_tag(self):
+    def _close_tag(self, cur_indent=''):
         return f'</{self.tag}>\n'
 
-    def render(self, out_file):
-        out_file.write(self._open_tag())
+    def render(self, out_file, cur_indent=''):
+        out_file.write(self._open_tag(cur_indent))
+        self.indent += cur_indent
         for content in self.content:
             try:
-                content.render(out_file)
+                content.render(out_file, self.indent)
             except AttributeError:
-                out_file.write(content)
+                out_file.write(self.indent + content)
             out_file.write('\n')
-        out_file.write(self._close_tag())
+        out_file.write(cur_indent + self._close_tag(cur_indent))
 
 class Html(Element):
     tag = 'html'
+    def render(self, out_file, cur_indent=''):
+        out_file.write(cur_indent + '<!DOCTYPE html>\n')
+        Element.render(self, out_file, cur_indent)
 
 class Body(Element):
     tag = 'body'
@@ -50,7 +55,8 @@ class Head(Element):
     tag = 'head'
 
 class OneLineTag(Element):
-    def render(self, out_file):
+    def render(self, out_file, cur_indent=''):
+        self.indent += cur_indent
         out_file.write(self._open_tag()[:-1])
         out_file.write(' '.join(self.content))
         out_file.write(self._close_tag())
@@ -62,7 +68,8 @@ class SelfClosingTag(Element):
     def __init__(self, **kwargs):
         self.attributes = kwargs.copy()
 
-    def render(self, out_file):
+    def render(self, out_file, cur_indent=''):
+        self.indent += cur_indent
         out_file.write(self._open_tag()[:-2] + ' />\n')
 
 class Hr(SelfClosingTag):
@@ -77,3 +84,15 @@ class A(OneLineTag):
     def __init__(self, link, content=None, **kwargs):
         kwargs['href'] = link
         super().__init__(content, **kwargs)
+
+class Ul(Element):
+    tag = 'ul'
+
+class Li(Element):
+    tag = 'li'
+
+class Header(OneLineTag):
+    def __init__(self, level, content=None, **kwargs):
+        self.tag = f'h{level}'
+        super().__init__(content, **kwargs)
+        
