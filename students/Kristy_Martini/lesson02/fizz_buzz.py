7bb6b1db29854af616f51794e71a102cf37849cf
Kristy Martini <martinikristy@gmail.com>
2020-09-27 18:41:30 -0700

    series function with sum functions, addition of docstring to fizzbuzz assignment

--- students/Kristy_Martini/lesson02/fizz_buzz.py
+++ students/Kristy_Martini/lesson02/fizz_buzz.py
@@ -1,4 +1,9 @@
 def fizz_buzz():
+    """
+    Function that prints numbers from 1-100 (inclusive) and will replace the number
+    with "Fizz" if it is a multiple of 3, "Buzz" if it is a multiple of 5, or 
+    "FizzBuzz" if it is a multiple of both 3 and 5.
+    """
     for i in range(1,101):
         if not (i % 5) and not (i % 3):
             print("FizzBuzz")
