812ea2a7f501d718a7f9e5876af0cee00cc337a3
bapandaOK44 <barbapy@uw.edu>
2020-09-29 14:13:02 -0500

    started Mailroom part 1

---
+++ students/barb_matthews/lesson3/mailroom.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+## Lesson 3.3 Mailroom Part 1
+## By: B. Matthews
+## 9/28/2020
+## This is the first of a four-part assignment that will keep track of an imaginary charity's
+## donors, the donation amounts, and will print a thank you message if desired.
+## It's a bit simple, but will add more improvements later
+
+import sys
+
--- students/barb_matthews/lesson3/strformat_lab.py
+++ students/barb_matthews/lesson3/strformat_lab.py
@@ -8,6 +8,8 @@ stuff = (2, 123.4567, 10000, 12345.67)
 l = len(stuff)
 test_tuple = (8, 4, 5, 11, 222, 9, 1)
 tt = (4, 30, 2017, 2, 27)
+list_of_stuff = ['oranges', 1.3, 'lemons', 1.1]
+table_data = ("Bilbo", 111, 1000.11), ("Gandalf", 150, 50.12), ("Sue", 22, 2.15)
 
 ## Task 1
 def do_this():
@@ -26,12 +28,26 @@ def formatter(in_tuple):
 
 ## Task 4
 def something(in_tuple):
-    k = formatter(in_tuple)
-    
+    a, b = "{:02d}".format(in_tuple[3]), "{:02d}".format(in_tuple[0])
+    print(f"{a} {in_tuple[4]} {in_tuple[2]} {b} {in_tuple[1]}")
 
+## Task 5
+def task5(list_one):
+    print(f"The weight of an {list_one[0][:-1]} is {list_one[1]} and the weight of a "
+          f"{list_one[2][:-1]} is {list_one[3]}")
+    print(f"The weight of an {list_one[0][:-1].upper()} is {list_one[1] * 1.2} and the weight of a "
+          f"{list_one[2][:-1].upper()} is {list_one[3] * 1.2}")
+
+## Task 6
+def task6(data):
+    for i in data:
+        print("{0:<12} {1:>5} {2:>12}".format(*i))
 
 do_this()
 do_another_thing()
 print("The test results = ")
 formatter(test_tuple)
+something(tt)
+task5(list_of_stuff)
+task6(table_data)
 
