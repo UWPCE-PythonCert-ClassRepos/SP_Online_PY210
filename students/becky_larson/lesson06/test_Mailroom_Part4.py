4903aecdb40fd82897ec5f7cc52e6af8a11e9333
belarsongit <68929696+belarsongit@users.noreply.github.com>
2020-10-04 14:29:28 -0700

    Final updates to Lesson06

--- students/becky_larson/lesson06/test_Mailroom_Part4.py
+++ students/becky_larson/lesson06/test_Mailroom_Part4.py
@@ -6,21 +6,26 @@ Testing Mailroom
 
 import mailroom_part4
 
+
 def test_donor_exists():
-     mailroom_part4.donor_exists('Cher') is True
+    mailroom_part4.donor_exists('Cher') is True
+
 
 def test_list_donors():
-     mailroom_part4.list_donors() == ['Cher','Drew Barrymore','Charlie Brown','Jack Black','Sam Smith',]
+    mailroom_part4.list_donors() == ['Cher','Drew Barrymore','Charlie Brown','Jack Black','Sam Smith',]
+
 
 def test_add_donor():
-     mailroom_part4.add_donor('Mike', 245.00) is True
-     expected = [245.00]
-     assert mailroom_part4.donor_db['Mike'] == expected
-     
+    mailroom_part4.add_donor('Mike', 245.00) is True
+    expected = [245.00]
+    assert mailroom_part4.donor_db['Mike'] == expected
+
+
 def test_update_donor():
-     mailroom_part4.update_donor('Cher', 44) is True
-     expected = [1000.00, 245.00, 44]
-     assert mailroom_part4.donor_db['Cher'] == expected
+    mailroom_part4.update_donor('Cher', 44) is True
+    expected = [1000.00, 245.00, 44]
+    assert mailroom_part4.donor_db['Cher'] == expected
+
 
 def test_get_report():
     results = mailroom_part4.get_report(mailroom_part4.assert_donor_db)
@@ -34,7 +39,8 @@ def test_get_report():
     assert row3 == "Sam Smith                  $     5,524.00             2  $    2,762.00"
     assert row4 == "Cher                       $     1,245.00             2  $      622.50"
     assert row5 == "Charlie Brown              $       175.01             3  $       58.34"
-    
+
+
 def test_get_letter_text_1():
     donation_dict = {}
     donation_dict['name'] = 'Frank'
@@ -43,7 +49,7 @@ def test_get_letter_text_1():
     actual = mailroom_part4.get_letter_text(donation_dict)
     assert actual[0:11] == expected
 
- 
+
 def test_get_letter_text_2():
     donation_dict = {}
     donation_dict['name'] = 'Frank'
@@ -53,5 +59,3 @@ def test_get_letter_text_2():
     money = "{0:.2f}".format(donation_dict['donation'])
     print(money)
     assert(money) in actual
-
-
