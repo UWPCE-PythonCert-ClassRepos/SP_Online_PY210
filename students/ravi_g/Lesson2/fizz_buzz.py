a04bc5b6b88358475f0b990fe8b322d88715c281
Ravi Ganta <grkumar82@gmail.com>
2020-10-03 19:05:13 -0700

    Added ReadMe and files for lesson2

---
+++ students/ravi_g/Lesson2/fizz_buzz.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBizz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
