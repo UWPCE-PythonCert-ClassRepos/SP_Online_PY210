d9e7956e909a8ddce6848904cfa4f586be93cd60
Harrison <edhar@3c22fb3f8b3b.ant.amazon.com>
2020-10-06 02:42:12 -0700

    Lesson 3.2 List Lab Series_1

--- students/phrost/Lesson03/Exercises/Series_1.py
+++ students/phrost/Lesson03/Exercises/Series_1.py
@@ -3,7 +3,7 @@ print(fruit)
 user_f = input('Please tell me another fav of your fruits: ')
 fruit.append(user_f)
 print(fruit)
-fruit_num = input('Please enter a number between 1 and 5 for your fav fruit in list : ') 
+fruit_num = input('Please enter a number between 1 and 5 for your fav fruit in list : ')
 fruit_num = int(fruit_num)
 for value, response in enumerate(fruit, start=1):
     if value == fruit_num:
