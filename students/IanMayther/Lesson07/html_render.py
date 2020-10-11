57fad152a4d34d56802f47ae9878927cdd7d0722
Ian Mayther <maytheri@gmail.com>
2020-10-11 12:21:52 -0700

    Reftactoring method that creates tag

--- students/IanMayther/Lesson07/html_render.py
+++ students/IanMayther/Lesson07/html_render.py
@@ -20,18 +20,25 @@ class Element(object):
     def append(self, new_content):
         self.contents.append(new_content)
 
+    def form_tag(self):
+        open_tag = ["<{}".format(self.tag)]
+        for key, value in self.attributes.items():
+            open_tag.append(" {0}=\"{1}\"".format(key, value))
+        open_tag.append(">\n")
+        output_file = "".join(open_tag)
+        output_file = "Test Text"
+        #return output_file
+
     #@classmethod
     def render(self, out_file):
         #loop through the contents
-        open_tag = ["<{}".format(self.tag)]
-        for key, value in self.attributes.items():
-            place_key = str(key)
-            place_value = str(value)
-            open_tag.append(" {0}=\"{1}\"".format(key, value))
-        open_tag.append(">\n")
-        out_file.write("".join(open_tag))
-        for content in self.contents:  
-            #out_file.write("<{}>\n".format(self.tag))          
+        # open_tag = ["<{}".format(self.tag)]
+        # for key, value in self.attributes.items():
+        #     open_tag.append(" {0}=\"{1}\"".format(key, value))
+        # open_tag.append(">\n")
+        place_text = form_tag()
+        out_file.write(place_text)
+        for content in self.contents:           
             try:
                 content.render(out_file)
             except AttributeError:
@@ -39,8 +46,6 @@ class Element(object):
             out_file.write("\n")
             out_file.write("</{}>\n".format(self.tag))
         
-
-
 class Html(Element):
     tag = 'html'
 
