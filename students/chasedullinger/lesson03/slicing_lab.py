8cd1b0a66dc4be69351fcdab3f2ad14cff54c58d
Chase Dullinger <chase.dullinger@gmail.com>
2020-10-13 18:43:25 -0700

    added exchange_first_last method

--- students/chasedullinger/lesson03/slicing_lab.py
+++ students/chasedullinger/lesson03/slicing_lab.py
@@ -3,12 +3,16 @@
 def exchange_first_last(seq):
     return seq[-1:] + seq[1:-1] + seq[:1]
 
+def remove_every_other(seq):
+    return seq[::2]
 
 if __name__=="__main__":
     a_string = "this is a string"
     a_tuple = (2, 54, 13, 12, 5, 32)
 
     print(exchange_first_last(a_string))
+    print(remove_every_other(a_string))
+    print(remove_every_other(a_tuple))
 
     assert exchange_first_last(a_string) == "ghis is a strint"
     assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
