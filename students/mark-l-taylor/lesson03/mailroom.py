e67515c424c45c22f8228c9ca84a2567a508ddbd
Mark L Taylor <taylormarkld@gmail.com>
2020-10-14 00:25:04 -0700

    Added mailroom.py,  built out the prompt_actions, create_report functions. Need to add sorting on report.

--- students/mark-l-taylor/lesson03/mailroom.py
+++ students/mark-l-taylor/lesson03/mailroom.py
@@ -15,20 +15,42 @@ def send_thanks():
     pass
     
 def create_report():
-    pass
+    #TODO: Need to sort by historical donation amount
+    frmt_header = '{:<26}|{:^13}|{:^11}|{:>13}'
+    frmt_data   = '{:<26} ${:>11.2f} {:>11}  ${:>12.2f}'
+    print('\nDonations Summary:\n')
+    print(frmt_header.format('Donor Name','Total Given','Num Gifts','Average Gift'))
+    print('-'*66)
+    for n,d in zip(donors,donations):
+        print(frmt_data.format(n,sum(d),len(d),sum(d)/len(d)))
+        
+    
 
 def prompt_actions():
-    print('What would you like to do:')
+    print('\nWhat would you like to do?')
     for i,act in enumerate(actions):
         print(f'\t({i+1}) {act}')
     
     #Get user action
-    response = input('Please select an action:')
+    response = input('Please select an action: ')
     while True:
-        if not response.isnumeric() or int(response) not in list(range(1,len(actions))):
-            response = input(f'Select a number between 1 and {len(actions)}:')
+        if not response.isnumeric() or int(response) not in list(range(1,len(actions)+1)):
+            response = input(f'Select a number between 1 and {len(actions)}: ')
         else:
             return int(response),actions[int(response)-1]
     
 if __name__ == '__main__':
-    print(prompt_actions())
+
+    #Begin the script by asking the user what they want to do
+    #program will continue to loop until the user selects the Quit option
+    while True:
+        resp_num,resp_str = prompt_actions()   
+        if resp_str == 'Send a Thank You':
+            send_thanks()
+        elif resp_str == 'Create a Report':
+            create_report()
+        elif resp_str == 'Quit':
+            print('Exiting Script.')
+            exit()
+    
+    
