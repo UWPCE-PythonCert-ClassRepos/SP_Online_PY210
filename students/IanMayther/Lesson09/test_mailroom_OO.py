1e0e6f3b8777fd8504aee82a4f50c843bd52e0ac
Ian Mayther <maytheri@gmail.com>
2020-10-24 08:55:29 -0700

    Updated file names

--- students/IanMayther/Lesson09/test_mailroom_OO.py
+++ students/IanMayther/Lesson09/test_mailroom_OO.py
@@ -1,1 +1,27 @@
-#!/usr/bin/env python3
+"""
+test code for mailroom_OO.py
+
+"""
+
+import io
+import pytest
+import math
+
+# import * is often bad form, but makes it easier to test everything in a module.
+from Donor_Models import *
+
+##############################
+# Step 1 - Donor Class Testing
+##############################
+
+def test_donor_init():
+    '''Instantiate with donation'''
+    with pytest.raises(AttributeError):
+        d = Donor(15)
+
+'''
+create donor
+append donation
+init
+str
+repr
