3b358a0eb7e3c49248641e182de65f3e8c929f9e
Ian Mayther <maytheri@gmail.com>
2020-10-24 09:36:36 -0700

    Got init to pass

--- students/IanMayther/Lesson09/Donor_Models.py
+++ students/IanMayther/Lesson09/Donor_Models.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+Donor Class and Donation Class
+"""
+
+class Donor(object):
+    """
+    Control all data related to a specific donor
+    """
+    donations = []
+
+    def __init__(self, name=None):
+        if name is None:
+            raise AttributeError("Must supply Donor Name")
+        elif isinstance(name, str):
+            self.name = name
+        else:
+            raise AttributeError()
--- students/IanMayther/Lesson09/test_mailroom_OO.py
+++ students/IanMayther/Lesson09/test_mailroom_OO.py
@@ -15,13 +15,46 @@ from Donor_Models import *
 ##############################
 
 def test_donor_init():
-    '''Instantiate with donation'''
+    '''Instantiate donors'''
+    with pytest.raises(AttributeError):
+        d = Donor()
+    
     with pytest.raises(AttributeError):
         d = Donor(15)
 
+    d = Donor("Morgan Stanley")
+
+def test_init_donation():
+    '''Creating first donation'''
+    d = Donor()
+    d.append(15)
+
+    assert d.donations == [15]
+
+#paramatize
+def test_str():
+    Morgan_Stanley = Donor()
+
+    assert str(Morgan_Stanley) == "Morgan Stanley"
+
+#paramatize
+def test_repr():
+    Morgan_Stanley = Donor()
+    Morgan_Stanley.append(16)
+
+    assert repr(Morgan_Stanley) == 'Morgan Stanley([16])'
+
 '''
 create donor
+track name?
+track donations
 append donation
 init
 str
 repr
+
+
+
+confirm existance
+
+'''
