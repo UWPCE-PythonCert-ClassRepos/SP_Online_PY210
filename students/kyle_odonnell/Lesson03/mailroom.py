b5df52ddf5debbf783411d843c5a5d1f09c5d1f7
kylenod <62862441+kylenod@users.noreply.github.com>
2020-10-25 23:47:14 -0400

    Added docstrings for functions

--- students/kyle_odonnell/Lesson03/mailroom.py
+++ students/kyle_odonnell/Lesson03/mailroom.py
@@ -1,7 +1,10 @@
-#if __name__ == '__main__'
 
 
 
+
+# if __name__ == '__main__':
+
+# Data -------------------------------------------------------------------- #
 donor_list = [
     {"Donor Name": "William Gates, III", "Total Given": 653784.49, "Num Gifts": 2, "Average Gift": 326892.24},
     {"Donor Name": "Mark Zuckerberg", "Total Given": 16396.10, "Num Gifts": 3, "Average Gift": 5465.37},
@@ -11,8 +14,14 @@ donor_list = [
 ]
 
 
-# Processing Functions
+# Processing  ------------------------------------------------------------- #
 def add_donation_amount(donor, donation, db):
+    """ Add new donation to database
+    :param donor: (string) with name of donor:
+    :param donation: (float) amount of donation:
+    :param db: (list) of dictionaries with donation information:
+    :return: (list) of dictionaries
+    """
     in_list = False
     for entry in db:
         if donor.lower() == entry["Donor Name"].lower():
@@ -29,31 +38,56 @@ def add_donation_amount(donor, donation, db):
     return db
 
 
-# I/O  Functions
+# Presentation (Input/Output)  -------------------------------------------- #
 def menu_options():
+    """ Print menu for users
+    :return: nothing
+    """
     print('''
     ******OPTIONS MENU*******
     Option 1: Send a Thank You
     Option 2: Create a Report
     Option 3: Exit \n''')
 
+
 def show_names(db):
+    """ Print list of donor names
+    :param db: (list) of dictionaries with donation information
+    :return: nothing
+    """
     for i, entry in enumerate(db):
         print(str(i + 1) + ".", "Donor Name:", entry["Donor Name"])
 
 def prompt_menu_option():
+    """ Prompt menu option
+    :return: string
+    """
     menu_option = input("Please select an option from the menu (1-3): ")
     return menu_option
 
+
 def enter_name():
+    """ Prompt donor name
+    :return: string
+    """
     name = input("Please enter a donor's full name: ")
     return name
 
+
 def enter_donation(name):
-    donation = round(float(input("Please enter the donation amount from {}: ".format(name_string))), 2)
+    """ Prompt donation amount for donor
+    :param name: (string) with name of donor:
+    :return: float
+    """
+    donation = round(float(input("Please enter the donation amount from {}: ".format(name))), 2)
     return donation
 
+
 def thank_you_letter(name, donation):
+    """ Print thank you letter for donors
+    :param name: (string_ with name of donor
+    :return: string
+    """
     print("""
     Dear {},
     Thank you for your recent donation of ${} to our organization. 
@@ -61,10 +95,20 @@ def thank_you_letter(name, donation):
     Sincerely,
     Kyle at Kelby Doggo Inc.""".format(name, donation))
 
-def get_total_given(donor_list):
+
+def get_total_given(db):
+    """ Get total donations per donor
+    :param db: (list) of dictionaries with donor information
+    :return: float
+    """
     return donor_list["Total Given"]
 
+
 def create_report(db):
+    """ Print report of donor database
+    :param db: (list) of dictionaries with donor information
+    :return: nothing
+    """
     db.sort(key=get_total_given, reverse=True)
     heading = "| {dn:<20s}\t| {tg:<10s}\t| {ng:<10s} | {ag:<10s} |".format
     row = "{dn:<20s} \t {ds:<1s} {tg:>9.2f} \t {ng:>10d} \t {ds2:<1} {ag:>9.2f} ".format
@@ -75,11 +119,21 @@ def create_report(db):
 
 
 def welcome_message():
+    """ Print welcome message when program is launched
+    :return: nothing
+    """
     print("\nWelcome to the Mailroom App!")
 
 
 def close_app():
+    """ Print goodbye message when user closes program
+    :return: nothing
+    """
     print("Closing Mailroom App...Goodbye!")
+
+
+
+# Main Body of Script  ---------------------------------------------------- #
 
 welcome_message()
 
