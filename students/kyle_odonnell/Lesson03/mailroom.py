b92f0dfb2c3f55ac0d398e73aa91d948f1827965
kylenod <62862441+kylenod@users.noreply.github.com>
2020-10-25 23:17:45 -0400

    Added code for option 2

--- students/kyle_odonnell/Lesson03/mailroom.py
+++ students/kyle_odonnell/Lesson03/mailroom.py
@@ -1,24 +1,33 @@
 #if __name__ == '__main__'
 
+
+
 donor_list = [
     {"Donor Name": "William Gates, III", "Total Given": 653784.49, "Num Gifts": 2, "Average Gift": 326892.24},
     {"Donor Name": "Mark Zuckerberg", "Total Given": 16396.10, "Num Gifts": 3, "Average Gift": 5465.37},
     {"Donor Name": "Jeff Bezos", "Total Given": 877.33, "Num Gifts": 1, "Average Gift": 877.33},
-    {"Donor Name": "Paul Allen", "Total Given": 708.42, "Num Gifts": 3, "Average Gift": 236.14}
+    {"Donor Name": "Paul Allen", "Total Given": 708.42, "Num Gifts": 3, "Average Gift": 236.14},
+    {"Donor Name": "Elon Musk", "Total Given": 50.00, "Num Gifts": 2, "Average Gift": 25.00}
 ]
 
 
 # Processing Functions
-def add_donation_amount(donor, donation, db = donor_list):
+def add_donation_amount(donor, donation, db):
     in_list = False
     for entry in db:
-        if donor.lower() == entry["Donor Name"]:
-            entry["Total Given"] = donation + entry["Total Given"]
+        if donor.lower() == entry["Donor Name"].lower():
+            total = float(donation) + float(entry["Total Given"])
+            num = int(entry["Num Gifts"]) + 1
+            average = round((total/num), 2)
+            entry["Total Given"] = total
+            entry["Num Gifts"] = num
+            entry["Average Gift"] = average
             in_list = True
-    if in_list == False:
+    if not in_list:
         new_entry = {"Donor Name": donor.title(), "Total Given": donation, "Num Gifts": 1, "Average Gift": donation}
         db.append(new_entry)
     return db
+
 
 # I/O  Functions
 def menu_options():
@@ -28,8 +37,8 @@ def menu_options():
     Option 2: Create a Report
     Option 3: Exit \n''')
 
-def show_names(db = donor_list):
-    for i, entry in enumerate(donor_list):
+def show_names(db):
+    for i, entry in enumerate(db):
         print(str(i + 1) + ".", "Donor Name:", entry["Donor Name"])
 
 def prompt_menu_option():
@@ -52,9 +61,25 @@ def thank_you_letter(name, donation):
     Sincerely,
     Kyle at Kelby Doggo Inc.""".format(name, donation))
 
+def get_total_given(donor_list):
+    return donor_list["Total Given"]
+
+def create_report(db):
+    db.sort(key=get_total_given, reverse=True)
+    heading = "| {dn:<20s}\t| {tg:<10s}\t| {ng:<10s} | {ag:<10s} |".format
+    row = "{dn:<20s} \t {ds:<1s} {tg:>9.2f} \t {ng:>10d} \t {ds2:<1} {ag:>9.2f} ".format
+    print(heading(dn="Donor Name", tg="Total Given", ng="Num Gifts", ag="Average Gift"))
+    print("---------------------------------------------------------------------")
+    for entry in db:
+        print(row(dn=entry["Donor Name"], ds="$", tg=entry["Total Given"], ng=entry["Num Gifts"], ds2="$", ag=entry["Average Gift"]))
+
 
 def welcome_message():
-    print("Welcome to the Mailroom App!")
+    print("\nWelcome to the Mailroom App!")
+
+
+def close_app():
+    print("Closing Mailroom App...Goodbye!")
 
 welcome_message()
 
@@ -65,14 +90,21 @@ while True:
         while True:
             name_string = enter_name()
             if name_string.lower().strip() == "list":
-                show_names()
+                show_names(donor_list)
             else:
                 break
         donation_amount = enter_donation(name_string)
-        donor_list = add_donation_amount(name_string, donation_amount)
+        donor_list = add_donation_amount(name_string, donation_amount, donor_list)
         thank_you_letter(name_string, donation_amount)
+
+    elif choice == 2:
+        create_report(donor_list)
+
+    elif choice == 3:
+        close_app()
+
     else:
-        break
+        pass
 
 
 
