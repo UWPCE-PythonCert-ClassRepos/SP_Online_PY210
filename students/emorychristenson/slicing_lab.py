01491f4da0ffc24a32f5759b8bf5a39179eb70b9
GLTY <glty@overcast.hsd1.wa.comcast.net>
2020-10-29 21:59:19 -0700

    Updating string

--- students/emorychristenson/slicing_lab.py
+++ students/emorychristenson/slicing_lab.py
@@ -16,27 +16,27 @@ def third_swap(seq):
 
 if __name__ == "__main__":
     #run some tests
-    a_string = "this is a string"
+    a_string = "strings are ok!"
     a_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8)
 
     # swap first and last items
-    assert exchange_first_last(a_string) == "ghis is a strint"
+    assert exchange_first_last(a_string) == "!trings are oks"
     assert exchange_first_last(a_tuple) == (8, 1, 2, 3, 4, 5, 6, 7, 0)
 
     # remove every other item
-    assert every_other_item(a_string) == "ti sasrn"
+    assert every_other_item(a_string) == "srnsaeo!"
     assert every_other_item(a_tuple) == (0, 2, 4, 6, 8)
     
     # remove first 4, last 4, print every other in remaining sequence
-    assert first_last_four(a_string) == (" sas")
+    assert first_last_four(a_string) == ("nsae")
     assert first_last_four(a_tuple) == (4,)
 
     # reverse sequence
-    assert reverse(a_string) == "gnirts a si siht"
+    assert reverse(a_string) == "!ko era sgnirts"
     assert reverse(a_tuple) == (8, 7, 6, 5, 4, 3, 2, 1, 0)
 
     # last third, first third, middle third
-    assert third_swap(a_string) == "tringthis is a "
+    assert third_swap(a_string) == "e ok!strings ar"
     assert third_swap(a_tuple) == (6, 7, 8, 0, 1, 2, 3, 4, 5)
 
     print("Tests passed.")
