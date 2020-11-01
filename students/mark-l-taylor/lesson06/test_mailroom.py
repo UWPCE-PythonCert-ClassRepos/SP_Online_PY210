846e6c2c4faa917c2a645889dff8979032e53ce1
Mark L Taylor <taylormarkld@gmail.com>
2020-11-01 10:48:26 -0800

    Revert "Creating lesson06 adding files."
    This reverts commit dbd561716dfa9227fa087ae6f5316b5244689886.

--- students/mark-l-taylor/lesson06/test_mailroom.py
+++
@@ -1,8 +0,0 @@
-"""Mailroom Testing"""
-
-import mailroom
-
-
-def check_get_donor_names():
-    assert mailroom.get_donor_names() == ['Homer Simpson', 'Charles Burns', 'Kent Brockman', 'Ned Flanders',
-                                          'Barney Gumble']
