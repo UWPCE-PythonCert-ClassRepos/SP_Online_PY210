9ac88515cd8c642062306292567404106ef39eaa
djgordon-maker <dragongordon@gmail.com>
2020-11-01 10:17:43 -0800

    Finished step 7

--- students/daniel_gordon/lesson08/circle.py
+++ students/daniel_gordon/lesson08/circle.py
@@ -21,5 +21,24 @@ class Circle():
     def area(self):
         return pi*(self.radius**2)
     
-    def __repr__(self):
+    def __str__(self):
         return f"A {self.__class__.__name__} of radius {self.radius}"
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.radius})"
+    
+    def __add__(self, other):
+        return self.__class__(self.radius+other.radius)
+        
+    def __iadd__(self, other):
+        return self + other
+    
+    #used value instead of other because I'm multiplying by a number, not another circle
+    def __mul__(self, value):
+        return self.__class__(self.radius*value)
+    
+    def __imul__(self, value):
+        return self * value
+
+    def __rmul__(self, value):
+        return self * value
--- students/daniel_gordon/lesson08/test_circle.py
+++ students/daniel_gordon/lesson08/test_circle.py
@@ -19,8 +19,23 @@ def test_area():
     c = Circle(5)
     assert c.area == 25*pi
     with pytest.raises(AttributeError):
-        c.area = 20
+        c.area = 30
 
 def test_print():
     c = Circle(4)
     assert str(c) == "A Circle of radius 4"
+    assert repr(c) == "Circle(4)"
+    
+def test_add():
+    c = Circle(3) + Circle(2)
+    assert c.radius == 5
+    c += Circle(5)
+    assert c.radius == 10
+
+def test_mult():
+    c = Circle(1) * 4
+    assert c.radius == 4
+    c *= 3
+    assert c.radius == 12
+    c = 0.5 * c
+    assert c.radius == 6
