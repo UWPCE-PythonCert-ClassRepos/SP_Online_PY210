a56783d209b5b6631b47b9aee7feba9091f0dd41
Chase Dullinger <chase.dullinger@gmail.com>
2020-11-03 08:24:30 -0800

    Adding comments and docstrings

--- students/chasedullinger/lesson07/html_render.py
+++ students/chasedullinger/lesson07/html_render.py
@@ -7,22 +7,24 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element(object):
+    """Basic html element class"""
     tag = ""
-    add_new_line = True
+    add_new_line = True  # Controls if new line should be added
     indent_spaces = 4
-    indent = " " * indent_spaces
+    indent = " " * indent_spaces  # string to pad indentation
 
     def __init__(self, content=None, **kwargs):
         if content:
             self.content = [content]
         else:
             self.content = []
-        self.attributes = kwargs
+        self.attributes = kwargs  # store kwargs as dict to unpack later
 
     def append(self, new_content):
         self.content.append(new_content)
 
     def get_opening_tag_string(self):
+        """Builds the opening tag for an element"""
         opening_tag_string = ""
         opening_tag_string += f"<{self.tag}"
         for k, v in self.attributes.items():
@@ -33,13 +35,14 @@ class Element(object):
         return opening_tag_string
 
     def render(self, out_file=None, cur_ind=""):
+        """Renders the section of html as text"""
         output_string = cur_ind
         if self.tag:
             output_string += self.get_opening_tag_string()
         for line in self.content:
-            if not isinstance(line, str):
+            if not isinstance(line, str):  # Recursively render objects
                 output_string += line.render(out_file, cur_ind + self.indent)
-            else:
+            else:  #If it's a string just render it with padding
                 output_string += cur_ind + self.indent
                 output_string += f"{line}"
             if self.add_new_line:
@@ -53,40 +56,51 @@ class Element(object):
 
 
 class Html(Element):
+    """Sub class for html tag"""
     tag = "html"
 
     def render(self, out_file=None, cur_ind=""):
+        """Html tag render method.  Overrides method in baseclass to add header
+        string showing the document type
+        """
         header_string = "<!DOCTYPE html>\n"
-        output_string = Element.render(self, out_file=None)
+        output_string = Element.render(self, out_file=None, cur_ind=cur_ind)
         if out_file:
             out_file.write(header_string + output_string)
         return header_string + output_string
 
 
 class Body(Element):
+    """Sub class for body tag"""
     tag = "body"
 
 
 class P(Element):
+    """Sub class for p (paragraph) tag"""
     tag = "p"
 
 
 class Head(Element):
+    """Sub class for head tag"""
     tag = "head"
 
 
 class Ul(Element):
+    """Sub class for ul (unordered list) tag"""
     tag = "ul"
 
 
 class Li(Element):
+    """Sub class for li (list item) tag"""
     tag = "li"
 
 
 class OneLineTag(Element):
+    """Sub class for tag that are all on one line"""
     add_new_line = False
 
     def render(self, out_file=None, cur_ind=""):
+        """Overrides render method in base class to eliminate line breaks"""
         output_string = cur_ind
         if self.tag:
             output_string += self.get_opening_tag_string()
@@ -100,18 +114,20 @@ class OneLineTag(Element):
 
 
 class Title(OneLineTag):
+    """Sub class for title tag"""
     tag = "title"
 
 
 class SelfClosingTag(Element):
-
+    """Sub class for tags that contain opening and closing in string"""
     def __init__(self, *args, **kwargs):
-        if len(args) > 0:
+        if len(args) > 0:  # Raise error if user tries to pass in content
             raise TypeError("SelfClosingTag can not have content")
         self.content = []
         self.attributes = kwargs
 
     def get_opening_tag_string(self):
+        """Overrides base method to include closing / at the end of the tag"""
         opening_tag_string = ""
         opening_tag_string += f"<{self.tag}"
         for k, v in self.attributes.items():
@@ -120,6 +136,7 @@ class SelfClosingTag(Element):
         return opening_tag_string
 
     def render(self, out_file=None, cur_ind=""):
+        """Overrides base method to eliminate content"""
         output_string = cur_ind
         if self.tag:
             output_string += self.get_opening_tag_string()
@@ -129,28 +146,36 @@ class SelfClosingTag(Element):
 
 
 class Hr(SelfClosingTag):
+    """Sub class for hr tag"""
     tag = "hr"
 
 
 class Br(SelfClosingTag):
+    """Sub class for br tag"""
     tag = "br"
 
 
 class Meta(SelfClosingTag):
+    """Sub class for meta tag"""
     tag = "meta"
 
 
 class A(OneLineTag):  # Want links to be one line
+    """Sub class for a (link) tag"""
     tag = "a"
     add_new_line = False
 
     def __init__(self, link, content, **kwargs):
+        """Override base method to pass link as href keyword"""
         Element.__init__(self, content=content, href=link, **kwargs)
 
 
 class H(OneLineTag):
+    """Sub class for h tag"""
+
     def __init__(self, level, content):
-        try:
+        """Overide base method to include level of h in tag"""
+        try:  # Check to make sure valid level is passed (must convert to int)
             int(level)
         except ValueError:
             raise TypeError("Level of H must be convertible to int")
