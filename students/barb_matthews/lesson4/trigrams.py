8fe901793e7e3c2c711e237d835b9e883edaf6f9
bapandaOK44 <barbapy@uw.edu>
2020-11-11 19:25:28 -0600

    Updated file

--- students/barb_matthews/lesson4/trigrams.py
+++ students/barb_matthews/lesson4/trigrams.py
@@ -1,21 +1,24 @@
 #! /usr/bin/env python3
-## Lesson 4 Exercise, Trigrams Kata 14
+## Lesson 4 Assignment 3, Trigrams Kata 14
 ## By: B. Matthews
-## 10/21/2020
 ## https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/kata_fourteen.html
+## Note: this somewhat works, but could be better. I'm not sure how to keep the new
+## text going after it doesn't find a key match in the dictionary
 
 import os
 import sys
 import string
 import random
 
-new_text = "nothing"
+new_text = ""
+max_size = 200
 
 def read_in_data(fn):
     """gets text from a file to use in generating new text file"""
 
     with open(fn, 'r') as f:
         all_lines = f.readlines()
+
     return all_lines
 
 def make_words(data):
@@ -25,24 +28,45 @@ def make_words(data):
     for eachLine in data:
         lots_of_words = lots_of_words + eachLine.split()
 
+    for thing in lots_of_words:
+        lots_of_words[lots_of_words.index(thing)] = lots_of_words[lots_of_words.index(thing)].replace('-', ' ')
+
+    print(lots_of_words)
+
     return lots_of_words
 
 def build_trigram(w):
     """build a dictionary using word pairs from the file as keys"""
 
-    key_words = w[0] + ' ' +w[1]
-    third_word = w[2]
-    print("key words=", key_words)
-    print("third=", third_word)
-
     trigrams = {}
-    return
+
+    for i in range(len(w)-2):
+        key_words = tuple(w[i:i + 2])
+        third_word = w[i + 2]
+
+        if key_words in trigrams:
+            trigrams[key_words].append(third_word)
+        else:
+            trigrams[key_words]=[third_word]
+
+    for k in trigrams.keys(): print(k, trigrams[k])
+
+    return trigrams
 
 def build_text(wp):
-    """generate a new text file using the word order data from the input file"""
+    """generate a new story using the words from the input file"""
 
+    start_key = ('I', 'rang')       #just picked one from the text in sherlock_small.txt
+    #start_key = ('the', 'cat')       # for testing with sample.txt
+    current_key = start_key
+    text = " ".join(start_key) + " "    #initialize the new story beginning
 
-    return
+    while (current_key in wp.keys() and len(text) <= max_size):
+        w = random.choice(wp[current_key])      #get a random word from dictonary
+        text = text + " " + w
+        current_key = current_key[1], w         #new key of 2nd word and new word
+
+    return text
 
 
 if __name__ == "__main__":
@@ -56,7 +80,7 @@ if __name__ == "__main__":
     in_data = read_in_data(filename)
     words = make_words(in_data)
     word_pairs = build_trigram(words)
-    #new_text = build_text(word_pairs)
+    new_text = build_text(word_pairs)
 
     print(new_text)
 
