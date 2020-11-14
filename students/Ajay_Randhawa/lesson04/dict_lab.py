1fb1598a96b725368c2dd28f7867b3c5e0c4de90
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-11-14 13:32:49 -0800

    formatted output

--- students/Ajay_Randhawa/lesson04/dict_lab.py
+++ students/Ajay_Randhawa/lesson04/dict_lab.py
@@ -1,4 +1,5 @@
 #Dictionaries 1
+print('Dictionaries 1:')
 d = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
 print(d)
 d.popitem()
@@ -11,6 +12,8 @@ print('cake' in d.keys())
 print('Mango' in d.values())
 
 #Dictionaries 2
+print('\n')
+print('Dictionaries 2:')
 e = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
 e['name'] = e['name'].count('t')
 e['city'] = e['city'].count('t')
@@ -18,6 +21,8 @@ e['cake'] = e['cake'].count('t')
 print(e)
 
 #Sets
+print('\n')
+print('Sets:')
 s2 = set(list(range(0,20,2)))
 s3 = set(list(range(0,20,3)))
 s4 = set(list(range(0,20,4)))
@@ -28,7 +33,10 @@ print(s3.issubset(s2))
 print(s4.issubset(s2))
 
 #Sets 2
+print('\n')
+print('Sets 2:')
 s1 = set('Python')
+print(s1)
 s1.update('i')
 print(s1)
 
