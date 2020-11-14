ecba76fedfbac4fc515e09426f21ccebec23e7fc
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-11-14 14:38:14 -0800

    fixed the list to remove nicknames

--- students/Ajay_Randhawa/lesson04/fileExercise.py
+++ students/Ajay_Randhawa/lesson04/fileExercise.py
@@ -1,3 +1,4 @@
+'''
 import pathlib
 p = pathlib.Path('./')
 for item in p.iterdir():
@@ -10,17 +11,28 @@ f.close()
 m = open('blah.txt', 'wb')
 m.write(data)
 m.close()
-
-
+'''
+#Create a empty list
 list1 = []
+#using open, read line by line in students.txt
 with open('students.txt', 'r') as g:
-    cnt = 0
     for line in g:
+        #read only the text to the left of the ':' and store it in variable "lang"
         lang = line.strip().split(':')[1]
-        #data.strip().split(',')
-        #lang.split(',')
+        #append 'lang' to empty list 'list1'
         list1.append(lang.strip().split(','))
+#gets rid of nested list
 list1 = sum(list1, [])
-l =set(list1)
-
-print(l)
+#converting the list to a set to get rid of duplicate values
+set1 = set(list1)
+set1 = list(set1)
+#only adding list items that start with a lower case to a seperate list
+finalList = []
+for i in range(len(set1)):
+    if (set1[i].islower()):
+        finalList.append(set1[i])
+#printing the new list with whitespace removed from front of text 
+for i in range(len(finalList)):
+    finalList[i] = finalList[i].lstrip()
+    print(finalList[i])
+    
