0574e65d2d05edcf05948dd4fe1f77cfc1a3683e
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-16 15:40:45 -0800

    almost done with basic logic on lab ex 5

--- students/csimmons/lesson03/test.py
+++ students/csimmons/lesson03/test.py
@@ -1,4 +1,5 @@
 #!usr/bin/env/python3
+seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 seq2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 seq3 = 'The quick brown fox jumps over the lazy dog'
 
@@ -7,11 +8,11 @@ def thirds(seq):
     thirds = len(seq)/3
     print(int(thirds))
     front = seq[:int(thirds)]
+    middle = seq[int(thirds):(int(thirds)*2)]
+    end = seq[(int(thirds)*2):]
     print(front)
-    middle = seq[:thirds]
-    end = seq[:thirds]
     print(middle)
     print(end)
   
 
-thirds(seq2)
+thirds(seq3)
