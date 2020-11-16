759b3ac0f62ca108fba311e3d7be951af54a0ebe
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-16 15:21:36 -0800

    5th slice lab - test

--- students/csimmons/lesson03/test.py
+++ students/csimmons/lesson03/test.py
@@ -3,7 +3,15 @@ seq2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 seq3 = 'The quick brown fox jumps over the lazy dog'
 
 
-def reverse(seq):
-    print(seq[::-1])
+def thirds(seq):
+    thirds = len(seq)/3
+    print(int(thirds))
+    front = seq[:thirds]
+    print(front)
+    middle = seq[:thirds]
+    end = seq[:thirds]
+    print(middle)
+    print(end)
+  
 
-reverse(seq2)
+thirds(seq2)
