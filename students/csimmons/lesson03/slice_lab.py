b8d00f2d0594a5d037dd578b85ab7348531ad896
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-17 11:33:10 -0800

    fixing weird assertion exceptions - fixed 1st 2

--- students/csimmons/lesson03/slice_lab.py
+++ students/csimmons/lesson03/slice_lab.py
@@ -4,6 +4,20 @@
 # slicing_lab.py: Slice Lab Exercises
 # Created 11/16/2020 - csimmons
 
+def exchange_first_last(seq):
+    if type(seq) == str:
+        first = seq[0]
+        last = seq[-1]
+        mid = seq[1:-1]
+        newstr= last + mid + first
+        return(newstr)
+    else:
+        seq[0],seq[-1] = seq[-1],seq[0]
+        return(seq)
+'''
+def remove_every_other(seq):
+    del seq[1::2]
+    return seq
 
 def remove_four_begin_end(seq):
     middle = seq[4:-4:2]
@@ -18,7 +32,7 @@ def rearrage_thirds(seq):
     middle = seq[int(third):(int(third)*2)]
     end = seq[(int(third)*2):]
     return (end + front + middle)
-
+'''
 #write tests
 
 a_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
@@ -26,27 +40,14 @@ b_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 a_string = 'April is the cruellest month, breeding Lilacs out of the dead land'
 b_string = 'GaiusJuliusCeasar'
 
-#print(exchange_first_last(a_list))
-#print(exchange_first_last(b_list))
-#print(exchange_first_last(a_string))
-#print(exchange_first_last(b_string))
 
 
 if __name__ == "__main__":
     # run some tests
-    assert rearrage_thirds(a_list) == [13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    assert rearrage_thirds(b_list) == [17, 18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    assert rearrage_thirds(a_string) == 's out of the dead landApril is the cruellest month, breeding Lilac'
-    assert rearrage_thirds(b_string) == 'sCeasarGaiusJuliu'
+    assert exchange_first_last(a_list) == [20,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,1]
+    assert exchange_first_last(b_list) == [24,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,1]
+    assert exchange_first_last(a_string) == 'dpril is the cruellest month, breeding Lilacs out of the dead lanA'
+    assert exchange_first_last(b_string) == 'raiusJuliusCeasaG'
 
-    assert reverse(a_list) == [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    assert reverse(b_list) == [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    assert reverse(a_string) == 'dnal daed eht fo tuo scaliL gnideerb ,htnom tselleurc eht si lirpA'
-    assert reverse(b_string) == 'rasaeCsuiluJsuiaG'
+    print('Passed')
 
-    assert remove_four_begin_end(a_list) == [5, 7, 9, 11, 13, 15]
-    assert remove_four_begin_end(b_list) == [5, 7, 9, 11, 13, 15, 17, 19]
-    assert remove_four_begin_end(a_string) == 'li h rels ot,bedn iasoto h ed'
-    assert remove_four_begin_end(b_string) == 'suise'
-
-    print("All tests passed")
--- students/csimmons/lesson03/test.py
+++ students/csimmons/lesson03/test.py
@@ -1,21 +1,22 @@
 #!usr/bin/env/python3
-seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-seq2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-seq3 = 'The quick brown fox jumps over the lazy dog'
+a_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+b_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+a_string = 'April is the cruellest month, breeding Lilacs out of the dead land'
+b_string = 'GaiusJuliusCeasar'
 
 
-def exchange_first_last(seq):
+def remove_every_other(seq):
     if type(seq) == str:
-        first = seq[0]
-        last = seq[-1]
-        mid = seq[1:-1]
-        newstr= last + mid + first
-        return(newstr)
+        print('This is a string')
     else:
-        seq[0],seq[-1] = seq[-1],seq[0]
-        return(seq)
+        del seq[1::2]
+        return seq
 
-print('list')
-print(exchange_first_last(seq2))
-print ('string')
-print(exchange_first_last(seq3))
+print('list1')
+print(remove_every_other(a_list))
+print('list2')
+print(remove_every_other(b_list))
+print ('string1')
+print(remove_every_other(a_string))
+print ('string2')
+print(remove_every_other(b_string))
