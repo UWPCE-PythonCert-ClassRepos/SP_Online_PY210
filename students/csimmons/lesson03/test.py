9aa7d73d38f4a66ed332fcf38c6051d1f08486cf
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-17 09:12:39 -0800

    FIxed exchangeFirstlast() to work with immutable str

--- students/csimmons/lesson03/test.py
+++ students/csimmons/lesson03/test.py
@@ -3,20 +3,19 @@ seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,2
 seq2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 seq3 = 'The quick brown fox jumps over the lazy dog'
 
+
 def exchange_first_last(seq):
-    seq[0],seq[-1] = seq[-1],seq[0]
-    return(seq)
+    if type(seq) == str:
+        first = seq[0]
+        last = seq[-1]
+        mid = seq[1:-1]
+        newstr= last + mid + first
+        return(newstr)
+    else:
+        seq[0],seq[-1] = seq[-1],seq[0]
+        return(seq)
 
-def exchange_first_last_str(seq):
-    alt_seq = seq
-    first = seq[0]
-    last = seq[-1]
-
-    mutable_str[0],mutable_str[-1] = mutable_str[-1],mutable_str[0]
-    return(seq)
-str = input("Enter a string : ")
-new_str = str[-1:] + str[1:-1] + str[:1]
-print(new_str)
-
+print('list')
 print(exchange_first_last(seq2))
-print(exchange_first_last_str(seq3))
+print ('string')
+print(exchange_first_last(seq3))
