c9c669ba539bdc0a4aa0a37bc0f3fa6b961bc3d8
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-16 18:12:35 -0800

    fixing issue with string slicing - cant because str is immutable

--- students/csimmons/lesson03/test.py
+++ students/csimmons/lesson03/test.py
@@ -3,17 +3,20 @@ seq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,2
 seq2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 seq3 = 'The quick brown fox jumps over the lazy dog'
 
+def exchange_first_last(seq):
+    seq[0],seq[-1] = seq[-1],seq[0]
+    return(seq)
 
-def thirds(seq):
-    thirds = len(seq)/3
-    print(int(thirds))
-    front = seq[:int(thirds)]
-    middle = seq[int(thirds):(int(thirds)*2)]
-    end = seq[(int(thirds)*2):]
-    print(front)
-    print(middle)
-    print(end)
-    print(end + front + middle)
-  
+def exchange_first_last_str(seq):
+    alt_seq = seq
+    first = seq[0]
+    last = seq[-1]
 
-thirds(seq)
+    mutable_str[0],mutable_str[-1] = mutable_str[-1],mutable_str[0]
+    return(seq)
+str = input("Enter a string : ")
+new_str = str[-1:] + str[1:-1] + str[:1]
+print(new_str)
+
+print(exchange_first_last(seq2))
+print(exchange_first_last_str(seq3))
