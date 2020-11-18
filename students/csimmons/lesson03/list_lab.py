252abfdba4b14b9873a0b1122471fea5485cab4e
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-17 19:03:54 -0800

    series_one() almost complete

--- students/csimmons/lesson03/list_lab.py
+++ students/csimmons/lesson03/list_lab.py
@@ -6,10 +6,25 @@
 
 def series_one():
     fruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
-    print('We have the following fruit for sale: ' + str(fruit))
-    
+    print('We have the following fruits available: ' + str(fruit))
+    add_fruit = input('What kind of fruit do you want to add?  ')
+    fruit.append(add_fruit)
+    print('The following fruits are now available: ' + str(fruit))
+    print('\nSelect one of the above fruits by its position in the list')
+    fruit_idx = input('Please enter a number:')
+    print(fruit[(int(fruit_idx)-1)] + ' are in position ' + str(fruit_idx))
+    anotherfruit = ['Grapes']
+    finalfruit = ['Pineapple']
+    fruit = fruit + anotherfruit
+    print(fruit)
+    #add_fruit2 = input('Add another fruit:  ')
+    #add_fruit3 = input('Add one last fruit:  ')
+    #another = add_fruit2 + fruit
+    #lastone = add_fruit3 + fruit
+    #print('Added another fruit by concatenation: ' + str(another))
+    #print('Added one last to list with insert(): ' + str(lastone))
 
 
 
-    
+
 series_one()
