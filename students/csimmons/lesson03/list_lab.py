7f384eeae60863e40dcd9d8d0bc370957de3c279
Craig Simmons <craig.a.simmons@gmail.com>
2020-11-21 08:35:14 -0800

    series one complete

--- students/csimmons/lesson03/list_lab.py
+++ students/csimmons/lesson03/list_lab.py
@@ -4,27 +4,25 @@
 # list_lab.py: List Lab Exercises
 # Created 11/16/2020 - csimmons
 
+fruit = ['Apples', 'Pears', 'Oranges', 'Blueberries']
+
 def series_one():
-    fruit = ['Apples', 'Pears', 'Oranges', 'Peaches']
     print('We have the following fruits available: ' + str(fruit))
     add_fruit = input('What kind of fruit do you want to add?  ')
     fruit.append(add_fruit)
     print('The following fruits are now available: ' + str(fruit))
     print('\nSelect one of the above fruits by its position in the list')
-    fruit_idx = input('Please enter a number:')
+    fruit_idx = input('Please enter a number: ')
     print(fruit[(int(fruit_idx)-1)] + ' are in position ' + str(fruit_idx))
-    anotherfruit = ['Grapes']
-    finalfruit = ['Pineapple']
-    fruit = fruit + anotherfruit
+    fruit = ['Pineapple'] + fruit
+    fruit.insert(0, 'Banana')
     print(fruit)
-    #add_fruit2 = input('Add another fruit:  ')
-    #add_fruit3 = input('Add one last fruit:  ')
-    #another = add_fruit2 + fruit
-    #lastone = add_fruit3 + fruit
-    #print('Added another fruit by concatenation: ' + str(another))
-    #print('Added one last to list with insert(): ' + str(lastone))
+    for i in fruit:
+        if i.startswith("P"):
+            print(i)
+    return fruit
 
-
+def series_one():
 
 
 series_one()
