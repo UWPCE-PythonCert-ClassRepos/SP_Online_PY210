ca3d597a6cf7f46d61d559fcd929c52edecd4289
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-06 09:49:01 -0800

    Initial commit for file_lab

---
+++ students/csimmons/lesson04/file_lab.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Craig Simmons
+# Python 210
+# file_lab.py 
+# Created 12/6/2020 - csimmons
+'''
+f = open('secrets.bin', 'rb')
+secret_data = f.read()
+f.close()
+
+f = open('secrets.txt')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    do_something_with_line()
+
+'''
+import os
+
