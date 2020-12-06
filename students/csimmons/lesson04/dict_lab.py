afd5c52d8bcb315903c5a0ace86cf6325dc309bc
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-05 19:41:42 -0800

    final version

--- students/csimmons/lesson04/dict_lab.py
+++ students/csimmons/lesson04/dict_lab.py
@@ -3,43 +3,37 @@
 # Python 210
 # dict_lab.py 
 # Created 12/4/2020 - csimmons
-'''
-# Part 1
+
+# Dictionary 1
 main_dict = {
             'name' : 'Chris',
             'city' : 'Seattle',
             'cake' : 'Chocolate',
             }
-
 print(main_dict)
-
 del main_dict['cake']
 print(main_dict)
-
 main_dict['fruit'] = 'Mango'
 print(main_dict)
 print(main_dict.keys()) 
 print(main_dict.values())
-print('cake' in main_dict)
-print('Mango' in main_dict.values())
+print('Is "cake" in main_dict? ','cake' in main_dict)
+print('Is "Mango" in main_dict? ','Mango' in main_dict.values())
 
-# Part 2
+# Dictionary 2
 main_dict2 = {
             'name' : 'Chris',
             'city' : 'Seattle',
-            'cake' : 'Chocolate',
+            'cake' : 'Chocolate'
             }
 def count_letters(main_dict2):
     for v in main_dict2:
         main_dict2[v] = main_dict2[v].lower().count('t')
-    print(main_dict2)
+    print('\n', main_dict2)
 
 count_letters(main_dict2)
-
-donorlist[i][1].append(f_gift)
-
-# Part3
 # Sets 1
+
 def set_modulus():
     s1 = set()
     s2 = set()
@@ -47,23 +41,30 @@ def set_modulus():
     s4 = set()
     for i in range(1,21):
         s1.update([i])
-        mod2 = (i % 2)
-        mod3 = (i % 3)
-        mod4 = (i % 4)
-        if mod2 == 0:
+        if (i % 2) == 0:
             s2.add(i)
-        if mod3 == 0:
+        if (i % 3) == 0:
             s3.add(i)
-        if mod4 == 0:
+        if (i % 4) == 0:
             s4.add(i)
-    print('\nNumbers from 1-20: ', s1)
-    print('\nNumbers from 1-20 divisible by 2: ', s2)
-    print('\nNumbers from 1-20 divisible by 3: ', s3)
-    print('\nNumbers from 1-20 divisible by 4: ', s4)
-    print('\nIs s3 a subset of s2? ', s3.issubset(s2))
-    print('\nIs s4 a subset of s2? ', s4.issubset(s2))
-
-# read python.org doc 4.6.6. Ranges()
-#learned about 3rd arg 'step' redo above as- 
+    print('\ns1: ', s1)
+    print('s2: ', s2)
+    print('s3: ', s3)
+    print('s4: ', s4)
+    print('Is s3 a subset of s2? ', s3.issubset(s2))
+    print('Is s4 a subset of s2? ', s4.issubset(s2))
 
 set_modulus()
+
+# Sets 2
+
+s1 = set(['P','y','t','h','o','n'])
+s2 = frozenset(('m','a','r','a','t','h','o','n',))
+
+print('\nSet s1: ',s1)
+print('Frozen Set s2: ',s2)
+s1.add('i')
+print('s1 with "i" added', s1)
+print('Union between the 2 sets: ',s1.union(s2))
+print('Intersection between the 2 sets: ',s1.intersection(s2))
+
