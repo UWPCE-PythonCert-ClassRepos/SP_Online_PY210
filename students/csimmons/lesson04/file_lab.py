75df8d5947153c3f48deecb33769452aeb4304d3
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-06 14:11:33 -0800

    file copy w/ large files working

--- students/csimmons/lesson04/file_lab.py
+++ students/csimmons/lesson04/file_lab.py
@@ -6,7 +6,7 @@
 
 import os 
 import pathlib
-'''
+
 def dir_list():
     cwd = os.getcwd()
     files = os.listdir(cwd)
@@ -16,31 +16,30 @@ def dir_list():
 
 dir_list()
 
+def file_copy(original, copy):
+    with open(original, 'rb') as original, open(copy, 'wb') as copy:
+        for byte in original:
+            copy.write(byte)
+    original.close()
+    copy.close()
+    print("Copied ", original)
 
-f = open('secrets.bin', 'rb')
-secret_data = f.read()
-f.close()
+file_copy('file1.txt', './copydir/file2.txt')
+file_copy('anniedog.jpg', './copydir/anniedog.jpg')
 
-f = open('secrets.txt')
-while True:
-    line = f.readline()
-    if not line:
-        break
-    do_something_with_line()
-    with open(d_path, 'r') as reader, open(d_r_path, 'w') as writer:
-    dog_breeds = reader.readlines()
-    writer.writelines(reversed(dog_breeds))
-
-'''
-def file_copy():
-    f_orig = 'file1.txt'
-    f_new = 'file2.txt'
-
-    with open('file1.txt', 'r') as f_orig, open('file2.txt', 'w') as f_new:
-        for line in f_orig:
-            f_new.write(line)
-    print("Copied filet")
-
-file_copy()
-
+def large_copy(original, copy):
+    buff = 4080
+    with open(original, 'rb') as original, open(copy, 'wb') as copy:
+        while True:
+            data_chunk = original.read(buff)
+            if not data_chunk:
+                break
+            copy.write(data_chunk)
+    original.close()
+    copy.close()
+    print("Copied ", original)
+# largefile.zip was a ~15GB file
+# copied it successfully in approx 11 mins
+# large_copy('largefile.zip', './copydir/largefile.zip')
+large_copy('anniedog.jpg', './copydir/anniedog2.jpg')
 
