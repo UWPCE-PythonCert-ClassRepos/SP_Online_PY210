3a7c0271c361f29a2b6cf9b4506e1630a433ff49
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-06 12:15:09 -0800

    fin  directory list, start file_copy

--- students/csimmons/lesson04/file_lab.py
+++ students/csimmons/lesson04/file_lab.py
@@ -3,7 +3,20 @@
 # Python 210
 # file_lab.py 
 # Created 12/6/2020 - csimmons
+
+import os 
+import pathlib
 '''
+def dir_list():
+    cwd = os.getcwd()
+    files = os.listdir(cwd)
+    print('CDW is: ', cwd)
+    for file in files:
+        print(file)
+
+dir_list()
+
+
 f = open('secrets.bin', 'rb')
 secret_data = f.read()
 f.close()
@@ -14,7 +27,19 @@ while True:
     if not line:
         break
     do_something_with_line()
+    with open(d_path, 'r') as reader, open(d_r_path, 'w') as writer:
+    dog_breeds = reader.readlines()
+    writer.writelines(reversed(dog_breeds))
 
 '''
-import os
+def file_copy():
+    f_orig = 'file1.txt'
+    f_new = 'file2.txt'
+   
+    with open('file1.txt', 'r') as f_orig:
+        for line in f_orig:
+            print(line, end='')
+        print(f_orig.read())
+
+
 
