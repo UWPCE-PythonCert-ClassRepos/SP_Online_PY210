2fdc588d7836bfec07be11918b37524588bba4dd
Ajay Randhawa <ajayrandhawa13@hotmail.com>
2020-12-18 13:16:11 -0800

    updated Trigrams Assignment

--- students/Ajay_Randhawa/lesson04/Trigrams.py
+++ students/Ajay_Randhawa/lesson04/Trigrams.py
@@ -2,6 +2,8 @@
 
 import random
 import itertools
+import string
+import re
 
 #words = ['I', 'wish', 'I', 'may', 'I', 'wish', 'I', 'might', 'I', 'wish', 'might', 'wish']
 
@@ -24,7 +26,7 @@ def build_trigrams(words):
             trigrams[key].append(next1_word)
         else:
             trigrams[key] = [next1_word]
-    print(trigrams)
+    #print(trigrams)
     return trigrams
 
 def build_text(word_pairs):
@@ -33,6 +35,8 @@ def build_text(word_pairs):
 
     starts with a known pair.
     checks if the known pair equals an existing dict key and adds the value to list
+    else
+    it find a random pair to keep going
     '''
 
     l = []
@@ -41,33 +45,49 @@ def build_text(word_pairs):
     for x, y in a:
         l.append(x)
         l.append(y)
-        
+    a = list(word_pairs.keys())
     for key, values in word_pairs.items():
         if key[0] == l[-2] and key[1] == l[-1]:
-            l.append(values[0])
+            z = random.randint(1,2)
+            try:
+                l.append(values[0])
+            except IndexError:
+                l.append(values[0])
+        else:
+            random_entry = random.choice(a)
+            random_entry = random_entry[:2]
+            l.append(random_entry[0])
+            l.append(random_entry[1])
+
+    l.append('......TO BE CONTINUED.')
     l = " ".join(l)
     return l
 
-def read_in_data():
+def read_in_data(filename):
     '''
     reads a text file and returns a list of words
     skips the first 62 lines
     '''
     a = []
-    with open('sherlock.txt', 'r') as g:
+    with open(filename, 'r') as g:
         for _ in range(62):
-            next(g)
+             next(g)
         for line in g:
-            lang = line.strip().split(" ")
-            #append 'lang' to empty list 'list1'
-            a.append(lang)
+            words = line.split()
+            #convert to lower case
+            words = [word.lower() for word in words]
+            #translate each word to remove the punctuation
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in words]
+            a.append(stripped)
     a = list(itertools.chain.from_iterable(a))
     return a
 
 
 
+
 if __name__ == "__main__":
-    words = read_in_data()
+    words = read_in_data('sherlock.txt')
     trigrams = build_trigrams(words)
     new_text = build_text(trigrams)
     print(new_text)
