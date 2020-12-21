a2898b25bd604b93ad0c2762ad76756de5728d2c
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-20 17:19:54 -0800

    starting to refactor initial attempt

--- students/csimmons/lesson04/test.py
+++ students/csimmons/lesson04/test.py
@@ -2,41 +2,9 @@
 # Craig Simmons
 # Python 210
 # trigram.py 
-# Created 12/17/2020 - csimmons
-
-import sys
-import os
-import random
-
+# Created 12/13/2020 - csimmons
 input = 'small.txt'
-welcome_prompt = "\nWelcome to the automatic book generator!\n"
-
-def open_file(input):
-    with open (input, 'r') as textfile:
-        while True:
-            line = textfile.readline()
-            if not line:
-                break
-
-def prep_file(line):
-    line = line.replace('\n', '').replace('--', ' ').replace(',','').replace('.', '').replace('(', '').replace(')', '')
-    clean_line = line.split(' ')
-    print(len(clean_line))
-    print(clean_line, '\n')
-
-def process_text(clean_line):
-    trigram ={}
-    for i in range(len(clean_line)-2):
-        pair = clean_line[i:i+2]
-        follower = clean_line[i+2]
-        trigram[tuple(pair)] = follower
-                #print(clean_line)
-                #print(pair)
-                #print(follower)clear
-def main():
-    print(welcome_prompt)
-
-def old_main(input):
+def main(input):
     trigram ={}
     with open (input, 'r') as textfile:
         while True:
