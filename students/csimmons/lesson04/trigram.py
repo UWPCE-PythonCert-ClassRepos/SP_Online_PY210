56ff532bbd47f71852781b13075f727b1307e0fc
Craig Simmons <craig.a.simmons@gmail.com>
2020-12-27 13:44:56 -0800

    Not working - back to clean slate

--- students/csimmons/lesson04/trigram.py
+++ students/csimmons/lesson04/trigram.py
@@ -7,7 +7,7 @@
 import sys
 import os
 import random
-input = 'small.txt'
+input = 'sherlock_small.txt'
 
 welcome_prompt = "\nWelcome to the automatic book generator!\n"
 trigrams = {}
@@ -38,68 +38,30 @@ def process_text(clean_words):
     build_text(trigrams)
 
 
+
+    # print(trigram_len)
 def build_text(trigrams):
     masterpiece_size = 500
-    #first, second = random.choice(tuple(trigrams.keys()))
-    # starting_key = [first.title(), second]
-    starting_key = random.choice(tuple(trigrams.keys()))
-    masterpiece = ' '.join(starting_key)
-    print(starting_key, type(starting_key))
-    print(masterpiece, type(masterpiece))
-    #while len(masterpiece) <= len(trigrams.items()):
+    first, second = random.choice(tuple(trigrams.keys()))
+    word_pair = (first.title(), second)
+    masterpiece = list(word_pair)
+    print('Starting key: ', word_pair)
+
+
     while len(masterpiece) <= masterpiece_size:
-        print(len(masterpiece), len(trigrams.items()), len(trigrams.keys()))
-        next = random.choice(trigrams[starting_key])
-        print(next)
-        masterpiece = masterpiece + ' ' + next
-        print(masterpiece)
-        starting_key = starting_key[1], next
-        print(starting_key)
-    
-    # trigram_len = len(trigrams.items())
-    # print(trigram_len)
-    
-   
-
- 
-    '''
-     text_len = random.randint(5,(len(trigrams.keys())-3))
-    key_test = tuple(word_list[-2:])
-    print(word_list, len(word_list), text_len, key_test)
-    while len(word_list) < text_len:    
-        if key_test in trigrams.keys():
-            word_list.append(random.choice(list(trigrams[(word_list[-2],word_list[-1])])))
+        if word_pair in trigrams:
+            print('word_pair in trigrams:')
+            print(word_pair)
+            print(len(masterpiece), masterpiece_size)
+            masterpiece.append(random.choice(trigrams[word_pair]))
+            print(masterpiece)
+            word_pair = tuple(masterpiece[-2:])
+            print('new word pair: ', word_pair)
         else:
-            word_list.append(random.choice(list(trigrams.values())))
-    '''
-
+            print('word_pair NOT in trigrams:')
+            word_pair = random.choice(list(trigrams.keys()))
+            print(word_pair)
     
-    
-    #return " ".join(list_wof_words).capitalize()clear
-    #for i in range(len(trigrams)):
-        #key = random.choice(tuple(trigrams.keys()))
-        #print(key)
- 
-
-
-'''
-    while (start_key in wp and len(text) <= max_size):
-        w = random.choice(wp[start_key])  # get a random word from dictonary
-        text = text + " " + w
-        start_key = start_key[1], w  # new key of 2nd word and new word
-        #print("current key ", start_key)
-    return text
-    new_text = ''.join(starting_pair)
-    #print(new_text)
-    #print(len(trigrams))
-
-    for i in range(len(trigrams)):
-        key = random.choice(tuple(trigrams.keys()))CLEr
-        print(key)
-'''
-
-
-
 
 
 def main(input):
