35391e992382e12b5f8c8d0378f8132e7a3bfb38
Jacob Daylong <jakedaylong@gmail.com>
2021-01-02 12:12:39 -0800

    Completed Circle through step 6

---
+++ students/jacob_daylong/Lesson 8/circle.py
@@ -0,0 +1,23 @@
+﻿import math
+
+class Circle(object):
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def area(self):
+        return (self.radius ** 2) * math.pi
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2
+
+    def __str__(self):
+        return "Circle with a radius of {0:.2f}".format(self.radius)
+
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
