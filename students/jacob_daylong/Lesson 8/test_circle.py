98f363edc19f3c38a04fd24f41808222cc56fbe8
Jacob Daylong <jakedaylong@gmail.com>
2021-01-03 11:31:33 -0800

    Circle assignment completed

--- students/jacob_daylong/Lesson 8/test_circle.py
+++ students/jacob_daylong/Lesson 8/test_circle.py
@@ -28,3 +28,41 @@ def test_str():
 def test_repr():
     c = Circle(4)
     assert repr(c) == "Circle(4)"
+
+def test_add():
+    c1 = Circle(3)
+    c2 = Circle(4)
+    c3 = c1 + c2
+    assert repr(c3) == "Circle(7)"
+
+def test_multi():
+    c1 = Circle(3)
+    c2 = Circle(4)
+    c3 = c1 * c2
+    assert repr(c3) == "Circle(12)"
+
+def test_compare():
+    c1 = Circle(3)
+    c2 = Circle(5)
+    c3 = Circle(3)
+
+    assert c1 < c2
+    assert c2 > c1
+    assert c1 == c3
+    assert c1 != c2
+    
+def test_sort():
+    c1 = Circle(3)
+    c2 = Circle(5)
+    c4 = Circle(2)
+
+    circles = [c1, c2, c4]
+    circles.sort()
+    assert circles == [Circle(2), Circle(3), Circle(5)]
+
+def test_sphere():
+    s = Sphere(5)
+    assert s.area == 314.15926535897932384626433832795
+    assert s.volume == 523.59877559829887307710723054658
+    assert str(s) == "Sphere with a radius of 5.00"
+    assert repr(s) == "Sphere(5)"
