43f5c7c8a475c61b42ea09f387a396f3a096c031
kylenod <62862441+kylenod@users.noreply.github.com>
2021-01-04 20:41:13 -0500

    Added unit test for area property

--- students/kyle_odonnell/Lesson08/circle_class.py
+++ students/kyle_odonnell/Lesson08/circle_class.py
@@ -1,6 +1,18 @@
-
+import math
 
 
 class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, r):
+        self.radius = r
+
+    @property
+    def diameter(self):
+        return 2 * self.radius
+
+    @diameter.setter
+    def diameter(self, d):
+        self.radius = d / 2
+
+    @property
+    def area(self):
+        return math.pi * self.radius ** 2
--- students/kyle_odonnell/Lesson08/test_circle_class.py
+++ students/kyle_odonnell/Lesson08/test_circle_class.py
@@ -1,8 +1,30 @@
 from circle_class import *
-
+import math
 
 # test radius
-
 def test_radius():
     c = Circle(4)
     assert c.radius == 4
+    d = Circle(8)
+    assert d.radius == 8
+
+
+# test diameter property
+def test_diameter():
+    c = Circle(4)
+    assert c.diameter == 8
+    d = Circle(8)
+    assert d.diameter == 16
+
+
+# test diameter setter
+def test_diameter_setter():
+    c = Circle(4)
+    c.diameter = 5
+    assert c.diameter == 5
+
+
+# test area propert
+def test_area_property():
+    c = Circle(5)
+    assert c.area == math.pi * 5 ** 2
