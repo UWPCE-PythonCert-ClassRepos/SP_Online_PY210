cc6324e940b474f9d908b37741a66837bfc8ac28
kylenod <62862441+kylenod@users.noreply.github.com>
2021-01-05 22:39:02 -0500

    Added tests for protocol methods

--- students/kyle_odonnell/Lesson08/circle_class.py
+++ students/kyle_odonnell/Lesson08/circle_class.py
@@ -5,6 +5,18 @@ class Circle:
     def __init__(self, r):
         self.radius = r
 
+    def __str__(self):
+        return "Circle with radius: {:.6f}".format(self.radius)
+
+    def __repr__(self):
+        return "Circle({})".format(self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other.radius)
+
     @property
     def diameter(self):
         return 2 * self.radius
@@ -15,10 +27,11 @@ class Circle:
 
     @property
     def area(self):
-        return math.pi * self.radius ** 2
+        return round(math.pi * self.radius ** 2, 6)
 
     @classmethod
     def from_diameter(Circle, d):
         self = Circle(d / 2)
         return self
+
 
--- students/kyle_odonnell/Lesson08/test_circle_class.py
+++ students/kyle_odonnell/Lesson08/test_circle_class.py
@@ -1,6 +1,7 @@
 from circle_class import *
 import math
 
+
 # test radius
 def test_radius():
     c = Circle(4)
@@ -24,10 +25,11 @@ def test_diameter_setter():
     assert c.diameter == 5
 
 
-# test area propert
+# test area property
 def test_area_property():
     c = Circle(5)
-    assert c.area == math.pi * 5 ** 2
+    assert c.area == round(math.pi * 5 ** 2, 6)
+
 
 # test alternate constructor
 def test_diameter_constructor():
@@ -35,6 +37,28 @@ def test_diameter_constructor():
     assert c.radius == 4
 
 
-def test_docstring():
+# test string method
+def test_str_method():
     c = Circle(4)
-    assert print(c) == "Circle with radius: 4.000000"
+    assert str(c) == "Circle with radius: 4.000000"
+    d = Circle.from_diameter(5)
+    assert str(d) == "Circle with radius: 2.500000"
+
+
+def test_repr_method():
+    c = Circle(4)
+    assert repr(c) == 'Circle(4)'
+
+
+def test_add_protocol():
+    c1 = Circle(2)
+    c2 = Circle(4)
+    assert (c1 + c2).radius == 6
+
+def test_mul_protocal():
+    c1 = Circle(5)
+    y = 3
+    assert (c1 * 3).radius == 20
+    assert (3 * c1).radius == 20
+
+
