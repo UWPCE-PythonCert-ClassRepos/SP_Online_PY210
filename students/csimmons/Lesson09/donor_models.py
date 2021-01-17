e2f9d834887693974186c26297d07cc23068c364
Craig Simmons <craig.a.simmons@gmail.com>
2021-01-17 13:52:40 -0800

    Starting the refactor

---
+++ students/csimmons/Lesson09/donor_models.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Craig Simmons
+# Python 210
+# mailroom_oo.py assignment
+# Donor classes 
+# Created 1/17/2021 - csimmons
---
+++ students/csimmons/Lesson09/test_mailroom_oo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Craig Simmons
+# Python 210
+# mailroom_oo.py assignment
+# Mailroom unit test suite w/ pytest
+# Created 1/17/2021 - csimmons
+
